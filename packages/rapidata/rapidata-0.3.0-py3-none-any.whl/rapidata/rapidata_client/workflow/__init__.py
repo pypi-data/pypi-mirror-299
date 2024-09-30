7964a628a7ebe07a01a145604fbd083155be14a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:04:27 +0000

    Add package rapidata/0.3.0/rapidata-0.3.0-py3-none-any.whl

---
+++ packages/rapidata/rapidata-0.3.0-py3-none-any.whl/rapidata/rapidata_client/workflow/__init__.py
@@ -0,0 +1,4 @@
+from .base_workflow import Workflow as Workflow
+from .classify_workflow import ClassifyWorkflow as ClassifyWorkflow
+from .compare_workflow import CompareWorkflow as CompareWorkflow
+from .free_text_workflow import FreeTextWorkflow as FreeTextWorkflow
