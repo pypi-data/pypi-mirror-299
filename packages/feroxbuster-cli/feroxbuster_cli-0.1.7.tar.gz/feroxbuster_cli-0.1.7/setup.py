1c8308e3e7d736e95c6ae17b3f2c396cdbbc75dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:05:02 +0000

    Add package feroxbuster-cli/0.1.7/feroxbuster_cli-0.1.7.tar.gz

---
+++ packages/feroxbuster-cli/feroxbuster_cli-0.1.7.tar.gz/feroxbuster_cli-0.1.7/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Get the long description from the README file
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="feroxbuster-cli",
+    version="0.1.7",  # Increment the version number
+    author="Adithya A N",
+    description="A Python CLI tool to download and run Feroxbuster",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "requests",
+    ],
+    entry_points={
+        "console_scripts": [
+            "feroxbuster-cli=feroxbuster_cli.cli:main",  # maps to cli.py's main function
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
