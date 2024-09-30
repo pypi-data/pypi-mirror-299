ce31d0ea26c8bd4cc77e9421756c494346d8eb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 14:04:11 +0000

    Add package bald-jonga/0.1.1/bald-jonga-0.1.1.tar.gz

---
+++ packages/bald-jonga/bald-jonga-0.1.1.tar.gz/bald-jonga-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bald-jonga',
+    version='0.1.1',
+    description='A Python library that intercepts print statements automatically.',
+    author='Mateus Elias',
+    author_email='meap@cin.ufpe.br',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
