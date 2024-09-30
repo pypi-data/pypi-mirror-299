b6953fb8e33288d6c5d4985e39be1b9cc58e3bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:12:20 +0000

    Add package datapyrse/0.6.1/datapyrse-0.6.1-py3-none-any.whl

---
+++ packages/datapyrse/datapyrse-0.6.1-py3-none-any.whl/datapyrse/__init__.py
@@ -0,0 +1,11 @@
+from datapyrse.services.service_client import ServiceClient
+from datapyrse.models.entity import Entity
+from datapyrse.models.entity_collection import EntityCollection
+from datapyrse.models.entity_reference import EntityReference
+
+__all__ = [
+    "ServiceClient",
+    "Entity",
+    "EntityCollection",
+    "EntityReference",
+]
