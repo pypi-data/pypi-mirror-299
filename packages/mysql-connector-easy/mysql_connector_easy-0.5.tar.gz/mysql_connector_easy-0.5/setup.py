84d9716afe73d46f7bd1ce28570b666e6d62b770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:12:13 +0000

    Add package mysql-connector-easy/0.5/mysql_connector_easy-0.5.tar.gz

---
+++ packages/mysql-connector-easy/mysql_connector_easy-0.5.tar.gz/mysql_connector_easy-0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+
+# ================= #
+
+setup(
+    name='mysql-connector-easy',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=["mysql-connector-python"], 
+    description="""An interface library to summarize database queries """,
+    author='mdev2007',
+    author_email='m.programmer20070@gmail.com',
+)
