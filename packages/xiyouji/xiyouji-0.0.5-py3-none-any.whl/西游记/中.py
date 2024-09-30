e79f619e7d893a6c6d4bc757a88160ba3a69cc0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 04:03:50 +0000

    Add package xiyouji/0.0.5/xiyouji-0.0.5-py3-none-any.whl

---
+++ packages/xiyouji/xiyouji-0.0.5-py3-none-any.whl/西游记/中.py
@@ -0,0 +1,29 @@
+
+import sys
+
+from cmd import Cmd
+from 西游记.问答 import 数字母
+
+class 猜数字(Cmd):
+    intro = "我想了个 100 之内的数，猜猜是几？"
+    想的 = 81
+    prompt = '请猜吧: '
+
+    def default(self, 行):
+        数 = int(行)
+        if 数 > self.想的:
+            print("太多了!")
+        elif 数 < self.想的:
+            print("太少了!")
+        else:
+            print("取到经了!")
+            self.do_quit(行)
+
+    def do_quit(self, arg):
+        sys.exit()
+
+def 中(args=None):
+    游记 = 数字母()
+    游记.cmdloop()
+
+中(sys.argv)
