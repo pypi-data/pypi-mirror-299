bef34c211d3eed6651eb2c835900ef9079bc7ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:05:02 +0000

    Add package keelson/0.3.8rc1/keelson-0.3.8rc1-py3-none-any.whl

---
+++ packages/keelson/keelson-0.3.8rc1-py3-none-any.whl/keelson/payloads/FrameTransforms_pb2.pyi
@@ -0,0 +1,13 @@
+import FrameTransform_pb2 as _FrameTransform_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class FrameTransforms(_message.Message):
+    __slots__ = ("transforms",)
+    TRANSFORMS_FIELD_NUMBER: _ClassVar[int]
+    transforms: _containers.RepeatedCompositeFieldContainer[_FrameTransform_pb2.FrameTransform]
+    def __init__(self, transforms: _Optional[_Iterable[_Union[_FrameTransform_pb2.FrameTransform, _Mapping]]] = ...) -> None: ...
