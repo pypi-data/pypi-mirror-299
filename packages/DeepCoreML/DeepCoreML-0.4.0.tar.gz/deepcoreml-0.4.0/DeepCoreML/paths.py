2b385b162dffd70736544cf06e518a68cb7eaf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:03:59 +0000

    Add package DeepCoreML/0.4.0/DeepCoreML-0.4.0.tar.gz

---
+++ packages/DeepCoreML/DeepCoreML-0.4.0.tar.gz/deepcoreml-0.4.0/DeepCoreML/paths.py
@@ -0,0 +1,13 @@
+base_path = '/media/leo/7CE54B377BB9B18B/'
+# base_path = 'C:/Users/Owner/PycharmProjects/'
+
+output_path_performance = base_path + 'dev/Python/ctdGAN_results/results/'
+output_path_loss = output_path_performance + 'loss/'
+
+bin_cont = base_path + 'datasets/Imbalanced/bin_continuous/'
+bin_disc = base_path + 'datasets/Imbalanced/bin_discrete/'
+bin_mix = base_path + 'datasets/Imbalanced/bin_mixed/'
+
+multi_cont = base_path + 'datasets/Imbalanced/multiclass_continuous/'
+multi_disc = base_path + 'datasets/Imbalanced/multiclass_discrete/'
+multi_mix = base_path + 'datasets/Imbalanced/multiclass_mixed/'
