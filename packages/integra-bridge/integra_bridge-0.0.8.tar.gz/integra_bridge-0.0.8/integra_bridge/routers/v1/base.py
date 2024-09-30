c7108479a2228121088f5cfb0b0b2536998b0400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 14:03:43 +0000

    Add package integra-bridge/0.0.8/integra_bridge-0.0.8.tar.gz

---
+++ packages/integra-bridge/integra_bridge-0.0.8.tar.gz/integra_bridge-0.0.8/integra_bridge/routers/v1/base.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from integra_bridge.common.settings import SETTINGS
+from integra_bridge.routers.v1.configuration import configuration_router
+from integra_bridge.routers.v1.connector import connector_router
+from integra_bridge.routers.v1.processor import processor_router
+
+base_router = APIRouter(prefix=SETTINGS.API_PREFIX)
+
+base_router.include_router(configuration_router)
+base_router.include_router(processor_router)
+base_router.include_router(connector_router)
