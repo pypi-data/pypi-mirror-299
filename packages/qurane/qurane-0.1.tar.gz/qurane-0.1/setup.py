a8da49aeec253793ad0bacd1cfe3ea6468290584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:11:41 +0000

    Add package qurane/0.1/qurane-0.1.tar.gz

---
+++ packages/qurane/qurane-0.1.tar.gz/qurane-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qurane",
+    version="0.1",
+    packages=find_packages(),
+    description="A modern and advanced Quran library for Arabic text and Tafsir",
+    long_description=open("readme.md").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    author="Khattab_Aluqba",
+    author_email="exmidleg@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
