f19b60e99cf8d64a6a61b3e3cfe6ca29de29743d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:03:42 +0000

    Add package concrete-python/2.8.1/concrete_python-2.8.1-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/concrete-python/concrete_python-2.8.1-cp311-cp311-macosx_11_0_arm64.whl/concrete/compiler/__init__.py
@@ -0,0 +1,92 @@
+#  Part of the Concrete Compiler Project, under the BSD3 License with Zama Exceptions.
+#  See https://github.com/zama-ai/concrete/blob/main/LICENSE.txt for license information.
+
+"""Compiler submodule."""
+import atexit
+
+# pylint: disable=no-name-in-module,import-error
+from mlir._mlir_libs._concretelang._compiler import (
+    terminate_df_parallelization as _terminate_df_parallelization,
+    init_df_parallelization as _init_df_parallelization,
+    check_gpu_runtime_enabled as _check_gpu_runtime_enabled,
+    check_cuda_device_available as _check_cuda_device_available,
+)
+from mlir._mlir_libs._concretelang._compiler import round_trip as _round_trip
+from mlir._mlir_libs._concretelang._compiler import (
+    set_llvm_debug_flag,
+    set_compiler_logging,
+)
+
+# pylint: enable=no-name-in-module,import-error
+
+from .compilation_options import CompilationOptions, Encoding
+from .compilation_context import CompilationContext
+from .key_set_cache import KeySetCache
+from .client_parameters import ClientParameters
+from .compilation_feedback import ProgramCompilationFeedback, CircuitCompilationFeedback
+from .key_set import KeySet
+from .public_result import PublicResult
+from .public_arguments import PublicArguments
+from .lambda_argument import LambdaArgument
+from .library_compilation_result import LibraryCompilationResult
+from .library_lambda import LibraryLambda
+from .client_support import ClientSupport
+from .library_support import LibrarySupport
+from .lwe_secret_key import LweSecretKey, LweSecretKeyParam
+from .evaluation_keys import EvaluationKeys
+from .tfhers_int import (
+    TfhersExporter,
+    TfhersFheIntDescription,
+)
+from .value import Value
+from .value_decrypter import ValueDecrypter
+from .value_exporter import ValueExporter
+from .simulated_value_decrypter import SimulatedValueDecrypter
+from .simulated_value_exporter import SimulatedValueExporter
+from .parameter import Parameter
+from .server_program import ServerProgram
+
+
+def init_dfr():
+    """Initialize dataflow parallelization.
+
+    It is not always required to initialize the dataflow runtime as it can be implicitely done
+    during compilation. However, it is required in case no compilation has previously been done
+    and the runtime is needed"""
+    _init_df_parallelization()
+
+
+def check_gpu_enabled() -> bool:
+    """Check whether the compiler and runtime support GPU offloading.
+
+    GPU offloading is not always available, in particular in non-GPU wheels."""
+    return _check_gpu_runtime_enabled()
+
+
+def check_gpu_available() -> bool:
+    """Check whether a CUDA device is available and online."""
+    return _check_cuda_device_available()
+
+
+# Cleanly terminate the dataflow runtime if it has been initialized
+# (does nothing otherwise)
+atexit.register(_terminate_df_parallelization)
+
+
+def round_trip(mlir_str: str) -> str:
+    """Parse the MLIR input, then return it back.
+
+    Useful to check the validity of an MLIR representation
+
+    Args:
+        mlir_str (str): textual representation of an MLIR code
+
+    Raises:
+        TypeError: if mlir_str is not of type str
+
+    Returns:
+        str: textual representation of the MLIR code after parsing
+    """
+    if not isinstance(mlir_str, str):
+        raise TypeError(f"mlir_str must be of type str, not {type(mlir_str)}")
+    return _round_trip(mlir_str)
---
+++ packages/concrete-python/concrete_python-2.8.1-cp311-cp311-macosx_11_0_arm64.whl/concrete/fhe/compilation/client.py
@@ -0,0 +1,239 @@
+"""
+Declaration of `Client` class.
+"""
+
+# pylint: disable=import-error,no-member,no-name-in-module
+
+import shutil
+import tempfile
+from pathlib import Path
+from typing import Dict, List, Optional, Tuple, Union
+
+import numpy as np
+from concrete.compiler import EvaluationKeys, LweSecretKey, ValueDecrypter, ValueExporter
+
+from .keys import Keys
+from .specs import ClientSpecs
+from .utils import validate_input_args
+from .value import Value
+
+# pylint: enable=import-error,no-member,no-name-in-module
+
+
+class Client:
+    """
+    Client class, which can be used to manage keys, encrypt arguments and decrypt results.
+    """
+
+    specs: ClientSpecs
+    _keys: Keys
+
+    def __init__(
+        self,
+        client_specs: ClientSpecs,
+        keyset_cache_directory: Optional[Union[str, Path]] = None,
+    ):
+        self.specs = client_specs
+        self._keys = Keys(client_specs, keyset_cache_directory)
+
+    def save(self, path: Union[str, Path]):
+        """
+        Save the client into the given path in zip format.
+
+        Args:
+            path (Union[str, Path]):
+                path to save the client
+        """
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            with open(Path(tmp_dir) / "client.specs.json", "wb") as f:
+                f.write(self.specs.serialize())
+
+            path = str(path)
+            if path.endswith(".zip"):
+                path = path[: len(path) - 4]
+
+            shutil.make_archive(path, "zip", tmp_dir)
+
+    @staticmethod
+    def load(
+        path: Union[str, Path],
+        keyset_cache_directory: Optional[Union[str, Path]] = None,
+    ) -> "Client":
+        """
+        Load the client from the given path in zip format.
+
+        Args:
+            path (Union[str, Path]):
+                path to load the client from
+
+            keyset_cache_directory (Optional[Union[str, Path]], default = None):
+                keyset cache directory to use
+
+        Returns:
+            Client:
+                client loaded from the filesystem
+        """
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            shutil.unpack_archive(path, tmp_dir, "zip")
+            with open(Path(tmp_dir) / "client.specs.json", "rb") as f:
+                client_specs = ClientSpecs.deserialize(f.read())
+
+        return Client(client_specs, keyset_cache_directory)
+
+    @property
+    def keys(self) -> Keys:
+        """
+        Get the keys for the client.
+        """
+        return self._keys
+
+    @keys.setter
+    def keys(self, new_keys: Keys):
+        """
+        Set the keys for the client.
+        """
+        # TODO: implement verification for compatibility with keyset.
+
+        self._keys = new_keys
+
+    def keygen(
+        self,
+        force: bool = False,
+        seed: Optional[int] = None,
+        encryption_seed: Optional[int] = None,
+        initial_keys: Optional[Dict[int, LweSecretKey]] = None,
+    ):
+        """
+        Generate keys required for homomorphic evaluation.
+
+        Args:
+            force (bool, default = False):
+                whether to generate new keys even if keys are already generated
+
+            seed (Optional[int], default = None):
+                seed for private keys randomness
+
+            encryption_seed (Optional[int], default = None):
+                seed for encryption randomness
+
+            initial_keys (Optional[Dict[int, LweSecretKey]] = None):
+                initial keys to set before keygen
+        """
+
+        self.keys.generate(
+            force=force,
+            seed=seed,
+            encryption_seed=encryption_seed,
+            initial_keys=initial_keys,
+        )
+
+    def encrypt(
+        self,
+        *args: Optional[Union[int, np.ndarray, List]],
+        function_name: Optional[str] = None,
+    ) -> Optional[Union[Value, Tuple[Optional[Value], ...]]]:
+        """
+        Encrypt argument(s) to for evaluation.
+
+        Args:
+            *args (Optional[Union[int, np.ndarray, List]]):
+                argument(s) for evaluation
+            function_name (str):
+                name of the function to encrypt
+
+        Returns:
+            Optional[Union[Value, Tuple[Optional[Value], ...]]]:
+                encrypted argument(s) for evaluation
+        """
+
+        if function_name is None:
+            functions = self.specs.client_parameters.function_list()
+            if len(functions) == 1:
+                function_name = functions[0]
+            else:  # pragma: no cover
+                msg = "The client contains more than one functions. \
+Provide a `function_name` keyword argument to disambiguate."
+                raise TypeError(msg)
+
+        ordered_sanitized_args = validate_input_args(self.specs, *args, function_name=function_name)
+
+        self.keygen(force=False)
+        keyset = self.keys._keyset  # pylint: disable=protected-access
+
+        exporter = ValueExporter.new(keyset, self.specs.client_parameters, function_name)
+        exported = [
+            (
+                None
+                if arg is None
+                else Value(
+                    exporter.export_tensor(position, arg.flatten().tolist(), list(arg.shape))
+                    if isinstance(arg, np.ndarray) and arg.shape != ()
+                    else exporter.export_scalar(position, int(arg))
+                )
+            )
+            for position, arg in enumerate(ordered_sanitized_args)
+        ]
+
+        return tuple(exported) if len(exported) != 1 else exported[0]
+
+    def decrypt(
+        self,
+        *results: Union[Value, Tuple[Value, ...]],
+        function_name: Optional[str] = None,
+    ) -> Optional[Union[int, np.ndarray, Tuple[Optional[Union[int, np.ndarray]], ...]]]:
+        """
+        Decrypt result(s) of evaluation.
+
+        Args:
+            *results (Union[Value, Tuple[Value, ...]]):
+                result(s) of evaluation
+            function_name (str):
+                name of the function to decrypt for
+
+        Returns:
+            Optional[Union[int, np.ndarray, Tuple[Optional[Union[int, np.ndarray]], ...]]]:
+                decrypted result(s) of evaluation
+        """
+
+        if function_name is None:
+            functions = self.specs.client_parameters.function_list()
+            if len(functions) == 1:
+                function_name = functions[0]
+            else:  # pragma: no cover
+                msg = "The client contains more than one functions. \
+Provide a `function_name` keyword argument to disambiguate."
+                raise TypeError(msg)
+
+        flattened_results: List[Value] = []
+        for result in results:
+            if isinstance(result, tuple):  # pragma: no cover
+                # this branch is impossible to cover without multiple outputs
+                flattened_results.extend(result)
+            else:
+                flattened_results.append(result)
+
+        self.keygen(force=False)
+        keyset = self.keys._keyset  # pylint: disable=protected-access
+
+        decrypter = ValueDecrypter.new(keyset, self.specs.client_parameters, function_name)
+        decrypted = tuple(
+            decrypter.decrypt(position, result.inner)
+            for position, result in enumerate(flattened_results)
+        )
+
+        return decrypted if len(decrypted) != 1 else decrypted[0]
+
+    @property
+    def evaluation_keys(self) -> EvaluationKeys:
+        """
+        Get evaluation keys for encrypted computation.
+
+        Returns:
+            EvaluationKeys
+                evaluation keys for encrypted computation
+        """
+
+        self.keygen(force=False)
+        return self.keys.evaluation
