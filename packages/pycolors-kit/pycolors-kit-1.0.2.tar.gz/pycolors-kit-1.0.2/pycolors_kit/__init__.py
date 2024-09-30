6e67f8628a699ceeee3603031c9ada785525bd54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:04:38 +0000

    Add package pycolors-kit/1.0.2/pycolors-kit-1.0.2.tar.gz

---
+++ packages/pycolors-kit/pycolors-kit-1.0.2.tar.gz/pycolors-kit-1.0.2/pycolors_kit/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time  : 2024/9/30 14:30
+# @Author: game4d@163.com
+
+from .color import ColorEx
+
+
+__version__ = '1.0.2'
+__author__ = 'game4d'
+__description__ = 'Search for color names through HEX, or search for color information through names'
+__email__ = 'game4d@163.com'
+__names__ = 'pycolors-kit'
+__url__ = 'https://github.com/game4d/pycolors-kit'
