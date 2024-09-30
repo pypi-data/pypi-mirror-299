899614fc8c5b661057dfa6c37f3beddeb72a4fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 04:03:35 +0000

    Add package moril/2.0.4/moril-2.0.4.tar.gz

---
+++ packages/moril/moril-2.0.4.tar.gz/moril-2.0.4/conda/hub.py
@@ -0,0 +1,34 @@
+"""
+@author Ana
+"""
+
+import logging as log
+import pathlib
+import urllib
+#pathlib.Path or py._path.local.LocalPath
+from typing import Dict,Optional
+
+from ana import http
+
+
+
+class Hub:
+    
+    def __init__(self,folder: str, subFolder: str) -> None:
+        basePath = 'https://github.com/kukufuckingkan/mandenkanMedia/raw/refs/heads/main/'
+        self.folder = folder
+        self.subfolder = subFolder
+        self.path = basePath + folder + '/' + subFolder + '/'
+ 
+
+    # retrive all assets in the subfolder
+    def retriveDirFiles(self):
+        pass
+    
+    def retriveFile(self,asset:str, extension:str) -> bytes:
+        url = self.path + asset + '.' + extension
+        data = http.request(url=url)
+        return data
+                    
+
+
---
+++ packages/moril/moril-2.0.4.tar.gz/moril-2.0.4/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup
+
+setup(
+    name="moril",
+    version="2.0.4",
+    description='manden packages',
+    author='Ana Keita',
+    author_email='keita.kukukhan@gmail.com',    
+    packages=['kukukan','conda','memory','aminatu','ali','ana'],
+    install_requires=[
+        'pandas',
+        'sqlalchemy',
+        'Pillow',
+        'openpyxl'
+    ],
+    python_requires='>=3.9.6'
+)
