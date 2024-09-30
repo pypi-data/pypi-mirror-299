8fe86a502c25281af53e01e032e56e64b74adb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 14:04:12 +0000

    Add package Limit-Uncertainty-Control-Yourself/1.0.0/Limit_Uncertainty_Control_Yourself-1.0.0.tar.gz

---
+++ packages/Limit-Uncertainty-Control-Yourself/Limit_Uncertainty_Control_Yourself-1.0.0.tar.gz/Limit_Uncertainty_Control_Yourself-1.0.0/README.md
@@ -0,0 +1,11 @@
+# My ETH Predictor
+
+This package helps predict Ethereum prices using an LSTM model and various technical indicators like RSI, EMA, MACD, and Bollinger Bands. This is LUCY or limiting uncertainty, control yourself.
+
+Here is the API token for pypi: 
+pypi-AgEIcHlwaS5vcmcCJDY1ZDEzMzAyLWIxODgtNGU4OS1hZmMyLWI4YmViY
+2U1MGM5ZAACKlszLCJjNTI4NWUyNS0zYjU0LTQzY2UtYTFkYy0xM2E3MzY1NzBiNj
+giXQAABiBaqFZGUoczJ-JZuLhQaiU-zd12hk3ahhqleqdrYt2s8A
+
+## Installation
+
---
+++ packages/Limit-Uncertainty-Control-Yourself/Limit_Uncertainty_Control_Yourself-1.0.0.tar.gz/Limit_Uncertainty_Control_Yourself-1.0.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Limit_Uncertainty_Control_Yourself",
+    version="1.0.0",
+    author="Lucy",
+    author_email="connectchario@gmail.com",
+    description="Ethereum Price Prediction Package for Short-Term Trading",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/LUCY-1986-2009-project-ethereum-market-prediction-unit-limit_uncertainty,control_yourself--OurFriendLucy",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "tensorflow>=2.0",
+        "pandas",
+        "ccxt",
+        "pandas-ta",
+        "numpy",
+        "joblib",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
+    # Add entry_points to make a command-line tool
+    entry_points={
+        'console_scripts': [
+            'heylucy=LUCY.app:main',  # Entry point for your command-line tool
+        ],
+    },
+
+    # Include model and scaler files
+    package_data={
+        'LUCY': [
+            'eth_lstm_model_1h_optimized.h5',
+            'scaler-1h.pkl',
+        ],
+    },
+)
