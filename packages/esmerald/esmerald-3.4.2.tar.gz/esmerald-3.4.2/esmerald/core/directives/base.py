61c578688d30da62e11cbde23f3c881e7ead36f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:05:08 +0000

    Add package esmerald/3.4.2/esmerald-3.4.2.tar.gz

---
+++ packages/esmerald/esmerald-3.4.2.tar.gz/esmerald-3.4.2/esmerald/core/directives/base.py
@@ -0,0 +1,19 @@
+from lilya.cli.base import BaseDirective as LilyaBaseDirective
+
+import esmerald
+from esmerald.core.terminal.print import Print
+from esmerald.parsers import ArbitraryExtraBaseModel
+
+printer = Print()
+
+
+class BaseDirective(ArbitraryExtraBaseModel, LilyaBaseDirective):
+    """The base class from which all directrives derive"""
+
+    help: str = ""
+
+    def get_version(self) -> str:
+        """
+        Returns the current version of Esmerald.
+        """
+        return esmerald.__version__
