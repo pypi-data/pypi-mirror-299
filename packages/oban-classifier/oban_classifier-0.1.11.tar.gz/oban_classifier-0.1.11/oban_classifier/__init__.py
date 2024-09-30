0a485b3fbc924364b27c9504522555b5d151d909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:11:55 +0000

    Add package oban-classifier/0.1.11/oban_classifier-0.1.11.tar.gz

---
+++ packages/oban-classifier/oban_classifier-0.1.11.tar.gz/oban_classifier-0.1.11/oban_classifier/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py
+from .classifier import oban_classifier, post_classification_analysis
+
---
+++ packages/oban-classifier/oban_classifier-0.1.11.tar.gz/oban_classifier-0.1.11/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Get the long description from the README file
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+setup(
+    name="oban_classifier",
+    version="0.1.11",  # Versiyon numarasını arttır
+    description="OBAN Classifier: A Skorch-based flexible neural network for binary and multiclass classification",
+    author="Dr. Volkan OBAN",
+    author_email="volkanobn@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        'numpy', 
+        'pandas', 
+        'torch', 
+        'scikit-learn', 
+        'skorch', 
+        'seaborn', 
+        'matplotlib'
+    ],
+)
+
