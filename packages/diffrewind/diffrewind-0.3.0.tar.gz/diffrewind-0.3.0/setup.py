d1da83988619f1330ebf6f287eee0d4e4176f1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:04:00 +0000

    Add package diffrewind/0.3.0/diffrewind-0.3.0.tar.gz

---
+++ packages/diffrewind/diffrewind-0.3.0.tar.gz/diffrewind-0.3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="diffrewind",
+    version="0.3.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.26.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=7.1.2",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "diffrewind=main:main",
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
