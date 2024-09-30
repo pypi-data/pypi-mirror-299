821cd3189d72138fa9c154760b925f756acacd23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:11:32 +0000

    Add package django-payments-chile/2024.9.29/django_payments_chile-2024.9.29-py3-none-any.whl

---
+++ packages/django-payments-chile/django_payments_chile-2024.9.29-py3-none-any.whl/django_payments_chile/KushkiProvider.py
@@ -0,0 +1,12 @@
+# from payments.core import BasicProvider
+
+
+# class KushkiProvider(BasicProvider):
+#     """
+#     Kushki
+
+#     https://api-docs.kushkipagos.com/docs/online-payments/payment-button
+#     """
+
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
---
+++ packages/django-payments-chile/django_payments_chile-2024.9.29-py3-none-any.whl/django_payments_chile/OneclickProvider.py
@@ -0,0 +1,6 @@
+# from payments.core import BasicProvider
+
+
+# class OneclickProvider(BasicProvider):
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
