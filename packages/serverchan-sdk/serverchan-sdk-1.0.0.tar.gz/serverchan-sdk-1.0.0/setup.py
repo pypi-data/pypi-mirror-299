4d0306d51d0af75dcc8332198883a0b1638c7f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:03:58 +0000

    Add package serverchan-sdk/1.0.0/serverchan-sdk-1.0.0.tar.gz

---
+++ packages/serverchan-sdk/serverchan-sdk-1.0.0.tar.gz/serverchan-sdk-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='serverchan-sdk',
+    version='1.0.0',
+    description='Python SDK for ServerChan',
+    author='easychen',
+    author_email='easychen@gmail.com',
+    packages=['serverchan_sdk'],
+    install_requires=[
+        'requests>=2.0.0'
+    ],
+    url='https://sct.ftqq.com'
+)
