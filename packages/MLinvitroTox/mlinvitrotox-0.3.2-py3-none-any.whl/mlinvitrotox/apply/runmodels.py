948d39c50447183f15b22175fc5cdc14f2ba5f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:03:36 +0000

    Add package MLinvitroTox/0.3.2/mlinvitrotox-0.3.2-py3-none-any.whl

---
+++ packages/MLinvitroTox/mlinvitrotox-0.3.2-py3-none-any.whl/mlinvitrotox/apply/runmodels.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+
+import click
+
+import mlinvitrotox.utils.predict as predict
+from mlinvitrotox.utils.model import Model
+
+
+@click.command("run")
+@click.option(
+    "--model", 
+    "-m", 
+    required=True, 
+    default="mlinvitrotox_model.itox",
+    help="name of the model or path to model file"
+)
+@click.option(
+    "--input_file",
+    "-i",
+    help="input file",
+    required=True,
+    type=click.Path(exists=True, path_type=Path),
+)
+@click.option(
+    "--id",
+    "-d",
+    help="column name of id",
+    default=None,
+    required=False,
+)
+@click.option(
+    "--output_folder",
+    "-o",
+    required=True,
+    type=click.Path(path_type=Path),
+    help="path to directory where the processed output should be stored. It will be created if it does not exist.",
+)
+def run_models(input_file, model, id, output_folder):
+    """
+    Run models on molecular fingerprints
+
+    """
+    model_instance = Model()
+    model_path = Path(model)
+    model_instance.load(model_path)
+
+    # load predicted fingerprints and get chemical ids
+    df_predfps = predict.load_application_data(input_file, id)
+    df_predfps.set_index("chem_id", inplace=True)
+
+    # Collect predictions for each aeid
+    df_predictions = model_instance.predict(df_predfps)
+
+    # set file name for predictions output file
+    output_predictions_file = output_folder / f"{model_path.stem}_predictions.csv"
+
+    # Save predictions
+    try:
+        df_predictions = predict.sort_by_aeid_and_chem_id(df_predictions)
+    except:
+        print("The predictions dataframe was not sorted.")
+    df_predictions.round(5).to_csv(output_predictions_file, index=False)
+    print(f"Predictions stored to {output_predictions_file}")
+
+
+if __name__ == "__main__":
+    run_models()
