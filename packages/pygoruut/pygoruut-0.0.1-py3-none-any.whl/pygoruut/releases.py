02748ca196a2edd99df2aaf3c33ab53630d18348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 04:03:45 +0000

    Add package pygoruut/0.0.1/pygoruut-0.0.1-py3-none-any.whl

---
+++ packages/pygoruut/pygoruut-0.0.1-py3-none-any.whl/pygoruut/releases.py
@@ -0,0 +1,9 @@
+releases = [
+	[
+		9, "0.0.9",
+		18893608,
+		"bcb341e795162e5ca00331a2eb4017c51506e529c4532b57452e3718131735a9",
+		"amd64", "linux",
+		["https://github.com/neurlang/goruut/releases/download/v0.0.9/"],
+	]
+]
