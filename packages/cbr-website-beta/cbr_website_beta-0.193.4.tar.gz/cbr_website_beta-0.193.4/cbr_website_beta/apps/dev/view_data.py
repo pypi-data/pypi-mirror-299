6d1dfcb207f9cf88d8f5f44a82a7c33e99dc8a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 14:03:36 +0000

    Add package cbr-website-beta/0.193.4/cbr_website_beta-0.193.4.tar.gz

---
+++ packages/cbr-website-beta/cbr_website_beta-0.193.4.tar.gz/cbr_website_beta-0.193.4/cbr_website_beta/apps/dev/view_data.py
@@ -0,0 +1,25 @@
+# github_repos = [ {'name': 'cbr-website-beta'      , 'url': 'https://github.com/the-cyber-boardroom/cbr-website-beta'  ,
+#                   'badge_shields_io':'v0.6.6'     ,'badge_codecov_io': 'NZDJOJ2CYH'                                   },
+#                  {'name': 'cbr-athena'            , 'url': 'https://github.com/the-cyber-boardroom/cbr-athena'        ,
+#                   'badge_shields_io':'v0.9.0'     , 'badge_codecov_io': 'NWP3YE7FXK'                                  },
+#                  {'name': 'cbr-hf-gradio'         , 'url': 'https://github.com/the-cyber-boardroom/cbr-hf-gradio'     ,
+#                   'badge_shields_io':'v0.3.0'     , 'badge_codecov_io': 'V8G1I28OND'                                  }]
+
+useful_links = [{'label': 'Athena - Fast API (PROD )'  , 'url': 'https://athena.thecyberboardroom.com'  , 'target': '_blank'},
+                {'label': 'Athena - Fast API (DEV  )'  , 'url': 'https://athena-dev.cyber-boardroom.com', 'target': '_blank'},
+                {'label': 'Athena - Fast API (LOCAL)'  , 'url': 'http://localhost:5111'                 , 'target': '_blank'},]
+
+for_danny    = [ {'label': 'Current Users'        , 'url': '/dev/current-users'   , 'target': ''      },
+                 {'label': 'Current Sessions'     , 'url': '/dev/current-sessions', 'target': ''      },
+                 {'label': 'Athena Prompts'       , 'url': 'https://github.com/the-cyber-boardroom/cbr-hf-gradio/tree/main/docs/prompts', 'target': '_blank'      },
+                 {'label': 'Security content'     , 'url': 'https://github.com/the-cyber-boardroom/cbr-athena/tree/main/docs'          , 'target': '_blank'}]
+
+prompt_uis   = [
+                  {'label': 'Athena - Cyber security in boardroom', 'url': '/content/user/cybersecurity-in-boardroom', 'target': '' },
+                  {'label': 'LLMs - no prompt'                    , 'url': '/llms/dev/no-system-prompt'              , 'target': '' },
+#                 {'label': 'UI Simple'            , 'url': '/llms/chat-gpt/ui-simple'      , 'target': '_blank' },
+#                 {'label': 'UI with Prompts'      , 'url': '/llms/chat-gpt/ui-with-prompts', 'target': '_blank'}
+]
+
+user_management = [ {'label': 'Current Users'        , 'url': '/dev/current-users'   , 'target': ''      },
+                    {'label': 'Current Sessions'     , 'url': '/dev/current-sessions', 'target': ''      }]
