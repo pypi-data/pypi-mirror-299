70ac84066942c58eabebf72a3b8cefde3fc9e011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:05:04 +0000

    Add package srai-core/0.15.5/srai_core-0.15.5.tar.gz

---
+++ packages/srai-core/srai_core-0.15.5.tar.gz/srai_core-0.15.5/README.md
@@ -0,0 +1,52 @@
+# srai-core
+
+
+## Changleog
+
+
+### 0.15.5
+- enabled mongo querries
+
+### 0.15.4
+- added port mapping
+
+### 0.15.3
+- fixed deploy issui
+
+### 0.15.2
+- added databases3
+
+### 0.15.1
+- typos in object store
+- autocreate bucket in bytestore s3
+
+### 0.15.0
+- added object store
+- added various ml types
+
+### 0.14.4
+- added srai_killall
+
+### 0.14.3
+- cleaned up much of the CI scripting
+
+### 0.14.2
+- added get_posix_timestamp()
+- added SourceStore
+- added pydantic dependancy
+
+### 0.14.1
+- changes to srai-release
+
+### 0.14.0
+- migrating to pyproject.toml
+
+### 0.13.1
+- added try load options to stores
+
+### 0.13.0
+- added dirstore options
+
+### 0.12.0
+- added in memory database
+- added default release option
---
+++ packages/srai-core/srai_core-0.15.5.tar.gz/srai_core-0.15.5/srai_core/store/document_store_base.py
@@ -0,0 +1,65 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Optional
+
+
+class DocumentStoreBase(ABC):
+    # file store for
+
+    def __init__(self) -> None:
+        pass
+
+    def copy_to(self, store_to: "DocumentStoreBase") -> None:
+        dict_document = self.load_document_all()
+        for document_id, document in dict_document.items():
+            store_to.save_document(document_id, document)
+
+    @abstractmethod
+    def exists_document(self, document_id: str) -> bool:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def count_document(self) -> int:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def load_document(self, document_id: str) -> dict:
+        raise NotImplementedError()
+
+    def try_load_document(self, bytes_id: str) -> Optional[dict]:
+        if self.exists_document(bytes_id):
+            return self.load_document(bytes_id)
+        else:
+            return None
+
+    @abstractmethod
+    def load_document_all(self) -> Dict[str, dict]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def load_list_document_id(self) -> list:
+        raise NotImplementedError()
+
+    def load_document_for_query(self, query: Dict[str, str]) -> Dict[str, dict]:
+        dict_document = self.load_document_all()
+        dict_selected = {}
+        for document_id, document in dict_document.items():
+            for key in query:
+                if key not in document:
+                    continue
+                if document[key] == query[key]:
+                    dict_selected[document_id] = document
+        return dict_selected
+
+    @abstractmethod
+    def delete_document(self, document_id: str) -> None:
+        raise NotImplementedError()
+
+    def delete_document_all(self) -> int:
+        dict_document = self.load_list_document_id()
+        for document_id in dict_document:
+            self.delete_document(document_id)
+        return len(dict_document)
+
+    @abstractmethod
+    def save_document(self, document_id: str, document: dict) -> None:
+        raise NotImplementedError()
