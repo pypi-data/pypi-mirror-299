885aa3d7f0edc169d5b13e8c5aaa248bc9ba9fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:04:33 +0000

    Add package isee-club/0.1.0/isee_club-0.1.0.tar.gz

---
+++ packages/isee-club/isee_club-0.1.0.tar.gz/isee_club-0.1.0/isee_club/datetime_utils.py
@@ -0,0 +1,6 @@
+import datetime
+
+
+def current_date():
+    current_date_ = str(datetime.date.today())
+    return current_date_
---
+++ packages/isee-club/isee_club-0.1.0.tar.gz/isee_club-0.1.0/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='isee_club',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A first library',
+    author='isee',
+    author_email='',
+    url='',  # GitHub 链接
+    install_requires=[],  # 如果有依赖项，列在这里
+)
