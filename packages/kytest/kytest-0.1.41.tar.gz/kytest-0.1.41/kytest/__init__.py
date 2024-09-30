3fe71089cc0f58380bbb8150d508713e74b5d04d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 14:03:29 +0000

    Add package kytest/0.1.41/kytest-0.1.41.tar.gz

---
+++ packages/kytest/kytest-0.1.41.tar.gz/kytest-0.1.41/kytest/__init__.py
@@ -0,0 +1,12 @@
+from .running.runner import main
+from .running.conf import App
+from .utils.config import kconfig
+from .utils.pytest_util import *
+from .utils.allure_util import *
+from .utils.log import logger
+from .page import Page
+from .core.api.request import HttpReq
+from .core.api.case import TestCase as TC
+
+__version__ = "0.1.41"
+__description__ = "API/安卓/IOS/WEB平台自动化测试框架"
