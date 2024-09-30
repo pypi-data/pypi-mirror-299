ead23e2143798c62b33fd9e1d27506e131c7c655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 14:03:54 +0000

    Add package sisicepy/0.1.6/sisicepy-0.1.6-py3-none-any.whl

---
+++ packages/sisicepy/sisicepy-0.1.6-py3-none-any.whl/sisicepy/traces/recorder/datacube.py
@@ -0,0 +1,21 @@
+import xarray as xr
+import numpy as np
+import pint_xarray
+from pint_xarray import unit_registry as ureg
+
+def count2volt_datacube(self,Vpp=4.096,nb_bin=2**24):
+    '''
+    Convert numeric value to Voltage for DataCube
+
+    :param Vpp: peak to peak value, default: 4.096 (unit V)
+    :type Vpp: float
+    :param nb_bin: number of integer for the numerisation, default: 2**24
+    :type nb_bin: int
+    '''
+
+    coeff   =  Vpp/nb_bin/np.int32(self._obj.attrs['P_AMPL']) * ureg.volt/ureg.bit
+    
+    return self._obj*coeff
+
+#-----------------------------------------------------------------------------
+xr.DataArray.traces.count2volt_datacube = count2volt_datacube
