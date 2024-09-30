a0675bee3a3297a40d4597c4f11e31d10f7450ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 04:03:47 +0000

    Add package spedpy/1.1.3/spedpy-1.1.3.tar.gz

---
+++ packages/spedpy/spedpy-1.1.3.tar.gz/spedpy-1.1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from setuptools import find_packages
+
+from sped import __version__
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='spedpy',
+    version=__version__,
+    license='MIT License',
+    author='Ismael Nascimento',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='ismaelnjr@icloud.com.br',
+    keywords='sped fiscal receita federal',
+    description=u'Biblioteca para geração dos arquivos do Sistema Público de Escrituração Digital (SPED) para Python',
+    packages=find_packages(exclude=['test']),
+    include_package_data=True,
+    url='https://github.com/Trust-Code/python-sped',
+    package_data={
+        'sped': ['leiautes/*'],
+    },
+    install_requires=['six'],
+    tests_require=['pytest'],
+    classifiers=
+        ["Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
+   
+    )
