01137295805f73cfb3566d7df77b555ceb806e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:11:38 +0000

    Add package base64c/0.0.5/base64c-0.0.5-py3-none-any.whl

---
+++ packages/base64c/base64c-0.0.5-py3-none-any.whl/base64c/__init__.py
@@ -0,0 +1,3 @@
+from .base64c import b64encode, b64decode, urlsafe_b64encode, urlsafe_b64decode, standard_b64encode, standard_b64decode
+
+__all__ = ['b64encode', 'b64decode', 'urlsafe_b64encode', 'urlsafe_b64decode', 'standard_b64encode', 'standard_b64decode']
