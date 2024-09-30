e538e54213df03d51eb865df04febb3c62b252a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 03:12:07 +0000

    Add package lymbo/0.2.1/lymbo-0.2.1-py3-none-any.whl

---
+++ packages/lymbo/lymbo-0.2.1-py3-none-any.whl/lymbo/__init__.py
@@ -0,0 +1,28 @@
+from multiprocessing.managers import DictProxy
+import queue
+from typing import Union
+
+from lymbo.cm import args
+from lymbo.cm import expand
+from lymbo.cm import test
+from lymbo.resource_manager import scope_class
+from lymbo.resource_manager import scope_function
+from lymbo.resource_manager import scope_global
+from lymbo.resource_manager import scope_module
+
+__version__ = "0.2.1"
+
+__all__ = [
+    "args",
+    "expand",
+    "test",
+    "scope_class",
+    "scope_function",
+    "scope_global",
+    "scope_module",
+]
+
+
+_local_resources: Union[dict, None] = None
+_shared_queue: Union[queue.Queue, None] = None
+_shared_scopes: Union[DictProxy, None] = None
---
+++ packages/lymbo/lymbo-0.2.1-py3-none-any.whl/lymbo/cm.py
@@ -0,0 +1,59 @@
+import contextlib
+import os
+
+from lymbo.env import LYMBO_TEST_COLLECTION
+
+
+@contextlib.contextmanager
+def test(args=None):
+    yield
+
+
+def args(*args, **kwargs):
+    """
+    Define the parameters used to call the test function.
+
+    If a parameter is defined using the `lymbo.expand` function,
+    the resulting list is flattened before being passed to the test function.
+    """
+
+    if LYMBO_TEST_COLLECTION in os.environ:
+
+        flattened_params = [(args, kwargs)]
+
+        pos = 0
+        for arg in args:
+            if type(arg) is ArgParams:
+                new_flattened_params = []
+                for elt in arg.args:
+                    for params in flattened_params:
+                        gargs, gkwargs = params
+                        gargs = list(gargs)
+                        gargs[pos] = elt
+                        gargs = tuple(gargs)
+                        new_flattened_params.append((gargs, gkwargs))
+                flattened_params = new_flattened_params
+            pos += 1
+
+        for key, value in kwargs.items():
+            if type(value) is ArgParams:
+                new_flattened_params = []
+                for elt in value.args:
+                    for params in flattened_params:
+                        gargs, gkwargs = params
+                        gkwargs = gkwargs.copy()
+                        gkwargs[key] = elt
+                        new_flattened_params.append((gargs, gkwargs))
+                flattened_params = new_flattened_params
+
+        return flattened_params
+
+
+class ArgParams:
+
+    def __init__(self, *args):
+        self.args = list(*args)
+
+
+def expand(*args) -> ArgParams:
+    return ArgParams(args)
