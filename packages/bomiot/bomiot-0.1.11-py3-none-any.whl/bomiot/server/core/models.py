6fe8bf895eb50e21f0d7234a6519fc36f38ec57f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 15:04:40 +0000

    Add package bomiot/0.1.11/bomiot-0.1.11-py3-none-any.whl

---
+++ packages/bomiot/bomiot-0.1.11-py3-none-any.whl/bomiot/server/core/models.py
@@ -0,0 +1,4 @@
+# Create your models here.
+from django.db.models import Model, CharField, GenericIPAddressField, IntegerField, TextField, DateTimeField, \
+    ManyToManyField, ForeignKey, DO_NOTHING, BooleanField
+
