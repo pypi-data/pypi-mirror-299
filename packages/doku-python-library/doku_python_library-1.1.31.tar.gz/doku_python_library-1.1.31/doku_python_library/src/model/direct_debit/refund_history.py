7c73064d4ab27a7e8638cb1db5b8ac2e96b6daad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-30 14:04:21 +0000

    Add package doku-python-library/1.1.31/doku_python_library-1.1.31.tar.gz

---
+++ packages/doku-python-library/doku_python_library-1.1.31.tar.gz/doku_python_library-1.1.31/doku_python_library/src/model/direct_debit/refund_history.py
@@ -0,0 +1,8 @@
+from doku_python_library.src.model.va.total_amount import TotalAmount
+
+
+class RefundHistory:
+
+    def __init__(self, refundNo: str = None, partnerReferenceNo: str = None, refundAmount: TotalAmount = None,
+                 refundDate: str = None, reason: str = None) -> None:
+        pass
