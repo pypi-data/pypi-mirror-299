9737af7d51ed9562a44ef55f1fb03f18fa061634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:05:08 +0000

    Add package udata-front/5.2.3.dev31313/udata_front-5.2.3.dev31313-py2.py3-none-any.whl

---
+++ packages/udata-front/udata_front-5.2.3.dev31313-py2.py3-none-any.whl/udata_front/theme/gouvfr/datagouv-components/src/types/organizations.ts
@@ -0,0 +1,33 @@
+import type { User } from "..";
+import type { Badges } from "./badges";
+
+export type MemberRole = "admin" | "editor";
+
+export type Member = {
+  role: MemberRole;
+  user: User;
+};
+
+export type NewOrganization = {
+  acronym: string | null;
+  name: string;
+  business_number_id: string | null;
+  description: string;
+  url: string | null;
+};
+
+export type Organization = NewOrganization & {
+  id: string;
+  created_at: string;
+  last_modified: string;
+  last_update: string;
+  deleted: string | null;
+  logo: string;
+  logo_thumbnail: string;
+  members: Array<Member>;
+  metrics: { datasets: number; followers: number; members: number; reuses: number; views: number; };
+  page: string;
+  slug: string;
+  uri: string;
+  badges: Badges;
+};
