b954eb7b67c5544d01f672cfdcd68c26fc69bfe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 03:14:31 +0000

    Add package PyCosmo/2.2.0/PyCosmo-2.2.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/PyCosmo/PyCosmo-2.2.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/PyCosmo/cython/__init__.py
@@ -0,0 +1,24 @@
+# This file is part of PyCosmo, a multipurpose cosmology calculation tool in Python.
+#
+# Copyright (C) 2013-2021 ETH Zurich, Institute for Particle and Astrophysics and SIS
+# ID.
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from . import halo_integral  # noqa: F401
+from . import halo_integral_1h  # noqa: F401
+from . import halo_integral_2h  # noqa: F401
+from . import halo_integral_HI_1h  # noqa: F401
+from . import halo_integral_HI_2h  # noqa: F401
+from . import rho_HI_integral  # noqa: F401
+from . import rho_m_integral  # noqa: F401
