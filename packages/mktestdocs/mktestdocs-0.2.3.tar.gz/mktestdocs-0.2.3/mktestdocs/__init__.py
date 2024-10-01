b34555bdc37f050ff42a895d489ab199ecd8f85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 04:03:33 +0000

    Add package mktestdocs/0.2.3/mktestdocs-0.2.3.tar.gz

---
+++ packages/mktestdocs/mktestdocs-0.2.3.tar.gz/mktestdocs-0.2.3/mktestdocs/__init__.py
@@ -0,0 +1,20 @@
+from mktestdocs.__main__ import (
+    register_executor,
+    check_codeblock,
+    grab_code_blocks,
+    check_docstring,
+    check_md_file,
+    get_codeblock_members,
+)
+
+__version__ = "0.2.3"
+
+__all__ = [
+    "__version__",
+    "register_executor",
+    "check_codeblock",
+    "grab_code_blocks",
+    "check_docstring",
+    "check_md_file",
+    "get_codeblock_members",
+]
