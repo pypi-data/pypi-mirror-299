d80379d5d60b7d54713b192d327b89af58ffa161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 06:04:35 +0000

    Add package wdoc/2.1.4/wdoc-2.1.4.tar.gz

---
+++ packages/wdoc/wdoc-2.1.4.tar.gz/wdoc-2.1.4/WDoc/utils/__init__.py
@@ -0,0 +1,10 @@
+from . import batch_file_loader, loaders, misc, prompts, tasks, customs
+
+__all__ = [
+    'batch_file_loader',
+    'loaders',
+    'misc',
+    'prompts',
+    'tasks',
+    'customs',
+]
