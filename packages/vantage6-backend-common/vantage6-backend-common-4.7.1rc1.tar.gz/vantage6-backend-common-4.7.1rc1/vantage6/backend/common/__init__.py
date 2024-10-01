4dc9555c66e5b0ffae871ae6fd5c264234b981fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 04:04:01 +0000

    Add package vantage6-backend-common/4.7.1rc1/vantage6-backend-common-4.7.1rc1.tar.gz

---
+++ packages/vantage6-backend-common/vantage6-backend-common-4.7.1rc1.tar.gz/vantage6-backend-common-4.7.1rc1/vantage6/backend/common/__init__.py
@@ -0,0 +1,5 @@
+""" Common functionality for the vantage6 server and algorithm store.
+"""
+
+# make sure the version is available
+from vantage6.backend.common._version import __version__  # noqa: F401
