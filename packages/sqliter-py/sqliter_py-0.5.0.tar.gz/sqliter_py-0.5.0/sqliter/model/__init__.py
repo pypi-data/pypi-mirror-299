39843c13c5a9a35a2c780db0448cb99a4bc84852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 04:03:51 +0000

    Add package sqliter-py/0.5.0/sqliter_py-0.5.0.tar.gz

---
+++ packages/sqliter-py/sqliter_py-0.5.0.tar.gz/sqliter_py-0.5.0/sqliter/model/__init__.py
@@ -0,0 +1,9 @@
+"""This module provides the base model class for SQLiter database models.
+
+It exports the BaseDBModel class, which is used to define database
+models in SQLiter applications.
+"""
+
+from .model import BaseDBModel
+
+__all__ = ["BaseDBModel"]
