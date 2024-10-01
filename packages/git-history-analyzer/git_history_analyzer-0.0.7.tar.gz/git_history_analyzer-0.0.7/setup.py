8df1d956bb5460806d047d7804d2eee678febb7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 14:03:46 +0000

    Add package git-history-analyzer/0.0.7/git_history_analyzer-0.0.7.tar.gz

---
+++ packages/git-history-analyzer/git_history_analyzer-0.0.7.tar.gz/git_history_analyzer-0.0.7/setup.py
@@ -0,0 +1,21 @@
+# python3 setup.py sdist bdist_wheel
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="git-history-analyzer",
+    version="0.0.7",
+    author="Jorge Cardona",
+    description="Descripción del paquete",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jorgecardona/git-history-analyzer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
