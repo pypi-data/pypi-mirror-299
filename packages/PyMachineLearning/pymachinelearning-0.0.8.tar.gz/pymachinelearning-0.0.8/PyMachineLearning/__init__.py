130280de9b5b42e47f099e02f55b2166670c05d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:05:34 +0000

    Add package PyMachineLearning/0.0.8/pymachinelearning-0.0.8.tar.gz

---
+++ packages/PyMachineLearning/pymachinelearning-0.0.8.tar.gz/pymachinelearning-0.0.8/PyMachineLearning/__init__.py
@@ -0,0 +1,4 @@
+from .preprocessing import imputer, encoder, scaler, discretizer, features_selector
+from .metrics import absolute_r2_score
+from .evaluation import SimpleEvaluation, NestedEvaluation, SemiNestedEvaluation
+from .plots import predictive_plots, predictive_intervals
