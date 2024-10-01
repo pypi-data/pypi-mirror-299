6035326ede178db581c60ae60008e7c6473cc474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 14:04:10 +0000

    Add package mtmai-client/0.3.1007/mtmai_client-0.3.1007.tar.gz

---
+++ packages/mtmai-client/mtmai_client-0.3.1007.tar.gz/mtmai_client-0.3.1007/mtmai_client/models/doc_coll_create.py
@@ -0,0 +1,58 @@
+from typing import Any, Dict, List, Type, TypeVar, Union
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="DocCollCreate")
+
+
+@_attrs_define
+class DocCollCreate:
+    """
+    Attributes:
+        title (Union[Unset, str]):  Default: 'default'.
+    """
+
+    title: Union[Unset, str] = "default"
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        title = self.title
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        if title is not UNSET:
+            field_dict["title"] = title
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        title = d.pop("title", UNSET)
+
+        doc_coll_create = cls(
+            title=title,
+        )
+
+        doc_coll_create.additional_properties = d
+        return doc_coll_create
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
