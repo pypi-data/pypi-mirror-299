b850da98a9e48158363817e267641512030654f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 05:04:56 +0000

    Add package decoding/0.1.1/decoding-0.1.1.tar.gz

---
+++ packages/decoding/decoding-0.1.1.tar.gz/decoding-0.1.1/setup.py
@@ -0,0 +1,39 @@
+"""
+Setup file for the decoding library.
+"""
+
+import pathlib
+
+import setuptools
+
+core_requirements = [
+    "jax==0.4.31",
+    "jaxtyping==0.2.34",
+    "vllm==0.6.2",
+]
+dev_requirements = [
+    "pdoc==14.7.0",
+    "pre-commit==3.8.0",
+    "pyright==1.1.382.post1",
+    "pytest==8.3.3",
+    "pytest-cov==5.0.0",
+    "pytest-html==4.1.1",
+    "ruff==0.6.8",
+]
+
+with pathlib.Path("README.md").open(encoding="utf-8") as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="decoding",
+    version="0.1.1",
+    description="Composable inference algorithms with LLMs and programmable logic",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    authors=["Ben Lipkin"],
+    license="Apache 2.0",
+    install_requires=core_requirements,
+    extras_require={"dev": dev_requirements},
+    python_requires=">=3.11",
+    packages=["decoding"],
+)
