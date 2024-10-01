193e31d6f691a7ca9a9f521b37bd33476dfcbbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:05:31 +0000

    Add package rea-copilot/0.0.1/rea_copilot-0.0.1.tar.gz

---
+++ packages/rea-copilot/rea_copilot-0.0.1.tar.gz/rea_copilot-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rea-copilot',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.rea-group.com/category/tech/',
+    author='rea',
+    author_email='pip@rea-group.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
