b1b81b14e30066c1c27b131fb9157b37462b2bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 07:06:23 +0000

    Add package vo-models/0.4.0.dev1/vo_models-0.4.0.dev1.tar.gz

---
+++ packages/vo-models/vo_models-0.4.0.dev1.tar.gz/vo_models-0.4.0.dev1/vo_models/voresource/__init__.py
@@ -0,0 +1,32 @@
+"""IVOA VOResource-v1.1.xsd pydantic-xml models"""
+
+from vo_models.voresource.models import (
+    AccessURL,
+    Capability,
+    Contact,
+    Content,
+    Creator,
+    Curation,
+    Date,
+    Interface,
+    MirrorURL,
+    Organisation,
+    Relationship,
+    Resource,
+    ResourceName,
+    Rights,
+    SecurityMethod,
+    Service,
+    Source,
+    Validation,
+    WebBrowser,
+    WebService,
+)
+from vo_models.voresource.types import (
+    AuthorityID,
+    IdentifierURI,
+    ResourceKey,
+    UTCDateTime,
+    UTCTimestamp,
+    ValidationLevel,
+)
