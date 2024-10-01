6a44b6d4f3f8628f73984c66ed5443682302a036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 04:04:07 +0000

    Add package easy-utils-dev/2.43/easy_utils_dev-2.43.tar.gz

---
+++ packages/easy-utils-dev/easy_utils_dev-2.43.tar.gz/easy_utils_dev-2.43/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '2.43'
+
+# Setting up
+setup(
+    name="easy_utils_dev",
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=['psutil' 
+                      , 'ping3' , 
+                      'flask' , 
+                      'flask_cors' , 
+                      'xmltodict' , 
+                      ],
+    keywords=['python3'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
