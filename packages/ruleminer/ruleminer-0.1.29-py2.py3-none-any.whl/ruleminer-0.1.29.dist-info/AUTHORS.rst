aa48bcd4b9f6539076539d06e3c907083bc0cbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 04:04:23 +0000

    Add package ruleminer/0.1.29/ruleminer-0.1.29-py2.py3-none-any.whl

---
+++ packages/ruleminer/ruleminer-0.1.29-py2.py3-none-any.whl/ruleminer-0.1.29.dist-info/AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Willem Jan Willemse <w.j.willemse@dnb.nl>
+
+Contributors
+------------
+
+* Kees-Karel Taris <k.k.h.taris@dnb.nl>
---
+++ packages/ruleminer/ruleminer-0.1.29-py2.py3-none-any.whl/ruleminer/__init__.py
@@ -0,0 +1,31 @@
+"""Top-level package for ruleminer."""
+
+__author__ = """Willem Jan Willemse"""
+__email__ = "w.j.willemse@dnb.nl"
+__version__ = "0.1.28"
+
+from .ruleminer import (
+    rule_expression,
+    RuleMiner,
+    RULE_ID,
+    RULE_GROUP,
+    RULE_DEF,
+    RULE_STATUS,
+    ABSOLUTE_SUPPORT,
+    ABSOLUTE_EXCEPTIONS,
+    CONFIDENCE,
+    ENCODINGS,
+    flatten_and_sort,
+)
+from .parser import (
+    math_expression,
+    function_expression,
+    _quoted_string,
+    _column,
+)
+
+from .utils import (
+    tree_to_expressions,
+    fit_ensemble_and_extract_expressions,
+    fit_dataframe_to_ensemble,
+)
