719740e06409ebceda85d3bd7c8e7e095dd23747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 07:04:12 +0000

    Add package superset-python-client/1.0.0/superset_python_client-1.0.0-py3-none-any.whl

---
+++ packages/superset-python-client/superset_python_client-1.0.0-py3-none-any.whl/supersetapiclient/exceptions.py
@@ -0,0 +1,35 @@
+"""Custom Exception."""
+
+import json
+
+from requests import HTTPError
+
+
+class NotFound(Exception):
+    pass
+
+
+class MultipleFound(Exception):
+    pass
+
+
+class QueryLimitReached(Exception):
+    pass
+
+
+class BadRequestError(HTTPError):
+    def __init__(self, *args, **kwargs):
+        self.message = kwargs.pop("message", None)
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        return json.dumps(self.message, indent=4)
+
+
+class ComplexBadRequestError(HTTPError):
+    def __init__(self, *args, **kwargs):
+        self.errors = kwargs.pop("errors", None)
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        return json.dumps(self.errors, indent=4)
