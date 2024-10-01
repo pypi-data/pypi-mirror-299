f5b673f635a6992243c50eb61d8d48821088d898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 05:05:03 +0000

    Add package Resemblyzer-python3.9/3.0.1/Resemblyzer-python3.9-3.0.1.tar.gz

---
+++ packages/Resemblyzer-python3.9/Resemblyzer-python3.9-3.0.1.tar.gz/Resemblyzer-python3.9-3.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("requirements_demos.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="Resemblyzer-python3.9",
+    version="3.0.1",
+    packages=find_packages(),
+    package_data={
+        "resemblyzer": ["pretrained.pt"]
+    },
+    python_requires=">=3.5",
+    install_requires=requirements,
+    # author="Corentin Jemine",
+    # author_email="corentin@resemble.ai",
+    description="Analyze and compare voices with deep learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zixian-zang-ai/Resemblyzer-python3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
