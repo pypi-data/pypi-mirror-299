0a97b597b906ed463c33d9606a4e7b4994540b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 03:15:02 +0000

    Add package canalruidoso/0.2/canalruidoso-0.2.tar.gz

---
+++ packages/canalruidoso/canalruidoso-0.2.tar.gz/canalruidoso-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="canalruidoso",
+    version="0.2",
+    packages= find_packages(),
+    description="A simple library with foo function",
+    author="Valentín Bonás y Jaime Amigo",
+    author_email="jaimeamigoUTDT@mail.utdt.edu",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
