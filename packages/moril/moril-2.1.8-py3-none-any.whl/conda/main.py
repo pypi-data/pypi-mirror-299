9bdaf0d81416b03d950fa84fd00d720e21da8c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 14:04:15 +0000

    Add package moril/2.1.8/moril-2.1.8-py3-none-any.whl

---
+++ packages/moril/moril-2.1.8-py3-none-any.whl/conda/main.py
@@ -0,0 +1,8 @@
+
+# import moril
+# from git import Git
+ 
+# if __name__ == "__main__":
+#     test = Git(folder='dictionary',subFolder='english')
+#     res  = test.retrive(assetName='word',extension='xlsx')
+#     x = 2
