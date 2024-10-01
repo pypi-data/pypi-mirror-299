e5e86dbecf06eee3960ee40b664ae01762e54439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:06:04 +0000

    Add package litewebagent/0.1.6/litewebagent-0.1.6-py3-none-any.whl

---
+++ packages/litewebagent/litewebagent-0.1.6-py3-none-any.whl/litewebagent/utils/utils.py
@@ -0,0 +1,83 @@
+import base64
+import os
+import json
+import logging
+from openai import OpenAI
+from dotenv import load_dotenv
+_ = load_dotenv()
+
+logger = logging.getLogger(__name__)
+openai_client = OpenAI()
+
+
+def setup_logger(log_folder, log_file="log.txt"):
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(os.path.join(log_folder, log_file), mode="w"),
+            logging.StreamHandler()
+        ]
+    )
+    return logging.getLogger(__name__)
+
+
+def query_openai_model(system_msg, prompt, screenshot_path, num_outputs):
+    base64_image = encode_image(screenshot_path)
+    response = openai_client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": system_msg},
+            {"role": "user",
+             "content": [
+                 {"type": "text", "text": prompt},
+                 {"type": "image_url",
+                  "image_url": {
+                      "url": f"data:image/jpeg;base64,{base64_image}",
+                      "detail": "high"
+                  }
+                  }
+             ]
+             },
+        ],
+        n=num_outputs
+    )
+
+    answer: list[str] = [x.message.content for x in response.choices]
+    return answer
+
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+
+def search_interactive_elements(interactive_elements, extracted_number):
+    for element in interactive_elements:
+        if element.get('bid') == extracted_number:
+            return {
+                'text': element.get('text'),
+                'type': element.get('type'),
+                'tag': element.get('tag'),
+                'id': element.get('id'),
+                'href': element.get('href'),
+                'title': element.get('title'),
+                'ariaLabel': element.get('ariaLabel')
+            }
+    return {}  # Return empty dictionary if no matching element is found
+
+
+def parse_function_args(function_args):
+    if not function_args or not isinstance(function_args, list):
+        return None
+    first_arg = function_args[0]
+    return first_arg if isinstance(first_arg, str) and first_arg.replace('.', '', 1).isdigit() else None
+
+
+def append_to_steps_json(result, file_path):
+    json_line = json.dumps(result)
+    with open(file_path, 'a') as file:
+        file.write(json_line + '\n')
+    print(f"Appended result to {file_path}")
