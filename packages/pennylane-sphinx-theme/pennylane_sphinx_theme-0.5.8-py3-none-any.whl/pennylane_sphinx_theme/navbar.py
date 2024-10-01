f2f0caa551583ea82bb13f1bddf6ce4eda820766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 04:04:07 +0000

    Add package pennylane-sphinx-theme/0.5.8/pennylane_sphinx_theme-0.5.8-py3-none-any.whl

---
+++ packages/pennylane-sphinx-theme/pennylane_sphinx_theme-0.5.8-py3-none-any.whl/pennylane_sphinx_theme/navbar.py
@@ -0,0 +1,101 @@
+"""
+This module contains the common PennyLane navigation bar data.
+"""
+
+NAVBAR_LEFT = [
+    {
+        "name": "Learn",
+        "href": "https://pennylane.ai/qml/",
+        "dropdown": [
+            {
+                "name": "Demos",
+                "href": "https://pennylane.ai/qml/demonstrations/",
+            },
+            {
+                "name": "Codebook",
+                "href": "https://pennylane.ai/codebook/",
+            },
+            {
+                "name": "Challenges",
+                "href": "https://pennylane.ai/challenges/",
+            },
+            {
+                "name": "Videos",
+                "href": "https://pennylane.ai/qml/videos/",
+            },
+            {
+                "name": "Glossary",
+                "href": "https://pennylane.ai/qml/glossary/",
+            },
+            {
+                "name": "Features",
+                "href": "https://pennylane.ai/features/",
+            },
+            {
+                "name": "Education",
+                "href": "https://pennylane.ai/education/",
+            },
+            {
+                "name": "Performance",
+                "href": "https://pennylane.ai/performance/",
+            },
+            {
+                "name": "FAQ",
+                "href": "https://pennylane.ai/faq/",
+            },
+        ],
+    },
+    {
+        "name": "Documentation",
+        "href": "https://docs.pennylane.ai",
+        "dropdown": [
+            {
+                "name": "API",
+                "href": "https://docs.pennylane.ai/en/stable/code/qml.html",
+            },
+            {
+                "name": "Devices",
+                "href": "https://pennylane.ai/plugins/",
+            },
+            {
+                "name": "GitHub",
+                "href": "https://github.com/PennyLaneAI/pennylane",
+                "external": True,
+            },
+            {
+                "name": "Catalyst",
+                "href": "https://docs.pennylane.ai/projects/catalyst/",
+            },
+        ],
+    },
+    {
+        "name": "Get involved",
+        "href": "https://pennylane.ai/get-involved/",
+    },
+    {
+        "name": "Datasets",
+        "href": "https://pennylane.ai/datasets/",
+    },
+    {
+        "name": "Blog",
+        "href": "https://pennylane.ai/blog/",
+    },
+    {
+        "name": "Support",
+        "href": "https://discuss.pennylane.ai",
+        "external": True,
+    },
+]
+
+
+NAVBAR_RIGHT = [
+    {
+        "name": "GitHub",
+        "href": "https://github.com/PennyLaneAI/pennylane",
+        "icon": "bx bxl-github",
+    },
+    {
+        "name": "Install",
+        "href": "https://pennylane.ai/install",
+    },
+]
