3640da49f1939f88b2e15eaac1d514d37d450702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:05:46 +0000

    Add package pvsite-datamodel/1.0.40/pvsite_datamodel-1.0.40-py3-none-any.whl

---
+++ packages/pvsite-datamodel/pvsite_datamodel-1.0.40-py3-none-any.whl/pvsite_datamodel/__init__.py
@@ -0,0 +1,16 @@
+"""Python SDK for reading/writing to/from pvsite database."""
+
+from .connection import DatabaseConnection
+from .sqlmodels import (
+    APIRequestSQL,
+    ForecastSQL,
+    ForecastValueSQL,
+    GenerationSQL,
+    InverterSQL,
+    SiteGroupSQL,
+    SiteSQL,
+    StatusSQL,
+    UserSQL,
+)
+
+__version__ = "1.0.40"
