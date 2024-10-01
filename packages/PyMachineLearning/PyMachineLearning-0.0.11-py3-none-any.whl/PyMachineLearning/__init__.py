88694f6c8053540734c2c938ed205729d499ee3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 16:04:01 +0000

    Add package PyMachineLearning/0.0.11/PyMachineLearning-0.0.11-py3-none-any.whl

---
+++ packages/PyMachineLearning/PyMachineLearning-0.0.11-py3-none-any.whl/PyMachineLearning/__init__.py
@@ -0,0 +1,4 @@
+from .preprocessing import Imputer, Encoder, Scaler, Discretizer, FeaturesSelector, PCA, ToPandas
+from .metrics import absolute_r2_score
+from .evaluation import SimpleEvaluation, NestedEvaluation, SemiNestedEvaluation
+from .plots import predictive_plots, predictive_intervals
