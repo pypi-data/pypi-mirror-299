44216f3c25098bd273be5a94286b4992d4d3382d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 16:03:53 +0000

    Add package passkit-python-grpc-sdk/1.1.97.2/passkit-python-grpc-sdk-1.1.97.2.tar.gz

---
+++ packages/passkit-python-grpc-sdk/passkit-python-grpc-sdk-1.1.97.2.tar.gz/passkit-python-grpc-sdk-1.1.97.2/passkit_io/__init__.py
@@ -0,0 +1,60 @@
+# passkit_io/__init__.py
+
+def get_analytics():
+    from passkit_io import analytics
+    return analytics
+
+
+def get_common():
+    from passkit_io import common
+    return common
+
+
+def get_certificate():
+    from passkit_io import certificate
+    return certificate
+
+
+def get_core():
+    from passkit_io import core
+    return core
+
+
+def get_event_tickets():
+    from passkit_io import event_tickets
+    return event_tickets
+
+
+def get_flights():
+    from passkit_io import flights
+    return flights
+
+
+def get_image():
+    from passkit_io import image
+    return image
+
+
+def get_member():
+    from passkit_io import member
+    return member
+
+
+def get_raw():
+    from passkit_io import raw
+    return raw
+
+
+def get_scheduler():
+    from passkit_io import scheduler
+    return scheduler
+
+
+def get_single_use_coupons():
+    from passkit_io import single_use_coupons
+    return single_use_coupons
+
+
+def get_user():
+    from passkit_io import user
+    return user
