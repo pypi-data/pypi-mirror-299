d564a15e20dddaab6a8feadc63e7ad2191ddaecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 14:03:44 +0000

    Add package resumeassistant/0.0.2/resumeassistant-0.0.2-py3-none-any.whl

---
+++ packages/resumeassistant/resumeassistant-0.0.2-py3-none-any.whl/resumeassistant/__init__.py
@@ -0,0 +1,13 @@
+'''
+Copyright (C), 2024, Harish Sista. 
+
+resumeassistant project
+'''
+
+__name__ = ''
+__author__ = 'Harish Sista'
+__all__ = ['data', 'utils', 'operator']
+
+
+from resumeassistant.operator import worker
+from resumeassistant.data import candidate_record
