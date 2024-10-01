a7c352cc3b2f6fbfd52f04111921d8343ddbaf38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 14:04:15 +0000

    Add package NikGapps/3.24/NikGapps-3.24-py3-none-any.whl

---
+++ packages/NikGapps/NikGapps-3.24-py3-none-any.whl/NikGapps/helper/compression/CompOps.py
@@ -0,0 +1,43 @@
+from NikGapps.helper.FileOp import FileOp
+from NikGapps.helper.Statics import Statics
+from NikGapps.helper.Package import Package
+from .Modes import Modes
+
+
+class CompOps:
+
+    @staticmethod
+    def get_compression_obj(file_name, compression_mode=Modes.ZIP):
+        match compression_mode:
+            case Modes.TAR_XZ:
+                from .Tar import Tar
+                return Tar(file_name)
+            case _:
+                from .Zip import Zip
+                return Zip(file_name)
+
+    @staticmethod
+    def compress_package(pkg_zip_path, pkg: Package, compression_mode=Modes.DEFAULT):
+        try:
+            pkg_size = 0
+            pkg_txt_path = pkg_zip_path.replace(compression_mode, "") + "_" + compression_mode[1:] + ".txt"
+            cpkg = CompOps.get_compression_obj(pkg_zip_path, compression_mode=compression_mode)
+            file_index = 1
+            for x in pkg.file_dict:
+                y = pkg.file_dict[x]
+                file_index = file_index + 1
+                pkg_size = pkg_size + Statics.get_file_bytes(x)
+                cpkg.add_file(x, y)
+            if pkg.clean_flash_only:
+                cpkg.add_string("", "___etc___permissions/" + pkg.package_title + ".prop")
+            pkg.pkg_size = pkg_size
+            if pkg.priv_app_permissions_str is not None:
+                cpkg.add_string(pkg.priv_app_permissions_str, "___etc___permissions/" + pkg.package_name + ".xml")
+            cpkg.add_string(pkg.get_installer_script(str(pkg_size)), "installer.sh")
+            cpkg.add_string(pkg.get_uninstaller_script(), "uninstaller.sh")
+            cpkg.close()
+            FileOp.write_string_file(str(pkg_size), pkg_txt_path)
+        except Exception as e:
+            print(e)
+            return False
+        return True
