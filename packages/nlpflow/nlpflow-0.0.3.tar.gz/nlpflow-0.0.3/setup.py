32eacafaff0c1ecb1074a5f03bf41b23df2f9326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 05:05:36 +0000

    Add package nlpflow/0.0.3/nlpflow-0.0.3.tar.gz

---
+++ packages/nlpflow/nlpflow-0.0.3.tar.gz/nlpflow-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nlpflow",
+    version="0.0.3",
+    author="Konstantinos Giannopoulos",
+    author_email="giannopoulosk.data@gmail.com",  
+    description="A powerful toolkit for automating NLP workflows.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/GiannopoulosK/nlpflow",
+    packages=find_packages(),
+     install_requires=[
+        'numpy>=1.23.0 ',             
+        'spacy>=3.0.0',
+        'scikit-learn>=1.0.0',
+        'optuna>=4.0.0',
+    ],
+    license="Apache License 2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha"
+    ],
+    python_requires='>=3.0',
+)
