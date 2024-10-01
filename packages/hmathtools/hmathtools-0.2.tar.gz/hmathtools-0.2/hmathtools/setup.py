8116214f990a0b8416e11fd885f1c7d5901adf3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 05:03:57 +0000

    Add package hmathtools/0.2/hmathtools-0.2.tar.gz

---
+++ packages/hmathtools/hmathtools-0.2.tar.gz/hmathtools-0.2/hmathtools/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='hmathtools',
+    version='0.1',
+    packages=['hmathtools'],
+    url='https://github.com/veczero/example_package',
+    author='Harman Singh',
+    author_email='the.programming.hacker.hk+1@email.com',
+    description='This is an example package by HS.'
+)
---
+++ packages/hmathtools/hmathtools-0.2.tar.gz/hmathtools-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='hmathtools',
+    version='0.2',
+    packages=['hmathtools'],
+    url='https://github.com/veczero/example_package',
+    author='Harman Singh',
+    author_email='the.programming.hacker.hk+1@email.com',
+    description='This is an example package by HS.',
+    
+    install_requires=[
+        'numpy>=1.19.5',
+        #'scikit-learn>=1.0.2'
+    ]
+)
