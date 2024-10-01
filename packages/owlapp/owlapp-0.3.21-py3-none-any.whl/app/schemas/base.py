e3d3aaa0f4019835fb9bc124451100b02337bac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 03:14:43 +0000

    Add package owlapp/0.3.21/owlapp-0.3.21-py3-none-any.whl

---
+++ packages/owlapp/owlapp-0.3.21-py3-none-any.whl/app/schemas/base.py
@@ -0,0 +1,37 @@
+from dataclasses import field
+from typing import Any, Optional
+
+from apiflask.validators import Email
+from marshmallow_dataclass import dataclass
+from pydantic import BaseModel
+
+
+@dataclass
+class ExistsOut:
+    exists: bool = field()
+
+
+@dataclass
+class MessageOut:
+    message: str = field()
+
+
+@dataclass
+class EmailIn:
+    email: str = field(metadata={"validate": Email()})
+
+
+@dataclass
+class ContentOut:
+    content: str = field()
+
+
+@dataclass
+class RenderContentIn:
+    content: str = field()
+    command: Optional[str] = field()
+
+
+@dataclass
+class RenderContentOut:
+    content: str = field()
