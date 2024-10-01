7aa35f3214d1d39eb26e4ad2898b7ee7d704a8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 05:05:02 +0000

    Add package mpesaflow/1.0.0/mpesaflow-1.0.0-py3-none-any.whl

---
+++ packages/mpesaflow/mpesaflow-1.0.0-py3-none-any.whl/mpesaflow/resources/payments.py
@@ -0,0 +1,204 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+import httpx
+
+from ..types import payment_create_params
+from .._types import NOT_GIVEN, Body, Query, Headers, NotGiven
+from .._utils import (
+    maybe_transform,
+    async_maybe_transform,
+)
+from .._compat import cached_property
+from .._resource import SyncAPIResource, AsyncAPIResource
+from .._response import (
+    to_raw_response_wrapper,
+    to_streamed_response_wrapper,
+    async_to_raw_response_wrapper,
+    async_to_streamed_response_wrapper,
+)
+from .._base_client import make_request_options
+from ..types.payment import Payment
+
+__all__ = ["PaymentsResource", "AsyncPaymentsResource"]
+
+
+class PaymentsResource(SyncAPIResource):
+    @cached_property
+    def with_raw_response(self) -> PaymentsResourceWithRawResponse:
+        """
+        This property can be used as a prefix for any HTTP method call to return the
+        the raw response object instead of the parsed content.
+
+        For more information, see https://www.github.com/MpesaFlow/mpesaflow-python#accessing-raw-response-data-eg-headers
+        """
+        return PaymentsResourceWithRawResponse(self)
+
+    @cached_property
+    def with_streaming_response(self) -> PaymentsResourceWithStreamingResponse:
+        """
+        An alternative to `.with_raw_response` that doesn't eagerly read the response body.
+
+        For more information, see https://www.github.com/MpesaFlow/mpesaflow-python#with_streaming_response
+        """
+        return PaymentsResourceWithStreamingResponse(self)
+
+    def create(
+        self,
+        *,
+        account_reference: str,
+        amount: float,
+        phone_number: str,
+        transaction_desc: str,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> Payment:
+        """
+        Initiate a payment
+
+        Args:
+          account_reference: A reference for the transaction
+
+          amount: The amount to be paid
+
+          phone_number: The phone number of the payer
+
+          transaction_desc: A description of the transaction
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._post(
+            "/paybill",
+            body=maybe_transform(
+                {
+                    "account_reference": account_reference,
+                    "amount": amount,
+                    "phone_number": phone_number,
+                    "transaction_desc": transaction_desc,
+                },
+                payment_create_params.PaymentCreateParams,
+            ),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=Payment,
+        )
+
+
+class AsyncPaymentsResource(AsyncAPIResource):
+    @cached_property
+    def with_raw_response(self) -> AsyncPaymentsResourceWithRawResponse:
+        """
+        This property can be used as a prefix for any HTTP method call to return the
+        the raw response object instead of the parsed content.
+
+        For more information, see https://www.github.com/MpesaFlow/mpesaflow-python#accessing-raw-response-data-eg-headers
+        """
+        return AsyncPaymentsResourceWithRawResponse(self)
+
+    @cached_property
+    def with_streaming_response(self) -> AsyncPaymentsResourceWithStreamingResponse:
+        """
+        An alternative to `.with_raw_response` that doesn't eagerly read the response body.
+
+        For more information, see https://www.github.com/MpesaFlow/mpesaflow-python#with_streaming_response
+        """
+        return AsyncPaymentsResourceWithStreamingResponse(self)
+
+    async def create(
+        self,
+        *,
+        account_reference: str,
+        amount: float,
+        phone_number: str,
+        transaction_desc: str,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> Payment:
+        """
+        Initiate a payment
+
+        Args:
+          account_reference: A reference for the transaction
+
+          amount: The amount to be paid
+
+          phone_number: The phone number of the payer
+
+          transaction_desc: A description of the transaction
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return await self._post(
+            "/paybill",
+            body=await async_maybe_transform(
+                {
+                    "account_reference": account_reference,
+                    "amount": amount,
+                    "phone_number": phone_number,
+                    "transaction_desc": transaction_desc,
+                },
+                payment_create_params.PaymentCreateParams,
+            ),
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=Payment,
+        )
+
+
+class PaymentsResourceWithRawResponse:
+    def __init__(self, payments: PaymentsResource) -> None:
+        self._payments = payments
+
+        self.create = to_raw_response_wrapper(
+            payments.create,
+        )
+
+
+class AsyncPaymentsResourceWithRawResponse:
+    def __init__(self, payments: AsyncPaymentsResource) -> None:
+        self._payments = payments
+
+        self.create = async_to_raw_response_wrapper(
+            payments.create,
+        )
+
+
+class PaymentsResourceWithStreamingResponse:
+    def __init__(self, payments: PaymentsResource) -> None:
+        self._payments = payments
+
+        self.create = to_streamed_response_wrapper(
+            payments.create,
+        )
+
+
+class AsyncPaymentsResourceWithStreamingResponse:
+    def __init__(self, payments: AsyncPaymentsResource) -> None:
+        self._payments = payments
+
+        self.create = async_to_streamed_response_wrapper(
+            payments.create,
+        )
---
+++ packages/mpesaflow/mpesaflow-1.0.0-py3-none-any.whl/mpesaflow/types/__init__.py
@@ -0,0 +1,7 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .payment import Payment as Payment
+from .transaction_status import TransactionStatus as TransactionStatus
+from .payment_create_params import PaymentCreateParams as PaymentCreateParams
