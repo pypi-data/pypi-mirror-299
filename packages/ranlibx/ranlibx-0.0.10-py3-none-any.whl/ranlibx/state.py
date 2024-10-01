e502bfa9de6abe722f2965a769e6ce0168b0774d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 05:06:02 +0000

    Add package ranlibx/0.0.10/ranlibx-0.0.10-py3-none-any.whl

---
+++ packages/ranlibx/ranlibx-0.0.10-py3-none-any.whl/ranlibx/state.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+from ranlibx import server
+
+
+class AuthFlowState(Enum):
+    INACTIVE = None
+    IN_PROGRESS = 0
+    SUCCESS = 1
+    FAILURE = -1
+
+
+AUTH_FLOW_STATE: AuthFlowState = AuthFlowState.INACTIVE
+
+
+def set_auth_flow_state(st: AuthFlowState):
+    global AUTH_FLOW_STATE
+
+    AUTH_FLOW_STATE = st
+
+
+def kill_server(verbose: bool = False):
+    """Closes the RAN Authentication Server"""
+    global AUTH_FLOW_STATE
+
+    AUTH_FLOW_STATE = AuthFlowState.INACTIVE
+
+    # KILL that uvicorn server in COLD BLOOD
+    server.active_uvicorn_server_process.end(verbose=verbose)
+
+    if verbose:
+        print("Server murder complete.")
