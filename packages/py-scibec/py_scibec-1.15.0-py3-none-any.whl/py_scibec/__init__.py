d274dcfdc1b39233d2e1f00187f158478356b62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:05:40 +0000

    Add package py-scibec/1.15.0/py_scibec-1.15.0-py3-none-any.whl

---
+++ packages/py-scibec/py_scibec-1.15.0-py3-none-any.whl/py_scibec/__init__.py
@@ -0,0 +1,2 @@
+from .bec import SciBecCore
+import py_scibec_openapi_client as openapi_client
---
+++ packages/py-scibec/py_scibec-1.15.0-py3-none-any.whl/py_scibec/exceptions.py
@@ -0,0 +1,5 @@
+from py_scibec_openapi_client.exceptions import ApiException
+
+
+class SciBecError(Exception):
+    pass
