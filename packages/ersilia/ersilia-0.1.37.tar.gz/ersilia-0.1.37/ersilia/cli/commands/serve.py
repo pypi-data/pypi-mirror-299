5b7e6ae4e94e67f20114d68cfd4c36fecd4bd2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 14:03:44 +0000

    Add package ersilia/0.1.37/ersilia-0.1.37.tar.gz

---
+++ packages/ersilia/ersilia-0.1.37.tar.gz/ersilia-0.1.37/ersilia/cli/commands/serve.py
@@ -0,0 +1,71 @@
+import click
+
+from .. import echo
+from . import ersilia_cli
+from ... import ErsiliaModel
+from ..messages import ModelNotFound
+
+
+def serve_cmd():
+    """Creates serve command"""
+
+    # Example usage: ersilia serve {MODEL}
+    @ersilia_cli.command(short_help="Serve model", help="Serve model")
+    @click.argument("model", type=click.STRING)
+    @click.option("--lake/--no-lake", is_flag=True, default=True)
+    @click.option("--docker/--no-docker", is_flag=True, default=False)
+    @click.option(
+        "--port",
+        "-p",
+        default=None,
+        type=click.INT,
+        help="Preferred port to use (integer)",
+    )
+    # Add the new flag for tracking the serve session
+    @click.option(
+        "-t",
+        "--track",
+        "track",
+        is_flag=True,
+        required=False,
+        default=False,
+    )
+    def serve(model, lake, docker, port, track):
+        if docker:
+            service_class = "docker"
+        else:
+            service_class = None
+        mdl = ErsiliaModel(
+            model,
+            save_to_lake=lake,
+            service_class=service_class,
+            preferred_port=port,
+            track_runs=track,
+        )
+        if not mdl.is_valid():
+            ModelNotFound(mdl).echo()
+
+        mdl.serve()
+        if mdl.url is None:
+            echo("No URL found. Service unsuccessful.", fg="red")
+            return
+        echo(
+            ":rocket: Serving model {0}: {1}".format(mdl.model_id, mdl.slug), fg="green"
+        )
+        echo("")
+        echo("   URL: {0}".format(mdl.url), fg="yellow")
+        echo("   PID: {0}".format(mdl.pid), fg="yellow")
+        echo("   SRV: {0}".format(mdl.scl), fg="yellow")
+        echo("")
+        echo(":backhand_index_pointing_right: To run model:", fg="blue")
+        echo("   - run", fg="blue")
+        apis = mdl.get_apis()
+        if apis != ["run"]:
+            echo("")
+            echo("   These APIs are also valid:", fg="blue")
+            for api in apis:
+                if api != "run":
+                    echo("   - {0}".format(api), fg="blue")
+        echo("")
+        echo(":person_tipping_hand: Information:", fg="blue")
+        echo("   - info", fg="blue")
