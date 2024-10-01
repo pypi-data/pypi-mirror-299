9bfc3f95975d0e1cd965f6dfdac692c40945b712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 04:04:15 +0000

    Add package pyVIA/0.2.2/pyvia-0.2.2.tar.gz

---
+++ packages/pyVIA/pyvia-0.2.2.tar.gz/pyvia-0.2.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+import sys
+from setuptools import setup
+from warnings import warn
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pyVIA',
+    version='0.2.02', ##May 27, 2024
+    packages=['pyVIA',],
+    license='MIT',
+    author_email = 'shobana.venkat88@gmail.com',
+    url = 'https://github.com/ShobiStassen/VIA',
+    setup_requires = ['numpy>=1.17','pybind11'],
+    python_requires = ">=3.8",
+    install_requires=['pybind11','numpy>=1.17','scipy','pandas>=0.25','hnswlib','igraph','leidenalg>=0.7.0', 'scikit-learn', 'termcolor','pygam', 'matplotlib','scanpy','umap-learn>=0.5.0','phate','datashader', 'scikit-image', 'pillow','wget','gdown','seaborn','pecanpy','holoviews'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
