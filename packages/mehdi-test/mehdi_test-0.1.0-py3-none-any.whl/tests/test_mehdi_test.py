5177b0f4491dd72a608fc2232448aec1e36cbb89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 16:04:02 +0000

    Add package mehdi-test/0.1.0/mehdi_test-0.1.0-py3-none-any.whl

---
+++ packages/mehdi-test/mehdi_test-0.1.0-py3-none-any.whl/tests/test_mehdi_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `mehdi_test` package."""
+
+
+import unittest
+
+from mehdi_test import mehdi_test
+
+
+class TestMehdi_test(unittest.TestCase):
+    """Tests for `mehdi_test` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
