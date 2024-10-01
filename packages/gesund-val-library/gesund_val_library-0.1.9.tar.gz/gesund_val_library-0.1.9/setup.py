028aaf196cdb10237efc4278b7492bf03b127b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 04:03:58 +0000

    Add package gesund-val-library/0.1.9/gesund_val_library-0.1.9.tar.gz

---
+++ packages/gesund-val-library/gesund_val_library-0.1.9.tar.gz/gesund_val_library-0.1.9/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gesund_val_library',
+    version='0.1.9',
+    author='Gesund AI',
+    author_email='hammadbink@gmail.com',
+    license="MIT",
+    description='Gesund.ai package for running validation metrics for classification, semantic segmentation, instance segmentation, and object detection models.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/gesund-ai/gesund_val_library',
+    packages=find_packages(),
+    install_requires=[
+        'bson',
+        'jsonschema',
+        'scikit-learn',
+        'pandas',
+        'pydicom',
+        'nibabel',
+        'opencv-python',
+        'SimpleITK',
+        'pycocotools',
+        'dictances==1.5.3',
+        'miseval==1.2.2',
+        'numpy==1.21.0',
+        'numba==0.58.1',
+        'tqdm'
+    ],
+    dependency_links=[
+        'git+https://github.com/HammadK44/cocoapi.git@Dev#egg=pycocotools&subdirectory=PythonAPI'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'run_metrics=gesund_val_library.scripts.run_metrics:main',
+        ],
+    },
+)
