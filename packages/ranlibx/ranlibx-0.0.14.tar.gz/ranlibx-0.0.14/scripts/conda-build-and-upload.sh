1db09ecf601505ef702f4c1698af8e3e5ebbca8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 05:04:28 +0000

    Add package ranlibx/0.0.14/ranlibx-0.0.14.tar.gz

---
+++ packages/ranlibx/ranlibx-0.0.14.tar.gz/ranlibx-0.0.14/scripts/conda-build-and-upload.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Install rattler-build, grayskull, and conda-recipe-manager
+pixi global install rattler-build grayskull conda-recipe-manager
+
+# Generate a meta.yaml
+mkdir temp && cd temp
+
+grayskull pypi ranlibx
+
+# Convert meta.yaml to a recipe.yaml
+conda-recipe-manager convert ranlibx/meta.yaml > ./recipe.yaml
+
+# Build it for conda with rattler-build
+rattler-build build --recipe ./recipe.yaml
+
+# Move the output folder outside
+mv ./output ../output
+
+# go back and remove the directory
+cd ..
+rm -rf temp
+
+# Get the .conda file to upload
+build=$(find ./output/noarch -maxdepth 1 -type f -name "*.conda" | head -n 1)
+
+# Upload to prefix.dev
+pixi run -e dev python3 scripts/upload-prefixdev.py "$build" "$PREFIX_DEV_TOKEN"
