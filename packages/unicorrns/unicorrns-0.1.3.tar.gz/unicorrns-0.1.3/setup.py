2066922795ec2f5d688476235e0d61f5b31cb4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 06:03:58 +0000

    Add package unicorrns/0.1.3/unicorrns-0.1.3.tar.gz

---
+++ packages/unicorrns/unicorrns-0.1.3.tar.gz/unicorrns-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='unicorrns',
+      version='0.1.3',
+      description='UNIversal Correlations Neutron Stars package',
+      url='',
+      author='Peter Shternin',
+      author_email='pshternin@gmail.com',
+      license='MIT',
+      packages=['unicorrns'],
+      zip_safe=False,
+      include_package_data=True)
