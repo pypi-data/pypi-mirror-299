bed099781481858ff209dbcbebb953e392c425b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:06:13 +0000

    Add package mdslicer/0.1.5/mdslicer-0.1.5.tar.gz

---
+++ packages/mdslicer/mdslicer-0.1.5.tar.gz/mdslicer-0.1.5/tests/test_mdslicer.py
@@ -0,0 +1,16 @@
+import pytest
+from mdslicer import mdslicer
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    pass
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    pass
