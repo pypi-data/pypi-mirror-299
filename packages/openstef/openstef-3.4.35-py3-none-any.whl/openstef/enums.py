f7d571cbd7662e7ce6ece63d85ef58f6a25f81eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 03:15:04 +0000

    Add package openstef/3.4.35/openstef-3.4.35-py3-none-any.whl

---
+++ packages/openstef/openstef-3.4.35-py3-none-any.whl/openstef/enums.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2017-2023 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+from enum import Enum
+
+
+# TODO replace this with ModelType (MLModelType == Machine Learning model type)
+class MLModelType(Enum):
+    XGB = "xgb"
+    XGB_QUANTILE = "xgb_quantile"
+    XGB_MULTIOUTPUT_QUANTILE = "xgb_multioutput_quantile"
+    LGB = "lgb"
+    LINEAR = "linear"
+    LINEAR_QUANTILE = "linear_quantile"
+    ARIMA = "arima"
+    FLATLINER = "flatliner"
+
+
+class ForecastType(Enum):
+    DEMAND = "demand"
+    WIND = "wind"
+    SOLAR = "solar"
+    BASECASE = "basecase"
+
+
+class PipelineType(Enum):
+    FORECAST = "forecast"
+    TRAIN = "train"
+    HYPER_PARMATERS = "hyper_parameters"
