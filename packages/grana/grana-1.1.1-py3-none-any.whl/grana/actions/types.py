f1782e854ebb239ad6a96f98da51c3346e56134d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 04:03:57 +0000

    Add package grana/1.1.1/grana-1.1.1-py3-none-any.whl

---
+++ packages/grana/grana-1.1.1-py3-none-any.whl/grana/actions/types.py
@@ -0,0 +1,37 @@
+"""Types collection"""
+
+import dataclasses
+import typing as t
+
+OutcomeStorageType = t.Dict[str, str]
+
+__all__ = [
+    "OutcomeStorageType",
+    "Stderr",
+    "Import",
+    "ObjectTemplate",
+    "qualify_string_as_potentially_renderable",
+]
+
+
+class Stderr(str):
+    """Strings related to standard error stream"""
+
+
+@dataclasses.dataclass
+class Import:
+    """Import clause"""
+
+    path: str
+
+
+@dataclasses.dataclass
+class ObjectTemplate:
+    """Complex object expression to be rendered later"""
+
+    expression: str
+
+
+def qualify_string_as_potentially_renderable(data: str) -> bool:
+    """Check that a string should be templated later"""
+    return "@{" in data
