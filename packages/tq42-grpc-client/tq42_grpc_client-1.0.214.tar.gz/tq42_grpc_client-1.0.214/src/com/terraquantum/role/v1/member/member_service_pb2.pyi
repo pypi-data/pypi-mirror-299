52fb6427818e236f5ad3de06fe71cbaaf72a7cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 03:14:09 +0000

    Add package tq42-grpc-client/1.0.214/tq42_grpc_client-1.0.214.tar.gz

---
+++ packages/tq42-grpc-client/tq42_grpc_client-1.0.214.tar.gz/tq42_grpc_client-1.0.214/src/com/terraquantum/role/v1/member/member_service_pb2.pyi
@@ -0,0 +1,10 @@
+from com.terraquantum.role.v1.member import list_editable_members_pb2 as _list_editable_members_pb2
+from com.terraquantum.role.v1.member import list_project_groups_request_pb2 as _list_project_groups_request_pb2
+from com.terraquantum.role.v1.member import list_project_members_pb2 as _list_project_members_pb2
+from com.terraquantum.role.v1.member import list_user_groups_request_pb2 as _list_user_groups_request_pb2
+from com.terraquantum.role.v1.member import member_pb2 as _member_pb2
+from com.terraquantum.role.v1.member import object_ids_response_pb2 as _object_ids_response_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
