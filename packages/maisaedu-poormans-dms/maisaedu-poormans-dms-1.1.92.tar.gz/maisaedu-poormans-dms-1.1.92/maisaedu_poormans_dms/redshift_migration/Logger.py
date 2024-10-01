c4532011e11153ef7fcd7f4826ce3ae8ce626126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 07:05:11 +0000

    Add package maisaedu-poormans-dms/1.1.92/maisaedu-poormans-dms-1.1.92.tar.gz

---
+++ packages/maisaedu-poormans-dms/maisaedu-poormans-dms-1.1.92.tar.gz/maisaedu-poormans-dms-1.1.92/maisaedu_poormans_dms/redshift_migration/Logger.py
@@ -0,0 +1,16 @@
+from .Types import LOGGER_PREFECT
+
+
+class Logger:
+    def __init__(self, logger=None):
+        if logger is not None:
+            if logger["type"] == LOGGER_PREFECT:
+                self.logger = logger["instance"]
+        else:
+            self.logger = None
+
+    def info(self, message):
+        if self.logger is not None:
+            self.logger.info(message)
+        else:
+            print(message)
