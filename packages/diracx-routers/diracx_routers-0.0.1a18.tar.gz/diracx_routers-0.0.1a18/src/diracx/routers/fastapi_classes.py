6dbb230d20f7d23f7c26170835d9334654d33fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:05:47 +0000

    Add package diracx-routers/0.0.1a18/diracx_routers-0.0.1a18.tar.gz

---
+++ packages/diracx-routers/diracx_routers-0.0.1a18.tar.gz/diracx_routers-0.0.1a18/src/diracx/routers/fastapi_classes.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+import asyncio
+import contextlib
+from typing import TypeVar
+
+from fastapi import APIRouter, FastAPI
+
+T = TypeVar("T")
+
+
+def _downgrade_openapi_schema(data):
+    """Modify an openapi schema in-place to be compatible with AutoRest."""
+    if isinstance(data, dict):
+        for k, v in list(data.items()):
+            if k == "anyOf":
+                if {"type": "null"} in v:
+                    v.pop(v.index({"type": "null"}))
+                    data["nullable"] = True
+                    if len(v) == 1:
+                        data |= v[0]
+            elif k == "const":
+                data.pop(k)
+            _downgrade_openapi_schema(v)
+    if isinstance(data, list):
+        for v in data:
+            _downgrade_openapi_schema(v)
+
+
+class DiracFastAPI(FastAPI):
+    def __init__(self):
+        @contextlib.asynccontextmanager
+        async def lifespan(app: DiracFastAPI):
+            async with contextlib.AsyncExitStack() as stack:
+                await asyncio.gather(
+                    *(stack.enter_async_context(f()) for f in app.lifetime_functions)
+                )
+                yield
+
+        self.lifetime_functions = []
+        super().__init__(
+            swagger_ui_init_oauth={
+                "clientId": "myDIRACClientID",
+                "scopes": "property:NormalUser",
+                "usePkceWithAuthorizationCodeGrant": True,
+            },
+            generate_unique_id_function=lambda route: f"{route.tags[0]}_{route.name}",
+            title="Dirac",
+            lifespan=lifespan,
+            openapi_url="/api/openapi.json",
+            docs_url="/api/docs",
+            swagger_ui_oauth2_redirect_url="/api/docs/oauth2-redirect",
+        )
+        # FIXME: when autorest will support 3.1.0
+        # From 0.99.0, FastAPI is using openapi 3.1.0 by default
+        # This version is not supported by autorest yet
+        self.openapi_version = "3.0.2"
+
+    def openapi(self, *args, **kwargs):
+        if not self.openapi_schema:
+            super().openapi(*args, **kwargs)
+            _downgrade_openapi_schema(self.openapi_schema)
+            # Remove 422 responses as we don't want autorest to use it
+            for _, method_item in self.openapi_schema.get("paths").items():
+                for _, param in method_item.items():
+                    responses = param.get("responses")
+                    if "422" in responses:
+                        del responses["422"]
+
+        return self.openapi_schema
+
+
+class DiracxRouter(APIRouter):
+    def __init__(
+        self,
+        *,
+        dependencies=None,
+        require_auth: bool = True,
+        path_root: str = "/api",
+    ):
+        super().__init__(dependencies=dependencies)
+        self.diracx_require_auth = require_auth
+        self.diracx_path_root = path_root
