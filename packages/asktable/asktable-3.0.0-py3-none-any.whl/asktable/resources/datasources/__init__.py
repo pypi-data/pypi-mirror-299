37e867d706744056b1b7e44eee2c25f426674dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 03:14:28 +0000

    Add package asktable/3.0.0/asktable-3.0.0-py3-none-any.whl

---
+++ packages/asktable/asktable-3.0.0-py3-none-any.whl/asktable/resources/datasources/__init__.py
@@ -0,0 +1,75 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .meta import (
+    MetaResource,
+    AsyncMetaResource,
+    MetaResourceWithRawResponse,
+    AsyncMetaResourceWithRawResponse,
+    MetaResourceWithStreamingResponse,
+    AsyncMetaResourceWithStreamingResponse,
+)
+from .datasources import (
+    DatasourcesResource,
+    AsyncDatasourcesResource,
+    DatasourcesResourceWithRawResponse,
+    AsyncDatasourcesResourceWithRawResponse,
+    DatasourcesResourceWithStreamingResponse,
+    AsyncDatasourcesResourceWithStreamingResponse,
+)
+from .upload_file import (
+    UploadFileResource,
+    AsyncUploadFileResource,
+    UploadFileResourceWithRawResponse,
+    AsyncUploadFileResourceWithRawResponse,
+    UploadFileResourceWithStreamingResponse,
+    AsyncUploadFileResourceWithStreamingResponse,
+)
+from .download_url import (
+    DownloadURLResource,
+    AsyncDownloadURLResource,
+    DownloadURLResourceWithRawResponse,
+    AsyncDownloadURLResourceWithRawResponse,
+    DownloadURLResourceWithStreamingResponse,
+    AsyncDownloadURLResourceWithStreamingResponse,
+)
+from .upload_params import (
+    UploadParamsResource,
+    AsyncUploadParamsResource,
+    UploadParamsResourceWithRawResponse,
+    AsyncUploadParamsResourceWithRawResponse,
+    UploadParamsResourceWithStreamingResponse,
+    AsyncUploadParamsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "MetaResource",
+    "AsyncMetaResource",
+    "MetaResourceWithRawResponse",
+    "AsyncMetaResourceWithRawResponse",
+    "MetaResourceWithStreamingResponse",
+    "AsyncMetaResourceWithStreamingResponse",
+    "UploadParamsResource",
+    "AsyncUploadParamsResource",
+    "UploadParamsResourceWithRawResponse",
+    "AsyncUploadParamsResourceWithRawResponse",
+    "UploadParamsResourceWithStreamingResponse",
+    "AsyncUploadParamsResourceWithStreamingResponse",
+    "UploadFileResource",
+    "AsyncUploadFileResource",
+    "UploadFileResourceWithRawResponse",
+    "AsyncUploadFileResourceWithRawResponse",
+    "UploadFileResourceWithStreamingResponse",
+    "AsyncUploadFileResourceWithStreamingResponse",
+    "DownloadURLResource",
+    "AsyncDownloadURLResource",
+    "DownloadURLResourceWithRawResponse",
+    "AsyncDownloadURLResourceWithRawResponse",
+    "DownloadURLResourceWithStreamingResponse",
+    "AsyncDownloadURLResourceWithStreamingResponse",
+    "DatasourcesResource",
+    "AsyncDatasourcesResource",
+    "DatasourcesResourceWithRawResponse",
+    "AsyncDatasourcesResourceWithRawResponse",
+    "DatasourcesResourceWithStreamingResponse",
+    "AsyncDatasourcesResourceWithStreamingResponse",
+]
---
+++ packages/asktable/asktable-3.0.0-py3-none-any.whl/asktable/types/datasources/__init__.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .meta import Meta as Meta
+from .meta_update_params import MetaUpdateParams as MetaUpdateParams
+from .meta_retrieve_params import MetaRetrieveParams as MetaRetrieveParams
+from .upload_file_create_params import UploadFileCreateParams as UploadFileCreateParams
+from .upload_param_create_params import UploadParamCreateParams as UploadParamCreateParams
