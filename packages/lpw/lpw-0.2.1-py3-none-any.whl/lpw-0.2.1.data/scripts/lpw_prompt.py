b7568a9987c585723410e5fad5a9f967b04d3b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 14:03:46 +0000

    Add package lpw/0.2.1/lpw-0.2.1-py3-none-any.whl

---
+++ packages/lpw/lpw-0.2.1-py3-none-any.whl/lpw-0.2.1.data/scripts/lpw_prompt.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from lpw_ollamaClient import OllamaClient
+from typing import List
+
+def returnSystemText(pcap_data : str) -> str:
+    PACKET_WHISPERER = f"""
+        {st.session_state['system_message']}
+        packet_capture_info : {pcap_data}
+    """
+    return PACKET_WHISPERER
+
+oClient = OllamaClient()
+
+def setLLMServer(server):
+    oClient.setServer(server)
+
+def initLLM(pcap_data) -> None:
+    oClient.set_system_message(system_message=returnSystemText(pcap_data)) 
+
+def getModelList() -> List[str]:
+    return oClient.getModelList()
+
+def chatWithModel(prompt:str, model: str):
+    return oClient.chat(prompt=prompt, model=model, temp=0.4)
+
+def clearHistory():
+    oClient.clear_history()
+
+def modifySM(new_sm: str) -> None:
+    oClient.edit_system_message(new_sm)
