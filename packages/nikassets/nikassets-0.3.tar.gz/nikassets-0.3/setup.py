c6ac113085053a5ea7d79a895d957634685297ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 07:05:17 +0000

    Add package nikassets/0.3/nikassets-0.3.tar.gz

---
+++ packages/nikassets/nikassets-0.3.tar.gz/nikassets-0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nikassets",
+version="0.3",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['*.sh', '*.yml'],
+        'nikassets.helper': ['assets/*'],
+        'nikassets.helper.assets': ['*'],
+    },
+    author="Nikhil Menghani",
+    author_email="nikhil@menghani.com",
+    description="A short description of your project",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/nikhilmenghani/nikassets",
+    install_requires=[
+        'setuptools>=72.1.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'nikassets=main:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.12',
+)
