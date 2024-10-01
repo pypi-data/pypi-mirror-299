e014b0eb35185dbe70a967fce72c4f835a7982a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 07:04:35 +0000

    Add package cashews/7.3.2/cashews-7.3.2.tar.gz

---
+++ packages/cashews/cashews-7.3.2.tar.gz/cashews-7.3.2/cashews/helpers.py
@@ -0,0 +1,62 @@
+from typing import Optional
+
+from ._typing import AsyncCallable_T, Middleware, Result_T
+from .backends.interface import Backend
+from .commands import PATTERN_CMDS, Command
+from .key import get_call_values
+from .utils import get_obj_size
+
+
+def add_prefix(prefix: str) -> Middleware:
+    async def _middleware(call: AsyncCallable_T, cmd: Command, backend: Backend, *args, **kwargs) -> Result_T:
+        if cmd in (Command.GET_MANY, Command.DELETE_MANY):
+            return await call(*[prefix + key for key in args])
+        call_values = get_call_values(call, args, kwargs)
+        if cmd == Command.SET_MANY:
+            call_values["pairs"] = {prefix + key: value for key, value in call_values["pairs"].items()}
+            return await call(**call_values)
+
+        as_key = "pattern" if cmd in PATTERN_CMDS else "key"
+        key = call_values.get(as_key)
+        if key:
+            call_values[as_key] = prefix + key
+            return await call(**call_values)
+        return await call(*args, **kwargs)
+
+    return _middleware
+
+
+def all_keys_lower() -> Middleware:
+    async def _middleware(call: AsyncCallable_T, cmd: Command, backend: Backend, *args, **kwargs) -> Result_T:
+        if cmd in (Command.GET_MANY, Command.DELETE_MANY):
+            return await call(*[key.lower() for key in args])
+        call_values = get_call_values(call, args, kwargs)
+
+        if cmd == Command.SET_MANY:
+            call_values["pairs"] = {key.lower(): value for key, value in call_values["pairs"].items()}
+            return await call(**call_values)
+
+        as_key = "pattern" if cmd in PATTERN_CMDS else "key"
+
+        key = call_values.get(as_key)
+        if key:
+            call_values[as_key] = key.lower()
+            return await call(**call_values)
+        return await call(*args, **kwargs)
+
+    return _middleware
+
+
+def memory_limit(min_bytes=0, max_bytes=None) -> Middleware:
+    async def _middleware(
+        call: AsyncCallable_T, cmd: Command, backend: Backend, *args, **kwargs
+    ) -> Optional[Result_T]:
+        if cmd != Command.SET:
+            return await call(*args, **kwargs)
+        call_values = get_call_values(call, args, kwargs)
+        value_size = get_obj_size(call_values["value"])
+        if max_bytes and value_size > max_bytes or value_size < min_bytes:
+            return None
+        return await call(*args, **kwargs)
+
+    return _middleware
