1b31d87c01878d57fdaed6907e18b99fab6c6135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 05:05:02 +0000

    Add package aiovaksms/1.1.2/aiovaksms-1.1.2.tar.gz

---
+++ packages/aiovaksms/aiovaksms-1.1.2.tar.gz/aiovaksms-1.1.2/main.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+setup(
+    name = 'aiovaksms',
+    version='1.1.2',
+    packages=find_packages(),
+    long_description=description
+)
+
+# import asyncio
+#
+# from aiovaksms import VakSms
+
+
+# async def main():
+#     client = VakSms('cfb40c75fbe44c0f836af0f0a14175f0')
+#
+#     data = await client.get_count_number_list('RU', 'mts', True)
+#
+#     # data = await client.get_count_number_list()
+#     print(data)  # Prints payment url for customer
+#     print(len(data.keys()))
+#
+#
+# asyncio.run(main())
+
+# build command: python .\main.py sdist bdist_wheel
+# publish command: twine upload dist/*
