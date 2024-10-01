3a989632aa8e474a975c5ec3f30be223e5ddd7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 05:04:41 +0000

    Add package ord-mediascout-client/0.0.35/ord_mediascout_client-0.0.35-py3-none-any.whl

---
+++ packages/ord-mediascout-client/ord_mediascout_client-0.0.35-py3-none-any.whl/ord_mediascout_client/config.py
@@ -0,0 +1,15 @@
+import dotenv
+from pydantic.env_settings import BaseSettings
+from pydantic.fields import Field
+
+dotenv.load_dotenv()
+
+
+class ORDMediascoutConfig(BaseSettings):
+    class Config:
+        case_sensitive = True
+
+    url: str = Field(..., env='URL')
+    username: str = Field(..., env='MEDIASCOUT_USERNAME')
+    password: str = Field(..., env='MEDIASCOUT_PASSWORD')
+    api_url_prefix: str = Field(..., env='API_URL_PREFIX')
