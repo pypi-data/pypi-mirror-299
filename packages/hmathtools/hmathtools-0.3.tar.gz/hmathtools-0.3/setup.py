a48790a1859b02f62ef3e922c6d656d6ee198412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 14:04:16 +0000

    Add package hmathtools/0.3/hmathtools-0.3.tar.gz

---
+++ packages/hmathtools/hmathtools-0.3.tar.gz/hmathtools-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='hmathtools',
+    version='0.3',
+    packages=['hmathtools'],
+    url='https://github.com/veczero/example_package',
+    author='Harman Singh',
+    author_email='the.programming.hacker.hk+1@email.com',
+    description='This is an example package by HS.',
+    
+    install_requires=[
+        'numpy>=2.1.0',
+        #'scikit-learn>=1.0.2'
+    ]
+)
