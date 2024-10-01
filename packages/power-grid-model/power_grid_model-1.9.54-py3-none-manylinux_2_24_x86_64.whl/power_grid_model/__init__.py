ee8f2020a7662153946fef67aa13d87e705956ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 03:17:10 +0000

    Add package power-grid-model/1.9.54/power_grid_model-1.9.54-py3-none-manylinux_2_24_x86_64.whl

---
+++ packages/power-grid-model/power_grid_model-1.9.54-py3-none-manylinux_2_24_x86_64.whl/power_grid_model/__init__.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+"""Power Grid Model"""
+
+from power_grid_model.core.dataset_definitions import ComponentType, DatasetType
+from power_grid_model.core.power_grid_meta import initialize_array, power_grid_meta_data
+from power_grid_model.core.power_grid_model import PowerGridModel
+from power_grid_model.enum import (
+    Branch3Side,
+    BranchSide,
+    CalculationMethod,
+    CalculationType,
+    FaultPhase,
+    FaultType,
+    LoadGenType,
+    MeasuredTerminalType,
+    ShortCircuitVoltageScaling,
+    TapChangingStrategy,
+    WindingType,
+)
