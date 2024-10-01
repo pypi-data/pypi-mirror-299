7b64682a57f760284630b905d5d393234374173b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:05:15 +0000

    Add package rea-copilot-tgw-attachment/0.0.1/rea_copilot_tgw_attachment-0.0.1.tar.gz

---
+++ packages/rea-copilot-tgw-attachment/rea_copilot_tgw_attachment-0.0.1.tar.gz/rea_copilot_tgw_attachment-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rea-copilot-tgw-attachment',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.rea-group.com/category/tech/',
+    author='rea',
+    author_email='pip@rea-group.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
