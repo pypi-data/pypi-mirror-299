8e0ad46f5789e5f22c72682615cfaddde6908afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 07:04:08 +0000

    Add package blue-geo/4.551.1/blue_geo-4.551.1.tar.gz

---
+++ packages/blue-geo/blue_geo-4.551.1.tar.gz/blue_geo-4.551.1/blue_geo/watch/workflow/map.py
@@ -0,0 +1,39 @@
+from blueness import module
+from blue_objects import file
+from blue_objects.metadata import post_to_object
+
+from blue_geo import NAME
+from blue_geo.watch.workflow.common import load_watch
+from blue_geo.logger import logger
+
+
+NAME = module.name(__file__, NAME)
+
+
+def map_function(
+    datacube_id: str,
+    object_name: str,
+) -> bool:
+    success, target, list_of_files = load_watch(object_name)
+    if not success or not list_of_files:
+        return success
+    filename = list_of_files[0]
+
+    logger.info(
+        "{}.map: {} @ {} -> {}".format(
+            NAME,
+            target,
+            datacube_id,
+            object_name,
+        )
+    )
+
+    return post_to_object(
+        object_name,
+        "map",
+        {
+            "datacube_id": datacube_id,
+            "filename": file.name_and_extension(filename),
+            "target": target.__dict__,
+        },
+    )
