756bca6bc31aa8b5876162ba5f16f96cce6b907a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:05:56 +0000

    Add package hikyuu/2.2.1/hikyuu-2.2.1-cp38-none-win_amd64.whl

---
+++ packages/hikyuu/hikyuu-2.2.1-cp38-none-win_amd64.whl/hikyuu/include/hikyuu/version.h
@@ -0,0 +1,24 @@
+/*
+ * version.h
+ *
+ *  Copyright (c) 2020, hikyuu.org
+ *
+ *  Created on: 2020-4-19
+ *      Author: fasiondog
+ */
+
+#pragma once
+#ifndef HKU_VERSION_H
+#define HKU_VERSION_H
+
+// clang-format off
+#define HKU_VERSION "2.2.1"
+#define HKU_VERSION_MAJOR 2
+#define HKU_VERSION_MINOR 2
+#define HKU_VERSION_ALTER 1
+#define HKU_VERSION_BUILD 202410011518
+#define HKU_VERSION_MODE "RELEASE"
+#define HKU_VERSION_GIT "2.2.1 master.89f17bf1 (RELEASE)"
+// clang-format on
+
+#endif /* HKU_VERSION_H */
