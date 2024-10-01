38b9f790ab4fee0ee4f15a5e8f70922e6a035b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 05:05:01 +0000

    Add package wdoc/2.1.3/wdoc-2.1.3.tar.gz

---
+++ packages/wdoc/wdoc-2.1.3.tar.gz/wdoc-2.1.3/WDoc/utils/env.py
@@ -0,0 +1,58 @@
+import os
+from typing import Any, Optional, Union
+from beartype import beartype, BeartypeConf
+
+# must create it because we can't import it from typechecker.py
+warn_typecheck = beartype(conf=BeartypeConf(violation_type=UserWarning))
+
+@warn_typecheck
+def parse(val: str) -> Optional[Union[bool, int, str]]:
+    if val == "true":
+        return True
+    elif val == "false":
+        return False
+    elif val.isdigit():
+        return int(val)
+    elif val == "none" or val == "":
+        return None
+    return val
+
+WDOC_TYPECHECKING = "warn"
+WDOC_NO_MODELNAME_MATCHING = False
+WDOC_ALLOW_NO_PRICE = False
+WDOC_OPEN_ANKI = False
+WDOC_STRICT_DOCDICT = False
+WDOC_MAX_LOADER_TIMEOUT = -1
+WDOC_MAX_PDF_LOADER_TIMEOUT = -1  # disabled as it can make the parsing slower
+WDOC_PRIVATE_MODE = False
+WDOC_DEBUGGER = False
+WDOC_EXPIRE_CACHE_DAYS = 0
+WDOC_EMPTY_LOADER = False
+WDOC_BEHAVIOR_EXCL_INCL_USELESS = "warn"
+WDOC_DISABLE_LAZYLOADING = False
+WDOC_MOD_FAISS_SCORE_FN = False
+
+valid_types = {
+    'WDOC_TYPECHECKING': str,
+    'WDOC_NO_MODELNAME_MATCHING': bool,
+    'WDOC_ALLOW_NO_PRICE': bool,
+    'WDOC_OPEN_ANKI': bool,
+    'WDOC_STRICT_DOCDICT': bool,
+    'WDOC_MAX_LOADER_TIMEOUT': int,
+    'WDOC_MAX_PDF_LOADER_TIMEOUT': int,
+    'WDOC_PRIVATE_MODE': bool,
+    'WDOC_DEBUGGER': bool,
+    'WDOC_EXPIRE_CACHE_DAYS': int,
+    'WDOC_EMPTY_LOADER': bool,
+    'WDOC_BEHAVIOR_EXCL_INCL_USELESS': str,
+    'WDOC_DISABLE_LAZYLOADING': Any,
+    'WDOC_MOD_FAISS_SCORE_FN': bool,
+}
+
+for k in os.environ.keys():
+    if not k.startswith("WDOC_"):
+        continue
+    v = parse(os.environ[k])
+    assert k in locals().keys(), f"Unexpected key for WDOC env variable: {k}"
+    assert isinstance(v, valid_types[k]), f"Unexpected type of env variable '{k}': '{type(v)}' but expected '{valid_types['k']}'"
+    locals()[k] = v
