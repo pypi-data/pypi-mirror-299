8634efa8ccad49e34d90264eba9deaf9edc00b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:06:06 +0000

    Add package seuils/0.3.3/seuils-0.3.3-py3-none-any.whl

---
+++ packages/seuils/seuils-0.3.3-py3-none-any.whl/seuils/tests/test_couverture_temporelle.py
@@ -0,0 +1,16 @@
+from datetime import date, timedelta
+
+from seuils.usure import get_lien
+
+
+def daterange(start_date: date, end_date: date):
+    days = int((end_date - start_date).days)
+    for n in range(days):
+        yield start_date + timedelta(n)
+
+
+def test_historique():
+    start_date = date(2005, 1, 1)
+    end_date = date.today()
+    for single_date in daterange(start_date, end_date):
+        assert type(get_lien(single_date)) is str
