e0118c98087f8135134e980faf55888a762a6b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 14:04:09 +0000

    Add package spider-ai/0.3/spider_ai-0.3.tar.gz

---
+++ packages/spider-ai/spider_ai-0.3.tar.gz/spider_ai-0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="spider-ai",
+    version="0.3",
+    author="spiderXR",
+    description="  مكتبه تعمل لخدمات الذكاء الاصطناعي spider_ai",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+
