0f46515600307fc968154c3e3ea0a1314d489e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:05:09 +0000

    Add package fmu-dataio/2.5.1/fmu_dataio-2.5.1.tar.gz

---
+++ packages/fmu-dataio/fmu_dataio-2.5.1.tar.gz/fmu_dataio-2.5.1/schema/app/install-schema-files.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+die () {
+    exitcode="$1"; shift
+    echo $*
+    exit $exitcode
+}
+
+if [ -z "$URIPREFIX" ]; then
+    test \! -z "$RADIX_PUBLIC_DOMAIN_NAME"  || die 1 "Unable to build PREFIX."
+    URIPREFIX="https://${RADIX_PUBLIC_DOMAIN_NAME}"
+fi
+
+echo "URIPREFIX=$URIPREFIX"
+
+test -d ./definitions || die 1 "Definitions not found."
+
+mkdir schemas || die 1 "Unable to create directory 'schemas'."
+
+find ./definitions -type d -name schema -print | \
+    while read dir; do
+        pdir=`dirname "$dir"`
+        sdir=schemas/`basename "$pdir"`
+        mkdir "$sdir"
+        find "$dir" -maxdepth 1 -name '*.json' -print | \
+            while read file; do
+                #echo "file: $file"
+                ffile=`basename "$file"`
+                id=`echo "$URIPREFIX"/"$sdir"/"$ffile" | sed -e 's/ /%20/g'`
+                #echo "id: $id"
+                #echo "creating $sdir/$ffile"
+                cat "$file" | jq '.["$id"]='\""$id"\" > "$sdir"/"$ffile"
+            done
+
+        # Temporarily copy the fmu_results.json schema to fmu_meta.json
+        # TODO: Remove once Sumo has switched to fmu_results.json
+        id="$URIPREFIX"/"$sdir"/fmu_meta.json
+        cat "$sdir"/fmu_results.json | jq '.["$id"]='\""$id"\" > "$sdir"/fmu_meta.json
+
+    done
+
+
