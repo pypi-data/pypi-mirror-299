ad9a279c463d5959a1824551273aaa66088e7b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 15:05:34 +0000

    Add package lima-api/1.3.1/lima_api-1.3.1-py3-none-any.whl

---
+++ packages/lima-api/lima_api-1.3.1-py3-none-any.whl/lima_api/exceptions.py
@@ -0,0 +1,57 @@
+import contextlib
+import json
+from typing import Optional, TypeVar, Union
+
+import pydantic
+
+from .utils import parse_data
+
+T = TypeVar("T")
+
+
+class LimaException(Exception):
+    detail: str = ""
+    model: Optional[T] = None
+
+    def __init__(
+        self,
+        detail: Optional[str] = None,
+        status_code: Optional[int] = None,
+        content: Optional[bytes] = None,
+    ):
+        if detail is not None:
+            self.detail = detail
+        self.status_code = status_code
+        self.content = content
+
+    def __repr__(self) -> str:
+        class_name = self.__class__.__name__
+        return f"{class_name}(detail={self.detail!r}, status_code={self.status_code!r})"
+
+    def __str__(self) -> str:
+        return self.detail
+
+    def json(self):
+        return json.loads(self.content.decode())
+
+    def object(self):
+        return parse_data(self.model, self.content)
+
+    def response(self) -> Union[T, dict, bytes]:
+        response = self.content
+        if self.content:
+            with contextlib.suppress(json.JSONDecodeError):
+                response = self.json()
+                if self.model:
+                    with contextlib.suppress(pydantic.ValidationError):
+                        response = self.object()
+        return response
+
+
+class ValidationError(LimaException):
+    detail = "Validation error"
+
+    def __str__(self):
+        if getattr(self, "__cause__", None):
+            return str(self.__cause__)
+        return self.detail
