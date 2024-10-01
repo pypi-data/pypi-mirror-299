3fac1e42cc80c7f30d2ee95fd828e82ef8e4da68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 03:17:26 +0000

    Add package amnemone/0.1.2/amnemone-0.1.2-py3-none-any.whl

---
+++ packages/amnemone/amnemone-0.1.2-py3-none-any.whl/amnemone/data/__init__.py
@@ -0,0 +1,4 @@
+from amnemone.data.countries_states_cities import Cities, Countries, regions, states, subregions
+from amnemone.data.words import an_array_of_english_words
+
+__all__ = ['an_array_of_english_words', 'Cities', 'Countries', 'regions', 'states', 'subregions']
