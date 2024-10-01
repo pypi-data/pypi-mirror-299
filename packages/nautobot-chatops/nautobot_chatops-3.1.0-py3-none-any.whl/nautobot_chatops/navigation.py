40c789c5f67cb214acd185a9fd74474668a64d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 14:03:40 +0000

    Add package nautobot-chatops/3.1.0/nautobot_chatops-3.1.0-py3-none-any.whl

---
+++ packages/nautobot-chatops/nautobot_chatops-3.1.0-py3-none-any.whl/nautobot_chatops/navigation.py
@@ -0,0 +1,59 @@
+"""App additions to the Nautobot navigation menu."""
+
+from nautobot.apps.config import get_app_settings_or_config
+from nautobot.apps.ui import NavMenuAddButton, NavMenuGroup, NavMenuItem, NavMenuTab
+
+if get_app_settings_or_config("nautobot_chatops", "enable_grafana"):
+    from .integrations.grafana.navigation import items as grafana_items
+else:
+    grafana_items = ()
+
+
+items = [
+    NavMenuItem(
+        link="plugins:nautobot_chatops:accessgrant_list",
+        name="Access Grants",
+        permissions=["nautobot_chatops.view_accessgrant"],
+        buttons=(
+            NavMenuAddButton(
+                link="plugins:nautobot_chatops:accessgrant_add",
+                permissions=["nautobot_chatops.add_accessgrant"],
+            ),
+        ),
+    ),
+    NavMenuItem(
+        link="plugins:nautobot_chatops:chatopsaccountlink_list",
+        name="Link ChatOps Account",
+        permissions=["nautobot_chatops.view_chatopsaccountlink"],
+        buttons=(
+            NavMenuAddButton(
+                link="plugins:nautobot_chatops:chatopsaccountlink_add",
+                permissions=["nautobot_chatops.add_chatopsaccountlink"],
+            ),
+        ),
+    ),
+    NavMenuItem(
+        link="plugins:nautobot_chatops:commandtoken_list",
+        name="Command Tokens",
+        permissions=["nautobot_chatops.view_commandtoken"],
+        buttons=(
+            NavMenuAddButton(
+                link="plugins:nautobot_chatops:commandtoken_add",
+                permissions=["nautobot_chatops.add_commandtoken"],
+            ),
+        ),
+    ),
+    NavMenuItem(
+        link="plugins:nautobot_chatops:commandlog_list",
+        name="Command Usage Records",
+        permissions=["nautobot_chatops.view_commandlog"],
+    ),
+    *grafana_items,
+]
+
+menu_items = (
+    NavMenuTab(
+        name="Plugins",
+        groups=(NavMenuGroup(name="Nautobot ChatOps", items=tuple(items)),),
+    ),
+)
