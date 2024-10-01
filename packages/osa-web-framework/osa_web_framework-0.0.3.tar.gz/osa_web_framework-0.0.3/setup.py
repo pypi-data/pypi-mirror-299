ab19c4c8273e463eed01ddedfbe41f0be65e86b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 06:04:15 +0000

    Add package osa-web-framework/0.0.3/osa_web_framework-0.0.3.tar.gz

---
+++ packages/osa-web-framework/osa_web_framework-0.0.3.tar.gz/osa_web_framework-0.0.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setup(
+    name='osa-web-framework',  
+    version='0.0.3',
+    description='A simple web framework for Python developers to learn how some fundamental concepts of web frameworks work under the hood.',  
+    author='Alyahyawy Osama',
+    author_email='alyhyawyosama@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  
+    packages=find_packages(),  # Automatically find packages in the directory
+    include_package_data=True,  # Include non-code files specified in MANIFEST.in
+    install_requires=[
+        'click',        # For command-line interface
+        'webob',        # For request and response objects
+        'requests',     # For the test client
+        'parse',        # For URL parsing
+        'requests-wsgi-adapter',  # For integrating with WSGI in the test client,
+        'whitenoise',   # For serving static files
+        'jinja2',       # For rendering templates
+
+    ],
+    entry_points={
+        'console_scripts': [
+            'osa=osa.cli:main',  # Expose the CLI command
+        ],
+    },
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  
+)
