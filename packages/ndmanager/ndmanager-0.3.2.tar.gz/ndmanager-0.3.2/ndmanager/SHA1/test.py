7c40b03052dd374317e33148408a9af76c6de04a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 03:17:00 +0000

    Add package ndmanager/0.3.2/ndmanager-0.3.2.tar.gz

---
+++ packages/ndmanager/ndmanager-0.3.2.tar.gz/ndmanager-0.3.2/ndmanager/SHA1/test.py
@@ -0,0 +1,12 @@
+# pylint: disable=too-many-lines
+"""SHA1 values for the test tapes"""
+TEST_SHA1 = {
+    "test/tsl/tsl_0037_H(CH2)": "d376d40451876d8d8f7a0d2d55d1dfb38428c266",
+    "test/n/C12": "af075e8479d0bc26bcbb4c99084f7d048dd41f0a",
+    "test/n/H1": "56b2841125aa995717bce934447a313e23b71a80",
+    "test/n/Am242_m1": "1c86a69fc496dcfb1a88c70c60fc8a986518ba47",
+    "test/ard/H0": "04f484aabbcd488f65c3e95ff6b2ab660a0599cc",
+    "test/ard/C0": "25baa0fa7074046cf29cdcafe655d301238c4e06",
+    "test/photo/H0": "a72035d425898ee1eb5f727cd52ce041b5b8fbc9",
+    "test/photo/C0": "a384c9fb865a0a9f812ec84242b68d84c27b698c",
+}
---
+++ packages/ndmanager/ndmanager-0.3.2.tar.gz/ndmanager-0.3.2/ndmanager/__init__.py
@@ -0,0 +1,9 @@
+"""The NDManager module"""
+
+__version__="0.3.2"
+
+from .API.endf6 import Endf6
+from .API.iaea import download_endf6, fetch_sublibrary_list
+from .API.nuclide import Nuclide
+from .API.sha1 import compute_file_sha1
+from .API.utils import get_endf6
