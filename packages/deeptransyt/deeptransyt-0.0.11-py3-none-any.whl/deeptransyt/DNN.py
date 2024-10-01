fd59414ec4064fa5e35b5cbdecc9e5f1a773efc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-01 04:03:47 +0000

    Add package deeptransyt/0.0.11/deeptransyt-0.0.11-py3-none-any.whl

---
+++ packages/deeptransyt/deeptransyt-0.0.11-py3-none-any.whl/deeptransyt/DNN.py
@@ -0,0 +1,302 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+import torchmetrics
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torchmetrics.classification import BinaryAccuracy
+
+
+class DNN(pl.LightningModule):
+    def __init__(self, num_classes):
+        super(DNN, self).__init__()
+
+        self.fc1 = nn.Linear(1280, 946)
+        self.bn1 = nn.BatchNorm1d(946)
+        self.dropout1 = nn.Dropout(0.233)
+        
+        self.fc2 = nn.Linear(946, 592)
+        self.bn2 = nn.BatchNorm1d(592)
+        self.dropout2 = nn.Dropout(0.233)
+        
+        self.fc3 = nn.Linear(592, 341)
+        self.bn3 = nn.BatchNorm1d(341)
+        self.dropout3 = nn.Dropout(0.233)
+        
+        self.fc4 = nn.Linear(341, num_classes)
+        
+        self.criterion = nn.CrossEntropyLoss()
+        self.accuracy = torchmetrics.Accuracy("multiclass", num_classes=num_classes) 
+    
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.bn1(x)
+        x = self.dropout1(x)
+
+        x = torch.relu(self.fc2(x))
+        x = self.bn2(x)
+        x = self.dropout2(x)
+
+        x = torch.relu(self.fc3(x))
+        x = self.bn3(x)
+        x = self.dropout3(x)
+
+        x = self.fc4(x)  
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        acc = self.accuracy(y_hat, y)
+        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('train_acc', acc, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        acc = self.accuracy(y_hat, y)
+        self.log('val_loss', loss, on_epoch=True, prog_bar=True)
+        self.log('val_acc', acc, on_epoch=True, prog_bar=True)
+        return {'val_loss': loss, 'val_acc': acc}
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        acc = self.accuracy(y_hat, y)
+        self.log('test_loss', loss)
+        self.log('test_acc', acc)
+        return {'test_loss': loss, 'test_acc': acc}
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        lr_scheduler = {'scheduler': ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, verbose=True),
+                        'monitor': 'val_loss',  
+                        'reduce_on_plateau': True}
+        return [optimizer], [lr_scheduler]
+    
+
+
+class DNN_binary(pl.LightningModule):
+    def __init__(self):
+        super(DNN_binary, self).__init__()
+
+        self.fc1 = nn.Linear(1280, 946)
+        self.bn1 = nn.BatchNorm1d(946)
+        self.dropout1 = nn.Dropout(0.233)
+        
+        self.fc2 = nn.Linear(946, 592)
+        self.bn2 = nn.BatchNorm1d(592)
+        self.dropout2 = nn.Dropout(0.233)
+        
+        self.fc3 = nn.Linear(592, 341)
+        self.bn3 = nn.BatchNorm1d(341)
+        self.dropout3 = nn.Dropout(0.233)
+        
+        self.fc4 = nn.Linear(341, 1)
+        
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.accuracy = BinaryAccuracy()
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.bn1(x)
+        x = self.dropout1(x)
+
+        x = torch.relu(self.fc2(x))
+        x = self.bn2(x)
+        x = self.dropout2(x)
+
+        x = torch.relu(self.fc3(x))
+        x = self.bn3(x)
+        x = self.dropout3(x)
+
+        x = self.fc4(x)  
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x).squeeze()
+        loss = self.criterion(y_hat, y.float())
+        acc = self.accuracy(torch.sigmoid(y_hat), y)
+        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('train_acc', acc, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x).squeeze()
+        loss = self.criterion(y_hat, y.float())
+        acc = self.accuracy(torch.sigmoid(y_hat), y)
+        self.log('val_loss', loss, on_epoch=True, prog_bar=True)
+        self.log('val_acc', acc, on_epoch=True, prog_bar=True)
+        return {'val_loss': loss, 'val_acc': acc}
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x).squeeze()
+        loss = self.criterion(y_hat, y.float())
+        acc = self.accuracy(torch.sigmoid(y_hat), y)
+        self.log('test_loss', loss)
+        self.log('test_acc', acc)
+        return {'test_loss': loss, 'test_acc': acc}
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        lr_scheduler = {'scheduler': ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, verbose=True),
+                        'monitor': 'val_loss',  
+                        'reduce_on_plateau': True}
+        return [optimizer], [lr_scheduler]
+    
+
+class DNN_weight(pl.LightningModule):
+    def __init__(self, num_classes, weight):
+        super(DNN_weight, self).__init__()
+
+        self.fc1 = nn.Linear(1280, 946)
+        self.bn1 = nn.BatchNorm1d(946)
+        self.dropout1 = nn.Dropout(0.233)
+        
+        self.fc2 = nn.Linear(946, 592)
+        self.bn2 = nn.BatchNorm1d(592)
+        self.dropout2 = nn.Dropout(0.233)
+        
+        self.fc3 = nn.Linear(592, 341)
+        self.bn3 = nn.BatchNorm1d(341)
+        self.dropout3 = nn.Dropout(0.233)
+        
+        self.fc4 = nn.Linear(341, num_classes)
+        
+        self.criterion = nn.CrossEntropyLoss(weight=weight)
+        self.accuracy = torchmetrics.Accuracy("multiclass", num_classes=num_classes) 
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.bn1(x)
+        x = self.dropout1(x)
+
+        x = torch.relu(self.fc2(x))
+        x = self.bn2(x)
+        x = self.dropout2(x)
+
+        x = torch.relu(self.fc3(x))
+        x = self.bn3(x)
+        x = self.dropout3(x)
+
+        x = self.fc4(x)  
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        acc = self.accuracy(y_hat, y)
+        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('train_acc', acc, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        acc = self.accuracy(y_hat, y)
+        self.log('val_loss', loss, on_epoch=True, prog_bar=True)
+        self.log('val_acc', acc, on_epoch=True, prog_bar=True)
+        return {'val_loss': loss, 'val_acc': acc}
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        acc = self.accuracy(y_hat, y)
+        self.log('test_loss', loss)
+        self.log('test_acc', acc)
+        return {'test_loss': loss, 'test_acc': acc}
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        lr_scheduler = {'scheduler': ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, verbose=True),
+                        'monitor': 'val_loss',  
+                        'reduce_on_plateau': True}
+        return [optimizer], [lr_scheduler]
+    
+class HierarchicalDNN(pl.LightningModule):
+    def __init__(self, num_families, num_subfamilies):
+        super(HierarchicalDNN, self).__init__()
+
+        self.fc1 = nn.Linear(1280, 1024)
+        self.bn1 = nn.BatchNorm1d(1024)
+        self.dropout1 = nn.Dropout(0.3)
+        
+        self.fc2 = nn.Linear(1024, 640)
+        self.bn2 = nn.BatchNorm1d(640)
+        self.dropout2 = nn.Dropout(0.3)
+        
+        self.fc3 = nn.Linear(640, 230)
+        self.bn3 = nn.BatchNorm1d(230)
+        self.dropout3 = nn.Dropout(0.3)
+        
+        self.fc_family = nn.Linear(230, num_families)
+        self.fc_subfamily = nn.Linear(230, num_subfamilies)
+        
+        self.criterion = nn.CrossEntropyLoss()
+        self.family_accuracy = torchmetrics.Accuracy("multiclass", num_classes=num_families)
+        self.subfamily_accuracy = torchmetrics.Accuracy("multiclass", num_classes=num_subfamilies)
+    
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.bn1(x)
+        x = self.dropout1(x)
+
+        x = torch.relu(self.fc2(x))
+        x = self.bn2(x)
+        x = self.dropout2(x)
+
+        x = torch.relu(self.fc3(x))
+        x = self.bn3(x)
+        x = self.dropout3(x)
+
+        family_output = self.fc_family(x)
+        subfamily_output = self.fc_subfamily(x)
+        return family_output, subfamily_output
+
+    def training_step(self, batch, batch_idx):
+        x, y_fam, y_sub = batch
+        fam_output, sub_output = self(x)
+        
+        loss_fam = self.criterion(fam_output, y_fam)
+        loss_sub = self.criterion(sub_output, y_sub)
+        loss = loss_fam + loss_sub
+
+        fam_acc = self.family_accuracy(fam_output, y_fam)
+        sub_acc = self.subfamily_accuracy(sub_output, y_sub)
+        
+        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('train_fam_acc', fam_acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('train_sub_acc', sub_acc, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        x, y_fam, y_sub = batch
+        fam_output, sub_output = self(x)
+
+        loss_fam = self.criterion(fam_output, y_fam)
+        loss_sub = self.criterion(sub_output, y_sub)
+        loss = loss_fam + loss_sub
+
+        fam_acc = self.family_accuracy(fam_output, y_fam)
+        sub_acc = self.subfamily_accuracy(sub_output, y_sub)
+        
+        self.log('test_loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('test_fam_acc', fam_acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('test_sub_acc', sub_acc, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        lr_scheduler = {'scheduler': ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, verbose=True),
+                        'monitor': 'train_loss',  
+                        'reduce_on_plateau': True}
+        return [optimizer], [lr_scheduler]
---
+++ packages/deeptransyt/deeptransyt-0.0.11-py3-none-any.whl/deeptransyt/make_predictions.py
@@ -0,0 +1,222 @@
+import torch
+import numpy as np
+import pandas as pd
+from .DNN import DNN, DNN_binary, DNN_weight, HierarchicalDNN
+import json
+import os
+import torch.nn.functional as F
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_DIR = os.path.join(BASE_DIR, 'models_mappings')
+        
+def predict_binary(encodings: np.ndarray, accession: list) -> pd.DataFrame:
+    model_path = os.path.join(MODEL_DIR, 'DNN_allclasses.ckpt')
+
+    model = DNN_binary.load_from_checkpoint(model_path)
+    device = torch.device('cpu')
+    model = model.to(device)
+    model.eval()
+
+    tensor_encodings = torch.tensor(encodings, dtype=torch.float32)
+    with torch.no_grad():
+        predictions = torch.sigmoid(model(tensor_encodings)).numpy().flatten()
+
+    df_binary_predictions = pd.DataFrame({'Accession': accession, "Binary_Predictions": predictions})
+
+    binary_labels = (predictions > 0.5).astype(int)
+    #num_transporters = np.sum(binary_labels)
+
+    return df_binary_predictions, binary_labels
+
+
+def predict_family(transporter_encodings: np.ndarray, transporter_accessions: list) -> pd.DataFrame:
+    family_models = [
+        # {'path': os.path.join(MODEL_DIR, 'family_DNN_no9_10.ckpt'), 'num_classes': 402, 'column_name': 'PredictedFamily_>10', 'label_map': os.path.join(MAPPING_DIR, 'mapping_family10.json')},
+        {'path': os.path.join(MODEL_DIR, 'family_DNN_no9_12.ckpt'), 'num_classes': 328, 'column_name': 'PredictedFamily_>12', 'label_map': os.path.join(MODEL_DIR, 'mapping_family12.json')}
+        # {'path': os.path.join(MODEL_DIR, 'family_DNN_no9_15.ckpt'), 'num_classes': 279, 'column_name': 'PredictedFamily_>15', 'label_map': os.path.join(MAPPING_DIR, 'mapping_family15.json')},
+        # {'path': os.path.join(MODEL_DIR, 'family_DNN_no9_20.ckpt'), 'num_classes': 196, 'column_name': 'PredictedFamily_>20', 'label_map': os.path.join(MAPPING_DIR, 'mapping_family20.json')},
+        # {'path': os.path.join(MODEL_DIR, 'family_DNN_no9_30.ckpt'), 'num_classes': 109, 'column_name': 'PredictedFamily_>30', 'label_map': os.path.join(MAPPING_DIR, 'mapping_family30.json')},
+        # {'path': os.path.join(MODEL_DIR, 'family_DNN_no9_40.ckpt'), 'num_classes': 75, 'column_name': 'PredictedFamily_>40', 'label_map': os.path.join(MAPPING_DIR, 'mapping_family40.json')},
+        # {'path': os.path.join(MODEL_DIR, 'family_DNN_no9_50.ckpt'), 'num_classes': 51, 'column_name': 'PredictedFamily_>50', 'label_map': os.path.join(MAPPING_DIR, 'mapping_family50.json')},
+    ]
+
+    df_family_predictions = pd.DataFrame({'Accession': transporter_accessions})
+
+    for model_info in family_models:
+        num_classes = model_info['num_classes']
+        
+        family_model = DNN.load_from_checkpoint(checkpoint_path=model_info['path'], num_classes=num_classes)
+        device = torch.device('cpu')
+        family_model = family_model.to(device)
+        family_model.eval()
+
+        transporter_tensor = torch.tensor(transporter_encodings, dtype=torch.float32)
+        with torch.no_grad():
+            transporter_predictions = family_model(transporter_tensor)
+        predicted_families = transporter_predictions.argmax(dim=1).numpy()
+
+        with open(model_info['label_map'], 'r') as f:
+            label_map = json.load(f)
+
+        predicted_family_labels = [label_map[str(label)] for label in predicted_families]
+
+        df_family_predictions[model_info['column_name']] = predicted_family_labels
+
+    return df_family_predictions
+
+
+# def predict_subfamily(transporter_encodings: np.ndarray, transporter_accessions: list) -> pd.DataFrame:
+#     subfamily_models = [
+#         {'path': os.path.join(MODEL_DIR, 'subfamily_DNN_no9_15.ckpt'), 'num_classes': 267, 'column_name': 'PredictedsubFamily_>15', 'label_map': os.path.join(MODEL_DIR, 'mapping_subfamily15.json')}
+#         #{'path': os.path.join(MODEL_DIR, 'subfamily_DNN_no9_20.ckpt'), 'num_classes': 159, 'column_name': 'PredictedsubFamily_>20', 'label_map': os.path.join(MAPPING_DIR, 'mapping_subfamily20.json')},
+#         #{'path': os.path.join(MODEL_DIR, 'subfamily_DNN_no9_30.ckpt'), 'num_classes': 75, 'column_name': 'PredictedsubFamily_>30', 'label_map': os.path.join(MAPPING_DIR, 'mapping_subfamily30.json')},
+#         #{'path': os.path.join(MODEL_DIR, 'subfamily_DNN_no9_50.ckpt'), 'num_classes': 30, 'column_name': 'PredictedSubFamily_>50', 'label_map': os.path.join(MAPPING_DIR, 'mapping_subfamily50.json')}
+#     ]
+
+#     df_subfamily_predictions = pd.DataFrame({'Accession': transporter_accessions})
+
+#     for model_info in subfamily_models:
+#         num_classes = model_info['num_classes']
+        
+#         subfamily_model = DNN.load_from_checkpoint(checkpoint_path=model_info['path'], num_classes=num_classes)
+#         device = torch.device('cpu')
+#         subfamily_model = subfamily_model.to(device)
+#         subfamily_model.eval()
+
+#         transporter_tensor = torch.tensor(transporter_encodings, dtype=torch.float32)
+#         with torch.no_grad():
+#             transporter_predictions = subfamily_model(transporter_tensor)
+#         predicted_subfamilies = transporter_predictions.argmax(dim=1).numpy()
+
+#         with open(model_info['label_map'], 'r') as f:
+#             label_map = json.load(f)
+
+#         predicted_subfamily_labels = [label_map[str(label)] for label in predicted_subfamilies]
+
+#         df_subfamily_predictions[model_info['column_name']] = predicted_subfamily_labels
+
+#     return df_subfamily_predictions
+
+
+# def predict_metabolic_important(transporter_encodings: np.ndarray, transporter_accessions: list) -> pd.DataFrame:    
+#     model_path = os.path.join(MODEL_DIR, 'newdataset_test_no9.ckpt')
+#     label_map_path = os.path.join(MAPPING_DIR, 'mapping_newdataset.json')
+    
+#     new_model = DNN.load_from_checkpoint(checkpoint_path=model_path, num_classes=4)
+#     device = torch.device('cpu')
+#     new_model = new_model.to(device)
+#     new_model.eval()
+
+#     transporter_tensor = torch.tensor(transporter_encodings, dtype=torch.float32)
+#     with torch.no_grad():
+#         transporter_predictions = new_model(transporter_tensor)
+#     predicted_labels = transporter_predictions.argmax(dim=1).numpy()
+
+#     with open(label_map_path, 'r') as f:
+#         label_map = json.load(f)
+
+#     predicted_labels_names = [label_map[str(label)] for label in predicted_labels]
+
+#     df_new_predictions = pd.DataFrame({'Accession': transporter_accessions, 'Predicted_newdataset': predicted_labels_names})
+
+#     return df_new_predictions
+
+
+def predict_family_subfamily(transporter_encodings: np.ndarray, transporter_accessions: list) -> pd.DataFrame:
+
+    label_map_path = os.path.join(MODEL_DIR, 'family_subfamily_mappings.json')
+    label_map_path1 = os.path.join(MODEL_DIR, 'family_to_subfamily_map.json')
+    with open(label_map_path, 'r') as f:
+        mappings = json.load(f)
+        family_mapping = mappings['family_mapping']
+        subfamily_mapping = mappings['subfamily_mapping'] 
+
+    with open(label_map_path1, 'r') as f:
+        family_to_subfamily_map = json.load(f)
+
+    num_family_classes = len(family_mapping)
+    num_subfamily_classes = len(subfamily_mapping)
+
+    model_path = os.path.join(MODEL_DIR, 'family_subfamily_10.ckpt')
+    model = HierarchicalDNN.load_from_checkpoint(checkpoint_path=model_path, 
+                                                 num_families=num_family_classes, 
+                                                 num_subfamilies=num_subfamily_classes)
+    
+    device = torch.device('cpu')
+    model = model.to(device)
+    model.eval()
+
+    transporter_tensor = torch.tensor(transporter_encodings, dtype=torch.float32)
+
+    with torch.no_grad():
+        fam_pred, sub_pred = model(transporter_tensor)
+
+        fam_prob = F.softmax(fam_pred, dim=1)
+        sub_prob = F.softmax(sub_pred, dim=1)
+
+        predicted_families = []
+        predicted_subfamilies = []
+        family_confidences = []
+        subfamily_confidences = []
+
+        for i in range(len(transporter_accessions)):
+            fam_pred_label = torch.argmax(fam_prob[i]).item()
+            fam_pred_confidence = fam_prob[i][fam_pred_label].item()
+
+            original_family = family_mapping.get(str(fam_pred_label), "Unknown Family")
+            predicted_families.append(original_family)
+            family_confidences.append(fam_pred_confidence)
+
+            valid_subfamilies = family_to_subfamily_map.get(str(fam_pred_label), [])
+
+            if valid_subfamilies:
+                filtered_sub_pred = torch.full_like(sub_pred[i], float('-inf'))
+                filtered_sub_pred[valid_subfamilies] = sub_pred[i][valid_subfamilies]
+
+                filtered_sub_prob = F.softmax(filtered_sub_pred, dim=0)
+
+                sub_pred_label = torch.argmax(filtered_sub_prob).item()
+                sub_pred_confidence = filtered_sub_prob[sub_pred_label].item()
+
+                original_subfamily = subfamily_mapping.get(str(sub_pred_label), "Unknown Subfamily")
+                predicted_subfamilies.append(original_subfamily)
+                subfamily_confidences.append(sub_pred_confidence)
+            else:
+                predicted_subfamilies.append("Unknown Subfamily")
+                subfamily_confidences.append(0.0)
+
+    df_predictions = pd.DataFrame({
+        'Accession': transporter_accessions,
+        'PredictedFamily': predicted_families,
+        'FamilyConfidence': family_confidences,
+        'PredictedSubfamily': predicted_subfamilies,
+        'SubfamilyConfidence': subfamily_confidences
+    })
+
+    return df_predictions
+
+def predict_substrate_classes(transporter_encodings: np.ndarray, transporter_accessions: list) -> pd.DataFrame:     
+    
+    model_path = os.path.join(MODEL_DIR, 'substrate_classes.ckpt')
+    label_map_path = os.path.join(MODEL_DIR, 'mapping_susbtrate_classes.json')
+
+    pos_weight = torch.tensor([0.2224, 0.0389, 0.0757, 0.3443, 0.0426, 0.0761, 0.2000], dtype=torch.float32)
+    
+    substrate_classes_model = DNN_weight.load_from_checkpoint(checkpoint_path=model_path, num_classes=7, weight=pos_weight)
+    device = torch.device('cpu')
+    substrate_classes_model = substrate_classes_model.to(device)
+    substrate_classes_model.eval()
+
+    transporter_tensor = torch.tensor(transporter_encodings, dtype=torch.float32)
+    with torch.no_grad():
+        transporter_predictions = substrate_classes_model(transporter_tensor)
+    predicted_labels = transporter_predictions.argmax(dim=1).numpy()
+
+    with open(label_map_path, 'r') as f:
+        label_map = json.load(f)
+
+    predicted_labels_names = [label_map[str(label)] for label in predicted_labels]
+
+    df_substrate_classes = pd.DataFrame({'Accession': transporter_accessions, 'Class_substrate': predicted_labels_names})
+
+    return df_substrate_classes
