4277ac6dfd78f1705aec6d707286ab567d8c17df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:08:54 +0000

    Add package ag-port-killer/0.2/ag_port_killer-0.2.tar.gz

---
+++ packages/ag-port-killer/ag_port_killer-0.2.tar.gz/ag_port_killer-0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ag-port-killer",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+        "psutil",
+        "typing_extensions",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ag-port-killer=ag_port_killer.main:main',
+        ],
+    },
+    author="Avni Gashi",
+    author_email="avnig92@gmail.com",
+    description="An advanced tool to manage and kill processes on specific ports",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/avnigashi/ag-port-killer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
