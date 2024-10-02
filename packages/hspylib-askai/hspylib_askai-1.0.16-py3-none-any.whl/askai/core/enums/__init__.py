65774f095cb6002806f47ca7ad2958428f0b061e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 07:04:37 +0000

    Add package hspylib-askai/1.0.16/hspylib_askai-1.0.16-py3-none-any.whl

---
+++ packages/hspylib-askai/hspylib_askai-1.0.16-py3-none-any.whl/askai/core/enums/__init__.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-askai v1.0.16
+#
+# Package: main.askai.core.enums
+"""Package initialization."""
+
+__all__ = [
+    'acc_color', 
+    'response_model', 
+    'router_mode', 
+    'verbosity'
+]
+__version__ = '1.0.16'
---
+++ packages/hspylib-askai/hspylib_askai-1.0.16-py3-none-any.whl/askai/core/model/search_result.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HsPyLib-AskAI
+   @package: askai.core.model
+      @file: search_result.py
+   @created: Sun, 12 Mar 2024
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior
+      @site: https://github.com/yorevs/askai
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright (c) 2024, HomeSetup
+"""
+from askai.core.component.geo_location import geo_location
+from askai.core.support.llm_parser import parse_field, parse_list
+from dataclasses import dataclass
+from os.path import expandvars
+from typing import Literal
+
+import json
+import os
+
+
+@dataclass
+class SearchResult:
+    """Track and store responses from internet searches.
+    This class encapsulates the results returned from an internet search, including any relevant data
+    associated with the search response.
+    """
+
+    question: str = None
+    engine: Literal["Google", "Bing"] = None
+    category: str = None
+    keywords: list[str] = None
+    sites: list[str] = None
+    filters: list[str] = None
+    response: str = None
+    datetime: str = geo_location.datetime
+
+    @classmethod
+    def parse_response(cls, question: str, query_response: str) -> "SearchResult":
+        """TODO"""
+
+        # FIXME: Remove log the response
+        with open(os.path.expandvars("${HOME}/Desktop/search-result-resp.txt"), "w") as f_resp:
+            f_resp.write(query_response + os.linesep)
+            f_resp.flush()
+
+        # Parse fields
+        engine: Literal["Google", "Bing"] = parse_field("@engine", query_response)
+        category: str = parse_field("@category", query_response)
+        keywords: list[str] = parse_list("@keywords", query_response, is_dict=False)
+        sites: list[str] = parse_list("@sites", query_response, is_dict=False)
+        filters: list[str] = parse_list("@filters", query_response, is_dict=False)
+
+        return SearchResult(question, engine, category, keywords, sites, filters)
+
+    def __str__(self):
+        return f"Search Results: {json.dumps(self.__dict__, default=lambda obj: obj.__dict__)}"
+
+    def __eq__(self, other: "SearchResult") -> bool:
+        """TODO"""
+        return (
+            self.category == other.category
+            and self.keywords == other.keywords
+            and self.sites == other.sites
+            and self.filters == other.filters
+        )
+
+    def build(self) -> str:
+        """TODO"""
+        return f"{self.keywords} {self.sites} {self.filters}"
