a856803b6eb1634bfb7980aaabe8218a3a07db01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 06:05:11 +0000

    Add package satisfactory-api-client/0.1.1/satisfactory_api_client-0.1.1.tar.gz

---
+++ packages/satisfactory-api-client/satisfactory_api_client-0.1.1.tar.gz/satisfactory_api_client-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='satisfactory_api_client',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[req.strip() for req in open('requirements.txt').readlines()],
+    description='A Python Package for interacting with the Satisfactory Dedicated Server API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Programmer-Timmy/satisfactory-dedicated-server-api-SDK',  # Replace with your repo URL
+    author='Programmer-Timmy',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
