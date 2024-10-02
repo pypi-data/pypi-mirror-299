3bb7d525da7a62f3e84d4e3fc50837a36b66661d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 07:04:15 +0000

    Add package dataplaybook/1.0.19/dataplaybook-1.0.19-py3-none-any.whl

---
+++ packages/dataplaybook/dataplaybook-1.0.19-py3-none-any.whl/dataplaybook/tasks/io_xml.py
@@ -0,0 +1,75 @@
+"""Read XML files."""
+
+import json
+import logging
+import typing
+from collections import defaultdict
+from xml.etree import ElementTree
+
+# from lxml import etree
+from dataplaybook import RowData, Tables, task
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@task
+def read_xml(tables: Tables, file: str, targets: list[str]) -> None:
+    """Read xml file.
+
+    https://stackoverflow.com/questions/1912434/how-do-i-parse-xml-in-python
+    """
+    tree = ElementTree.parse(file)
+    root = tree.getroot()
+    dct = _etree_to_dict(root)
+    # writejson('zza.json', dct)
+
+    _notok = list(targets)
+
+    for _t1 in dct.values():
+        for key, val in _t1.items():
+            key = _ns(key.replace("-", "_"))
+            if isinstance(val, list):
+                if key in _notok:
+                    _notok.remove(key)
+                tables[key] = val
+                # tables[key] = val
+            else:
+                _LOGGER.warning("Ignored %s: %s", key, str(val)[:20])
+
+    if _notok:
+        _LOGGER.warning("Expected table %s", ",".join(_notok))
+
+
+def _writejson(filename: str, dct: dict[str, typing.Any]) -> None:
+    """Write dict to file."""
+    with open(filename, "w", encoding="utf-8") as __f:
+        __f.write(json.dumps(dct))
+
+
+def _ns(_ss: str) -> str:
+    return _ss.replace("{http://www.rfc-editor.org/rfc-index}", "")
+
+
+# pylint: disable=invalid-name
+def _etree_to_dict(t: ElementTree.Element) -> RowData:
+    """Elementtree to dict."""
+    t_tag = _ns(t.tag)
+    d: RowData = {t_tag: {} if t.attrib else None}
+
+    children = list(t)
+    if children:
+        dd = defaultdict(list)
+        for dc in map(_etree_to_dict, children):
+            for k, v in dc.items():
+                dd[_ns(k)].append(v)
+        d = {t_tag: {k: v[0] if len(v) == 1 else v for k, v in dd.items()}}
+    if t.attrib:
+        d[t_tag].update(("@" + k, v) for k, v in t.attrib.items())
+    if t.text:
+        text = t.text.strip()
+        if children or t.attrib:
+            if text:
+                d[t_tag]["#text"] = text
+        else:
+            d[t_tag] = text
+    return d
