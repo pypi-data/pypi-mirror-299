10e6bcf9c21f7b4ea110a72d941ca3c672100543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:08:02 +0000

    Add package holidaylist/0.3/holidaylist-0.3.tar.gz

---
+++ packages/holidaylist/holidaylist-0.3.tar.gz/holidaylist-0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='holidaylist',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=['requests'],
+    description='A simple Python client for fetching holiday data.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
