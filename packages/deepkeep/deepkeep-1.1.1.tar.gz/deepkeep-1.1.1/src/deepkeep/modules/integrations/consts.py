531b8002dbda131a6dd7085ff620f025d3fba256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:09:28 +0000

    Add package deepkeep/1.1.1/deepkeep-1.1.1.tar.gz

---
+++ packages/deepkeep/deepkeep-1.1.1.tar.gz/deepkeep-1.1.1/src/deepkeep/modules/integrations/consts.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class IntegrationsType(Enum):
+    Azure_OpenAI = "Azure_OpenAI"
+    OpenAI = "OpenAI"
+    # Add more integrations - when ready
---
+++ packages/deepkeep/deepkeep-1.1.1.tar.gz/deepkeep-1.1.1/src/deepkeep/modules/model/consts.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class IntegrationModels(Enum):
+    Azure_OpenAI = "Azure_OpenAI"
+    OpenAI = "OpenAI"
+    # Add more integrations - when ready
