cbb1a6ef997a1632dba20c0d45d8fa55f388b435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 04:04:39 +0000

    Add package arcgis/2.4.0/arcgis-2.4.0.tar.gz

---
+++ packages/arcgis/arcgis-2.4.0.tar.gz/arcgis-2.4.0/arcgis/apps/storymap/__init__.py
@@ -0,0 +1,37 @@
+"""
+StoryMap Implementation
+"""
+
+from .story import StoryMap, Themes
+from .briefing import Briefing
+from .collection import Collection
+from .story_content import (
+    Image,
+    Video,
+    Audio,
+    Embed,
+    Text,
+    Button,
+    Map,
+    Sidecar,
+    Gallery,
+    Timeline,
+    Swipe,
+    TextStyles,
+    Scales,
+    MapTour,
+    BriefingSlide,
+    Code,
+    Language,
+    SlideLayout,
+    SlideSubLayout,
+    Block,
+    Table,
+    ExpressMap,
+    Navigation,
+    Cover,
+    CollectionNavigation,
+    Separator,
+)
+
+__all__ = ["StoryMap", "Briefing", "Collection"]
---
+++ packages/arcgis/arcgis-2.4.0.tar.gz/arcgis-2.4.0/arcgis/gis/server/_service/__init__.py
@@ -0,0 +1,8 @@
+"""
+Classes for ArcGIS Services
+"""
+
+from __future__ import absolute_import
+
+__version__ = "2.4.0"
+__all__ = ["Service"]
