15e4ac57dbc9a40ea829f7fcaf6b7b1e573e2d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 05:05:05 +0000

    Add package mumbleice/1.5.0/mumbleice-1.5.0.tar.gz

---
+++ packages/mumbleice/mumbleice-1.5.0.tar.gz/mumbleice-1.5.0/mumbleice/__main__.py
@@ -0,0 +1,85 @@
+###########################################################################
+# MumbleIce is Copyright (C) 2021 Kyle Robbertze <kyle@bitcast.co.za>
+#
+# MumbleIce is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3, or
+# any later version as published by the Free Software Foundation.
+#
+# MumbleIce is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MumbleIce. If not, see <http://www.gnu.org/licenses/>.
+###########################################################################
+import argparse
+import logging
+import os
+import sys
+
+from pyaml_env import parse_config, BaseConfig
+from .bot import Bot, IcecastConnector, MumbleConnector
+
+LOGGING_FORMAT = "%(asctime)s - %(levelname)s (%(name)s): %(message)s"
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="the configuration file to use",
+        default=os.environ.get("MUMBLEICE_CONFIG_FILE", "mumbleice.yml"),
+    )
+    parser.add_argument(
+        "-v", "--verbose", help="show debug information", action="store_true"
+    )
+    args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(
+            level=logging.DEBUG, format=LOGGING_FORMAT, datefmt=DATE_FORMAT
+        )
+    else:
+        logging.basicConfig(
+            level=logging.INFO, format=LOGGING_FORMAT, datefmt=DATE_FORMAT
+        )
+
+    logger = logging.getLogger(__name__)
+    try:
+        if not os.path.isfile(args.config):
+            raise FileNotFoundError()
+        config = BaseConfig(parse_config(args.config))
+
+        cfg = config.mumble
+        mumble = MumbleConnector(
+            cfg.server,
+            int(cfg.port),
+            cfg.username,
+            cfg.password,
+            cfg.channel,
+            int(cfg.max_silence),
+        )
+        cfg = config.icecast
+        icecast = IcecastConnector(
+            cfg.server, int(cfg.port), cfg.username, cfg.password, cfg.mount_point
+        )
+
+        # Required because pyaml_env environment variables are returned as
+        # strings, regardless of actual type
+        autoconnect = str(config.icecast.autoconnect).lower() in ["true", "yes"]
+        bot = Bot(mumble, icecast, config.mumble.command_prefix, autoconnect)
+        bot.run()
+    except KeyError:
+        logger.error("Error reading config file")
+        parser.print_help(file=sys.stderr)
+        exit(1)
+    except FileNotFoundError:
+        logger.error(f"Config file {args.config} does not exist")
+        parser.print_help(file=sys.stderr)
+        exit(1)
+
+
+if __name__ == "__main__":
+    run()
---
+++ packages/mumbleice/mumbleice-1.5.0.tar.gz/mumbleice-1.5.0/mumbleice/bot.py
@@ -0,0 +1,148 @@
+###########################################################################
+# MumbleIce is Copyright (C) 2021 Kyle Robbertze <kyle@bitcast.co.za>
+#
+# MumbleIce is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3, or
+# any later version as published by the Free Software Foundation.
+#
+# MumbleIce is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MumbleIce. If not, see <http://www.gnu.org/licenses/>.
+###########################################################################
+import logging
+import signal
+from importlib.resources import files, as_file
+from .mumble import MumbleConnector
+from .icecast import IcecastConnector
+from .utils import (
+    Watchdog,
+    SilenceError,
+    ConfigurationError,
+    parse_message,
+    BUFFER_DURATION,
+    WATCHDOG_RATE,
+    NUM_CHANNELS,
+)
+
+
+class Bot:
+    def __init__(self, mumble, icecast, command_prefix, autoconnect=False):
+        if BUFFER_DURATION % 10 != 0:
+            raise ConfigurationError(
+                "BUFFER_DURATION must be 10ms or a multiple thereof"
+            )
+        if NUM_CHANNELS not in [1, 2]:
+            raise ConfigurationError(
+                "NUM_CHANNELS must be 1 (mono) or (stereo). Other values are not supported"
+            )
+
+        signal.signal(signal.SIGINT, self.stop)
+        signal.signal(signal.SIGTERM, self.stop)
+        signal.signal(signal.SIGQUIT, self.stop)
+
+        self.mumble = mumble
+        self.mumble.set_message_callback(self.mumble_message)
+        self.icecast = icecast
+        self.logger = logging.getLogger(__name__)
+        self.command_prefix = command_prefix
+        self.commands = {
+            "connect": self.connect_icecast,
+            "disconnect": self.disconnect_icecast,
+            "status": self.show_icecast_status,
+        }
+        self.autoconnect = autoconnect
+        self.timer = Watchdog(WATCHDOG_RATE / 1000, self.write_audio)
+
+    def run(self):
+        self.logger.debug("Starting MumbleIce bot")
+        self.logger.debug("Connecting to Mumble...")
+        self.mumble.start()
+        self.logger.info("Connected to Mumble")
+        self.logger.info("Started MumbleIce bot")
+
+        if self.autoconnect:
+            self.connect_icecast()
+
+    def stop(self, _signal, _frame):
+        if hasattr(self, "timer"):
+            self.timer.stop()
+        if hasattr(self, "icecast"):
+            self.icecast.stop()
+        if hasattr(self, "mumble"):
+            self.mumble.stop()
+
+    def mumble_message(self, message):
+        self.logger.info(f"Message received: {message.message}")
+        msg = parse_message(message.message)
+        if msg.startswith(self.command_prefix) and len(msg) > len(self.command_prefix):
+            msg = msg[len(self.command_prefix) :]
+            msg = msg.split(" ")
+            cmd = self.commands.get(msg[0])
+            if cmd == None:
+                self.logger.debug(f"Not recognized command: {message.message}")
+                self.mumble.send_message("Command not recognised")
+                return
+            cmd()
+
+    def connect_icecast(self):
+        if self.icecast.is_connected:
+            self.mumble.send_message("Icecast already connected")
+        else:
+            self.icecast.start()
+            self.mumble.set_get_sound(True)
+            self.mumble.send_message("Icecast stream started")
+            audio_file = (
+                files("mumbleice")
+                .joinpath("resources")
+                .joinpath("streaming_started.wav")
+            )
+            with as_file(audio_file) as audio:
+                self.mumble.send_audio(audio.resolve())
+            self.timer.start()
+
+    def disconnect_icecast(self):
+        self.timer.stop()
+        if self.icecast.is_connected:
+            self.icecast.stop()
+            self.mumble.send_message("Icecast streaming stopped")
+            audio_file = (
+                files("mumbleice")
+                .joinpath("resources")
+                .joinpath("streaming_stopped.wav")
+            )
+            with as_file(audio_file) as audio:
+                self.mumble.send_audio(audio.resolve())
+            self.mumble.set_get_sound(False)
+            # Ensure that the timer has stopped and will not restart
+            self.timer.stop()
+        else:
+            self.mumble.send_message("Icecast already disconnected")
+
+    def write_audio(self):
+        try:
+            audio = self.mumble.get_audio(BUFFER_DURATION)
+            self.icecast.write(audio)
+            self.timer.reset()
+        except SilenceError:
+            self.logger.warning(
+                f"No audio received from Mumble for the last {self.mumble.max_silence} s. Disconnecting from Icecast..."
+            )
+            self.mumble.send_message(
+                f"No audio received for the last {self.mumble.max_silence} s. Disconnecting from Icecast..."
+            )
+            self.disconnect_icecast()
+        except BrokenPipeError:
+            self.mumble.send_message(
+                "Icecast disconnected unexpectedly. Streaming stopped"
+            )
+            self.disconnect_icecast()
+
+    def show_icecast_status(self):
+        if self.icecast.is_connected:
+            self.mumble.send_message("Icecast is <b>connected</b> and streaming")
+        else:
+            self.mumble.send_message("Icecast is <b>disconnected</b> and not streaming")
