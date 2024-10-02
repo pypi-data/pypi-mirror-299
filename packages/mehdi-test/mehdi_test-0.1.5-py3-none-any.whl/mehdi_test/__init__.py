5bc631c7cb9e3aa1ad6ec9dfe2848bf38219dec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:08:20 +0000

    Add package mehdi-test/0.1.5/mehdi_test-0.1.5-py3-none-any.whl

---
+++ packages/mehdi-test/mehdi_test-0.1.5-py3-none-any.whl/mehdi_test/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for mehdi_test."""
+
+__author__ = """mehdi hamzeluie"""
+__email__ = 'mehdihamze73@gmail.com'
+__version__ = '0.1.0'
---
+++ packages/mehdi-test/mehdi_test-0.1.5-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for mehdi_test."""
