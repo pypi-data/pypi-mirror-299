d092359567831470e8b080754e49828136dcc075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:09:38 +0000

    Add package mkdocs-strings-replacement/0.0.50/mkdocs_strings_replacement-0.0.50-py3-none-any.whl

---
+++ packages/mkdocs-strings-replacement/mkdocs_strings_replacement-0.0.50-py3-none-any.whl/mkdocs_strings_replacement/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+__all__ = ['StringsReplacement']
+
+from mkdocs_strings_replacement.plugin import StringsMarkdownReplacementPlugin
+
+
+
+# ------------------------ TESTING-----------------
+# # __init__.py
+# from __future__ import annotations
+
+# __all__ = ['StringsReplacement']
+
+# # Import StringsMarkdownReplacementPlugin only when needed
+# def get_plugin():
+#     from .plugin import StringsMarkdownReplacementPlugin
+#     return StringsMarkdownReplacementPlugin
