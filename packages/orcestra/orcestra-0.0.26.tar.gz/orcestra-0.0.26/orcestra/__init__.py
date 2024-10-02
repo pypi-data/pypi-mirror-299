f13dae67ef37f727e0a0eeb5317708732cbd2472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:09:12 +0000

    Add package orcestra/0.0.26/orcestra-0.0.26.tar.gz

---
+++ packages/orcestra/orcestra-0.0.26.tar.gz/orcestra-0.0.26/orcestra/__init__.py
@@ -0,0 +1,17 @@
+from .flightplan import LatLon, bco, sal
+from .utils import parse_datestr
+from .io import read_igi, read_bahamas_100hz
+
+import warnings
+
+# intake_xarray will have a bug and eventually needs an update >= 0.4.1
+# while this update is merged in intake_xarray main, it's not yet released
+# for now, we just silence the warning, as the current implementation still works
+warnings.filterwarnings(
+    action="ignore",
+    message="",
+    category=FutureWarning,
+    module="intake_xarray",
+)
+
+__all__ = ["LatLon", "bco", "sal", "parse_datestr", "read_igi", "read_bahamas_100hz"]
