f71a93b66a3e1c748c5e5fcd6b6cd91782bb5bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 04:04:11 +0000

    Add package whiteduck/0.1.3/whiteduck-0.1.3.tar.gz

---
+++ packages/whiteduck/whiteduck-0.1.3.tar.gz/whiteduck-0.1.3/setup.py
@@ -0,0 +1,31 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="whiteduck",
+    version="0.1.3",
+    author="Andre Ratzenberger",
+    author_email="andre.ratzenberger@whiteduck.com",
+    description="A tool to scaffold projects with Whiteduck's recommended tech stack",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/whiteduck",  # Replace with your repository URL
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "click",  # Include if you're using click
+        # Add other dependencies here
+    ],
+    entry_points={
+        "console_scripts": [
+            "whiteduck=whiteduck.cli:cli",  # Use 'cli' if using click
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
