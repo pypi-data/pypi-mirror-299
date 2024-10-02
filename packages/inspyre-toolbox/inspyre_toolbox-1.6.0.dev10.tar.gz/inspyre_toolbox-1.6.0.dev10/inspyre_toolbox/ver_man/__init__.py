61434e8a445e20cde6cf144620b25b3bf85a4991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 07:04:25 +0000

    Add package inspyre-toolbox/1.6.0.dev10/inspyre_toolbox-1.6.0.dev10.tar.gz

---
+++ packages/inspyre-toolbox/inspyre_toolbox-1.6.0.dev10.tar.gz/inspyre_toolbox-1.6.0.dev10/inspyre_toolbox/ver_man/__init__.py
@@ -0,0 +1,28 @@
+"""
+This module contains classes and functions to instantiate and provide detailed package version information.
+
+Classes:
+    VersionParser:
+        A class to parse and provide detailed version information.
+
+    PyPiVersionInfo:
+        A class to provide version information from PyPi.
+
+Functions:
+    None
+
+Since:
+    v1.6.0
+"""
+
+from inspyre_toolbox.log_engine import ROOT_LOGGER as PARENT_LOGGER
+from inspyre_toolbox.ver_man.classes import PyPiVersionInfo, RELEASE_MAP, VersionParser
+
+MOD_LOGGER = PARENT_LOGGER.get_child('ver_man')
+
+
+__all__ = [
+        'RELEASE_MAP',
+    'VersionParser',
+    'PyPiVersionInfo',
+]
