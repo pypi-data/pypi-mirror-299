c172c0071229747987dab5762a23ea98aee221e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 05:05:26 +0000

    Add package Gugan-77/0.2/Gugan_77-0.2-py3-none-any.whl

---
+++ packages/Gugan-77/Gugan_77-0.2-py3-none-any.whl/Gugan_77/main.py
@@ -0,0 +1,6 @@
+def hello():
+    print('This is Gugan, welcome to my Library...')
+def addition(a, b):
+    a = int(input("Enter value for a :"))
+    b = int(input("Enter value for b :"))
+    print(a+b)
