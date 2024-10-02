e19ddbe1eae047b7ea249dcdc3429288ea96290b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:08:21 +0000

    Add package nio-bot/1.2.0b1/nio_bot-1.2.0b1-py3-none-any.whl

---
+++ packages/nio-bot/nio_bot-1.2.0b1-py3-none-any.whl/niobot/patches/nio__responses.py
@@ -0,0 +1,8 @@
+# OUTDATED AND NOT USED ANYMORE
+# Pull from https://github.com/poljar/matrix-nio/pull/451 temporarily.
+from nio.responses import DirectRoomsErrorResponse, DirectRoomsResponse
+
+__all__ = (
+    "DirectRoomsErrorResponse",
+    "DirectRoomsResponse",
+)
---
+++ packages/nio-bot/nio_bot-1.2.0b1-py3-none-any.whl/niobot/utils/__init__.py
@@ -0,0 +1,9 @@
+from .checks import *
+from .federation import *
+from .help_command import *
+from .lib import *
+from .mentions import Mentions as Mentions
+from .parsers import *
+from .string_view import *
+from .typing import *
+from .unblocking import *
