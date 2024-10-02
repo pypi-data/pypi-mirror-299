07fb295ca7523afa8cd56caaca47b29d409fcf90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 04:04:56 +0000

    Add package julep/1.6.0/julep-1.6.0.tar.gz

---
+++ packages/julep/julep-1.6.0.tar.gz/julep-1.6.0/src/julep/types/agents/tool_patch_params.py
@@ -0,0 +1,55 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Union, Optional
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["ToolPatchParams", "Function", "Integration", "System"]
+
+
+class ToolPatchParams(TypedDict, total=False):
+    agent_id: Required[str]
+
+    function: Optional[Function]
+    """Function definition"""
+
+    integration: Optional[Integration]
+    """Integration definition"""
+
+    name: Optional[str]
+
+    system: Optional[System]
+    """System definition"""
+
+    type: Literal["function", "integration", "system", "api_call"]
+
+
+class Function(TypedDict, total=False):
+    description: Optional[str]
+
+    name: Optional[object]
+
+    parameters: Optional[object]
+
+
+class Integration(TypedDict, total=False):
+    arguments: Optional[object]
+
+    description: Optional[str]
+
+    method: Optional[str]
+
+    provider: Union[
+        Literal["dummy", "hacker_news", "weather", "wikipedia", "spider", "brave", "browserbase"], str, None
+    ]
+
+    setup: Optional[object]
+
+
+class System(TypedDict, total=False):
+    arguments: Optional[object]
+
+    call: Optional[str]
+
+    description: Optional[str]
