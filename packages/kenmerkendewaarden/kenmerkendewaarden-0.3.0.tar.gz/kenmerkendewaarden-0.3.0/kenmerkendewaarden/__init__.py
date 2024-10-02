699cffa88bcbb912afbbf885804336d7c230e762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 04:04:06 +0000

    Add package kenmerkendewaarden/0.3.0/kenmerkendewaarden-0.3.0.tar.gz

---
+++ packages/kenmerkendewaarden/kenmerkendewaarden-0.3.0.tar.gz/kenmerkendewaarden-0.3.0/kenmerkendewaarden/__init__.py
@@ -0,0 +1,15 @@
+"""
+Derive indicators from waterlevel measurements
+"""
+
+__author__ = """Jelmer Veenstra"""
+__email__ = "jelmer.veenstra@deltares.nl"
+__version__ = "0.3.0"
+
+from kenmerkendewaarden.data_retrieve import *
+from kenmerkendewaarden.data_analysis import *
+from kenmerkendewaarden.slotgemiddelden import *
+from kenmerkendewaarden.havengetallen import *
+from kenmerkendewaarden.gemiddeldgetij import *
+from kenmerkendewaarden.overschrijding import *
+from kenmerkendewaarden.tidalindicators import *
