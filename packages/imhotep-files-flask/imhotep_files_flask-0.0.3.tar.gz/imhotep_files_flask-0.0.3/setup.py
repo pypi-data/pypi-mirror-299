57b130cd127c058a09224f6f6369529b9cbbeca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 07:04:17 +0000

    Add package imhotep-files-flask/0.0.3/imhotep_files_flask-0.0.3.tar.gz

---
+++ packages/imhotep-files-flask/imhotep_files_flask-0.0.3.tar.gz/imhotep_files_flask-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='imhotep_files_flask',
+  version='0.0.3',
+  description='A Flask library for secure file uploads and deletions',
+  long_description=open('README.md').read(),
+  url='https://github.com/Imhotep-Tech/imhotep_files_flask',  
+  author="Karim Bassem",
+  author_email="imhoteptech@outlook.com",
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='files_flask', 
+  packages=find_packages(),
+  install_requires=['werkzeug'],
+  long_description_content_type="text/markdown"
+)
