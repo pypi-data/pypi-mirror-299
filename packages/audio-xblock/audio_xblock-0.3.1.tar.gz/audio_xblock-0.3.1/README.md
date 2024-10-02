c9433a0cbafec125e615a604049eb3e994827f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 04:05:26 +0000

    Add package audio-xblock/0.3.1/audio_xblock-0.3.1.tar.gz

---
+++ packages/audio-xblock/audio_xblock-0.3.1.tar.gz/audio_xblock-0.3.1/README.md
@@ -0,0 +1,9 @@
+AudioXBlock
+===========
+
+This is a simple XBlock which will play audio files as an HTML5 audio
+element. If unavailable, it will fall back to an embed element.
+
+Usage: 
+
+    <audio src="http://server.tld/static/song.mp3" />
---
+++ packages/audio-xblock/audio_xblock-0.3.1.tar.gz/audio_xblock-0.3.1/setup.py
@@ -0,0 +1,109 @@
+"""Setup for audio XBlock."""
+
+
+import os
+import re
+
+from setuptools import setup
+
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+
+def package_data(pkg, roots):
+    """
+    Generic function to find package_data.
+
+    All files under each of the `roots` will be declared as package data for package `pkg`.
+    """
+    data = []
+    for root in roots:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+    return {pkg: data}
+
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+
+    Requirements will include any constraints from files specified
+    with -c in the requirements files.
+    Returns a list of requirement strings.
+    """
+    # UPDATED VIA SEMGREP - if you need to remove/modify this method remove this line and add a comment specifying why.
+
+    requirements = {}
+    constraint_files = set()
+
+    # groups "my-package-name<=x.y.z,..." into ("my-package-name", "<=x.y.z,...")
+    requirement_line_regex = re.compile(r"([a-zA-Z0-9-_.]+)([<>=][^#\s]+)?")
+
+    def add_version_constraint_or_raise(current_line, current_requirements, add_if_not_present):
+        regex_match = requirement_line_regex.match(current_line)
+        if regex_match:
+            package = regex_match.group(1)
+            version_constraints = regex_match.group(2)
+            existing_version_constraints = current_requirements.get(package, None)
+            # it's fine to add constraints to an unconstrained package, but raise an error if there are already
+            # constraints in place
+            if existing_version_constraints and existing_version_constraints != version_constraints:
+                raise BaseException(f'Multiple constraint definitions found for {package}:'
+                                    f' "{existing_version_constraints}" and "{version_constraints}".'
+                                    f'Combine constraints into one location with {package}'
+                                    f'{existing_version_constraints},{version_constraints}.')
+            if add_if_not_present or package in current_requirements:
+                current_requirements[package] = version_constraints
+
+    # process .in files and store the path to any constraint files that are pulled in
+    for path in requirements_paths:
+        with open(path) as reqs:
+            for line in reqs:
+                if is_requirement(line):
+                    add_version_constraint_or_raise(line, requirements, True)
+                if line and line.startswith('-c') and not line.startswith('-c http'):
+                    constraint_files.add(os.path.dirname(path) + '/' + line.split('#')[0].replace('-c', '').strip())
+
+    # process constraint files and add any new constraints found to existing requirements
+    for constraint_file in constraint_files:
+        with open(constraint_file) as reader:
+            for line in reader:
+                if is_requirement(line):
+                    add_version_constraint_or_raise(line, requirements, False)
+
+    # process back into list of pkg><=constraints strings
+    constrained_requirements = [f'{pkg}{version or ""}' for (pkg, version) in sorted(requirements.items())]
+    return constrained_requirements
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement.
+
+    Returns:
+        bool: True if the line is not blank, a comment,
+        a URL, or an included file
+    """
+    # UPDATED VIA SEMGREP - if you need to remove/modify this method remove this line and add a comment specifying why
+
+    return line and line.strip() and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+
+setup(
+    name='audio-xblock',
+    version='0.3.1',
+    description='Audio XBlock, to play audio files in the course',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    packages=[
+        'audio',
+    ],
+    install_requires=load_requirements('requirements/base.in'),
+    entry_points={
+        'xblock.v1': [
+            'audio = audio:AudioXBlock',
+        ]
+    },
+    package_data=package_data("audio", ["static", "templates", "translations"]),
+)
