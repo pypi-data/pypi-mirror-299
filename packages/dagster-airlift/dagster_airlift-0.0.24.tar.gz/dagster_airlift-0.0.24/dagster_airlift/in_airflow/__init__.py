acfd41d9db018d0e2a5fcf7dfaab0e805cae68bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 06:05:33 +0000

    Add package dagster-airlift/0.0.24/dagster_airlift-0.0.24.tar.gz

---
+++ packages/dagster-airlift/dagster_airlift-0.0.24.tar.gz/dagster_airlift-0.0.24/dagster_airlift/in_airflow/__init__.py
@@ -0,0 +1,5 @@
+from .base_proxy_operator import (
+    BaseProxyToDagsterOperator as BaseProxyToDagsterOperator,
+    DefaultProxyToDagsterOperator as DefaultProxyToDagsterOperator,
+)
+from .proxying_fn import proxying_to_dagster as proxying_to_dagster
