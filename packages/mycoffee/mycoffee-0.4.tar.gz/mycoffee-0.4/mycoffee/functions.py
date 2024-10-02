7752c3be4829614f1aab7fa711df39de234678eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:09:15 +0000

    Add package mycoffee/0.4/mycoffee-0.4.tar.gz

---
+++ packages/mycoffee/mycoffee-0.4.tar.gz/mycoffee-0.4/mycoffee/functions.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""mycoffee functions."""
+from mycoffee.params import MESSAGE_TEMPLATE, METHODS_LIST_TEMPLATE, EMPTY_INFO
+from mycoffee.params import MY_COFFEE_VERSION, DEFAULT_PARAMS, METHODS_MAP
+from art import tprint
+
+
+def is_int(number):
+    """
+    Check that input number is int or not.
+
+    :param number: input number
+    :type number: float or int
+    :return: result as bool
+    """
+    if int(number) == number:
+        return True
+    return False
+
+
+def print_message(params):
+    """
+    Print message.
+
+    :param params: parameters
+    :type params: dict
+    :return: None
+    """
+    tprint("MyCoffee", font="bulbhead")
+    print(
+        MESSAGE_TEMPLATE.format(
+            params["method"],
+            params["cups"],
+            params["coffee"],
+            params["water"],
+            params["coffee_ratio"],
+            params["water_ratio"],
+            params["info"]))
+
+
+def load_method_params(method_name):
+    """
+    Load method params.
+
+    :param method_name: method name
+    :type method_name: str
+    :return: method params as dict
+    """
+    method_params = dict()
+    for item in DEFAULT_PARAMS:
+        if item in METHODS_MAP[method_name]:
+            method_params[item] = METHODS_MAP[method_name][item]
+        else:
+            method_params[item] = DEFAULT_PARAMS[item]
+    return method_params
+
+
+def show_methods_list():
+    """
+    Show methods list.
+
+    :return: None
+    """
+    print("Methods list:\n")
+    for i, method in enumerate(sorted(METHODS_MAP), 1):
+        print(
+            METHODS_LIST_TEMPLATE.format(
+                i,
+                method,
+                METHODS_MAP[method]['info']))
+
+
+def load_params(args):
+    """
+    Load params.
+
+    :param args: input arguments
+    :type args: argparse.Namespace
+    :return: params as dict
+    """
+    params = load_method_params(args.method)
+    for item in params:
+        if getattr(args, item) is not None:
+            params[item] = getattr(args, item)
+    params["method"] = args.method
+    return params
+
+
+def filter_params(params):
+    """
+    Filter params.
+
+    :param params: parameters
+    :type params: dict
+    :return: filtered parameters as dict
+    """
+    digits = params["digits"]
+    params["coffee"] = round(params["coffee"], digits)
+    if is_int(params["coffee"]):
+        params["coffee"] = int(params["coffee"])
+    if is_int(params["water_ratio"]):
+        params["water_ratio"] = int(params["water_ratio"])
+    if is_int(params["coffee_ratio"]):
+        params["coffee_ratio"] = int(params["coffee_ratio"])
+    if is_int(params["water"]):
+        params["water"] = int(params["water"])
+    if len(params["info"]) == 0:
+        params["info"] = EMPTY_INFO
+    return params
+
+
+def calc_coffee(params):
+    """
+    Calculate coffee.
+
+    :param params: parameters
+    :type params: dict
+    :return: coffee amount as float
+    """
+    coffee = params["water"] * params["coffee_ratio"] / params["water_ratio"]
+    return coffee
+
+
+def run(args):
+    """
+    Run program.
+
+    :param args: input arguments
+    :type args: argparse.Namespace
+    :return: None
+    """
+    if args.version:
+        print(MY_COFFEE_VERSION)
+    elif args.methods_list:
+        show_methods_list()
+    else:
+        params = load_params(args)
+        params["coffee"] = calc_coffee(params)
+        params = filter_params(params)
+        print_message(params)
---
+++ packages/mycoffee/mycoffee-0.4.tar.gz/mycoffee-0.4/mycoffee/params.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""mycoffee params."""
+
+MY_COFFEE_VERSION = "0.4"
+INPUT_ERROR_MESSAGE = "[Error] Wrong input"
+INPUT_EXAMPLE = "Example: mycoffee --method=v60"
+EXIT_MESSAGE = "See you. Bye!"
+EMPTY_INFO = "Nothing :)"
+MESSAGE_TEMPLATE = """
+
+Method: `{0}`
+
+Cups: {1}
+
+Coffee: {2} gr
+
+Water: {3} gr
+
+Ratio: {4}/{5}
+
+Info: {6}
+"""
+
+METHODS_LIST_TEMPLATE = "{0}. `{1}` - {2}"
+
+
+DEFAULT_PARAMS = {
+    "cups": 1,
+    "water": 0,
+    "coffee_ratio": 1,
+    "water_ratio": 1,
+    "digits": 3,
+    "info": ""
+
+}
+
+METHODS_MAP = {
+    "custom": {
+        "coffee_ratio": 1,
+        "water_ratio": 17,
+        "water": 240,
+        "info": "Custom brewing method"
+    },
+    "v60": {
+        "coffee_ratio": 3,
+        "water_ratio": 50,
+        "water": 250,
+        "info": "V60 method"
+    },
+    "espresso": {
+        "coffee_ratio": 1,
+        "water_ratio": 2,
+        "water": 36,
+        "info": "Espresso method"
+    },
+    "ristretto": {
+        "coffee_ratio": 1,
+        "water_ratio": 1,
+        "water": 18,
+        "info": "Ristretto method"
+    },
+    "lungo": {
+        "coffee_ratio": 1,
+        "water_ratio": 4,
+        "water": 72,
+        "info": "Lungo method"
+    },
+    "chemex": {
+        "coffee_ratio": 1,
+        "water_ratio": 15,
+        "water": 240,
+        "info": "Chemex method"
+    },
+    "french-press": {
+        "coffee_ratio": 1,
+        "water_ratio": 15,
+        "water": 120,
+        "info": "French press method"
+    },
+    "siphon": {
+        "coffee_ratio": 1,
+        "water_ratio": 15,
+        "water": 240,
+        "info": "Siphon method"
+    },
+    "pour-over": {
+        "coffee_ratio": 1,
+        "water_ratio": 15,
+        "water": 240,
+        "info": "Pour-over method"
+    },
+    "auto-drip": {
+        "coffee_ratio": 1,
+        "water_ratio": 16,
+        "water": 128,
+        "info": "Auto drip method"
+    },
+    "cold-brew": {
+        "coffee_ratio": 1,
+        "water_ratio": 11,
+        "water": 242,
+        "info": "Cold brew method"
+    },
+    "cold-brew-conc": {
+        "coffee_ratio": 1,
+        "water_ratio": 5,
+        "water": 120,
+        "info": "Cold brew concentrate method"
+    },
+    "moka-pot": {
+        "coffee_ratio": 1,
+        "water_ratio": 10,
+        "water": 60,
+        "info": "Moka pot method"
+    },
+    "turkish": {
+        "coffee_ratio": 1,
+        "water_ratio": 10,
+        "water": 50,
+        "info": "Turkish method"
+    },
+    "cupping": {
+        "coffee_ratio": 11,
+        "water_ratio": 200,
+        "water": 150,
+        "info": "Cupping method"
+    },
+    "aero-press": {
+        "coffee_ratio": 1,
+        "water_ratio": 15,
+        "water": 135,
+        "info": "AeroPress standard method"
+    },
+    "aero-press-conc": {
+        "coffee_ratio": 1,
+        "water_ratio": 6,
+        "water": 90,
+        "info": "AeroPress concentrate method"
+    },
+    "aero-press-inv": {
+        "coffee_ratio": 1,
+        "water_ratio": 12,
+        "water": 132,
+        "info": "AeroPress inverted method"
+    },
+    "steep-and-release": {
+        "coffee_ratio": 1,
+        "water_ratio": 16,
+        "water": 255,
+        "info": "Steep-and-release method"
+    }
+}
