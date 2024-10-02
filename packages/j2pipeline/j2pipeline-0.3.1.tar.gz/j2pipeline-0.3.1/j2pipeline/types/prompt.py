bf9322b6aa56cb2bff94d862b6f23be7d6a36723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 05:04:37 +0000

    Add package j2pipeline/0.3.1/j2pipeline-0.3.1.tar.gz

---
+++ packages/j2pipeline/j2pipeline-0.3.1.tar.gz/j2pipeline-0.3.1/j2pipeline/types/prompt.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+from j2pipeline.template import assemble
+from j2pipeline.tcp import client
+from typing import Callable, NoReturn
+
+@dataclass(slots=True)
+class Prompt[T]:
+    path: str
+    process: Callable[[str], T] = field(default=lambda response: response)
+    auto_upper: bool = field(default=True)
+
+    def __call__(self, **subs: str) -> T:
+        if self.auto_upper:
+            subs = { key.upper(): value for key, value in subs.items() }
+        prompt: str = assemble(path=self.path, subs=subs)
+        with client() as clt:
+            return clt.send(prompt)
