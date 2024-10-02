db814853da67d86c8f4e51177aaad647d471eda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:09:13 +0000

    Add package iati-sphinx-theme/1.4.0/iati_sphinx_theme-1.4.0.tar.gz

---
+++ packages/iati-sphinx-theme/iati_sphinx_theme-1.4.0.tar.gz/iati_sphinx_theme-1.4.0/iati_sphinx_theme/__init__.py
@@ -0,0 +1,11 @@
+"""A sphinx theme for IATI documentation sites."""
+
+from os import path
+
+import sphinx.application
+
+
+def setup(app: sphinx.application.Sphinx) -> None:
+    app.add_html_theme("iati_sphinx_theme", path.abspath(path.dirname(__file__)))
+    app.config["html_permalinks_icon"] = "#"
+    app.config["html_favicon"] = "static/favicon-16x16.png"
