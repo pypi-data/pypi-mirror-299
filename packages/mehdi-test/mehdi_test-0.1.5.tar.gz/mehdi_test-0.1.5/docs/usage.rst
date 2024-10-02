d2d297b5b3ae1587a564b79567b44ec78a928779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:08:20 +0000

    Add package mehdi-test/0.1.5/mehdi_test-0.1.5.tar.gz

---
+++ packages/mehdi-test/mehdi_test-0.1.5.tar.gz/mehdi_test-0.1.5/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use mehdi_test in a project::
+
+    import mehdi_test
---
+++ packages/mehdi-test/mehdi_test-0.1.5.tar.gz/mehdi_test-0.1.5/mehdi_test/mehdi_test.py
@@ -0,0 +1,11 @@
+from src.utils import subtract
+
+
+def add_tow_num(a, b):
+    return a + b
+
+class Mat:
+    def mult(self, a, b):
+        return a*b
+    def sub(self, a, b):
+        return subtract(a, b)
