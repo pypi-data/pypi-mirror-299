97be6a3c4d139d024751560391fe94345135326c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 05:04:43 +0000

    Add package josecompiler/0.1/josecompiler-0.1.tar.gz

---
+++ packages/josecompiler/josecompiler-0.1.tar.gz/josecompiler-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='josecompiler',
+    version='0.1',
+    packages=find_packages(),
+    description='Um projeto simples de compilador.',
+    author='Jose Eduardo',
+    author_email='josedumoura@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
