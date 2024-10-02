e5920bed4dbd5246e74c4f9f909bb633c4bdcd60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 06:05:13 +0000

    Add package vinmap/1.0.6/vinmap-1.0.6.tar.gz

---
+++ packages/vinmap/vinmap-1.0.6.tar.gz/vinmap-1.0.6/vinmap/utils/html_utils.py
@@ -0,0 +1,27 @@
+# ./utils./html_utils.py 
+
+import subprocess
+from pathlib import Path
+
+BOLD = '\033[1;37m'
+LINK = '\033[4;34m'
+ORANGE = '\033[1;31m'
+END = '\033[0m'
+
+def generate_html_report(output_file):
+    home = str(Path.home())
+    output_file = str(output_file)
+    output_rm_path = output_file.split('/')[-1].split('.')[0]
+    
+    xml_file = output_rm_path + '.xml'
+    html_file = output_rm_path + '.html' 
+    
+    html_dir = str(Path(__file__).parent.parent) + '/scan-results/' + 'html'
+    
+    xml_path = Path(home + '/NMAP/' + xml_file)
+    html_path = Path(html_dir + '/' + html_file)
+
+    html_report = subprocess.run(['xsltproc', xml_path, '-o', html_path])
+    
+    print(f'{BOLD}HTML Report saved to:{END}\n{LINK}{html_path}{END}')
+    return html_file
