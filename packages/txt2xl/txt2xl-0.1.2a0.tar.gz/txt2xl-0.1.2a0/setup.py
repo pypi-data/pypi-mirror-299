6b063f176351a881ccc5afeefad4caa55aa1fb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:09:05 +0000

    Add package txt2xl/0.1.2a0/txt2xl-0.1.2a0.tar.gz

---
+++ packages/txt2xl/txt2xl-0.1.2a0.tar.gz/txt2xl-0.1.2a0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="txt2xl",
+    version="0.1.2-alpha",
+    description="Text classification Python functions for txt2xl",
+    author="alfiinyang",
+    author_email="alfiinyang@gmail.com",
+    packages=find_packages(),
+    #package_dir={'': 'txt2xl/txt2xl'},
+    install_requires=[
+        "transformers==4.44.2",
+        "torch==2.4.1+cu121",
+        "pandas==2.1.4",
+        "requests==2.32.3",
+    ],
+    python_requires='>=3.6',
+)
