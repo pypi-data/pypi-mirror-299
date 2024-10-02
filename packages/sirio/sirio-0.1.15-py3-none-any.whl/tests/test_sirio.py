84246e957664d55ea4e31144352fb24c0333a0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 07:04:29 +0000

    Add package sirio/0.1.15/sirio-0.1.15-py3-none-any.whl

---
+++ packages/sirio/sirio-0.1.15-py3-none-any.whl/tests/test_sirio.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `sirio` package."""
+
+
+import unittest
+
+from sirio import event
+
+
+class TestSirio(unittest.TestCase):
+    """Tests for `sirio` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
