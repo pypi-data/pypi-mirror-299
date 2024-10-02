3f99c16beb753ceaea094efc628885aa18f862ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 07:05:18 +0000

    Add package utsam/0.1.18/utsam-0.1.18.tar.gz

---
+++ packages/utsam/utsam-0.1.18.tar.gz/utsam-0.1.18/utsam/dbs/schemas/transactions/assignment_criteria_student_scores.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class PgAssignmentCriteriaStudentScoresTable:
+    name: str = "assignment_criteria_student_scores"
+    criteria_student_score_id: str = 'criteria_student_score_id'
+    criteria_student_score: str = 'criteria_student_score'
+    criteria_student_score_comments: str = 'criteria_student_score_comments'
+    criteria_student_mark: str = 'criteria_student_mark'
+
