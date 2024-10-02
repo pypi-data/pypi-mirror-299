74838dbde5fef3a91c0af2370c9cdc815abc691f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 05:05:08 +0000

    Add package pymdown-extensions/10.11.2/pymdown_extensions-10.11.2.tar.gz

---
+++ packages/pymdown-extensions/pymdown_extensions-10.11.2.tar.gz/pymdown_extensions-10.11.2/tests/test_extensions/test_emoji.py
@@ -0,0 +1,71 @@
+"""Test cases for Highlight."""
+from .. import util
+import pytest
+import pymdownx.emoji as emoji
+import markdown
+
+
+def _old_style_index():
+    """Custom index with no arguments (old style)."""
+
+    return emoji.twemoji({}, None)
+
+
+def _new_style_index(options, md):
+    """Custom index with arguments (new style)."""
+
+    index = emoji.twemoji({}, None)
+
+    if 'append_alias' in options:
+        for alias, original in options['append_alias']:
+            index['aliases'][alias] = original
+
+    return index
+
+
+class TestEmojiOldIndex(util.MdCase):
+    """Test old style index."""
+
+    extension = [
+        'pymdownx.emoji'
+    ]
+    extension_configs = {
+        'pymdownx.emoji': {
+            'emoji_index': _old_style_index
+        }
+    }
+
+    @pytest.mark.filterwarnings("ignore")
+    def test_old_index(self):
+        """Test that index works."""
+
+        with pytest.warns(DeprecationWarning):
+            self.assertEqual(
+                markdown.Markdown(
+                    extensions=self.extension,
+                    extension_configs=self.extension_configs
+                ).convert(':smile:'),
+                '<p><img alt="\U0001f604" class="twemoji" src="https://cdn.jsdelivr.net/gh/jdecked/twemoji@15.0.3/assets/72x72/1f604.png" title=":smile:" /></p>'  # noqa: E501
+            )
+
+
+class TestEmojiNewIndex(util.MdCase):
+    """Test new style index."""
+
+    extension = [
+        'pymdownx.emoji'
+    ]
+    extension_configs = {
+        'pymdownx.emoji': {
+            'emoji_index': _new_style_index,
+            'options': {'append_alias': [(':grin:', ":smile:")]}
+        }
+    }
+
+    def test_new_index(self):
+        """Test that we can pass options using the new style."""
+
+        self.check_markdown(
+            ':grin:',
+            '<p><img alt="\U0001f604" class="twemoji" src="https://cdn.jsdelivr.net/gh/jdecked/twemoji@15.0.3/assets/72x72/1f604.png" title=":grin:" /></p>'  # noqa: E501
+        )
