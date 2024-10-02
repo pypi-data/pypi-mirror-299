9980588936e99919fe1cef26660b1c718db62d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:09:47 +0000

    Add package libdc3/0.6.0/libdc3-0.6.0.tar.gz

---
+++ packages/libdc3/libdc3-0.6.0.tar.gz/libdc3-0.6.0/libdc3/methods/t0_actions.py
@@ -0,0 +1,11 @@
+from ..config import dc3_config
+from ..services.t0.client import T0WM
+
+
+class T0Actions:
+    def __init__(self):
+        self.client = T0WM(dc3_config.AUTH_CERT, dc3_config.AUTH_CERT_KEY)
+
+    def eras_history(self, era: str):
+        eras = self.client.get_era_history(era=era)
+        return sorted(eras["result"], key=lambda x: x["era"])
