a49e9900eb8b99e6f02e7e328a6554db9c4d9894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 04:04:57 +0000

    Add package reddit-recs-updater/0.1.0/reddit_recs_updater-0.1.0-py3-none-any.whl

---
+++ packages/reddit-recs-updater/reddit_recs_updater-0.1.0-py3-none-any.whl/pipeline/archived/gen_use_cases.py
@@ -0,0 +1,23 @@
+import json
+from utils.ai_handler import get_openai_response
+
+# Product category (str) -> Dict of use cases (dict)
+async def gen_use_cases(product_category: str) -> tuple[dict, list[str]]:
+  print(f'Generating use cases for {product_category}...')
+
+  model = 'gpt-4o-2024-08-06'
+  response_format = { "type": "json_object" }
+  with open('llm_prompts/gen_use_cases_sys.txt', 'r') as file:
+    sys_prompt = file.read()
+  with open('llm_prompts/gen_use_cases_user.txt', 'r') as file:
+    user_prompt = file.read()
+  user_prompt = user_prompt.replace('{product_category}', product_category)
+  response = await get_openai_response(sys_prompt, user_prompt, model, response_format, 'gen_use_cases')
+  
+  use_cases_dict = response['Usage scenarios']
+  use_cases_names = list(use_cases_dict.keys())
+
+  print(json.dumps(use_cases_dict, indent=2))
+  print(f'--> Use cases generated: {use_cases_names}')
+  
+  return use_cases_dict, use_cases_names
