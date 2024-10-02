0dd75a486004b18a10fa3faf0cdbbe6a2863ec3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:10:14 +0000

    Add package jaxns/2.6.3/jaxns-2.6.3.tar.gz

---
+++ packages/jaxns/jaxns-2.6.3.tar.gz/jaxns-2.6.3/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages
+from setuptools import setup
+
+install_requires = [
+    'jax>=0.4.25',
+    'jaxlib',
+    'matplotlib',
+    'numpy<2',
+    'scipy',
+    'tensorflow_probability',
+    'tqdm',
+    'optax',
+    'jaxopt'
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='jaxns',
+      version='2.6.3',
+      description='Nested Sampling in JAX',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/joshuaalbert/jaxns",
+      author='Joshua G. Albert',
+      author_email='albert@strw.leidenuniv.nl',
+      install_requires=install_requires,
+      tests_require=[
+          'pytest>=2.8',
+      ],
+      package_dir={'': './'},
+      packages=find_packages('./'),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.9',
+      )
