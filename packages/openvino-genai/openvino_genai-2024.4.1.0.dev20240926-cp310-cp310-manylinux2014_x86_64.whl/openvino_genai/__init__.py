296040630c701d13cafdb5b3d30e9a4eeaea389e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:10:12 +0000

    Add package openvino-genai/2024.4.1.0.dev20240926/openvino_genai-2024.4.1.0.dev20240926-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/openvino-genai/openvino_genai-2024.4.1.0.dev20240926-cp310-cp310-manylinux2014_x86_64.whl/openvino_genai/__init__.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+"""openvino genai module namespace, exposing pipelines and configs to create these pipelines."""
+
+import openvino  # add_dll_directory for openvino lib
+import os
+from .__version__ import __version__
+
+
+if hasattr(os, "add_dll_directory"):
+    os.add_dll_directory(os.path.dirname(__file__))
+
+from .py_generate_pipeline import (
+    ContinuousBatchingPipeline,
+    DecodedResults, 
+    EncodedResults, 
+    GenerationConfig, 
+    GenerationResult,
+    LLMPipeline, 
+    PerfMetrics,
+    RawPerfMetrics,
+    SchedulerConfig,
+    StopCriteria,
+    StreamerBase, 
+    TokenizedInputs,
+    Tokenizer
+)
