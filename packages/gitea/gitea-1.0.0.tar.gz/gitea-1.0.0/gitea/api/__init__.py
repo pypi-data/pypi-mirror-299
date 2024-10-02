7bc355e405e5e6d5ba0c71e8a64507bfa10a4f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 05:04:18 +0000

    Add package gitea/1.0.0/gitea-1.0.0.tar.gz

---
+++ packages/gitea/gitea-1.0.0.tar.gz/gitea-1.0.0/gitea/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from gitea.api.activitypub_api import ActivitypubApi
+from gitea.api.admin_api import AdminApi
+from gitea.api.issue_api import IssueApi
+from gitea.api.miscellaneous_api import MiscellaneousApi
+from gitea.api.notification_api import NotificationApi
+from gitea.api.organization_api import OrganizationApi
+from gitea.api.package_api import PackageApi
+from gitea.api.repository_api import RepositoryApi
+from gitea.api.settings_api import SettingsApi
+from gitea.api.user_api import UserApi
