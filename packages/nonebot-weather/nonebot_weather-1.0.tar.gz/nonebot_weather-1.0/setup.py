7a3bd3bf8cdab903c3557da3a5e11acfaa409be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:09:17 +0000

    Add package nonebot-weather/1.0/nonebot_weather-1.0.tar.gz

---
+++ packages/nonebot-weather/nonebot_weather-1.0.tar.gz/nonebot_weather-1.0/setup.py
@@ -0,0 +1,36 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi (if you dont need delete it)
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+# you need to change all these
+VERSION = '1.0.1'
+DESCRIPTION = '一个以nonebot为框架的天气查询插件'
+LONG_DESCRIPTION = ''
+
+setup(
+    name="nonebot-weather",
+    version=VERSION,
+    author="hriyel",
+    author_email="1249781871@qq.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/hriyel/nonebot-weather.git",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'menu', 'dumb_menu','windows','mac','linux'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
