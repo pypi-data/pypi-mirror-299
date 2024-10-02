76b888863c125993ff715b6802bfe8c147804d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 05:05:27 +0000

    Add package Gugan-unique-lib/0.2/Gugan_unique_lib-0.2-py3-none-any.whl

---
+++ packages/Gugan-unique-lib/Gugan_unique_lib-0.2-py3-none-any.whl/Gugan_unique_lib/main.py
@@ -0,0 +1,6 @@
+def hello():
+    print('This is Gugan, welcome to my Library...')
+def addition():
+    a = int(input("Enter value for a :"))
+    b = int(input("Enter value for b :"))
+    print(a+b)
