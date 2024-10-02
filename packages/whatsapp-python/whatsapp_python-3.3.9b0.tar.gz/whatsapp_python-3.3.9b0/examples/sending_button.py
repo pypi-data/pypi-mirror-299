4cc3124c9f54c6704bc1c32de69789807a0ae2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 07:05:18 +0000

    Add package whatsapp-python/3.3.9b0/whatsapp_python-3.3.9b0.tar.gz

---
+++ packages/whatsapp-python/whatsapp_python-3.3.9b0.tar.gz/whatsapp_python-3.3.9b0/examples/sending_button.py
@@ -0,0 +1,33 @@
+from os import getenv
+from whatsapp import WhatsApp
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    load_dotenv()
+    messenger = WhatsApp(token=getenv("TOKEN"),
+                         phone_number_id=getenv("PHONE_NUMBER_ID"))
+
+    response = messenger.send_button(
+        recipient_id="255757xxxxxx",
+        button={
+            "header": "Header Testing",
+            "body": "Body Testing",
+            "footer": "Footer Testing",
+            "action": {
+                "button": "Button Testing",
+                "sections": [
+                    {
+                        "title": "iBank",
+                        "rows": [
+                            {"id": "row 1", "title": "Send Money", "description": ""},
+                            {
+                                "id": "row 2",
+                                "title": "Withdraw money",
+                                "description": "",
+                            },
+                        ],
+                    }
+                ],
+            },
+        },
+    )
