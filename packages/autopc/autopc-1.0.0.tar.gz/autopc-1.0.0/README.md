b998dc404a515c5f438c75dcc0bec578fd2d8826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 06:04:42 +0000

    Add package autopc/1.0.0/autopc-1.0.0.tar.gz

---
+++ packages/autopc/autopc-1.0.0.tar.gz/autopc-1.0.0/README.md
@@ -0,0 +1,4 @@
+# autopc
+**An image recognition framework running on a computer**
+
+**运行在电脑上的图像识别框架** （[中文版点这里](./README_zh.md)）
---
+++ packages/autopc/autopc-1.0.0.tar.gz/autopc-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='autopc',
+    version='v1.0.0',
+    packages=['basic'],
+    url='http://github.com/orcakill/autopc',
+    license='Apache',
+    author='orcakill',
+    author_email='orcakill@dingtalk.com',
+    description='基于opencv的图像识别框架'
+)
