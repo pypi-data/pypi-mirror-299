4a250f835acdace82f78f56a8a767b0cd4799783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 04:04:57 +0000

    Add package joseclasses/0.1/joseclasses-0.1.tar.gz

---
+++ packages/joseclasses/joseclasses-0.1.tar.gz/joseclasses-0.1/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='joseclasses',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple project for demonstration',
+    author='Jose Eduardo',
+    author_email='josedumoura@gmail.com',
+)
