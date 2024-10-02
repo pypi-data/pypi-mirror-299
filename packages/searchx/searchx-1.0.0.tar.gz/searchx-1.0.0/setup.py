a834affc7d5fdfd1e8f59d69ea20ab9e1d0713c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 05:04:27 +0000

    Add package searchx/1.0.0/searchx-1.0.0.tar.gz

---
+++ packages/searchx/searchx-1.0.0.tar.gz/searchx-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='searchx',
+	version='1.0.0',
+	packages=find_packages(),
+	install_requires=[
+		"cloudscraper",
+		"httpx",
+		"bs4"
+	],
+	author='RedPiar',
+	author_email='Regeonwix@gmail.com',
+	description='SearchX is a powerful library for convenient searching.',
+	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
+	url='https://github.com/RedPiarOfficial/searchx/',
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+	python_requires='>=3.8',
+)
