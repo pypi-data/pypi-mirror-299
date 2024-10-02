43389446f4b3d3181462727c229ad99024726c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 06:05:15 +0000

    Add package proof-business-api/0.1.0/proof_business_api-0.1.0.tar.gz

---
+++ packages/proof-business-api/proof_business_api-0.1.0.tar.gz/proof_business_api-0.1.0/proof_business_api/types.py
@@ -0,0 +1,4 @@
+from typing import Dict, Union, List, Any
+
+JsonValue = Union[str, int, float, Dict, List[Dict]]
+JsonObj = Dict[str, JsonValue]
---
+++ packages/proof-business-api/proof_business_api-0.1.0.tar.gz/proof_business_api-0.1.0/proof_business_api/watch.py
@@ -0,0 +1,5 @@
+from watchfiles import run_process
+
+
+def main() -> None:
+    run_process("./proof_business_api", "./tests", target="poetry run pytest -rP")
