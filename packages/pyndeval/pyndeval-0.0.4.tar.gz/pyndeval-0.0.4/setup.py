d83f23ca51ef001f799610957217413ef9807f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 04:05:24 +0000

    Add package pyndeval/0.0.4/pyndeval-0.0.4.tar.gz

---
+++ packages/pyndeval/pyndeval-0.0.4.tar.gz/pyndeval-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from glob import glob
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyndeval",
+    version="0.0.4",
+    author="Sean MacAvaney",
+    author_email="sean.macavaney@glasgow.ac.uk",
+    description="Interface to ndeval.c",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seanmacavaney/pyndeval",
+    include_package_data = True,
+    packages=setuptools.find_packages(include=['pyndeval']),
+    install_requires=[],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+    ext_modules=[setuptools.Extension("_pyndeval", ["src/pyndeval.c"], extra_compile_args=['-std=c99'])],
+)
