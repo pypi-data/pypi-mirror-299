c4498b63f8ebd84cafbb82bd42c527190508f108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 03:09:35 +0000

    Add package vizro/0.1.24/vizro-0.1.24-py3-none-any.whl

---
+++ packages/vizro/vizro-0.1.24-py3-none-any.whl/vizro/static/js/models/dashboard.js
@@ -0,0 +1,68 @@
+export function _update_dashboard_theme(theme_selector_checked) {
+  document.documentElement.setAttribute(
+    "data-bs-theme",
+    theme_selector_checked ? "light" : "dark",
+  );
+  return theme_selector_checked ? "vizro_light" : "vizro_dark";
+}
+
+export function _update_ag_grid_theme(theme_selector_checked) {
+  return theme_selector_checked
+    ? "ag-theme-quartz ag-theme-vizro"
+    : "ag-theme-quartz-dark ag-theme-vizro";
+}
+
+export function _update_graph_theme(
+  figure,
+  theme_selector_checked,
+  vizro_themes,
+) {
+  const theme_to_apply = theme_selector_checked ? "vizro_light" : "vizro_dark";
+
+  const updated_figure = {
+    ...figure,
+    layout: {
+      ...figure.layout,
+      template: vizro_themes[theme_to_apply],
+    },
+  };
+
+  // {} resets the figure.style to undo the {"visibility": "hidden"} from set_props in Graph.__call__.
+  return [updated_figure, {}];
+}
+
+export function _collapse_nav_panel(n_clicks, is_open) {
+  if (!n_clicks) {
+    /* Automatically collapses left-side if xs and s-devices are detected*/
+    if (window.innerWidth < 576 || window.innerHeight < 576) {
+      return [
+        false,
+        {
+          transform: "rotate(180deg)",
+          transition: "transform 0.35s ease-in-out",
+        },
+        "Show Menu",
+      ];
+    }
+    return dash_clientside.no_update;
+  }
+  if (is_open) {
+    return [
+      false,
+      {
+        transform: "rotate(180deg)",
+        transition: "transform 0.35s ease-in-out",
+      },
+      "Show Menu",
+    ];
+  } else {
+    return [
+      true,
+      {
+        transform: "rotate(0deg)",
+        transition: "transform 0.35s ease-in-out",
+      },
+      "Hide Menu",
+    ];
+  }
+}
