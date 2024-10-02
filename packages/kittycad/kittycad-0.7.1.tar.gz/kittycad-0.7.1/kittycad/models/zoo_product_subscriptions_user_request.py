34b303d27537e7f870e6bdf195642d59124df2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 05:04:53 +0000

    Add package kittycad/0.7.1/kittycad-0.7.1.tar.gz

---
+++ packages/kittycad/kittycad-0.7.1.tar.gz/kittycad-0.7.1/kittycad/models/zoo_product_subscriptions_user_request.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, ConfigDict
+
+from ..models.modeling_app_individual_subscription_tier import (
+    ModelingAppIndividualSubscriptionTier,
+)
+
+
+class ZooProductSubscriptionsUserRequest(BaseModel):
+    """A struct of Zoo product subscriptions a user can request."""
+
+    modeling_app: ModelingAppIndividualSubscriptionTier = "free"  # type: ignore
+
+    model_config = ConfigDict(protected_namespaces=())
