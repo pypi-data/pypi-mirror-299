dc8d62703b6de09f0e7ea96ad9b56c7ec220cbf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 05:04:40 +0000

    Add package MaRDMO/0.1.2/mardmo-0.1.2.tar.gz

---
+++ packages/MaRDMO/mardmo-0.1.2.tar.gz/mardmo-0.1.2/MaRDMO/config.py
@@ -0,0 +1,19 @@
+#RDMO BASE URI
+BASE_URI = 'https://rdmo.mardi4nfdi.de/terms/'
+
+#Portal Wiki, Api and SPARQL Endpoint
+mardi_wiki="https://portal.mardi4nfdi.de/wiki/"
+mardi_api="https://portal.mardi4nfdi.de/w/api.php"
+mardi_endpoint="https://query.portal.mardi4nfdi.de/proxy/wdqs/bigdata/namespace/wdq/sparql"
+
+#Wikidata SPARQL, Api Endpoint
+wikidata_endpoint="https://query.wikidata.org/sparql"
+wikidata_api="https://www.wikidata.org/w/api.php"
+
+#MathModDB SPARQL Endpoint
+mathmoddb_endpoint = 'https://sparql.mtsr2024.m1.mardi.ovh/mathalgodb/query'
+mathmoddb_update = 'https://sparql.mtsr2024.m1.mardi.ovh/mathalgodb/update'
+
+#SPARQL Prefixes
+wd = '<https://portal.mardi4nfdi.de/entity/>'
+wdt = '<https://portal.mardi4nfdi.de/prop/direct/>'
