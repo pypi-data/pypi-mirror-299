78438933633590ae89840dea4cc0ece2d39079ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 07:04:31 +0000

    Add package hive-messaging/0.0.5/hive_messaging-0.0.5-py3-none-any.whl

---
+++ packages/hive-messaging/hive_messaging-0.0.5-py3-none-any.whl/hive/messaging/__init__.py
@@ -0,0 +1,13 @@
+from pika.exceptions import UnroutableError
+
+from .channel import Channel, Notifier
+from .connection import Connection
+from .message_bus import MessageBus, Producer
+
+DEFAULT_MESSAGE_BUS = MessageBus()
+
+blocking_connection = DEFAULT_MESSAGE_BUS.blocking_connection
+send_to_queue = DEFAULT_MESSAGE_BUS.send_to_queue
+tell_user = DEFAULT_MESSAGE_BUS.tell_user
+
+producer_connection = DEFAULT_MESSAGE_BUS.producer_connection
