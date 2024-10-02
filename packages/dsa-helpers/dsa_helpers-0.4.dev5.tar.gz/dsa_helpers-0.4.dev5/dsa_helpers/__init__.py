f7299c2c9bb406a5dd80a5a700d3ab90b9b46fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-02 06:04:46 +0000

    Add package dsa-helpers/0.4.dev5/dsa_helpers-0.4.dev5.tar.gz

---
+++ packages/dsa-helpers/dsa_helpers-0.4.dev5.tar.gz/dsa_helpers-0.4.dev5/dsa_helpers/__init__.py
@@ -0,0 +1,8 @@
+# Shadow imports.
+from .imread import imread
+from .imwrite import imwrite
+
+# Modules that should be available.
+# from . import girder_utils, image_utils, dash
+
+__all__ = ["girder_utils", "image_utils", "dash", "ml"]
---
+++ packages/dsa-helpers/dsa_helpers-0.4.dev5.tar.gz/dsa_helpers-0.4.dev5/dsa_helpers/ml/metrics/binary_dice_coefficient_metric.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+from ...utils import binary_dice_coefficient
+
+
+def binary_dice_coefficient_metric(eval_pred):
+    with torch.no_grad():
+        logits, labels = eval_pred  # assumed both are numpy
+
+        logits_tensor = torch.from_numpy(logits)
+
+        # Scale the logits to the size of the label.
+        logits_tensor = nn.functional.interpolate(
+            logits_tensor,
+            size=labels.shape[-2:],
+            mode="bilinear",
+            align_corners=False,
+        ).argmax(dim=1)
+
+        pred_labels = logits_tensor.detach().cpu().numpy()
+
+        return {"dice_coefficient": binary_dice_coefficient(pred_labels, labels)}
