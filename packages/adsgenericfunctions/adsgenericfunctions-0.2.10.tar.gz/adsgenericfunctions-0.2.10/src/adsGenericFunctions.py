cb7d5ef80e73916816a97654e32821ec068c34c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 06:04:34 +0000

    Add package adsgenericfunctions/0.2.10/adsgenericfunctions-0.2.10.tar.gz

---
+++ packages/adsgenericfunctions/adsgenericfunctions-0.2.10.tar.gz/adsgenericfunctions-0.2.10/src/adsGenericFunctions.py
@@ -0,0 +1,18 @@
+#from email import *
+
+'''
+from postgre.dbPgsql import *
+from mssql.dbMssql import *
+from pipeline import *
+from timer import *
+from logger import *
+from global_config import *
+'''
+
+def test():
+    print("YES")
+
+if __name__ == "__main__":
+    print("Hello")
+
+
