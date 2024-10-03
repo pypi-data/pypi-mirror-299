7172cf8cdd24839e6840d5073d26dae278866d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 04:05:31 +0000

    Add package downitall-android/1/downitall_android-1.tar.gz

---
+++ packages/downitall-android/downitall_android-1.tar.gz/downitall_android-1/downitall_android/resources/__init__.py
@@ -0,0 +1,8 @@
+"""Importing Booksources from booksources in resource directory"""
+
+from downitall_android.resources.animesources import Animesources
+from downitall_android.resources.booksources import Booksources
+from downitall_android.resources.mangasources import Mangasources
+from downitall_android.resources.musicsources import Musicsources
+from downitall_android.resources.streamsources import Streamsources
+from downitall_android.resources.tvsources import Tvsources
