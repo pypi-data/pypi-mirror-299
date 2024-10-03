3a1e7e321ff46e66d201fd5cb96a57c43672c8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 07:06:12 +0000

    Add package vo-models/0.4.0/vo_models-0.4.0-py3-none-any.whl

---
+++ packages/vo-models/vo_models-0.4.0-py3-none-any.whl/vo_models/vodataservice/__init__.py
@@ -0,0 +1,13 @@
+"""Module containing models and resources for IVOA VODataService objects."""
+from vo_models.vodataservice.models import (
+    BaseParam,
+    DataType,
+    FKColumn,
+    ForeignKey,
+    InputParam,
+    ParamHTTP,
+    Table,
+    TableParam,
+    TableSchema,
+    TableSet,
+)
