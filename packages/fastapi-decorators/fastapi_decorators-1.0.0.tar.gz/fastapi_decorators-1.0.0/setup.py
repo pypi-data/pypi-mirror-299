381f13dff2b6720f1b6320746410d44c7a1d5a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 09:05:44 +0000

    Add package fastapi-decorators/1.0.0/fastapi_decorators-1.0.0.tar.gz

---
+++ packages/fastapi-decorators/fastapi_decorators-1.0.0.tar.gz/fastapi_decorators-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open('README.md') as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="fastapi-decorators",
+    version="1.0.0",
+    author="Anders Brams",
+    author_email="anders@brams.dk",
+    description="Decorate FastAPI endpoints with custom decorators.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[
+        "fastapi",
+        "decorators",
+        "middleware",
+        "dependency",
+        "dependencies",
+    ],
+    url="https://github.com/Minibrams/fastapi-decorators",
+    packages=setuptools.find_packages(where='src'),
+    package_dir={
+        '': 'src'
+    },
+    install_requires=[],
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
