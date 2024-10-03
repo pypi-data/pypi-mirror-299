ccf65e1331d16b2ae0fbfc078648b22a108e7158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 08:06:27 +0000

    Add package flexiagent/0.1.0a7/flexiagent-0.1.0a7.tar.gz

---
+++ packages/flexiagent/flexiagent-0.1.0a7.tar.gz/flexiagent-0.1.0a7/README.md
@@ -0,0 +1,183 @@
+FlexiAgent
+----------
+
+FlexiAgent is a simple and easy-to-use framework for creating LLM agents. The agent supports structured output and includes built-in practical agents such as a text2sql agent, allowing for quick deployment in applications.
+
+
+
+Features
+--------
+
+- **Efficient and Simple Agent Interface Design**: Supports structured output, making it easier to utilize in practical applications.
+
+- **DAG-Based Task Implementation**: The agent's tasks are built on DAGs, supporting distributed concurrent scheduling.
+
+- **Configuration-Driven Agent Creation**: Offers high flexibility and extensibility through comprehensive configuration support.
+
+- **Built-In Practical Agents**: Includes useful agents like `text2sql`, `llm_chat`, and `api_call` for fast deployment and reduced development time.
+
+
+
+Installation
+------------
+
+To begin using FlexiAgent, you can install from pypi
+```shell
+pip install flexiagent
+```
+
+Or clone the repository from GitHub:
+```shell
+git clone https://github.com/dzhsurf/flexiagent.git
+cd flexiagent
+pip install -e .
+# or use poetry
+# poetry install
+```
+
+Ensure you have Python installed and set up a virtual environment(conda recommand):
+```shell
+# python3.11 recommendation, support python3.8+
+conda create -n proj python=3.11 
+```
+
+If you encounter issues compiling llama-cpp-python during the dependency installation process, please visit https://github.com/abetlen/llama-cpp-python for documentation to help resolve the issue.
+
+
+Usage
+-----
+
+Before using FlexiAgent, you'll need to set up your `OPENAI_API_KEY`. You can set this environment variable in your system or include it in your code:
+
+```shell
+export OPENAI_API_KEY='your-api-key-here' 
+```
+
+FlexiAgent can be easily integrated into your existing projects. Below is a basic setup to get you started:
+
+![](./docs/simple-agent.jpg)
+
+
+```python
+from flexiagent.llm.config import LLMConfig
+from flexiagent.task.task_node import (
+    FxTaskAction,
+    FxTaskActionLLM,
+    FxTaskAgent,
+    FxTaskConfig,
+    FxTaskEntity,
+)
+
+llm_config = LLMConfig(engine="OpenAI", params={"openai_model": "gpt-4o-mini"})
+
+class Step1Output(FxTaskEntity):
+    num1: float 
+    num2: float 
+    op: str 
+
+class Step2Output(FxTaskEntity):
+    result: float
+
+def compute_nums(input: Dict[str, Any], addition: Dict[str, Any]) -> Step2Output:
+    nums: Step1Output = input["step_1"]
+    result = 0.0
+    if nums.op == "+":
+        result = nums.num1 + nums.num2
+    elif nums.op == "-":
+        result = nums.num1 - nums.num2
+    elif nums.op == "*":
+        result = nums.num1 * nums.num2
+    elif nums.op == "/":
+        result = nums.num1 / nums.num2
+    else:
+        result = 0
+    return Step2Output(
+        result=result,
+    )
+
+agent = FxTaskAgent(
+    task_graph=[
+        # step 1: llm extract data
+        FxTaskConfig(
+            task_key="step_1",
+            input_schema={"input": str},
+            output_schema=Step1Output,
+            action=FxTaskAction(
+                type="llm",
+                act=FxTaskActionLLM(
+                    llm_config=llm_config,
+                    instruction="""
+Extract the numbers and operators from mathematical expressions based on the user's questions. 
+Only support +, -, *, / operations with two numbers.
+
+Question: {input}
+""",
+                ),
+            ),
+        ),
+        # step 2: compute
+        FxTaskConfig(
+            task_key="output",
+            input_schema={"step_1": Step1Output},
+            output_schema=Step2Output,
+            action=FxTaskAction(
+                type="function",
+                act=compute_nums,
+            ),
+        ),
+    ],
+)
+
+output = agent.invoke("Compute: 3 + 5 =")
+# output is Step2Output, result is 8
+```
+
+
+
+Quickstart
+----------
+
+To get started quickly with FlexiAgent, please refer to the [Quickstart](https://github.com/dzhsurf/flexiagent/blob/master/Quickstart.md) Guide for example usage.
+
+
+
+Using Local Deployment Model (Llama.cpp)
+----------------------------------------
+
+FlexiAgent utilizes llama-cpp-python, allowing the LLM to support not only OpenAI but also Llama.cpp. 
+The above code example can load a local model using Llama.cpp by changing the configuration.
+
+**Parameter Explanation**
+
+**repo_id_or_model_path:** 
+If specified as a repo_id, the model will be downloaded to the cache directory `(~/.cache/huggingface/...)` using huggingface-cli. If it is a local model path (e.g., xxx_model.gguf), it will be loaded directly.
+
+**repo_filename:** 
+This is only effective when `repo_id_or_model_path` is a repo_id. It selects the specified model based on the filename rules, such as Q4_K_M, Q8_0, etc., and should be set according to the file names in the huggingface repo.
+
+**n_ctx:** Context window size; defaults to 512 if not set.
+
+```python
+llm_config = LLMConfig(
+    engine="LlamaCpp",
+    params={
+        "repo_id_or_model_path": "QuantFactory/Llama-3.2-3B-Instruct-GGUF",
+        "repo_filename": "*Q4_K_M.gguf",
+        "n_ctx": 4096,
+    },
+)
+````
+
+
+
+Contributing
+------------
+
+Contributions are welcome! Please fork the repository and use a branch for your feature or bug fix. Submitting a pull request is the best way to see your feature merged.
+
+
+
+License
+-------
+
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ packages/flexiagent/flexiagent-0.1.0a7.tar.gz/flexiagent-0.1.0a7/flexiagent/task/task_node.py
@@ -0,0 +1,256 @@
+import logging
+from typing import Any, Callable, Dict, List, Literal, Optional, Set, Tuple, Type, Union
+
+from pydantic import BaseModel, ConfigDict
+
+from flexiagent.llm.config import LLMConfig
+from flexiagent.llm.llm import LLM
+from flexiagent.llm.structured_schema import FxLLMStructuredSchema
+from flexiagent.prompts.prompt import PromptTemplate
+from flexiagent.task.base import FxTask
+
+logger = logging.getLogger(__name__)
+
+
+class FxTaskActionLLM(BaseModel):
+    llm_config: LLMConfig
+    instruction: str
+
+
+FxTaskActionFunction = Callable[[Dict[str, Any], Dict[str, Any]], Any]
+
+
+class FxTaskActionConditionResult(BaseModel):
+    skip_action: bool
+    action_ret_value: Any
+
+
+class FxTaskAction(BaseModel):
+    type: Literal["llm", "function", "agent"]
+    act: Union[
+        FxTaskActionLLM,
+        FxTaskActionFunction,
+        "FxTaskAgent",
+    ]
+    addition: Optional[Dict[str, Any]] = None
+    condition: Optional[Callable[[Dict[str, Any]], FxTaskActionConditionResult]] = None
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
+class FxTaskEntity(FxLLMStructuredSchema):
+    def __repr__(self):
+        field_strings = [f"{key}: {value}" for key, value in self.model_dump().items()]
+        return "\n".join(field_strings)
+
+
+class FxTaskConfig(BaseModel):
+    task_key: str
+    input_schema: Dict[str, Type[Union[FxTaskEntity, str]]]
+    output_schema: Type[Union[FxTaskEntity, str]]
+    action: FxTaskAction
+
+
+class FxTaskNode(FxTask):
+    def __init__(
+        self,
+        config: FxTaskConfig,
+        *,
+        preprocess_hook: Optional[Callable] = None,
+        postprocess_hook: Optional[Callable] = None,
+    ):
+        super().__init__(
+            preprocess_hook=preprocess_hook,
+            postprocess_hook=postprocess_hook,
+        )
+        self._config = config
+
+    @property
+    def config(self) -> FxTaskConfig:
+        return self._config
+
+    def _require_input(
+        self,
+        kwargs: Dict[str, Any],
+        key: str,
+        message: Optional[str] = None,
+        value_type: Optional[Type[Any]] = None,
+    ) -> Any:
+        if not message:
+            message = f"task[{self.__class__.__name__}] input must has field {key}"
+        assert key in kwargs, message
+        if value_type is not None:
+            if not isinstance(kwargs[key], value_type):
+                raise TypeError(
+                    f"_require_input: {kwargs[key]} type not match {value_type}"
+                )
+        return kwargs[key]
+
+    def _process_llm(self, _action: FxTaskAction, _inputs: Dict[str, Any]) -> Any:
+        if isinstance(_action.act, FxTaskActionLLM):
+            params = _action.act
+        else:
+            raise TypeError(f"action params type not match. {_action.act}")
+
+        llm = LLM(params.llm_config)
+        if issubclass(self.config.output_schema, FxTaskEntity):
+            prompt = PromptTemplate(
+                prompt="You are a helpful assistant that assist users in completing tasks and use formatted output.",
+                user_question_prompt=params.instruction,
+            )
+            response = llm.chat_completion_with_structured_output(
+                prompt=prompt,
+                variables=_inputs,
+                response_format=self.config.output_schema,
+            )
+        elif self.config.output_schema is str:
+            prompt = PromptTemplate(
+                prompt="You are a helpful assistant.",
+                user_question_prompt=params.instruction,
+            )
+            response = llm.chat_completion(prompt=prompt, variables=_inputs)
+        else:
+            raise TypeError(f"Output schema not support, {self.config.output_schema}")
+        return response
+
+    def _process_function(self, _action: FxTaskAction, _inputs: Dict[str, Any]) -> Any:
+        if callable(_action.act):
+            fn = _action.act
+        else:
+            raise TypeError(f"action params type not match. {_action.act}")
+        addition: Dict[str, Any] = {}
+        if _action.addition:
+            addition = {k: v for k, v in _action.addition.items()}
+        return fn(_inputs, addition)
+
+    def _process_agent(self, _action: FxTaskAction, _inputs: Dict[str, Any]) -> Any:
+        if isinstance(_action.act, FxTaskAgent):
+            agent = _action.act
+        else:
+            raise TypeError(f"action params type not match. {_action.act}")
+        return agent.invoke(**_inputs)
+
+    def process(self, *args: Any, **kwds: Any) -> Any:
+        processor_mapping = {
+            "llm": self._process_llm,
+            "function": self._process_function,
+            "agent": self._process_agent,
+        }
+        action = self.config.action
+        if isinstance(action, dict):
+            action = FxTaskAction(**action)
+        elif isinstance(action, FxTaskAction):
+            pass
+        else:
+            raise TypeError(f"action type not match. {action}")
+
+        # setup inputs from context
+        inputs = {}
+        for key, _type in self.config.input_schema.items():
+            inputs[key] = self._require_input(kwargs=kwds, key=key, value_type=_type)
+
+        # process condition
+        if action.condition:
+            condition_ret = action.condition(inputs)
+            if condition_ret.skip_action:
+                return condition_ret.action_ret_value
+
+        # execute task with inputs
+        if action.type in processor_mapping:
+            response = processor_mapping[action.type](action, inputs)
+        else:
+            raise ValueError(f"action not support. f{self.config.action}")
+
+        return response
+
+
+class FxTaskAgent:
+    def __init__(
+        self,
+        *,
+        task_graph: List[Union[FxTaskConfig, FxTaskNode]],
+        preprocess_hook: Optional[
+            Callable[[Dict[str, Any]], Tuple[Dict[str, Any], bool]]
+        ] = None,
+        postprocess_hook: Optional[Callable[[Dict[str, Any]], Dict[str, Any]]] = None,
+    ):
+        self.tasks = self._build_task_graph(task_graph)
+        self.preprocess_hook = preprocess_hook
+        self.postprocess_hook = postprocess_hook
+
+    def _build_graph(
+        self, item_list: List[Union[FxTaskConfig, FxTaskNode]]
+    ) -> Dict[str, FxTaskNode]:
+        graph_dict: Dict[str, FxTaskNode] = {}
+        for item in item_list:
+            if isinstance(item, FxTaskConfig):
+                graph_dict[item.task_key] = FxTaskNode(item)
+            elif isinstance(item, FxTaskNode):
+                graph_dict[item.config.task_key] = item
+        return graph_dict
+
+    def _topo_sort(self, node_graph: Dict[str, FxTaskNode]) -> List[FxTaskNode]:
+        visited: Set[str] = set()
+        temp_marked: Set[str] = set()
+        stack: List[FxTaskNode] = []
+
+        def dfs(node: FxTaskNode):
+            if node.config.task_key in temp_marked:
+                raise ValueError("Graph is not a DAG")
+
+            if node.config.task_key in visited:
+                return
+
+            temp_marked.add(node.config.task_key)
+            for neighbor_key, _ in node.config.input_schema.items():
+                if neighbor_key in node_graph:
+                    neighbor = node_graph[neighbor_key]
+                    dfs(neighbor)
+            temp_marked.remove(node.config.task_key)
+            visited.add(node.config.task_key)
+            stack.append(node)
+
+        for _, node in node_graph.items():
+            dfs(node)
+
+        return stack
+
+    def _build_task_graph(
+        self, config_items: List[Union[FxTaskConfig, FxTaskNode]]
+    ) -> List[FxTaskNode]:
+        node_graph = self._build_graph(config_items)
+        sorted_nodes = self._topo_sort(node_graph)
+        logger.info([(idx, n.config.task_key) for idx, n in enumerate(sorted_nodes)])
+        return sorted_nodes
+
+    def invoke(self, *args: Any, **kwds: Any) -> Any:
+        context: Dict[str, Union[FxTaskEntity, str]] = {}
+        if "input" not in kwds:
+            for arg in args:
+                if isinstance(arg, (FxTaskEntity, str)):
+                    context["input"] = arg
+        for key, value in kwds.items():
+            context[key] = value
+
+        # preprocess
+        if self.preprocess_hook:
+            context, stop = self.preprocess_hook(context)
+            if stop and "output" in context:
+                return context["output"]
+
+        # run task dag
+        for task in self.tasks:
+            logger.info(f"Step: {task.config.task_key}")
+            try:
+                output = task.invoke(**context)
+                logger.info(f"Step: {task.config.task_key}\nOutput: {output}")
+                context[task.config.task_key] = output
+            except Exception as e:
+                logger.error(e)
+
+        # postprocess
+        if self.postprocess_hook:
+            context = self.postprocess_hook(context)
+
+        if "output" in context:
+            return context["output"]
