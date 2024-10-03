14959d95ce493cacd3342914156c6152053b2e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 04:05:29 +0000

    Add package mangokit/1.0.7/mangokit-1.0.7-py3-none-any.whl

---
+++ packages/mangokit/mangokit-1.0.7-py3-none-any.whl/mangokit/enums/gui_enum.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description: 
+# @Time   : 2024-09-04 17:21
+# @Author : 毛鹏
+from src.enums import BaseEnum
+
+
+class InputEnum(BaseEnum):
+    INPUT = 0
+    SELECT = 1
+    CASCADER = 2
+    TOGGLE = 3
---
+++ packages/mangokit/mangokit-1.0.7-py3-none-any.whl/mangokit/tools/database/sql_statement.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description: 
+# @Time   : 2024-01-10 16:58
+# @Author : 毛鹏
+
+sql_statement_1 = 'SELECT * FROM user_info;'
+sql_statement_2 = f'INSERT INTO "user_info" ("username", "password", "ip", "port") VALUES (?, ?, ?, ?);'
+sql_statement_3 = f'DELETE FROM user_info;'
+sql_statement_4 = f'INSERT INTO "test_data" ("key", "value", "type") VALUES (?, ?, ?);'
+sql_statement_5 = f'SELECT * FROM test_data WHERE `key` = ?'
+sql_statement_6 = f'DELETE FROM test_data WHERE `key` = ?'
