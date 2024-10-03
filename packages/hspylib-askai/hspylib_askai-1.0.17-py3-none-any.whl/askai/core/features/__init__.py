954e2759fd598236d5c52e222a33c180b7f9a56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 15:05:15 +0000

    Add package hspylib-askai/1.0.17/hspylib_askai-1.0.17-py3-none-any.whl

---
+++ packages/hspylib-askai/hspylib_askai-1.0.17-py3-none-any.whl/askai/core/features/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-askai v1.0.17
+#
+# Package: main.askai.core.features
+"""Package initialization."""
+
+__all__ = [
+    'processors', 
+    'router', 
+    'tools'
+]
+__version__ = '1.0.17'
---
+++ packages/hspylib-askai/hspylib_askai-1.0.17-py3-none-any.whl/askai/core/features/processors/__init__.py
@@ -0,0 +1,16 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-askai v1.0.17
+#
+# Package: main.askai.core.features.processors
+"""Package initialization."""
+
+__all__ = [
+    'ai_processor', 
+    'chat', 
+    'qna', 
+    'qstring', 
+    'rag', 
+    'task_splitter'
+]
+__version__ = '1.0.17'
