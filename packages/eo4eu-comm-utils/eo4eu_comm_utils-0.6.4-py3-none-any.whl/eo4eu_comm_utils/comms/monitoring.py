2f8f4c5e6fedb58ad15718e9d2234e29525fe1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 04:07:14 +0000

    Add package eo4eu-comm-utils/0.6.4/eo4eu_comm_utils-0.6.4-py3-none-any.whl

---
+++ packages/eo4eu-comm-utils/eo4eu_comm_utils-0.6.4-py3-none-any.whl/eo4eu_comm_utils/comms/monitoring.py
@@ -0,0 +1,70 @@
+import json
+import os
+from datetime import datetime
+
+from .interface import Comm, LogLevel
+from eo4eu_comm_utils.kafka.producer import KafkaProducer
+
+
+class MonitoringComm(Comm):
+    def __init__(
+        self,
+        producer: KafkaProducer,
+        namespace: str,
+        source: str,
+        prefix: str,
+        level_dict: dict[LogLevel,str]|None = None,
+        **kwargs
+    ):
+        self.producer = producer
+        self.namespace = namespace
+        self.source = source
+        self.prefix = prefix
+        if level_dict is None:
+            level_dict = {
+                LogLevel.DEBUG:    ("INFO", "Info"),
+                LogLevel.INFO:     ("INFO", "Info"),
+                LogLevel.WARNING:  ("WARNING", "Error"),
+                LogLevel.ERROR:    ("WARNING", "Error"),
+                LogLevel.CRITICAL: ("CRITICAL", "Error"),
+                LogLevel.SUCCESS:  ("SUCCESS", "Info"),
+            }
+        self.level_dict = level_dict
+        self.kwargs = kwargs
+        self.pod = "unknown"
+        try:
+            self.pod = os.environ["HOSTNAME"]
+        except Exception:
+            pass
+
+    def send(
+        self,
+        level: LogLevel,
+        description: str,
+        *args,
+        description_business: str = None,
+        **kwargs
+    ):
+        if description_business is None:
+            description_business = description
+
+        level_name, msg_name = self.level_dict[level]
+        message = {
+            "source": self.source,
+            "name": f"{self.prefix}{msg_name}",
+            "level": level_name,
+            "description": description,
+            "description_business": description_business,
+            "timestamp": datetime.utcnow().isoformat(),
+            "extra": {
+                "namespace": self.namespace,
+                "pod": self.pod,
+                **(self.kwargs | kwargs),
+            }
+        }
+
+        self.producer.set_topic("monitoring.notify")
+        self.producer.send_message(
+            key = self.source,
+            msg = json.dumps(message)
+        )
