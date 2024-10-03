774d9147afad245ea127f2c3b90a993eb2eb4680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:06:23 +0000

    Add package torchenhanced/0.3.3b1/torchenhanced-0.3.3b1.tar.gz

---
+++ packages/torchenhanced/torchenhanced-0.3.3b1.tar.gz/torchenhanced-0.3.3b1/src/torchenhanced/util/__init__.py
@@ -0,0 +1,4 @@
+from .color_compression import *
+from .files import *
+# from .misc import *
+from .visualize import *
---
+++ packages/torchenhanced/torchenhanced-0.3.3b1.tar.gz/torchenhanced-0.3.3b1/tests/test_configmodule.py
@@ -0,0 +1,65 @@
+import torch, torch.nn as nn, sys, pathlib
+sys.path.append(pathlib.Path(__file__).parent.parent.as_posix())
+from src.torchenhanced import Trainer, DevModule, ConfigModule
+
+
+    
+def test_kwargs():
+    class LinSimple(ConfigModule):
+        def __init__(self, hidden = 28*28, out =10, device='cpu',**kwargs):
+            super().__init__()
+
+            self.layer = nn.Linear(hidden, out)
+        
+        def forward(self, x):
+            return self.layer(x)
+    
+    model = LinSimple(hidden=28, out=5, device='cuda:0', extra='extra')
+    testimony = torch.zeros((1,1)).to('cuda:0')
+    assert model.config == {'hidden': 28, 'out': 5, 'device': 'cuda:0', 'extra': 'extra'}, f'found {model.config=} instead of {{"hidden": 28, "out": 5, "device": "cuda:0", "extra": "extra"}}'
+    assert model.device == testimony.device, f'found {model.device=} instead of cuda:0'
+    
+    model = LinSimple()
+    testimony = testimony.to('cpu')
+    assert model.config == {'hidden': 28*28, 'out': 10, 'device': 'cpu'}, f'found {model.config=} instead of {{"hidden": 28*28, "out": 10, "device": "cpu"}}'
+    assert model.device == testimony.device, f'found {model.device=} instead of cpu'
+
+def test_no_device():
+    class LinSimple(ConfigModule):
+        def __init__(self, hidden = 28*28, out =10, **kwargs):
+            super().__init__(device='cpu')
+
+            self.layer = nn.Linear(hidden, out)
+        
+        def forward(self, x):
+            return self.layer(x)
+    
+    model = LinSimple(hidden=28, out=5, extra='extra')
+    testimony = torch.zeros((1,1)).to('cpu')
+    assert model.config == {'hidden': 28, 'out': 5, 'extra': 'extra'}, f'found {model.config=} instead of {{"hidden": 28, "out": 5, "extra": "extra"}}'
+    assert model.device == testimony.device , f'found {model.device=} instead of {testimony.device}'
+    
+    model = LinSimple(device='cuda:0')
+    testimony = testimony.to('cuda:0')
+    assert model.config == {'hidden': 28*28, 'out': 10, 'device': 'cuda:0'}, f'found {model.config=} instead of {{"hidden": 784, "out": 10}}'
+    assert model.device ==  testimony.device, f'found {model.device=} instead of cuda:0'
+
+def test_no_kwargs():
+    class LinSimple(ConfigModule):
+        def __init__(self, hidden = 28*28, out =10):
+            super().__init__(device='cpu')
+
+            self.layer = nn.Linear(hidden, out)
+        
+        def forward(self, x):
+            return self.layer(x)
+    
+    model = LinSimple(hidden=22)
+    testimony = torch.zeros((1,1)).to('cpu')
+    assert model.config == {'hidden': 22, 'out': 10}, f'found {model.config=} instead of {{"hidden": 22, "out": 10}}'
+    assert model.device == testimony.device, f'found {model.device=} instead of cpu'
+    
+if __name__ == '__main__':
+    test_kwargs()
+    test_no_device()
+    print("All tests passed !")
