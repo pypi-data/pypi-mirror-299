7d2275c6b8abcbe1ced406a8e3d24ba1b0615327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 16:05:06 +0000

    Add package llm-engines/0.0.13/llm_engines-0.0.13-py3-none-any.whl

---
+++ packages/llm-engines/llm_engines-0.0.13-py3-none-any.whl/llm_engines/openai_text.py
@@ -0,0 +1,60 @@
+from openai import OpenAI
+from typing import List
+
+# no image, multi-turn, do not use openai_generate, but can refer to it
+def call_worker_openai(messages:List[str], model_name, timeout:int=60, conv_system_msg=None, **generate_kwargs) -> str:
+    # change messages to openai format
+    new_messages = []
+    if conv_system_msg:
+        new_messages.append({"role": "system", "content": conv_system_msg})
+    for i, message in enumerate(messages):
+        new_messages.append({"role": "user" if i % 2 == 0 else "assistant", "content": message})
+    # initialize openai client
+    client = OpenAI()
+    # call openai
+    completion = client.chat.completions.create(
+        model=model_name,
+        messages=new_messages,
+        timeout=timeout,
+        **generate_kwargs,
+    )
+    stream = generate_kwargs.get("stream", False)
+    if not stream:
+        if len(completion.choices) > 1:
+            return [c.message.content for c in completion.choices]
+        else:
+            return completion.choices[0].message.content
+    else:
+        def generate_stream():
+            for chunk in completion:
+                if chunk.choices[0].delta.content is not None:
+                    yield chunk.choices[0].delta.content
+        return generate_stream()
+
+def call_worker_openai_completion(prompt:str, model_name, timeout:int=60, **generate_kwargs) -> str:
+    # initialize openai client
+    client = OpenAI()
+    # call openai
+    completion = client.completions.create(
+        model=model_name,
+        prompt=prompt,
+        timeout=timeout,
+        **generate_kwargs,
+    )
+    stream = generate_kwargs.get("stream", False)
+    if not stream:
+        if len(completion.choices) > 1:
+            return [c.text for c in completion.choices]
+        else:
+            return completion.choices[0].text
+    else:
+        def generate_stream():
+            for chunk in completion:
+                if chunk.choices[0].text is not None:
+                    yield chunk.choices[0].text
+        return generate_stream()
+
+if __name__ == "__main__":
+    from icecream import ic
+    ic(call_worker_openai(["Hello"], "gpt-3.5-turbo"))
+    ic(call_worker_openai_completion("Hello", "gpt-3.5-turbo-instruct"))
