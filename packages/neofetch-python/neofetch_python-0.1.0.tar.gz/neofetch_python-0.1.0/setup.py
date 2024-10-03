632cfe816892a2ac7edf36098b8f2965ce085897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 04:07:28 +0000

    Add package neofetch-python/0.1.0/neofetch_python-0.1.0.tar.gz

---
+++ packages/neofetch-python/neofetch_python-0.1.0.tar.gz/neofetch_python-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neofetch-python",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "psutil",
+        "colorama",
+        "wmi",
+    ],
+    entry_points={
+        "console_scripts": [
+            "neofetch=neofetch:run",
+        ],
+    },
+    author="Your Name",
+    author_email="firi8228@gmail.com",
+    description="A Python implementation of neofetch",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
