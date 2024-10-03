73a0efe4cd51e413b06accd38c1177da5018e967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 16:05:46 +0000

    Add package eth-rpc-py/0.1.9.post3/eth_rpc_py-0.1.9.post3-py3-none-any.whl

---
+++ packages/eth-rpc-py/eth_rpc_py-0.1.9.post3-py3-none-any.whl/eth_rpc/networks/__init__.py
@@ -0,0 +1,31 @@
+from .arbitrum import Arbitrum, ArbitrumSepolia
+from .base import Base, BaseSepolia
+from .ethereum import Ethereum
+from .linea import Linea
+from .sapphire import SapphireTestnet
+
+Networks = {
+    1: Ethereum,
+    8453: Base,
+    84532: BaseSepolia,
+    42161: Arbitrum,
+    421614: ArbitrumSepolia,
+    59144: Linea,
+    23295: SapphireTestnet,
+}
+
+
+def get_network_by_chain_id(chain_id):
+    return Networks.get(chain_id)
+
+
+__all__ = [
+    "Arbitrum",
+    "ArbitrumSepolia",
+    "Base",
+    "BaseSepolia",
+    "Ethereum",
+    "Linea",
+    "Networks",
+    "SapphireTestnet",
+]
