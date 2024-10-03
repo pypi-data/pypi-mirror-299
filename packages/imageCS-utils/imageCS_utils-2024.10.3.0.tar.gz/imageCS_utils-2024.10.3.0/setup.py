7070c27edecc9424c79f34d47fd429085ae65f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 09:05:49 +0000

    Add package imageCS-utils/2024.10.3.0/imageCS_utils-2024.10.3.0.tar.gz

---
+++ packages/imageCS-utils/imageCS_utils-2024.10.3.0.tar.gz/imageCS_utils-2024.10.3.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="imageCS_utils",
+    version="2024.10.3.0",
+    author="Liao Chen",
+    author_email="liaochen@bjtu.edu.cn",
+    description="Some useful utils for deep learning (PyTorch) image compressive sensing (CS).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FengodChen",
+    install_requires=["torch", "torchvision", "torchaudio", "timm", "einops", "thop", "pytorch-fid", "opencv-python"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
