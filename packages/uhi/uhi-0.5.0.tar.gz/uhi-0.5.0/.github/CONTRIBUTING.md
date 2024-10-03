6012c0db0d17ea87c8b664998b0628a08657f123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 15:05:39 +0000

    Add package uhi/0.5.0/uhi-0.5.0.tar.gz

---
+++ packages/uhi/uhi-0.5.0.tar.gz/uhi-0.5.0/.github/CONTRIBUTING.md
@@ -0,0 +1,42 @@
+See the [Scikit-HEP Developer introduction][skhep-dev-intro] for a
+detailed description of best practices for developing Scikit-HEP packages.
+
+[skhep-dev-intro]: https://scikit-hep.org/developer/intro
+
+# Quick run with Nox
+
+If you have nox, this project supports nox. These are the supplied sessions:
+
+```console
+nox -s lint
+nox -s tests
+nox -s docs -- serve
+nox -s build
+```
+
+
+# Post setup
+
+You should prepare pre-commit, which will help you by checking that commits
+pass required checks:
+
+```bash
+pip install pre-commit # or brew install pre-commit on macOS
+pre-commit install # Will install a pre-commit hook into the git repo
+```
+
+You can also/alternatively run `pre-commit run` (changes only) or `pre-commit
+run --all-files` to check even without installing the hook.
+
+# Testing
+
+Use pytest to run the unit checks:
+
+```bash
+pytest
+```
+
+# Bumping the version
+
+Use `nox -s bump -- <type>` to bump the version. Commit the result and push,
+release on GitHub. Make sure to add some release notes to the GitHub release.
---
+++ packages/uhi/uhi-0.5.0.tar.gz/uhi-0.5.0/noxfile.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+
+import argparse
+from pathlib import Path
+
+import nox
+
+nox.needs_version = ">=2024.4.15"
+nox.options.default_venv_backend = "uv|virtualenv"
+
+ALL_PYTHONS = [
+    c.split()[-1]
+    for c in nox.project.load_toml("pyproject.toml")["project"]["classifiers"]
+    if c.startswith("Programming Language :: Python :: 3.")
+]
+
+
+DIR = Path(__file__).parent.resolve()
+
+
+@nox.session
+def lint(session):
+    """
+    Run the linter.
+    """
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files", *session.posargs)
+
+
+@nox.session(python=ALL_PYTHONS)
+def tests(session):
+    """
+    Run the unit and regular tests.
+    """
+    session.install("-e.[test,schema]")
+    session.run("pytest", *session.posargs)
+
+
+@nox.session(reuse_venv=True, default=False)
+def docs(session: nox.Session) -> None:
+    """
+    Build the docs. Use "--non-interactive" to avoid serving. Pass "-b linkcheck" to check links.
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-b", dest="builder", default="html", help="Build target (default: html)"
+    )
+    args, posargs = parser.parse_known_args(session.posargs)
+
+    serve = args.builder == "html" and session.interactive
+    extra_installs = ["sphinx-autobuild"] if serve else []
+    session.install("-e.[docs]", *extra_installs)
+
+    session.chdir("docs")
+
+    shared_args = (
+        "-n",  # nitpicky mode
+        "-T",  # full tracebacks
+        f"-b={args.builder}",
+        ".",
+        f"_build/{args.builder}",
+        *posargs,
+    )
+
+    if serve:
+        session.run("sphinx-autobuild", "--open-browser", *shared_args)
+    else:
+        session.run("sphinx-build", "--keep-going", *shared_args)
+
+
+@nox.session(default=False)
+def build(session):
+    """
+    Build an SDist and wheel.
+    """
+
+    session.install("build")
+    session.run("python", "-m", "build")
+
+
+@nox.session(venv_backend="conda", default=False)
+def root_tests(session):
+    """
+    Test against ROOT.
+    """
+
+    session.conda_install("--channel=conda-forge", "ROOT", "pytest", "boost-histogram")
+    session.install(".")
+    session.run("pytest", "tests/test_root.py")
