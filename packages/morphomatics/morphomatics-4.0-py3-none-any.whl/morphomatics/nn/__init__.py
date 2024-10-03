0572078246f486ddec781ae5544a29d4a3dbd5fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:06:51 +0000

    Add package morphomatics/4.0/morphomatics-4.0-py3-none-any.whl

---
+++ packages/morphomatics/morphomatics-4.0-py3-none-any.whl/morphomatics/nn/__init__.py
@@ -0,0 +1,15 @@
+################################################################################
+#                                                                              #
+#   This file is part of the Morphomatics library                              #
+#       see https://github.com/morphomatics/morphomatics                       #
+#                                                                              #
+#   Copyright (C) 2024 Zuse Institute Berlin                                   #
+#                                                                              #
+#   Morphomatics is distributed under the terms of the MIT License.            #
+#       see $MORPHOMATICS/LICENSE                                              #
+#                                                                              #
+################################################################################
+
+from .tangent_layers import TangentMLP, TangentInvariant
+from .wFM_layers import MfdFC, MfdInvariant
+from .flow_layers import FlowLayer, MfdGcnBlock
