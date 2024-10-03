8265bee6270cdfb83ce8da23184f3e3db4c6264a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 06:04:44 +0000

    Add package jolt/0.9.289/jolt-0.9.289.tar.gz

---
+++ packages/jolt/jolt-0.9.289.tar.gz/jolt-0.9.289/jolt/options.py
@@ -0,0 +1,15 @@
+
+class JoltOptions(object):
+    def __init__(self, **kwargs):
+        self.debug = False
+        self.default = []
+        self.download = True
+        self.keep_going = False
+        self.local = False
+        self.network = False
+        self.upload = True
+        self.worker = False
+        self.salt = None
+        self.jobs = 1
+        self.mute = False
+        self.__dict__.update(kwargs)
