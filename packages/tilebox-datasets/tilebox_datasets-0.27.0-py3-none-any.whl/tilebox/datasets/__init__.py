5d7bf9ab31e59d93129b1cbe28f7c2d61f90a603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 07:06:05 +0000

    Add package tilebox-datasets/0.27.0/tilebox_datasets-0.27.0-py3-none-any.whl

---
+++ packages/tilebox-datasets/tilebox_datasets-0.27.0-py3-none-any.whl/tilebox/datasets/__init__.py
@@ -0,0 +1,4 @@
+from tilebox.datasets.sync.client import Client
+from tilebox.datasets.sync.timeseries import TimeseriesCollection, TimeseriesDataset
+
+__all__ = ["Client", "TimeseriesDataset", "TimeseriesCollection"]
