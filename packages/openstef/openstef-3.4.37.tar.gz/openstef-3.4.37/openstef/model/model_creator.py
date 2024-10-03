9b39669e4a7a9ff2009a02214b0febc6b8ffe2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:07:18 +0000

    Add package openstef/3.4.37/openstef-3.4.37.tar.gz

---
+++ packages/openstef/openstef-3.4.37.tar.gz/openstef-3.4.37/openstef/model/model_creator.py
@@ -0,0 +1,189 @@
+# SPDX-FileCopyrightText: 2017-2023 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+import logging
+from typing import Union
+
+import structlog
+
+from openstef.enums import MLModelType
+from openstef.model.regressors.arima import ARIMAOpenstfRegressor
+from openstef.model.regressors.custom_regressor import is_custom_type, load_custom_model
+from openstef.model.regressors.lgbm import LGBMOpenstfRegressor
+from openstef.model.regressors.linear import LinearOpenstfRegressor
+from openstef.model.regressors.linear_quantile import LinearQuantileOpenstfRegressor
+from openstef.model.regressors.regressor import OpenstfRegressor
+from openstef.model.regressors.flatliner import FlatlinerRegressor
+from openstef.model.regressors.xgb import XGBOpenstfRegressor
+from openstef.model.regressors.xgb_quantile import XGBQuantileOpenstfRegressor
+from openstef.model.regressors.xgb_multioutput_quantile import (
+    XGBMultiOutputQuantileOpenstfRegressor,
+)
+from openstef.settings import Settings
+
+structlog.configure(
+    wrapper_class=structlog.make_filtering_bound_logger(
+        logging.getLevelName(Settings.log_level)
+    )
+)
+logger = structlog.get_logger(__name__)
+
+valid_model_kwargs = {
+    MLModelType.XGB: [
+        "n_estimators",
+        "objective",
+        "max_depth",
+        "learning_rate",
+        "verbosity",
+        "booster",
+        "tree_method",
+        "gamma",
+        "min_child_weight",
+        "max_delta_step",
+        "subsample",
+        "colsample_bytree",
+        "colsample_bylevel",
+        "colsample_bynode",
+        "reg_alpha",
+        "reg_lambda",
+        "scale_pos_weight",
+        "base_score",
+        "missing",
+        "num_parallel_tree",
+        "kwargs",
+        "random_state",
+        "n_jobs",
+        "monotone_constraints",
+        "interaction_constraints",
+        "importance_type",
+        "gpu_id",
+        "validate_parameters",
+        "early_stopping_rounds",
+    ],
+    MLModelType.LGB: [
+        "boosting_type",
+        "objective",
+        "num_leaves",
+        "max_depth",
+        "learning_rate",
+        "n_estimators",
+        "subsample_for_bin",
+        "min_split_gain",
+        "min_child_weight",
+        "min_child_samples",
+        "subsample",
+        "subsample_freq",
+        "colsample_bytree",
+        "reg_alpha",
+        "reg_lambda",
+        "random_state",
+        "n_jobs",
+        "silent",
+        "importance_type",
+        "early_stopping_rounds",
+    ],
+    MLModelType.XGB_QUANTILE: [
+        "quantiles",
+        "gamma",
+        "colsample_bytree",
+        "subsample",
+        "min_child_weight",
+        "max_depth",
+        "early_stopping_rounds",
+    ],
+    MLModelType.XGB_MULTIOUTPUT_QUANTILE: [
+        "quantiles",
+        "gamma",
+        "colsample_bytree",
+        "subsample",
+        "min_child_weight",
+        "max_depth",
+        "early_stopping_rounds",
+        "arctan_smoothing",
+    ],
+    MLModelType.LINEAR: [
+        "missing_values",
+        "imputation_strategy",
+        "fill_value",
+    ],
+    MLModelType.FLATLINER: [
+        "quantiles",
+    ],
+    MLModelType.LINEAR_QUANTILE: [
+        "alpha",
+        "quantiles",
+        "solver",
+        "missing_values",
+        "imputation_strategy",
+        "fill_value",
+    ],
+    MLModelType.ARIMA: [
+        "backtest_max_horizon",
+        "order",
+        "seasonal_order",
+        "trend",
+    ],
+}
+
+
+class ModelCreator:
+    """Factory object for creating machine learning models."""
+
+    # Set object mapping
+    MODEL_CONSTRUCTORS = {
+        MLModelType.XGB: XGBOpenstfRegressor,
+        MLModelType.LGB: LGBMOpenstfRegressor,
+        MLModelType.XGB_QUANTILE: XGBQuantileOpenstfRegressor,
+        MLModelType.XGB_MULTIOUTPUT_QUANTILE: XGBMultiOutputQuantileOpenstfRegressor,
+        MLModelType.LINEAR: LinearOpenstfRegressor,
+        MLModelType.LINEAR_QUANTILE: LinearQuantileOpenstfRegressor,
+        MLModelType.ARIMA: ARIMAOpenstfRegressor,
+        MLModelType.FLATLINER: FlatlinerRegressor,
+    }
+
+    @staticmethod
+    def create_model(model_type: Union[MLModelType, str], **kwargs) -> OpenstfRegressor:
+        """Create a machine learning model based on model type.
+
+        Args:
+            model_type: Model type to construct.
+            kwargs: Optional keyword argument to pass to the model.
+
+        Raises:
+            NotImplementedError: When using an invalid model_type.
+
+        Returns:
+            OpenSTEF model
+
+        """
+        try:
+            # This will raise a ValueError when an invalid model_type str is used
+            # and nothing when a MLModelType enum is used.
+            if is_custom_type(model_type):
+                model_class = load_custom_model(model_type)
+                valid_kwargs = model_class.valid_kwargs()
+            else:
+                model_type = MLModelType(model_type)
+                model_class = ModelCreator.MODEL_CONSTRUCTORS[model_type]
+                valid_kwargs = valid_model_kwargs[model_type]
+                # Check if model as imported
+                if model_class is None:
+                    raise ImportError(
+                        f"Constructor not available for '{model_type}'. "
+                        "Perhaps you forgot to install an optional dependency? "
+                        "Please refer to the ReadMe for instructions"
+                    )
+        except ValueError as e:
+            valid_types = [t.value for t in MLModelType]
+            raise NotImplementedError(
+                f"No constructor for '{model_type}', "
+                f"valid model_types are: {valid_types} "
+                "or import a custom model"
+            ) from e
+
+        # only pass relevant arguments to model constructor to prevent warnings
+        model_kwargs = {
+            key: value for key, value in kwargs.items() if key in valid_kwargs
+        }
+
+        return model_class(**model_kwargs)
---
+++ packages/openstef/openstef-3.4.37.tar.gz/openstef-3.4.37/openstef/model/objective_creator.py
@@ -0,0 +1,64 @@
+# SPDX-FileCopyrightText: 2017-2023 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from typing import Union
+
+from openstef.enums import MLModelType
+from openstef.model.objective import (
+    ARIMARegressorObjective,
+    LGBRegressorObjective,
+    LinearRegressorObjective,
+    RegressorObjective,
+    XGBQuantileRegressorObjective,
+    XGBRegressorObjective,
+    XGBMultioutputQuantileRegressorObjective,
+)
+from openstef.model.regressors.custom_regressor import (
+    create_custom_objective,
+    is_custom_type,
+)
+
+
+class ObjectiveCreator:
+    OBJECTIVES = {
+        MLModelType.XGB: XGBRegressorObjective,
+        MLModelType.LGB: LGBRegressorObjective,
+        MLModelType.XGB_QUANTILE: XGBQuantileRegressorObjective,
+        MLModelType.XGB_MULTIOUTPUT_QUANTILE: XGBMultioutputQuantileRegressorObjective,
+        MLModelType.LINEAR: LinearRegressorObjective,
+        MLModelType.LINEAR_QUANTILE: LinearRegressorObjective,
+        MLModelType.ARIMA: ARIMARegressorObjective,
+    }
+
+    @staticmethod
+    def create_objective(model_type: Union[MLModelType, str]) -> RegressorObjective:
+        """Create an objective function based on model type.
+
+        Args:
+            model_type: Model type to construct.
+
+        Raises:
+            NotImplementedError: When using an invalid model_type.
+
+        Returns:
+            Objective function
+
+        """
+        try:
+            # This will raise a ValueError when an invalid model_type str is used
+            # and nothing when a MLModelType enum is used.
+            if is_custom_type(model_type):
+                objective = create_custom_objective(model_type)
+            else:
+                model_type = MLModelType(model_type)
+                objective = ObjectiveCreator.OBJECTIVES[model_type]
+        except ValueError as e:
+            valid_types = [t.value for t in MLModelType]
+            raise NotImplementedError(
+                f"No objective for '{model_type}', "
+                f"valid model_types are: {valid_types}"
+                "or import a custom model"
+            ) from e
+
+        return objective
