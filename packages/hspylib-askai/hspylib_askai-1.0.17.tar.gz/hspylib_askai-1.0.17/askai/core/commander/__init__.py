afadaabc872ff3f6820562820368ea0f0894f338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 15:05:15 +0000

    Add package hspylib-askai/1.0.17/hspylib_askai-1.0.17.tar.gz

---
+++ packages/hspylib-askai/hspylib_askai-1.0.17.tar.gz/hspylib_askai-1.0.17/askai/core/commander/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-askai v1.0.17
+#
+# Package: main.askai.core.commander
+"""Package initialization."""
+
+__all__ = [
+    'commander', 
+    'commands'
+]
+__version__ = '1.0.17'
