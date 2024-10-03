c75b2aeeaaf32e4e74eadbaef1cb642c9828711e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 17:05:22 +0000

    Add package classes-aux/0.1.9/classes_aux-0.1.9.tar.gz

---
+++ packages/classes-aux/classes_aux-0.1.9.tar.gz/classes_aux-0.1.9/classes_aux/__init__.py
@@ -0,0 +1,64 @@
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+
+
+# =====================================================================================================================
+# TEMPLATE
+# from .main import (
+#     # BASE
+#     EXACT_OBJECTS,
+#
+#     # AUX
+#
+#     # TYPES
+#
+#     # EXX
+# )
+# ---------------------------------------------------------------------------------------------------------------------
+from .cmp import (
+    # BASE
+    CmpInst,
+    # AUX
+    # TYPES
+    # EXX
+)
+from .number import (
+    # BASE
+    NumberArithmTranslateToAttr,
+    # AUX
+    # TYPES
+    TYPE__NUMBER,
+    # EXX
+    Exx__NumberArithm_NoName,
+)
+from .getattr import (
+    # BASE
+    GetattrPrefixInst,
+    GetattrPrefixInst_RaiseIf,
+    # AUX
+    GetattrCls_Meta__Template,
+    # TYPES
+    # EXX
+    Exx__GetattrPrefix,
+    Exx__GetattrPrefix_RaiseIf,
+)
+from .getattr_echo import (
+    # BASE
+    GetattrEcho,
+    GetattrEchoSpace,
+    # AUX
+    # TYPES
+    # EXX
+)
+from .middle_group import (
+    # BASE
+    ClsMiddleGroup,
+    # AUX
+    # TYPES
+    # EXX
+)
+
+
+# =====================================================================================================================
