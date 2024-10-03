e9cf67f9b8abf57f857f5af647d87130032234ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:07:15 +0000

    Add package sunpeek/0.4.1/sunpeek-0.4.1-py3-none-any.whl

---
+++ packages/sunpeek/sunpeek-0.4.1-py3-none-any.whl/sunpeek/core_methods/__init__.py
@@ -0,0 +1,3 @@
+from sunpeek.core_methods.common.main import CoreAlgorithm, CoreStrategy, AlgoResult
+
+
---
+++ packages/sunpeek/sunpeek-0.4.1-py3-none-any.whl/sunpeek/core_methods/virtuals/__init__.py
@@ -0,0 +1,1 @@
+from sunpeek.core_methods.virtuals.main import config_virtuals, calculate_virtuals
