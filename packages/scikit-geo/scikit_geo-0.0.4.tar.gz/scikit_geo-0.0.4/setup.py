74917740118df2c4b87fe918dc18ad9684e5f038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 16:05:11 +0000

    Add package scikit-geo/0.0.4/scikit_geo-0.0.4.tar.gz

---
+++ packages/scikit-geo/scikit_geo-0.0.4.tar.gz/scikit_geo-0.0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name="scikit-geo",
+      version="0.0.4",
+      author="www",
+      url="",
+      author_email="773598627@qq.com",
+      packages=find_packages())
