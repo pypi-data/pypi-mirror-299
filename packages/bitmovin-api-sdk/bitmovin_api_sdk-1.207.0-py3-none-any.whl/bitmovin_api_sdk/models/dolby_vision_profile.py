653fa728b879169014cdfb75353bd8f78165bf73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:06:53 +0000

    Add package bitmovin-api-sdk/1.207.0/bitmovin_api_sdk-1.207.0-py3-none-any.whl

---
+++ packages/bitmovin-api-sdk/bitmovin_api_sdk-1.207.0-py3-none-any.whl/bitmovin_api_sdk/models/dolby_vision_profile.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from enum import Enum
+from six import string_types, iteritems
+from bitmovin_api_sdk.common.poscheck import poscheck_model
+
+
+class DolbyVisionProfile(Enum):
+    DVHE_04 = "DVHE_04"
+    DVHE_05 = "DVHE_05"
+    DVHE_07 = "DVHE_07"
+    DVHE_08_1 = "DVHE_08_1"
+    HEV1_08 = "HEV1_08"
+    AVC3_09 = "AVC3_09"
