dc6c7bf4ac8ad816f50d4b4127eec59557443acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 09:06:25 +0000

    Add package evo-package-pdf/2024.10.22228/evo_package_pdf-2024.10.22228-py3-none-any.whl

---
+++ packages/evo-package-pdf/evo_package_pdf-2024.10.22228-py3-none-any.whl/evo_package_pdf/__init__.py
@@ -0,0 +1,3 @@
+from evo_package_pdf.entity import *
+from evo_package_pdf.control import *
+from evo_package_pdf.utility import *
---
+++ packages/evo-package-pdf/evo_package_pdf-2024.10.22228-py3-none-any.whl/evo_package_pdf/entity/EPdfOutput.py
@@ -0,0 +1,41 @@
+#========================================================================================================================================
+# CyborgAI CC BY-NC-ND 4.0 Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International  https://github.com/cyborg-ai-git 
+#========================================================================================================================================
+
+from evo_framework.entity.EObject import EObject
+from evo_framework.core.evo_core_type.entity.EvoMap import EvoMap
+
+from evo_package_pdf.entity.EPdfPage import EPdfPage
+#========================================================================================================================================
+"""EPdfOutput
+
+    this is EPdfOutput DESCRIPTION
+    
+"""
+class EPdfOutput(EObject):
+
+    VERSION:str="416e199190f29762a510f1e0b9554fc0350ff58a6dca8bfde901ab2d531e331a"
+
+    def __init__(self):
+        super().__init__()
+        
+        self.mapEPdfPage:EvoMap = EvoMap()
+  
+    def toStream(self, stream):
+        super().toStream(stream)
+        
+        self._doWriteMap(self.mapEPdfPage, stream)
+        
+    def fromStream(self, stream):
+        super().fromStream(stream)
+        
+        self.mapEPdfPage = self._doReadMap(EPdfPage, stream)
+    
+    def __str__(self) -> str:
+        strReturn = "\n".join([
+                super().__str__(),
+                            
+                f"\tmapEPdfPage:{self.mapEPdfPage}",
+                            ]) 
+        return strReturn
+    
