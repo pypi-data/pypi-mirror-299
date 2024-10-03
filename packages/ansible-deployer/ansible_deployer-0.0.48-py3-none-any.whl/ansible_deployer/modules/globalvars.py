21aee2915e486ca3432bc8fc0fa8df052f4bfec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:07:46 +0000

    Add package ansible-deployer/0.0.48/ansible_deployer-0.0.48-py3-none-any.whl

---
+++ packages/ansible-deployer/ansible_deployer-0.0.48-py3-none-any.whl/ansible_deployer/modules/globalvars.py
@@ -0,0 +1,14 @@
+"""Global variables"""
+
+APP_CONF = "/etc/ansible-deployer"
+CFG_PERMISSIONS = "0o644"
+SUBCOMMANDS = ("run", "lock", "unlock", "verify", "show")
+PRINT_FAIL = '\033[91m'
+PRINT_END = '\033[0m'
+OPTION_EXPANSION = {
+    "infra": "infrastructure",
+    "raw_output": "raw_runner_output",
+    "conf_val": "conf_validation"
+}
+ANSIBLE_DEFAULT_CALLBACK_PLUGIN_PATH = '~/.ansible/plugins/callback:/usr/share/ansible/plugins/' \
+                                       'callback'
