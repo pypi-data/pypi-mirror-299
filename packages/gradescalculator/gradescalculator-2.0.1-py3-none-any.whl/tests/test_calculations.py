1f8f2dfe65752d26da8dbf3cbe6e4db25a6e8903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 17:05:21 +0000

    Add package gradescalculator/2.0.1/gradescalculator-2.0.1-py3-none-any.whl

---
+++ packages/gradescalculator/gradescalculator-2.0.1-py3-none-any.whl/tests/test_calculations.py
@@ -0,0 +1,16 @@
+from gradescalculator.main import calculate_grades
+
+def test_calculate_grades():
+    grades = {
+        "grade 1": {"weight": 10, "grade": 3.6},
+        "grade 2": {"weight": 15, "grade": 4.6},
+        "grade 3": {"weight": 25, "grade": 5.3},
+        "grade 4": {"weight": 5, "grade": 4.0},
+        "grade 5": {"weight": 45, "grade": 4.9}
+    }
+    
+    precise_grade, rounded_grade = calculate_grades(grades)
+    
+    assert precise_grade == 4.78
+    assert rounded_grade == 5.0
+
