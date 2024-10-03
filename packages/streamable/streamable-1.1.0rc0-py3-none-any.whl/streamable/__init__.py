251bac5d4b3cb29c7c448b228312ab86fb5e505e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 07:05:27 +0000

    Add package streamable/1.1.0rc0/streamable-1.1.0rc0-py3-none-any.whl

---
+++ packages/streamable/streamable-1.1.0rc0-py3-none-any.whl/streamable/__init__.py
@@ -0,0 +1,1 @@
+from streamable.stream import Stream
---
+++ packages/streamable/streamable-1.1.0rc0-py3-none-any.whl/tests/test_util.py
@@ -0,0 +1,17 @@
+import unittest
+
+from streamable.util.functiontools import sidify
+
+
+class TestUtil(unittest.TestCase):
+    def test_sidify(self) -> None:
+        f = lambda x: x**2
+        self.assertEqual(f(2), 4)
+        self.assertEqual(sidify(f)(2), 2)
+
+        # test decoration
+        @sidify
+        def g(x):
+            return x**2
+
+        self.assertEqual(g(2), 2)
