d7a355582e5153fe281382d2a01fb7439237e1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 03:09:37 +0000

    Add package zuko/1.3.0/zuko-1.3.0-py3-none-any.whl

---
+++ packages/zuko/zuko-1.3.0-py3-none-any.whl/zuko/flows/mixture.py
@@ -0,0 +1,13 @@
+r"""Mixture models.
+
+Warning:
+    This sub-module is deprecated and will be removed in the future. Use
+    :mod:`zuko.mixtures` instead.
+"""
+
+__all__ = [
+    "GMM",
+]
+
+# isort: split
+from ..mixtures import GMM
