41bb17bc23e9b858adb7a09a009b0f8d07558b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 09:05:06 +0000

    Add package ak_pdf2md/0.0.1/ak_pdf2md-0.0.1-py3-none-any.whl

---
+++ packages/ak_pdf2md/ak_pdf2md-0.0.1-py3-none-any.whl/ak_pdf2md/cli_app.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+from typing import Literal
+
+import typer
+from rich.console import Console
+
+from ak_pdf2md.parser import LlamaParser
+
+app = typer.Typer()
+console = Console()
+test = typer.Typer()
+
+
+def get_filepath(filepath: str | Path | None) -> Path:
+    while True:
+        if filepath is None:
+            filepath = input("Enter the PDF filepath: ")
+
+        filepath = _strip_filepath(filepath)
+
+        if filepath.suffix.casefold() != ".pdf":
+            console.print(f'[red]{filepath.suffix=} does not match ".pdf"')
+            filepath = None
+            continue
+
+        if filepath.is_file():
+            return filepath
+        else:
+            console.print(f"[red]Specified file {filepath} does not exist.")
+            filepath = None
+            continue
+
+
+def _strip_filepath(filepath: str | Path) -> Path:
+    """Strip extra quotes from filepaths"""
+    filepath = str(filepath)
+    remove_chars = ("'", '"')
+    for char in remove_chars:
+        filepath = filepath.strip(char).strip()
+    return Path(filepath)
+
+
+@app.command()
+def convert_pdf(
+    filepath: str = "",
+    dest_dir: str = "",
+    markdown_output: bool = True,
+):
+    """convert pdf file to markdown"""
+    if filepath == "":
+        filepath = None  # type: ignore
+    if dest_dir == "":
+        dest_dir = None  # type: ignore
+    _filepath: Path = get_filepath(filepath)
+
+    if dest_dir is not None:
+        dest_dir = _strip_filepath(dest_dir)  # type: ignore
+
+    parser = LlamaParser()
+    ext = ".md" if markdown_output else ".txt"
+    parser.convert(filepath=_filepath, dest_dir=dest_dir, extension=ext)
+
+
+@test.command()
+def test_config():
+    from ak_pdf2md import test_configs
+
+    test_configs()
---
+++ packages/ak_pdf2md/ak_pdf2md-0.0.1-py3-none-any.whl/ak_pdf2md/parser.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+from typing import Literal
+
+import nest_asyncio
+from llama_parse import LlamaParse, ResultType
+from rich.console import Console
+from rich.panel import Panel
+
+from ak_pdf2md.utils.config_parser import Config
+
+_config = Config()
+
+nest_asyncio.apply()
+
+
+class LlamaParser:
+    VERBOSITY: bool = False
+    __console = Console()
+
+    def __init__(self) -> None:
+        pass
+
+    def __parser(
+        self, result_type: Literal[".md", ".txt"], verbose: bool = VERBOSITY, **kwargs
+    ):
+        match result_type.casefold():
+            case ".md":
+                _result_type = ResultType.MD
+            case ".txt":
+                _result_type = ResultType.TXT
+            case _:
+                raise Exception(f"{result_type=} not in `Literal['.md', '.txt']`")
+
+        return LlamaParse(
+            api_key=_config.get(keys=("llamaparse", "apiKey")),
+            result_type=_result_type,
+            verbose=verbose,
+            parsing_instruction="",
+            skip_diagonal_text=False,
+            do_not_unroll_columns=False,
+            **kwargs,
+        )
+
+    def __write(self, dest_path: Path, contents: str) -> Path:
+        """Write the contents of markdown document to file"""
+
+        with open(dest_path, "w", encoding="utf-8") as f:
+            f.write(contents)
+        return dest_path
+
+    def __process_pdf(
+        self, pdf_path: Path, result_type: Literal[".md", ".txt"], **kwargs
+    ) -> str:
+        docs = self.__parser(result_type=result_type, **kwargs).load_data(str(pdf_path))
+        return "\n".join([doc.text for doc in docs])
+
+    def convert(
+        self,
+        filepath: Path,
+        dest_dir: Path | None = None,
+        extension: Literal[".md", ".txt"] = ".md",
+        **kwargs,
+    ):
+        dest_dir = dest_dir or filepath.parent
+        destpath: Path = dest_dir / f"{filepath.stem}{extension}"
+        with self.__console.status(
+            f"Converting {filepath.name} to {destpath.name}...", spinner="dots"
+        ):
+            _data = self.__process_pdf(
+                pdf_path=filepath, result_type=extension, **kwargs
+            )
+
+        self.__write(dest_path=destpath, contents=_data)
+        self.__console.print(
+            Panel(
+                f'[green bold]Contents written to "{str(destpath)}"',
+                title="Success",
+                border_style="green",
+            )
+        )
