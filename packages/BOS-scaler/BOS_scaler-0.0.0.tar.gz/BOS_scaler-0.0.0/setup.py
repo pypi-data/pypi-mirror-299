b0d90fd9dcc32b17b4075c4e37aae3aae79309b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 15:05:39 +0000

    Add package BOS-scaler/0.0.0/BOS_scaler-0.0.0.tar.gz

---
+++ packages/BOS-scaler/BOS_scaler-0.0.0.tar.gz/BOS_scaler-0.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BOS_scaler',            # Tên thư viện
+    version='',                # Phiên bản thư viện
+    description='Scaler_image',  # Mô tả ngắn gọn về thư viện
+    author='minhle',           # Tên tác giả
+    author_email='xuanminh.xm201@gmail.com',  # Email của tác giả
+    packages=find_packages(),      # Tìm các package (thư mục chứa __init__.py)
+    install_requires=[             # Các thư viện phụ thuộc nếu có
+        # 'dependency1',
+        # 'dependency2',
+    ],
+    classifiers=[                  # Danh sách các phân loại (optional)
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',      # Phiên bản Python yêu cầu tối thiểu
+)
