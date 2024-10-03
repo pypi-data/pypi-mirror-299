ce74e4d61c607f83ca261223f640b34abce12e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 07:05:31 +0000

    Add package floatcsep/0.2.0/floatcsep-0.2.0-py3-none-any.whl

---
+++ packages/floatcsep/floatcsep-0.2.0-py3-none-any.whl/floatcsep/__init__.py
@@ -0,0 +1,8 @@
+from floatcsep import evaluation
+from floatcsep import experiment
+from floatcsep import model
+from floatcsep.infrastructure import engine, environments, registries, repositories, logger
+from floatcsep.utils import readers, accessors, helpers
+from floatcsep.postprocess import reporting, plot_handler
+
+__version__ = "0.1.4"
