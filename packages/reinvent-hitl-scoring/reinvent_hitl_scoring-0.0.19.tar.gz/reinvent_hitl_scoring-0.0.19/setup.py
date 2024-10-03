e79fcd0a05246d45db7fb6637f629c9ec0fc4f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 08:07:32 +0000

    Add package reinvent-hitl-scoring/0.0.19/reinvent_hitl_scoring-0.0.19.tar.gz

---
+++ packages/reinvent-hitl-scoring/reinvent_hitl_scoring-0.0.19.tar.gz/reinvent_hitl_scoring-0.0.19/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="reinvent_hitl_scoring",
+    version="0.0.19",
+    author="Yasmine Nahal",
+    author_email="yasminenahal@aalto.fi",
+    description="Scoring functions for Reinvent adapted to the HITL_AL_GOMG workflow",
+    url="https://github.com/yasminenahal/reinvent-hitl-scoring.git",
+    package_data={"reinvent_hitl_scoring": ["scoring/score_components/synthetic_accessibility/fpscores.pkl.gz", "configs/test_config.json"]},
+    packages=setuptools.find_packages(exclude='unittest_reinvent'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
