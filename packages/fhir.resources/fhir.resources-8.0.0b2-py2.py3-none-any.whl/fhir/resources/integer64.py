224c245451f41a208890ffb2336efaa0c6731d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 09:05:43 +0000

    Add package fhir.resources/8.0.0b2/fhir.resources-8.0.0b2-py2.py3-none-any.whl

---
+++ packages/fhir.resources/fhir.resources-8.0.0b2-py2.py3-none-any.whl/fhir/resources/integer64.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Profile: http://hl7.org/fhir/StructureDefinition/integer64
+Release: R5
+Version: 5.0.0
+Build ID: 2aecd53
+Last updated: 2023-03-26T15:21:02.749+11:00
+"""
+from pydantic import Field
+
+from . import fhirtypes, primitivetype
+
+
+class Integer64(primitivetype.PrimitiveType):
+    """Disclaimer: Any field name ends with ``__ext`` doesn't part of
+    Resource StructureDefinition, instead used to enable Extensibility feature
+    for FHIR Primitive Data Types.
+
+    Primitive Type integer64.
+    A very large whole number
+    """
+
+    __resource_type__ = "integer64"
+
+    value: fhirtypes.Integer64Type = Field(  # type: ignore
+        None,
+        alias="value",
+        title="Primitive value for integer64",
+        description="Primitive value for integer64",
+        json_schema_extra={
+            "element_property": True,
+        },
+    )
+    value__ext: fhirtypes.FHIRPrimitiveExtensionType = Field(  # type: ignore
+        None, alias="_value", title="Extension field for ``value``."
+    )
+
+    @classmethod
+    def elements_sequence(cls):
+        """returning all elements names from
+        ``Integer64`` according specification,
+        with preserving original sequence order.
+        """
+        return ["id", "extension", "value"]
