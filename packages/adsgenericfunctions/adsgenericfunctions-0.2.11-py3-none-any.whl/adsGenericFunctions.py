079560eefdfa3ce8b8da69c403d6dc0e7659aa9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 06:04:39 +0000

    Add package adsgenericfunctions/0.2.11/adsgenericfunctions-0.2.11-py3-none-any.whl

---
+++ packages/adsgenericfunctions/adsgenericfunctions-0.2.11-py3-none-any.whl/adsGenericFunctions.py
@@ -0,0 +1,17 @@
+#from email import *
+
+from adsGenericFunctions.postgre.dbPgsql import *
+from adsGenericFunctions.mssql.dbMssql import *
+from adsGenericFunctions.pipeline import *
+from adsGenericFunctions.timer import *
+from adsGenericFunctions.logger import *
+from adsGenericFunctions.global_config import *
+
+
+def test():
+    print("YES")
+
+if __name__ == "__main__":
+    print("Hello")
+
+
