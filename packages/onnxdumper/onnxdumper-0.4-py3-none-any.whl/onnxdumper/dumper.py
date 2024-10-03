8bb988336c708f227465db97c57e8bb2a91cc22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 04:05:54 +0000

    Add package onnxdumper/0.4/onnxdumper-0.4-py3-none-any.whl

---
+++ packages/onnxdumper/onnxdumper-0.4-py3-none-any.whl/onnxdumper/dumper.py
@@ -0,0 +1,45 @@
+import datetime
+import numpy as np
+import os
+import onnx
+import onnxruntime
+
+
+class InferenceSession(onnxruntime.InferenceSession):
+    def __init__(
+        self,
+        path_or_bytes: str | bytes | os.PathLike,
+        dump_path: str | os.PathLike = None,
+        **kwargs,
+    ) -> None:
+        # 获取模型名称
+        if isinstance(path_or_bytes, (str, os.PathLike)):
+            self.model_name = os.path.basename(path_or_bytes)
+        else:
+            self.model_name = "model"
+        # 设置dump路径
+        self.dump_path = dump_path
+        # 创建影子Session
+        self.model = onnx.load(path_or_bytes)
+        # 修改模型输出节点tag
+        for node in self.model.graph.node:
+            for output in node.output:
+                self.model.graph.output.extend([onnx.ValueInfoProto(name=output)])
+        self.ShadowSession = onnxruntime.InferenceSession(
+            self.model.SerializeToString()
+        )
+        super().__init__(path_or_bytes, **kwargs)
+
+    def run(self, output_names, input_feed, run_options=None):
+        # 调用父类的run方法
+        super_results = super().run(output_names, input_feed, run_options)
+        # 调用影子Session的run方法
+        outputs = [x.name for x in self.ShadowSession.get_outputs()]
+        ort_outs = self.ShadowSession.run(outputs, input_feed, run_options)
+        # 生成字典，便于查找层对应输出
+        ort_outs = dict(zip(outputs, ort_outs))
+        # numpy写出到文件
+        date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        file_name = f"{self.model_name}_{date}"
+        np.savez(self.dump_path if self.dump_path else file_name, **ort_outs)
+        return super_results
