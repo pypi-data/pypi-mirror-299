6ac89cd5da52ce99f4b85b6285859911731dc05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 17:05:29 +0000

    Add package gradescalculator/2.0.2/gradescalculator-2.0.2.tar.gz

---
+++ packages/gradescalculator/gradescalculator-2.0.2.tar.gz/gradescalculator-2.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gradescalculator',
+    version='2.0.2',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'average=gradescalculator.main:main',
+        ],
+    },
+    author='Quentin Berthet',
+    author_email='quentin@berthet.ch',
+    description='Un package pour calculer des notes avec des poids différents.',
+    url='https://github.com/BERTHETquentin/GradesCalculator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
