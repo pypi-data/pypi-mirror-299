a680a2e5b3eb173442e13c0e0ad17b0253dd9d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 06:04:42 +0000

    Add package pypath-omnipath/0.16.17/pypath_omnipath-0.16.17.tar.gz

---
+++ packages/pypath-omnipath/pypath_omnipath-0.16.17.tar.gz/pypath_omnipath-0.16.17/pypath/_metadata.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `pypath` python module
+#
+#  Copyright 2014-2023
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  Authors: see the file `README.rst`
+#  Contact: Dénes Türei (turei.denes@gmail.com)
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      https://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: https://pypath.omnipathdb.org/
+#
+
+"""
+Package metadata (version, authors, etc).
+"""
+
+__all__ = ['get_metadata']
+
+import os
+import pathlib
+import importlib.metadata
+
+import toml
+import itertools
+
+_VERSION = '0.16.17'
+
+
+def get_metadata():
+    """
+    Basic package metadata.
+
+    Retrieves package metadata from the current project directory or from
+    the installed package.
+    """
+
+    here = pathlib.Path(__file__).parent
+    pyproj_toml = 'pyproject.toml'
+    meta = {}
+
+    for project_dir in (here, here.parent, here.parent.parent):
+
+        toml_path = str(project_dir.joinpath(pyproj_toml).absolute())
+
+        if os.path.exists(toml_path):
+
+            pyproject = toml.load(toml_path)
+            try:
+                packages = set(
+                    itertools.chain(*(
+                        package.values() 
+                        for package in pyproject['tool']['poetry']['packages']
+                    ))
+                )
+            except KeyError:
+                packages = set()
+
+            name = __name__.split('.')[0]
+            if  name == pyproject['tool']['poetry']['name'] or name in packages:
+
+                meta = {
+                    'name': pyproject['tool']['poetry']['name'],
+                    'version': pyproject['tool']['poetry']['version'],
+                    'author': pyproject['tool']['poetry']['authors'],
+                    'license': pyproject['tool']['poetry']['license'],
+                    'full_metadata': pyproject,
+                }
+
+                break
+
+    if not meta:
+
+        try:
+
+            meta = {
+                k.lower(): v for k, v in
+                importlib.metadata.metadata(here.name).items()
+            }
+
+        except importlib.metadata.PackageNotFoundError:
+
+            pass
+
+    meta['version'] = meta.get('version', None) or _VERSION
+
+    return meta
+
+
+metadata = get_metadata()
+__version__ = metadata.get('version', None)
+__author__ = metadata.get('author', None)
+__license__ = metadata.get('license', None)
