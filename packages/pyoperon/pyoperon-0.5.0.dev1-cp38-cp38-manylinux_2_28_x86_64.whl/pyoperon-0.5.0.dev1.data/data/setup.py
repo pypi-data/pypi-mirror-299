2ee233ee8430ade65bfe02d61c266f87a69e7a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 09:06:22 +0000

    Add package pyoperon/0.5.0.dev1/pyoperon-0.5.0.dev1-cp38-cp38-manylinux_2_28_x86_64.whl

---
+++ packages/pyoperon/pyoperon-0.5.0.dev1-cp38-cp38-manylinux_2_28_x86_64.whl/pyoperon-0.5.0.dev1.data/data/setup.py
@@ -0,0 +1,18 @@
+from skbuild import setup  # This line replaces 'from setuptools import setup'
+import platform
+
+build_preset = ''
+if platform.system() == 'Linux':
+    build_preset = 'build-linux'
+elif platform.system() == 'Darwin':
+    build_preset = 'build-osx'
+
+setup(
+    name="pyoperon",
+    version="0.5.0.dev1",
+    description="python bindings for the operon library",
+    author='Bogdan Burlacu',
+    packages=['pyoperon'],
+    python_requires=">=3.8",
+    cmake_args=[f'--preset {build_preset}' if build_preset != '' else '']
+)
