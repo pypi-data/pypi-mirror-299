da32d066b376eb69fe95bf679c4b5abd6d84b8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 03:09:57 +0000

    Add package mtmai/0.3.1017/mtmai-0.3.1017-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.1017-py3-none-any.whl/mtmai/agents/states/state.py
@@ -0,0 +1,7 @@
+from typing import Annotated
+from langgraph.graph.message import AnyMessage, add_messages
+from typing_extensions import TypedDict
+
+class MainState(TypedDict):
+    prompt: str | None = None
+    messages: Annotated[list[AnyMessage], add_messages]
