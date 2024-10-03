539e3a1cda006d9ac520d42f00459a451b36e205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 04:07:27 +0000

    Add package torch-uncertainty/0.2.2.post2/torch_uncertainty-0.2.2.post2-py3-none-any.whl

---
+++ packages/torch-uncertainty/torch_uncertainty-0.2.2.post2-py3-none-any.whl/torch_uncertainty/transforms/cutout.py
@@ -0,0 +1,42 @@
+import numpy as np
+import torch
+from torch import nn
+
+
+class Cutout(nn.Module):
+    def __init__(self, length: int, value: int = 0) -> None:
+        """Cutout augmentation class.
+
+        Args:
+            length (int): Length of the cutout square.
+            value (int): Pixel value to be filled in the cutout square.
+        """
+        super().__init__()
+        self.rng = np.random.default_rng()
+
+        if length <= 0:
+            raise ValueError("Cutout length must be positive.")
+        self.length = length
+
+        if value < 0 or value > 255:
+            raise ValueError("Cutout value must be between 0 and 255.")
+        self.value = value
+
+    def __call__(self, img: torch.Tensor) -> torch.Tensor:
+        if len(img.shape) == 2:
+            img = img.unsqueeze(0)
+        h, w = img.size(1), img.size(2)
+        mask = np.ones((h, w), np.float32)
+        y = self.rng.integers(low=0, high=h - 1)
+        x = self.rng.integers(low=0, high=w - 1)
+
+        y1 = np.clip(y - self.length // 2, 0, h)
+        y2 = np.clip(y + self.length // 2, 0, h)
+        x1 = np.clip(x - self.length // 2, 0, w)
+        x2 = np.clip(x + self.length // 2, 0, w)
+
+        mask[y1:y2, x1:x2] = self.value
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img *= mask
+        return img
