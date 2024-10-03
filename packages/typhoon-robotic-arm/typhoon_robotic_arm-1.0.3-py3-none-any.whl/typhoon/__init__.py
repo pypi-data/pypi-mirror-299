66caee2d54f99e281bb36cb3cf146dfc1899f1da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 06:05:04 +0000

    Add package typhoon-robotic-arm/1.0.3/typhoon_robotic_arm-1.0.3-py3-none-any.whl

---
+++ packages/typhoon-robotic-arm/typhoon_robotic_arm-1.0.3-py3-none-any.whl/typhoon/__init__.py
@@ -0,0 +1,15 @@
+from .main import Typhoon
+
+from importlib.metadata import version
+__version__ = version("typhoon_robotic_arm")
+
+print(r"""
+  _______          _
+ |__   __|        | |
+    | |_   _ _ __ | |__   ___   ___  _ __
+    | | | | | '_ \| '_ \ / _ \ / _ \| '_ \
+    | | |_| | |_) | | | | (_) | (_) | | | |
+    |_|\__, | .__/|_| |_|\___/ \___/|_| |_|
+        __/ | |
+       |___/|_|
+""")
---
+++ packages/typhoon-robotic-arm/typhoon_robotic_arm-1.0.3-py3-none-any.whl/typhoon/utils/config.py
@@ -0,0 +1,4 @@
+LENGTH_UPPER_ARM = 250
+LENGTH_LOWER_ARM = 220
+HEIGHT_FROM_GROUND = 135
+STEPS_PER_REVOLUTION = 200 * 16 * 10 # stepper motor steps per revolution * micro stepping multiplier * planetary gear box multiplier
