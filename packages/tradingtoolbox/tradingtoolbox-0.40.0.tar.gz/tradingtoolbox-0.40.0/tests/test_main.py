47129cc68d55f9017ce88cf1d0daac3c10ecadd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 06:04:24 +0000

    Add package tradingtoolbox/0.40.0/tradingtoolbox-0.40.0.tar.gz

---
+++ packages/tradingtoolbox/tradingtoolbox-0.40.0.tar.gz/tradingtoolbox-0.40.0/tests/test_main.py
@@ -0,0 +1,12 @@
+from tradingtoolbox import print
+
+
+def test_main():
+    # os.remove("./logs/logs.log")
+    print("Hello")
+
+    # logger.warning("Warning")
+    # logger.error("Error message")
+
+    a = 4
+    assert a == 4
