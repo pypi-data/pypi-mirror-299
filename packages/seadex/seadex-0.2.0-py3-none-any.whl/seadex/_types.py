61186c8c84a65afd241cc0bb8d3da35893668d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 09:06:12 +0000

    Add package seadex/0.2.0/seadex-0.2.0-py3-none-any.whl

---
+++ packages/seadex/seadex-0.2.0-py3-none-any.whl/seadex/_types.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from datetime import datetime, timezone
+from os import PathLike
+from typing import Annotated, TypeAlias, Union
+
+from pydantic import AfterValidator
+
+StrPath: TypeAlias = Union[str, PathLike[str]]
+
+UTCDateTime: TypeAlias = Annotated[datetime, AfterValidator(lambda dt: dt.astimezone(timezone.utc))]
+"""Datetime that's always in UTC."""
