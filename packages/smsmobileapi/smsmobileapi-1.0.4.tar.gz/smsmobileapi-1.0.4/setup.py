579ec441b95c54d4bb7355a3b353735697dbdfef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:06:10 +0000

    Add package smsmobileapi/1.0.4/smsmobileapi-1.0.4.tar.gz

---
+++ packages/smsmobileapi/smsmobileapi-1.0.4.tar.gz/smsmobileapi-1.0.4/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="smsmobileapi",
+    version="1.0.4",
+    author="Quest-Concept",
+    author_email="info@smsmobileapi.com",
+    description="A module that allows sending SMS from your own mobile phone and receiving SMS on your mobile phone, all for free since the mobile plan is used",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/smsmobileapi",  # Remplace par ton URL GitHub
+    packages=find_packages(),
+    install_requires=[
+        'requests',  # Dépendance à requests pour faire des requêtes HTTP
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
