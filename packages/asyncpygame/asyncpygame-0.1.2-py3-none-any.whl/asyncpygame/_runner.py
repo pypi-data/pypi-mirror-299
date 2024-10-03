69e32b3aadf7d439db201575bf7fd9e9833f81a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 14:06:07 +0000

    Add package asyncpygame/0.1.2/asyncpygame-0.1.2-py3-none-any.whl

---
+++ packages/asyncpygame/asyncpygame-0.1.2-py3-none-any.whl/asyncpygame/_runner.py
@@ -0,0 +1,45 @@
+__all__ = ("run", "quit", )
+
+import pygame
+import asyncpygame as ap
+
+
+class AppQuit(Exception):
+    pass
+
+
+def quit(*args):
+    raise AppQuit()
+
+
+def run(main_func, *, fps=30, auto_quit=True):
+    pygame_clock = pygame.Clock()
+    clock = ap.Clock()
+    sdlevent = ap.SDLEvent()
+    executor = ap.PriorityExecutor()
+    main_task = ap.start(main_func(clock=clock, sdlevent=sdlevent, executor=executor, pygame_clock=pygame_clock))
+
+    if auto_quit:
+        sdlevent.subscribe((pygame.QUIT, ), quit)
+        sdlevent.subscribe((pygame.KEYDOWN, ), lambda e, K=pygame.K_ESCAPE: e.key == K and quit())
+
+    # LOAD_FAST
+    pygame_event_get = pygame.event.get
+    pygame_clock_tick = pygame_clock.tick
+    clock_tick = clock.tick
+    sdlevent_dispatch = sdlevent.dispatch
+
+    try:
+        while True:
+            for event in pygame_event_get():
+                sdlevent_dispatch(event)
+            clock_tick(pygame_clock_tick(fps))
+            executor()
+    except AppQuit:
+        pass
+    except ap.ExceptionGroup as group:
+        unignorable_excs = tuple(e for e in group.exceptions if not isinstance(e, AppQuit))
+        if unignorable_excs:
+            raise ap.ExceptionGroup(group.message, unignorable_excs)
+    finally:
+        main_task.cancel()
