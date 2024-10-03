9a644a49857ecfcc76a8502aaa2aa0f5b64c2df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 16:05:19 +0000

    Add package cytolncpred/1.2/cytolncpred-1.2.tar.gz

---
+++ packages/cytolncpred/cytolncpred-1.2.tar.gz/cytolncpred-1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from setuptools import  find_namespace_packages
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='cytolncpred',
+    version='1.2',
+    description='A computational method to predict the probability of lncRNA localizing to cytoplasm',
+    author='Gajendra P.S. Raghava',
+    author_email='raghava@iiitd.ac.in',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license_files = ('LICENSE',),
+    url='https://github.com/raghavagps/cytolncpred', 
+    packages=find_namespace_packages(where="src"),
+    package_dir={'':'src'},
+    package_data={'cytolncpred.nfeature':['*'],
+    'cytolncpred.models':['*']},
+    entry_points={ 'console_scripts' : ['cytolncpred = cytolncpred.python_scripts.cytolncpred:main']},
+    include_package_data=True,
+    python_requires='>=3.8',
+    install_requires=[
+        'numpy==2.1.1', 'pandas==2.2.3', 'scikit-learn==1.5.2', 'argparse', 'xgboost==2.1.1'  # Add any Python dependencies here
+    ]
+)
