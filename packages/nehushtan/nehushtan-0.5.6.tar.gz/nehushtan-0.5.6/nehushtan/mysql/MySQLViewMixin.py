468713a522d841d00fd21dfbcf154be09e63fe62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 03:09:42 +0000

    Add package nehushtan/0.5.6/nehushtan-0.5.6.tar.gz

---
+++ packages/nehushtan/nehushtan-0.5.6.tar.gz/nehushtan-0.5.6/nehushtan/mysql/MySQLViewMixin.py
@@ -0,0 +1,24 @@
+import time
+from abc import ABC
+from typing import Optional
+
+from nehushtan.mysql.MySQLTableExistence import MySQLTableExistence
+from nehushtan.mysql.MySQLTableSelection import MySQLTableSelection
+
+
+class MySQLViewMixin(MySQLTableExistence, ABC):
+    """
+    For View, actually, READONLY table.
+    Since 0.1.4
+    """
+
+    @staticmethod
+    def now() -> str:
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+    @staticmethod
+    def today() -> str:
+        return time.strftime("%Y-%m-%d", time.localtime())
+
+    def select_in_table(self, alias: Optional[str] = None):
+        return MySQLTableSelection(model=self, alias=alias)
