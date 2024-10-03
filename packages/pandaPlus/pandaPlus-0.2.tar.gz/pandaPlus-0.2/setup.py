b02f6c671f4ab1208123da15614e241352486916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 08:06:17 +0000

    Add package pandaPlus/0.2/pandaPlus-0.2.tar.gz

---
+++ packages/pandaPlus/pandaPlus-0.2.tar.gz/pandaPlus-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pandaPlus',
+    version='0.2',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    license = 'MIT',
+    author = 'Tiara Mathur',
+    author_email = 'mathurtiara@gmail.com',
+    url = 'https://github.com/tiaramathur/',
+    keywords = ['pandas', 'formatting', 'dataframes'],
+    install_requires=[
+        'pandas',
+	'xlsxwriter'
+    ],
+)
---
+++ packages/pandaPlus/pandaPlus-0.2.tar.gz/pandaPlus-0.2/src/pandaPlus/pandasPlus.py
@@ -0,0 +1,27 @@
+'''
+Function to write dataframe to excel without Pandas bold formatting.
+'''
+import xlsxwriter
+import os
+import pandas as pd
+
+def clearFormatXlsx(df, path = r'/Users/' + os.getlogin() + '/Downloads/', fileName = 'Data', sheet = 1, sheetName = 'Sheet' + 1, idx = False):
+
+    #Save the columns
+    columns = df.columns
+
+    #Output to excel
+    writer = pd.ExcelWriter(path + fileName + '.xlsx', engine = 'xlsxwriter')
+
+    #Write with no header
+    df.to_excel(writer, sheetName, startrow = sheet, header = False, index = idx)
+
+    # Get workbook and worksheet
+    workbook  = writer.book
+    worksheet = writer.sheets[sheetName]
+
+    #Add the columns back in
+    for i, val in enumerate(columns):
+        worksheet.write(0, i, val)
+
+    writer.close()
