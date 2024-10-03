d2763b664d437a1a3bb6c3970224aa99d13d51f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 04:07:15 +0000

    Add package mujoco-ar/1.2.0/mujoco_ar-1.2.0.tar.gz

---
+++ packages/mujoco-ar/mujoco_ar-1.2.0.tar.gz/mujoco_ar-1.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mujoco_ar',
+    version='1.2.0',
+    description='An AR controller designed to seamlessly control objects in MuJoCo and for other general uses by receiving position and rotation data from a connected iOS application.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Omar Rayyan',
+    author_email='olr7742@nyu.edu',
+    url='https://github.com/omarrayyann/mujocoar',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'asyncio',
+        'websockets',
+        'numpy',
+        'psutil',
+        'opencv-python',
+        'mujoco',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
