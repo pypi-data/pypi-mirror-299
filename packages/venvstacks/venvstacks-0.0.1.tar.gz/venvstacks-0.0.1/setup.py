81f5dab0d729b2d56f5520c8623912b941f2f716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 08:05:29 +0000

    Add package venvstacks/0.0.1/venvstacks-0.0.1.tar.gz

---
+++ packages/venvstacks/venvstacks-0.0.1.tar.gz/venvstacks-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='venvstacks',
+    version='0.0.1',
+    description='venvstacks',
+    author='LM Studio',
+    author_email='team@lmstudio.ai',
+    zip_safe=False
+)
