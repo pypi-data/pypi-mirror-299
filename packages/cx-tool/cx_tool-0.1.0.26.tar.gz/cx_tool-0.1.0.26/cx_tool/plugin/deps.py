24bdcdd8841e201b82ce98fda83b0ef2eb4b8da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 08:06:53 +0000

    Add package cx-tool/0.1.0.26/cx_tool-0.1.0.26.tar.gz

---
+++ packages/cx-tool/cx_tool-0.1.0.26.tar.gz/cx_tool-0.1.0.26/cx_tool/plugin/deps.py
@@ -0,0 +1,282 @@
+import importlib
+import json
+import sys
+from functools import cached_property
+import importlib.metadata
+from pathlib import Path
+from typing import Any, Dict
+
+from plumbum import ProcessExecutionError
+from pydantic import AnyUrl, validate_call
+from rich.console import Console
+from rich.table import Table
+
+from cx_tool.plugin.plugin import (
+    CrossOverControlPlugin,
+    Plugin,
+    clickable,
+    save_config,
+)
+
+import click
+
+
+class DepsPlugin(Plugin):
+    """Plugin to manage dependencies.
+
+    \b
+    Checks for missing dependencies and installs them.
+    Handles adding and removing plugins.
+    """
+
+    name = "deps"
+
+    @property
+    def crossover(self) -> CrossOverControlPlugin:
+        return Plugin.get_plugin("crossover")
+
+    @property
+    def requirements(self) -> dict[str, dict[str, Any]]:
+        return self.data.setdefault("requirements", {})
+
+    @cached_property
+    def req_group(self):
+        return self.cli.group("r", help="Manage plugin requirements.")(lambda: None)
+
+    @cached_property
+    def plugin_group(self):
+        return self.cli.group("p", help="Manage plugin modules.")(lambda: None)
+
+    @cached_property
+    def installed_packages(self) -> Dict[str, Dict[str, Any]]:
+        """Get installed packages in venv."""
+
+        return {
+            p["name"]: p
+            for p in json.loads(self.crossover.pip["list", "--format", "json"]())
+        }
+
+    @cached_property
+    def pip_inspect(self) -> Dict[str, Any]:
+        return json.loads(self.crossover.pip("inspect"))
+
+    def inspect_installed_package(self, package: AnyUrl | Path | str) -> Dict[str, Any]:
+        """Inspect installed package."""
+
+        for p in self.pip_inspect["installed"]:
+            if (
+                isinstance(package, AnyUrl)
+                and p.get("direct_url", {}).get("url") == package
+            ):
+                return p
+
+            elif (
+                isinstance(package, Path)
+                and p.get("direct_url", {}).get("url", "").startswith("file://")
+                and Path(p["direct_url"]["url"][7:]).resolve() == package.resolve()
+            ):
+                return p
+
+            elif p["metadata"]["name"] == package:
+                return p
+
+        return {}
+
+    @staticmethod
+    def find_package_in_metadata(metadata_path: Path):
+        metadata = importlib.metadata.PathDistribution(metadata_path)
+
+        return [f for f in metadata.files if ".dist-info" not in f.as_posix()]
+
+    def pip_install(self, *packages: str, allow_error: bool = False):
+        """Install package using pip."""
+
+        try:
+            self.crossover.pip["install", "--ignore-installed", *packages].run(
+                stderr=sys.stderr
+            )
+        except ProcessExecutionError as e:
+            if allow_error:
+                return
+
+            sys.exit(e.retcode)
+
+    @clickable
+    def list_requirements(self):
+        """List all requirements."""
+
+        table = Table(
+            title="Requirements", show_header=True, header_style="bold magenta"
+        )
+
+        table.add_column("Package", max_width=30)
+        table.add_column("Source", max_width=30)
+        table.add_column("Version")
+        table.add_column("Installed")
+        table.add_column("Modules")
+
+        installed = self.installed_packages
+
+        for name, req in self.requirements.items():
+            table.add_row(
+                name,
+                req.get("source"),
+                req.get("version", "*"),
+                installed.get(name, {}).get("version", ""),
+                "\n".join(req.get("modules", [])),
+            )
+
+        console = Console()
+        console.print(table)
+
+    @click.argument("package")
+    @click.option("--version", help="Version of the package.")
+    @clickable
+    @save_config
+    @validate_call
+    def add_requirement(self, package: AnyUrl | Path, version: str | None = None):
+        """Add a plugin to the list of plugins to load."""
+
+        if isinstance(package, Path) and not package.exists():
+            package = str(package)
+
+        if version is not None:
+            if version[0] not in ("=", "<", ">", "!"):
+                version = f"=={version}"
+
+        self.pip_install(f"{package}{version}" if version else package)
+
+        modules = set()
+        if pkg := self.inspect_installed_package(package):
+            for path in self.find_package_in_metadata(Path(pkg["metadata_location"])):
+                modules.add(path.parts[0])
+
+        package_name = pkg["metadata"]["name"]
+
+        self.requirements[package_name] = {
+            "modules": list(modules),
+            "source": package,
+        }
+
+        if version is not None:
+            self.requirements[package_name]["version"] = version
+
+        click.echo("Requirement added and installed.")
+
+    @click.argument("package")
+    @clickable
+    @save_config
+    def remove_requirement(self, package: str):
+        """Remove a plugin from the list of plugins to load."""
+
+        if package not in self.requirements:
+            raise click.UsageError(
+                f"Package `{package}` not found in list of requirements."
+            )
+
+        self.crossover.pip["uninstall", package, "-y"].run(stderr=sys.stderr)
+        self.requirements.pop(package)
+
+        click.echo("Requirement removed and uninstalled.")
+
+    @click.argument("module")
+    @clickable
+    @save_config
+    def enable_plugin(self, module: str):
+        """Enable a plugin module."""
+
+        try:
+            plugins = self.find_plugins_in_module(module)
+            if not plugins:
+                raise click.UsageError(
+                    f"Module `{module}` is not a crossover-util plugin library."
+                )
+        except ImportError:
+            raise click.UsageError(f"Package `{module}` not found.")
+
+        self.config.plugins.add(module)
+
+        click.echo("Plugin enabled.")
+
+    @click.argument("module")
+    @clickable
+    @save_config
+    def disable_plugin(self, module: str):
+        """Disable a plugin module."""
+
+        if module not in self.config.plugins:
+            raise click.UsageError(f"Module `{module}` not found in list of plugins.")
+
+        self.config.plugins.remove(module)
+
+    @clickable
+    def list_plugins(self):
+        """List all plugins."""
+
+        table = Table(title="Plugins", show_header=True, header_style="bold magenta")
+
+        table.add_column("Name", max_width=30)
+        table.add_column("Platform")
+        table.add_column("Summary")
+
+        for plugin in self.all_plugins():
+            table.add_row(
+                plugin.name,
+                ", ".join(plugin.platforms),
+                plugin.__doc__.strip().split("\n")[0],
+            )
+
+        console = Console()
+        console.print(table)
+
+    @clickable
+    def reset(self):
+        """Reset all requirements and plugins."""
+
+        ctx = click.get_current_context()
+
+        self.data.clear()
+        self.config.plugins.clear()
+        self.config.write()
+
+        click.echo("All requirements and plugins reset.")
+
+        ctx.invoke(self.crossover.install)
+
+    def setup_cli(self):
+        if self.config is None:
+            return
+
+        self.cli_command("list", self.req_group)(self.list_requirements)
+        self.cli_command("add", self.req_group)(self.add_requirement)
+        self.cli_command("del", self.req_group)(self.remove_requirement)
+        self.cli_command("enable", self.plugin_group)(self.enable_plugin)
+        self.cli_command("disable", self.plugin_group)(self.disable_plugin)
+        self.cli_command("list", self.plugin_group)(self.list_plugins)
+        self.cli_command("reset")(self.reset)
+
+    def on_load(self):
+        self.ensure_plugins()
+        self.setup_cli()
+
+    def ensure_plugins(self):
+        """Ensure dependencies are installed."""
+
+        if self.config is None:
+            return
+
+        installed = self.installed_packages
+        plugins_to_install = set()
+
+        for name, req in self.requirements.items():
+            pkg = installed.get(name)
+
+            if pkg is None:
+                plugins_to_install.add(req["source"])
+                continue
+
+            if req.get("version") is not None and pkg["version"] != req["version"]:
+                plugins_to_install.add(req["source"])
+
+        if plugins_to_install:
+            self.pip_install(*plugins_to_install, allow_error=True)
---
+++ packages/cx-tool/cx_tool-0.1.0.26.tar.gz/cx_tool-0.1.0.26/cx_tool/plugin/plugin.py
@@ -0,0 +1,257 @@
+import copy
+import importlib
+import inspect
+import sys
+
+import typing
+from enum import Enum
+from functools import lru_cache, partial, wraps
+from pathlib import Path
+from typing import List, Type, Optional, ClassVar
+
+from plumbum import local
+from plumbum.commands.base import BoundCommand
+
+
+if typing.TYPE_CHECKING:
+    from cx_tool.config import UtilConfig
+    from cx_tool.plugin.context import PluginContext
+    from click import Group
+
+
+class Platform(str, Enum):
+    linux = "linux"
+    macos = "darwin"
+
+
+class Architecture(str, Enum):
+    x86_64 = "x86_64"
+    arm64 = "arm64"
+
+
+class Plugin:
+    name: str
+    platforms: List[Platform] = [Platform.macos, Platform.linux]
+    arch: List[Architecture] = [Architecture.x86_64, Architecture.arm64]
+
+    __REGISTRY__ = {}
+
+    def __init__(self, config: "UtilConfig"):
+        self.config = config
+        self._cli = None
+
+    @property
+    def data(self):
+        return self.config.get_plugin_data(self)
+
+    @property
+    def cli(self) -> "Group":
+        from click import Group
+
+        if self._cli is None:
+            self._cli = Group(self.name, help=self.__doc__)
+            self.config.plugin_cli.add_command(self._cli)
+
+        return self._cli
+
+    @property
+    def python(self) -> BoundCommand:
+        return local.get(sys.executable)
+
+    @classmethod
+    @lru_cache
+    def get_arch(cls) -> str:
+        return local.get(sys.executable)[
+            "-c", "import platform;print(platform.machine())"
+        ]().strip()
+
+    @classmethod
+    @lru_cache
+    def get_platform(cls) -> str:
+        return local.get(sys.executable)[
+            "-c", "import sys;print(sys.platform)"
+        ]().strip()
+
+    def cli_command(
+        self,
+        name: str,
+        cli: Optional["Group"] = None,
+        no_args_is_help: Optional[bool] = None,
+        **kwargs,
+    ):
+        def wrapper(f: clickable):
+            cli_ = cli or self.cli
+
+            return cli_.command(
+                name,
+                context_settings=dict(
+                    allow_extra_args=True,
+                ),
+                no_args_is_help=(
+                    no_args_is_help
+                    if no_args_is_help is not None
+                    else bool(set(inspect.signature(f.f).parameters) - {"self"})
+                ),
+                **kwargs,
+            )(f.with_plugin(self))
+
+        return wrapper
+
+    def on_load(self): ...
+
+    def on_unload(self): ...
+
+    def on_start(self, ctx: "PluginContext"): ...
+
+    def on_stop(self, ctx: "PluginContext"): ...
+
+    @classmethod
+    def find_plugins_in_module(cls, module_name: str) -> List[Type["Plugin"]]:
+        """Find Plugins in module."""
+
+        import click
+
+        plugins = []
+
+        try:
+            for item in importlib.import_module(module_name).__dict__.values():
+                if (
+                    isinstance(item, type)
+                    and issubclass(item, Plugin)
+                    and item is not Plugin
+                ):
+                    if item.check_platform():
+                        plugins.append(item)
+        except ImportError:
+            click.echo(f"Module not found `{module_name}`")
+
+        return plugins
+
+    @classmethod
+    def get_plugin(cls, name: str):
+        """Get plugin by name."""
+
+        return cls.__REGISTRY__.get(name)
+
+    @classmethod
+    def check_platform(cls) -> bool:
+        """Check if the plugin is compatible with the current platform."""
+
+        if cls.get_platform() not in cls.platforms:
+            return False
+
+        if cls.get_arch() not in cls.arch:
+            return False
+
+        return True
+
+    @classmethod
+    def add_plugin(cls, plugin: "Plugin"):
+        import click
+
+        if not plugin.check_platform():
+            return
+
+        if plugin.name in cls.__REGISTRY__:
+            click.echo(
+                f"Plugin with name `{plugin.name}` already exists in other package."
+            )
+        else:
+            cls.__REGISTRY__[plugin.name] = plugin
+
+    @classmethod
+    def all_plugins(cls) -> List["Plugin"]:
+        return list(cls.__REGISTRY__.values())
+
+    @classmethod
+    def import_plugins(cls, plugin_module: str, config: "UtilConfig"):
+        """Import and init plugins."""
+
+        for plugin in cls.find_plugins_in_module(plugin_module):
+            cls.add_plugin(plugin(config))
+
+
+class clickable:  # noqa
+    def __init__(self, f):
+        self.f = f
+        self.args = []
+        self.kwargs = {}
+        self.orig_func = f
+
+    def with_plugin(self, plugin: "Plugin") -> "clickable":
+        self.f = partial(self.f, *[plugin, *self.args], **self.kwargs)
+        return self
+
+    def partial(self, *args, **kwargs):
+        clickable_ = copy.deepcopy(self)
+        clickable_.args.extend(args)
+        clickable_.kwargs.update(kwargs)
+        return clickable_
+
+    def __call__(self, *args, **kwargs):
+        return self.f(*args, **kwargs)
+
+    @property
+    def __doc__(self):
+        return self.orig_func.__doc__
+
+
+class CrossOverControlPlugin:
+    VENV_PATH: ClassVar[Path]
+    LOG_PATH: ClassVar[Path]
+
+    @property
+    def is_running(self):
+        raise NotImplementedError()
+
+    @property
+    def bottles_path(self) -> Path:
+        raise NotImplementedError()
+
+    @property
+    def python(self) -> BoundCommand:
+        raise NotImplementedError()
+
+    @property
+    def venv(self) -> BoundCommand:
+        raise NotImplementedError()
+
+    @property
+    def pip(self) -> BoundCommand:
+        raise NotImplementedError()
+
+    def kill_crossover(self):
+        raise NotImplementedError()
+
+    def install(self):
+        raise NotImplementedError()
+
+    def uninstall(self):
+        raise NotImplementedError()
+
+    def run_crossover(self, background: bool = False):
+        raise NotImplementedError()
+
+
+def restart_required(f):
+    @wraps(f)
+    def wrapper(self: "Plugin", *args, **kwargs):
+        try:
+            return f(self, *args, **kwargs)
+        finally:
+            if self.config.crossover_plugin.is_running:
+                self.config.crossover_plugin.kill_crossover()
+                self.config.crossover_plugin.run_crossover(background=True)
+
+    return wrapper
+
+
+def save_config(f):
+    @wraps(f)
+    def wrapper(self: "Plugin", *args, **kwargs):
+        try:
+            return f(self, *args, **kwargs)
+        finally:
+            self.config.write()
+
+    return wrapper
