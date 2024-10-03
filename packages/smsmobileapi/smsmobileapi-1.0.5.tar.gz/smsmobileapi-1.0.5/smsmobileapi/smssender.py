ec0a11855a15d5a3f0e61c5914fe8f7e8481df37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:06:11 +0000

    Add package smsmobileapi/1.0.5/smsmobileapi-1.0.5.tar.gz

---
+++ packages/smsmobileapi/smsmobileapi-1.0.5.tar.gz/smsmobileapi-1.0.5/smsmobileapi/smssender.py
@@ -0,0 +1,35 @@
+# smsmobileapi/smssender.py
+
+import requests
+
+class SMSSender:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.api_url_send = "https://api.smsmobileapi.com/sendsms"  # Remplace par l'URL d'envoi
+        self.api_url_received = "https://api.smsmobileapi.com/getsms"  # Remplace par l'URL pour lire les SMS reçus
+
+    def send_message(self, to, message):
+        payload = {
+            'apikey': self.api_key,
+            'recipients': to,
+            'message': message
+        }
+        
+        try:
+            response = requests.post(self.api_url_send, data=payload, verify=False)
+            response.raise_for_status()
+            return response.json()  # Retourne la réponse de l'API en JSON
+        except requests.exceptions.RequestException as e:
+            return {"error": str(e)}
+    
+    def get_received_messages(self):
+        payload = {
+            'apikey': self.api_key
+        }
+        
+        try:
+            response = requests.get(self.api_url_received, params=payload, verify=False)
+            response.raise_for_status()
+            return response.json()  # Retourne la liste des SMS reçus
+        except requests.exceptions.RequestException as e:
+            return {"error": str(e)}
