e0b73e3d7b87d74aa2154bb72a715223419b4eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 03:09:05 +0000

    Add package pyRestTable/2020.0.10/pyRestTable-2020.0.10-py3-none-any.whl

---
+++ packages/pyRestTable/pyRestTable-2020.0.10-py3-none-any.whl/pyRestTable/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: iso-8859-1 -*-
+
+"""Configuration of pyRestTable package."""
+
+from .rest_table import Table  # noqa
+
+try:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+    del get_version
+except (LookupError, ModuleNotFoundError):
+    from importlib.metadata import version
+
+    __version__ = version("pyRestTable")
+    del version
+
+# -----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2014-2022, Pete R. Jemian
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# -----------------------------------------------------------------------------
