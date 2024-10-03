581dfe2c70cdda68ca159a7c4b6e9f280436beb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 07:06:18 +0000

    Add package heartkit/1.5.1/heartkit-1.5.1.tar.gz

---
+++ packages/heartkit/heartkit-1.5.1.tar.gz/heartkit-1.5.1/tests/app_test.py
@@ -0,0 +1,13 @@
+def test_app_cli_help():
+    """Verify APP CLI provides help dialog."""
+    assert True
+
+
+def test_tf_model():
+    """Verify TF model produces correct results on small sample set."""
+    assert True
+
+
+def test_tfl_model():
+    """Verify TFLite and micro models produces correct results on small sample set."""
+    assert True
