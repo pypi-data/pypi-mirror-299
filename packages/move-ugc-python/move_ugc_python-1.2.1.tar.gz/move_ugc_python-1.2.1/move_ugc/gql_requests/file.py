92da2603cca27fa40d37faa2dd408380f0d2a247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 04:07:23 +0000

    Add package move-ugc-python/1.2.1/move_ugc_python-1.2.1.tar.gz

---
+++ packages/move-ugc-python/move_ugc_python-1.2.1.tar.gz/move_ugc_python-1.2.1/move_ugc/gql_requests/file.py
@@ -0,0 +1,87 @@
+"""File gql_requests for Move UGC API."""
+from move_ugc.gql_requests.client import expand_client_query
+from move_ugc.schemas.gql import UgcGql
+
+expand_file_base = """
+    id
+    created
+    type
+    metadata
+    presignedUrl
+    thumbnailUrl
+    __typename
+"""
+
+file_attributes = f"""
+    {expand_file_base}
+    {{expand}}
+"""
+
+
+retrieve = UgcGql(
+    query=f"""
+    query retrieve($id: ID!) {{{{
+        getFile(fileId: $id) {{{{
+            {file_attributes}
+        }}}}
+    }}}}
+    """,
+    key="getFile",
+    expand={"client": expand_client_query},
+)
+
+
+create = UgcGql(
+    query=f"""
+    mutation create($type: String!, $metadata: AWSJSON!) {{{{
+        createFile(type: $type, metadata: $metadata) {{{{
+            {file_attributes}
+        }}}}
+    }}}}
+    """,
+    key="createFile",
+    expand={"client": expand_client_query},
+)
+
+update = UgcGql(
+    query=f"""
+    mutation update($id: String!, $metadata: AWSJSON!) {{{{
+        updateFile(id: $id, metadata: $metadata) {{{{
+            {file_attributes}
+        }}}}
+    }}}}
+    """,
+    key="updateFile",
+    expand={"client": expand_client_query},
+)
+
+
+generate_share_code = UgcGql(
+    query="""
+    mutation generateShareCode($fileId: String!) {{
+        generateShareCode(fileId: $fileId) {{
+            code
+            created
+            file {{
+                id
+            }}
+            expires
+            url
+            __typename
+        }}
+    }}
+    """,
+    key="generateShareCode",
+)
+
+expand_video_file = f"""
+    file {{
+        {expand_file_base}
+    }}
+"""
+
+expand_file = f"""
+    file {{
+        {expand_file_base}
+    }}
+"""
