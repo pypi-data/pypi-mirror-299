00bfd2afbaa0fcda6792e1eadf6ec7d3942ae5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:07:49 +0000

    Add package tradingtoolbox/0.35.0/tradingtoolbox-0.35.0-py3-none-any.whl

---
+++ packages/tradingtoolbox/tradingtoolbox-0.35.0-py3-none-any.whl/tradingtoolbox/utils/cache.py
@@ -0,0 +1,82 @@
+import os
+import json
+import msgspec
+import asyncio
+from datetime import date
+from typing import Callable, Optional
+
+
+class Cache(msgspec.Struct):
+    """
+    Cache class for handling file-based caching with support for asynchronous data loading.
+
+    Attributes:
+        cache_path (str): The path where the cache file is stored.
+        method (Optional[Callable]): A callable, typically an async method, to fetch data when the cache doesn't exist or needs to be reloaded.
+        date_as_suffix (bool): If True, appends today's date to the cache file name as a suffix.
+
+    Methods:
+        get_async(reload=True): Asynchronously retrieves data from the cache. If the cache file doesn't exist, it will fetch data using the provided method and store it in the cache.
+        clean_cache(): Deletes the cache file from disk.
+    """
+
+    cache_path: str
+    """Where you want the file to be stored"""
+    method: Optional[Callable] = None
+    """A lambda method to run in case the file doesn't exist"""
+    date_as_suffix: bool = True
+    """If true, will append today's date to the cache file."""
+
+    def __post_init__(self):
+        # Custom initialization logic after the msgspec-generated init
+        if self.date_as_suffix:
+            extension = self.cache_path.split(".")[-1]
+            name_without_extension = self.cache_path.replace(f".{extension}", "")
+            self.cache_path = f"{name_without_extension}_{date.today().strftime('%Y%m%d')}.{extension}"
+        self._ensure_path_exists()
+
+    # ====================== Private Methods ======================= #
+    def _read_file(self) -> Optional[dict]:
+        if os.path.exists(self.cache_path):
+            with open(self.cache_path, "r") as f:
+                return json.load(f)
+        return None
+
+    def _ensure_path_exists(self):
+        directory_path = os.path.dirname(self.cache_path)
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path, exist_ok=True)
+
+    async def _reload_async(self):
+        if self.method:
+            data = await self.method()
+
+            with open(self.cache_path, "w") as f:
+                json.dump(data, f, indent=2)
+
+    # ====================== Public Methods ======================= #
+    async def get_async(self, reload=True) -> dict:
+        """
+        Gets the data from the cache. If it doesn't exist, it will reload it.
+
+        Args:
+            reload (bool, optional): Whether to reload the data. Defaults to True.
+
+        Returns:
+            dict: The data from the cache.
+        """
+        data = self._read_file()
+        if data:
+            if reload:
+                asyncio.create_task(self._reload_async())
+        else:
+            await self._reload_async()
+            data = self._read_file()
+        return data  # type: ignore
+
+    def clean_cache(self):
+        """
+        Removes the cache file.
+        """
+        if os.path.exists(self.cache_path):
+            os.remove(self.cache_path)
