b69c231d6debfc5cd9b325ddb74af1b678f6a4ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 03:09:23 +0000

    Add package image-processing-desafio-dio-jubssoares/0.0.1/image_processing_desafio_dio_jubssoares-0.0.1-py3-none-any.whl

---
+++ packages/image-processing-desafio-dio-jubssoares/image_processing_desafio_dio_jubssoares-0.0.1-py3-none-any.whl/image_processing/processing/transformation.py
@@ -0,0 +1,14 @@
+#Importando biblioteca necessária
+
+from skimage.transform import resize
+
+#Função para redimensionamento de imagem
+def resize_image(image, proportion):
+    assert 0 <= proportion <= 1, "Specify a valid proportion between 0 and 1."
+    
+    height = round(image.shape[0] * proportion)
+    width = round(image.shape[1] * proportion)
+    
+    image_resized = resize(image, (height, width), anti_aliasing = True)
+    
+    return image_resized
