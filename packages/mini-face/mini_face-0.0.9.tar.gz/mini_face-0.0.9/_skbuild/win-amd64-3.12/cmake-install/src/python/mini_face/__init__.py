167e593a0b05a292ad15e71c3828078b09647210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:07:50 +0000

    Add package mini-face/0.0.9/mini_face-0.0.9.tar.gz

---
+++ packages/mini-face/mini_face-0.0.9.tar.gz/mini_face-0.0.9/_skbuild/win-amd64-3.12/cmake-install/src/python/mini_face/__init__.py
@@ -0,0 +1,5 @@
+from .api import __doc__, __version__
+from .api import *
+from .gaze import GazeFeatureExtractor
+from .action_unit import AUFeatureExtractor
+from .mode import PredictionMode
---
+++ packages/mini-face/mini_face-0.0.9.tar.gz/mini_face-0.0.9/_skbuild/win-amd64-3.12/cmake-install/src/python/mini_face/mode.py
@@ -0,0 +1,5 @@
+from enum import Enum, auto
+
+class PredictionMode(Enum):
+    IMAGE = auto()
+    VIDEO = auto()
