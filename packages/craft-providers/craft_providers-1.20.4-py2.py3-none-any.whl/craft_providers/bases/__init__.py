e0cd8904f4cb62a885f78025e9c42a51db0b9508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 14:06:35 +0000

    Add package craft-providers/1.20.4/craft_providers-1.20.4-py2.py3-none-any.whl

---
+++ packages/craft-providers/craft_providers-1.20.4-py2.py3-none-any.whl/craft_providers/bases/__init__.py
@@ -0,0 +1,97 @@
+#
+# Copyright 2021-2023 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+"""Collection of bases used to configure build environments."""
+
+# Backward compatible, will be removed in 2.0
+import sys
+from typing import Dict, NamedTuple, Tuple, Type, Union
+
+from craft_providers.errors import BaseCompatibilityError, BaseConfigurationError
+from craft_providers.base import Base
+
+from . import almalinux, centos
+from . import ubuntu
+from . import ubuntu as buildd
+from .ubuntu import BuilddBase, BuilddBaseAlias
+
+sys.modules["craft_providers.bases.buildd"] = buildd
+
+BaseAlias = Union[
+    ubuntu.BuilddBaseAlias, almalinux.AlmaLinuxBaseAlias, centos.CentOSBaseAlias
+]
+
+__all__ = [
+    "ubuntu",
+    "centos",
+    "BaseAlias",
+    "BaseName",
+    "BuilddBase",
+    "BuilddBaseAlias",
+    "BaseCompatibilityError",
+    "BaseConfigurationError",
+]
+
+
+class BaseName(NamedTuple):
+    """A base image, by distribution and version."""
+
+    name: str
+    version: str
+
+
+BASE_NAME_TO_BASE_ALIAS: Dict[BaseName, BaseAlias] = {
+    BaseName("ubuntu", "16.04"): ubuntu.BuilddBaseAlias.XENIAL,
+    BaseName("ubuntu", "18.04"): ubuntu.BuilddBaseAlias.BIONIC,
+    BaseName("ubuntu", "20.04"): ubuntu.BuilddBaseAlias.FOCAL,
+    BaseName("ubuntu", "22.04"): ubuntu.BuilddBaseAlias.JAMMY,
+    BaseName("ubuntu", "devel"): ubuntu.BuilddBaseAlias.DEVEL,
+    BaseName("centos", "7"): centos.CentOSBaseAlias.SEVEN,
+    BaseName("almalinux", "9"): almalinux.AlmaLinuxBaseAlias.NINE,
+}
+
+
+def get_base_alias(
+    base_name: Tuple[str, str],
+) -> BaseAlias:
+    """Return a Base alias from a base (name, version) tuple."""
+    base_name = BaseName(*base_name)
+    if base_name.name == "ubuntu" and base_name in BASE_NAME_TO_BASE_ALIAS:
+        return BASE_NAME_TO_BASE_ALIAS[base_name]
+
+    # match other distributions sub-versions like 9.1 to 9
+    _base_name = BaseName(base_name.name, base_name.version.split(".")[0])
+    if _base_name in BASE_NAME_TO_BASE_ALIAS:
+        return BASE_NAME_TO_BASE_ALIAS[_base_name]
+
+    raise BaseConfigurationError(f"Base alias not found for {base_name}")
+
+
+def get_base_from_alias(
+    alias: BaseAlias,
+) -> Type[Base]:
+    """Return a Base class from a known base alias."""
+    if isinstance(alias, ubuntu.BuilddBaseAlias):
+        return ubuntu.BuilddBase
+
+    if isinstance(alias, centos.CentOSBaseAlias):
+        return centos.CentOSBase
+
+    if isinstance(alias, almalinux.AlmaLinuxBaseAlias):
+        return almalinux.AlmaLinuxBase
+
+    raise BaseConfigurationError(f"Base not found for alias {alias}")
---
+++ packages/craft-providers/craft_providers-1.20.4-py2.py3-none-any.whl/craft_providers/lxd/remotes.py
@@ -0,0 +1,212 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2021-2023 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+"""Manages LXD remotes and provides access to remote images."""
+
+import logging
+import warnings
+from dataclasses import dataclass
+from enum import Enum
+from typing import Dict, Union
+
+from craft_providers import Base
+from craft_providers.bases import BaseName, almalinux, centos, get_base_alias, ubuntu
+
+from .errors import LXDError
+from .lxc import LXC
+
+logger = logging.getLogger(__name__)
+
+BUILDD_RELEASES_REMOTE_NAME = "craft-com.ubuntu.cloud-buildd"
+BUILDD_RELEASES_REMOTE_ADDRESS = "https://cloud-images.ubuntu.com/buildd/releases"
+
+BUILDD_DAILY_REMOTE_NAME = "craft-com.ubuntu.cloud-buildd-daily"
+BUILDD_DAILY_REMOTE_ADDRESS = "https://cloud-images.ubuntu.com/buildd/daily"
+
+DAILY_REMOTE_NAME = "ubuntu-daily"
+DAILY_REMOTE_ADDRESS = "https://cloud-images.ubuntu.com/daily"
+
+
+class ProtocolType(Enum):
+    """Enumeration of protocols for LXD remotes."""
+
+    LXD = "lxd"
+    SIMPLESTREAMS = "simplestreams"
+
+
+@dataclass
+class RemoteImage:
+    """Contains the name, location, and details of a remote LXD image.
+
+    :param image_name: Name of the image on the remote (e.g. `core22` or `lunar`).
+    :param remote_name: Name of the remote server.
+    :param remote_address: Address of the remote (can be an IP, FDQN, URL, or token)
+    :param remote_protocol: Remote protocol (options are `lxd` and `simplestreams`)
+    """
+
+    image_name: str
+    remote_name: str
+    remote_address: str
+    remote_protocol: ProtocolType
+
+    @property
+    def is_stable(self) -> bool:
+        """Check if the image is stable.
+
+        Images are stable if they are from a release remote.
+        Images from daily, devel, and any other remotes are not stable.
+
+        :returns: True if the image is stable.
+        """
+        if (
+            self.remote_name == BUILDD_RELEASES_REMOTE_NAME
+            and self.remote_address == BUILDD_RELEASES_REMOTE_ADDRESS
+        ):
+            # Ubuntu official buildd images
+            return True
+
+        if self.remote_name == "images":
+            # LXD daily images
+            return False
+
+        return False
+
+    def add_remote(self, lxc: LXC) -> None:
+        """Add the LXD remote for an image.
+
+        If the remote already exists, it will not be re-added.
+
+        :param lxc: LXC client.
+        """
+        # TODO verify both the remote name and address
+        if self.remote_name in lxc.remote_list():
+            logger.debug("Remote %r already exists.", self.remote_name)
+        else:
+            try:
+                lxc.remote_add(
+                    remote=self.remote_name,
+                    addr=self.remote_address,
+                    protocol=self.remote_protocol.value,
+                )
+
+            except Exception as exc:
+                # the remote adding failed, no matter really how: if it was because a
+                # race condition on remote creation (it's not idempotent) and now the
+                # remote is there, the purpose of this function is done (otherwise we
+                # let the original exception fly)
+                if self.remote_name in lxc.remote_list():
+                    logger.debug(
+                        "Remote %r is present on second check, ignoring exception %r.",
+                        self.remote_name,
+                        exc,
+                    )
+                else:
+                    raise
+            else:
+                logger.debug("Remote %r was successfully added.", self.remote_name)
+
+
+# mapping from supported bases to actual lxd remote images
+_PROVIDER_BASE_TO_LXD_REMOTE_IMAGE: Dict[Enum, RemoteImage] = {
+    ubuntu.BuilddBaseAlias.BIONIC: RemoteImage(
+        image_name="core18",
+        remote_name=BUILDD_RELEASES_REMOTE_NAME,
+        remote_address=BUILDD_RELEASES_REMOTE_ADDRESS,
+        remote_protocol=ProtocolType.SIMPLESTREAMS,
+    ),
+    ubuntu.BuilddBaseAlias.FOCAL: RemoteImage(
+        image_name="core20",
+        remote_name=BUILDD_RELEASES_REMOTE_NAME,
+        remote_address=BUILDD_RELEASES_REMOTE_ADDRESS,
+        remote_protocol=ProtocolType.SIMPLESTREAMS,
+    ),
+    ubuntu.BuilddBaseAlias.JAMMY: RemoteImage(
+        image_name="core22",
+        remote_name=BUILDD_RELEASES_REMOTE_NAME,
+        remote_address=BUILDD_RELEASES_REMOTE_ADDRESS,
+        remote_protocol=ProtocolType.SIMPLESTREAMS,
+    ),
+    ubuntu.BuilddBaseAlias.DEVEL: RemoteImage(
+        image_name="devel",
+        remote_name=DAILY_REMOTE_NAME,
+        remote_address=DAILY_REMOTE_ADDRESS,
+        remote_protocol=ProtocolType.SIMPLESTREAMS,
+    ),
+    centos.CentOSBaseAlias.SEVEN: RemoteImage(
+        image_name="centos/7",
+        remote_name="images",
+        remote_address="https://images.linuxcontainers.org/images/",
+        remote_protocol=ProtocolType.SIMPLESTREAMS,
+    ),
+    almalinux.AlmaLinuxBaseAlias.NINE: RemoteImage(
+        image_name="almalinux/9",
+        remote_name="images",
+        remote_address="https://images.linuxcontainers.org/images/",
+        remote_protocol=ProtocolType.SIMPLESTREAMS,
+    ),
+}
+
+
+def get_remote_image(provider_base: Union[Base, str]) -> RemoteImage:
+    """Get a RemoteImage for a particular provider base.
+
+    :param provider_base: string containing the provider base
+
+    :returns: the RemoteImage for the provider base
+    """
+    # temporary backward compatibility before 2.0
+    if isinstance(provider_base, str):
+        alias = get_base_alias(BaseName("ubuntu", provider_base))
+        provider_base = ubuntu.BuilddBase(alias=alias)  # type: ignore
+
+    image = _PROVIDER_BASE_TO_LXD_REMOTE_IMAGE.get(provider_base.alias)
+    if not image:
+        raise LXDError(
+            brief=(
+                "could not find a lxd remote image for the provider base "
+                f"{provider_base!r}"
+            )
+        )
+
+    return image
+
+
+def configure_buildd_image_remote(lxc: LXC = LXC()) -> str:
+    """Configure the default buildd image remote.
+
+    This is a deprecated function to maintain the existing API. It will be
+    removed with the release of craft-providers 2.0.
+    Applications should call `get_remote_image()` and `RemoteImage.add_remote()`.
+
+    :param lxc: LXC client.
+
+    :returns: Name of remote to pass to launcher.
+    """
+    warnings.warn(
+        message=(
+            "configure_buildd_image_remote() is deprecated. "
+            "Use get_remote_image() and RemoteImage.add_remote()."
+        ),
+        category=DeprecationWarning,
+    )
+    # configure the buildd remote for core22
+    base = ubuntu.BuilddBase(alias=ubuntu.BuilddBaseAlias.JAMMY)
+    image = get_remote_image(base)
+    image.add_remote(lxc)
+
+    return BUILDD_RELEASES_REMOTE_NAME
