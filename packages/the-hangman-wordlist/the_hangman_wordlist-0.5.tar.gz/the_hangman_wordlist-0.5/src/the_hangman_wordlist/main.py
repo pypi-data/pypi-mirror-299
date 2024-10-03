352b5e1eeb2f2442f038342581b7161a7954d686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 06:04:41 +0000

    Add package the-hangman-wordlist/0.5/the_hangman_wordlist-0.5.tar.gz

---
+++ packages/the-hangman-wordlist/the_hangman_wordlist-0.5.tar.gz/the_hangman_wordlist-0.5/src/the_hangman_wordlist/main.py
@@ -0,0 +1,13 @@
+"""
+The Hangman Wordlist v0.5
+By Noah Bozkurt and Jurriaan Portier
+"""
+from the_hangman_wordlist import HangmanWordlist
+
+if __name__ == "__main__":
+    wordlist = HangmanWordlist()
+    difficulty = input("What difficulty do you want? (easy/medium/hard): ")
+    word, version = wordlist.pull_word(difficulty)
+    print(f"\nThe {difficulty} word is: '{word}'")
+    print(f"Wordlist v{version}")
+    input("\nPress Enter to exit...")
