186117debe89e27c2440bbe7da8220ed550b802f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 04:06:08 +0000

    Add package satisfactory-api-client/0.1.5/satisfactory_api_client-0.1.5.tar.gz

---
+++ packages/satisfactory-api-client/satisfactory_api_client-0.1.5.tar.gz/satisfactory_api_client-0.1.5/satisfactory_api_client/data/advanced_game_settings.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class AdvancedGameSettings:
+    """
+    Advanced game settings for a Satisfactory Dedicated Server
+
+    Attributes
+    ----------
+    some_setting : str
+        A setting for the server
+    """
+    some_setting: str  # Replace with actual settings fields
