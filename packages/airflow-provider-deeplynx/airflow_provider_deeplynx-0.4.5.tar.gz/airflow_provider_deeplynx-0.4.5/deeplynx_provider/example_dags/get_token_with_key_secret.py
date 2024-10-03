58f1c75548d2aa2ed49400585e14b411c7084e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 07:05:37 +0000

    Add package airflow-provider-deeplynx/0.4.5/airflow_provider_deeplynx-0.4.5.tar.gz

---
+++ packages/airflow-provider-deeplynx/airflow_provider_deeplynx-0.4.5.tar.gz/airflow_provider_deeplynx-0.4.5/deeplynx_provider/example_dags/get_token_with_key_secret.py
@@ -0,0 +1,40 @@
+# Copyright 2024, Battelle Energy Alliance, LLC, All Rights Reserved
+
+from airflow import DAG
+from datetime import datetime
+from deeplynx_provider.operators.get_token_operator import GetOauthTokenOperator
+from airflow.configuration import conf
+
+default_args = {
+    'owner': 'jack',
+    'concurrency': 1,
+    'retries': 0,
+    'start_date': datetime(2024, 1, 1),
+    'catchup': False,
+}
+
+dag_params = {
+  "deeplynx_url": "https://deeplynx.azuredev.inl.gov",
+  "api_key": "",
+  "api_secret": "",
+}
+
+dag = DAG(
+    'get_token_with_key_secret',
+    default_args=default_args,
+    description='Demonstrates obtaining a DeepLynx token using `GetOauthTokenOperator` by directly specifying `host`, `api_key`, and `api_secret`, without using `conn_id`.',
+    schedule=None,
+    catchup=False,
+    params=dag_params,
+    max_active_runs=1
+)
+
+get_token_task = GetOauthTokenOperator(
+    task_id='get_token',
+    host='{{ params.deeplynx_url }}',
+    api_key='{{ params.api_key }}',
+    api_secret='{{ params.api_secret }}',
+    dag=dag
+)
+
+get_token_task
