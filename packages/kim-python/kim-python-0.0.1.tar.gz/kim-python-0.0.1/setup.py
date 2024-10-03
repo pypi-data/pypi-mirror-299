2160df9b68d8ffda446ecdb55aadcb9e53440446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 06:05:03 +0000

    Add package kim-python/0.0.1/kim-python-0.0.1.tar.gz

---
+++ packages/kim-python/kim-python-0.0.1.tar.gz/kim-python-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kim-python",
+    version="0.0.1",
+    author="milan.dg@free.fr",
+    url="https://github.com/Dammmmmm/kim",
+    description="A package to Keep In Mind your constants and call them anytime anywhere",
+    packages=find_packages(),
+    readme = "README.md",
+    install_requires = [],
+    python_requires = ">=3.5",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]  
+)
