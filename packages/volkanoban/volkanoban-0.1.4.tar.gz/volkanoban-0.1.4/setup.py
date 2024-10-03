66357e59c0ce20ac1db5d292f4c8390ee4f09379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 04:06:39 +0000

    Add package volkanoban/0.1.4/volkanoban-0.1.4.tar.gz

---
+++ packages/volkanoban/volkanoban-0.1.4.tar.gz/volkanoban-0.1.4/setup.py
@@ -0,0 +1,34 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='volkanoban',
+    version='0.1.4',
+    author='Dr. Volkan OBAN',
+    author_email='volkanobn@gmail.com',
+    description='A library for data analysis and supervised learning modeling using various powerful machine learning techniques.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'matplotlib',
+        'seaborn',
+        'lime',
+        'xgboost',
+        'lightgbm',
+        'catboost',
+        'plotly',
+        'tabulate',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
