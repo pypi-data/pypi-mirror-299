7a1bc83473ffc39dfc83d97c95e3ed6930a90f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 04:07:20 +0000

    Add package diamond-shovel-api/0.0.4/diamond_shovel_api-0.0.4-py3-none-any.whl

---
+++ packages/diamond-shovel-api/diamond_shovel_api-0.0.4-py3-none-any.whl/diamond_shovel/function/binary_manager.py
@@ -0,0 +1,26 @@
+import io
+from contextlib import contextmanager
+from typing import Generator
+
+
+class BinaryManager:
+    def get_out_path_by_name(self, name: str):
+        ...
+
+    def register_binary_path(self, name: str, path: str):
+        ...
+
+    def execute_binary(self, name: str, cmd_args: list[str], timeout: int,
+                       error_manage: bool = True, separate_output: bool = True, text: bool = True) \
+            -> tuple[str | bytes, str | bytes]:
+        ...
+
+    def clear_outs_folder(self):
+        ...
+
+    def check_binary(self, name: str) -> bool:
+        ...
+
+    @contextmanager
+    def open_file(self, name: str, file_name: str) -> Generator[io.IOBase, None, None]:
+        ...
