b17310080c2bdb68ba313868351089cfbb0919d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 06:04:40 +0000

    Add package peertools/9.1.1/peertools-9.1.1.tar.gz

---
+++ packages/peertools/peertools-9.1.1.tar.gz/peertools-9.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='peertools', #Package_Name
+      version='9.1.1',
+      description='Python Package By Predator_97',
+      author='Predator_97',
+      license='MIT',
+      zip_safe=False
+     )
