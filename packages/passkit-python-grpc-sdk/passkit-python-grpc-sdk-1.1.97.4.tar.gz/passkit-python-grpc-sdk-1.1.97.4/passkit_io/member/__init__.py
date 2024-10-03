26521d5f49cfcaf3d459c7841fe762ac43357a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 04:06:40 +0000

    Add package passkit-python-grpc-sdk/1.1.97.4/passkit-python-grpc-sdk-1.1.97.4.tar.gz

---
+++ packages/passkit-python-grpc-sdk/passkit-python-grpc-sdk-1.1.97.4.tar.gz/passkit-python-grpc-sdk-1.1.97.4/passkit_io/member/__init__.py
@@ -0,0 +1,58 @@
+def get_a_rpc_pb2():
+    import member.a_rpc_pb2
+    return member.a_rpc_pb2
+
+
+def get_a_rpc_pb2_grpc():
+    import member.a_rpc_pb2_grpc
+    return member.a_rpc_pb2_grpc
+
+
+def get_member_pb2():
+    import member.member_pb2
+    return member.member_pb2
+
+
+def get_member_pb2_grpc():
+    import member.member_pb2_grpc
+    return member.member_pb2_grpc
+
+
+def get_program_pb2():
+    import member.program_pb2
+    return member.program_pb2
+
+
+def get_program_pb2_grpc():
+    import member.program_pb2_grpc
+    return member.program_pb2_grpc
+
+
+def get_event_pb2():
+    import member.event_pb2
+    return member.event_pb2
+
+
+def get_event_pb2_grpc():
+    import member.event_pb2_grpc
+    return member.event_pb2_grpc
+
+
+def get_member_events_pb2():
+    import member.member_events_pb2
+    return member.member_events_pb2
+
+
+def get_member_events_pb2_grpc():
+    import member.member_events_pb2_grpc
+    return member.member_events_pb2_grpc
+
+
+def get_tier_pb2():
+    import member.tier_pb2
+    return member.tier_pb2
+
+
+def get_tier_pb2_grpc():
+    import member.tier_pb2_grpc
+    return member.tier_pb2_grpc
