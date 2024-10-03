fdc28ba39b38612d6033f7c267cdf4b8601497c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:07:27 +0000

    Add package thermontfa/0.1.0/thermontfa-0.1.0-py3-none-any.whl

---
+++ packages/thermontfa/thermontfa-0.1.0-py3-none-any.whl/thermontfa/__init__.py
@@ -0,0 +1,7 @@
+"""
+The python package **thermontfa** with our reference implementation of the thermo-plastic
+NTFA contains following classes:
+"""
+
+from .tabular_interpolation import *
+from .thermoNTFA import *
