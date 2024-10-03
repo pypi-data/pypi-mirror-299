e51642ca4816dc275bc00dd4d1df4f56d0c2d072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 15:05:23 +0000

    Add package py-spring-core/0.0.4.post2/py_spring_core-0.0.4.post2.tar.gz

---
+++ packages/py-spring-core/py_spring_core-0.0.4.post2.tar.gz/py_spring_core-0.0.4.post2/py_spring_core/commons/config_file_template_generator/templates.py
@@ -0,0 +1,8 @@
+app_config_template = {
+    "app_src_target_dir": "./src",
+    "server_config": {"host": "0.0.0.0", "port": 8080, "enabled": True},
+    "properties_file_path": "./application-properties.json",
+    "loguru_config": {"log_file_path": "./logs/app.log", "log_level": "DEBUG"},
+}
+
+app_properties_template = {}
