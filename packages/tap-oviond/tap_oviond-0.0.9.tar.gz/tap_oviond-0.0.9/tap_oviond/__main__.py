173dae28cc47b2b01e36a80df005e0537062a149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 06:04:34 +0000

    Add package tap-oviond/0.0.9/tap_oviond-0.0.9.tar.gz

---
+++ packages/tap-oviond/tap_oviond-0.0.9.tar.gz/tap_oviond-0.0.9/tap_oviond/__main__.py
@@ -0,0 +1,7 @@
+"""oviond entry point."""
+
+from __future__ import annotations
+
+from tap_oviond.tap import Tapoviond
+
+Tapoviond.cli()
---
+++ packages/tap-oviond/tap_oviond-0.0.9.tar.gz/tap_oviond-0.0.9/tap_oviond/streams.py
@@ -0,0 +1,18 @@
+"""Stream type classes for tap-oviond."""
+
+from singer_sdk import typing as th  # JSON Schema typing helpers
+
+from tap_oviond.client import oviondStream
+
+
+class CommentsStream(oviondStream):
+    primary_keys = ["id"]
+    path = "/comments"
+    name = "comments"
+    schema = th.PropertiesList(
+        th.Property("postId", th.IntegerType),
+        th.Property("id", th.IntegerType),
+        th.Property("name", th.StringType),
+        th.Property("email", th.StringType),
+        th.Property("body", th.StringType),
+    ).to_dict()
