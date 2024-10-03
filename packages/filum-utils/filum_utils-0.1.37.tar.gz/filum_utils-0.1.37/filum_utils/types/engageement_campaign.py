0cec390d53dd14af94371ddf5c3628cda0450474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 03:09:57 +0000

    Add package filum-utils/0.1.37/filum_utils-0.1.37.tar.gz

---
+++ packages/filum-utils/filum_utils-0.1.37.tar.gz/filum_utils-0.1.37/filum_utils/types/engageement_campaign.py
@@ -0,0 +1,11 @@
+from typing import TypedDict, Optional, Dict, Any
+
+from filum_utils.types.account import Account
+
+
+class EngagementCampaign(TypedDict, total=False):
+    id: str
+    name: str
+    account: Account
+    data: Optional[Dict[str, Any]]
+    zalo_template_id: Optional[str]
