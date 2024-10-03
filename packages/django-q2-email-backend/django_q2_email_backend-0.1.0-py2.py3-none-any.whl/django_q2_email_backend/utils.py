86dbff5e71335122693c0374d7a4743d76f4f552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 16:05:12 +0000

    Add package django-q2-email-backend/0.1.0/django_q2_email_backend-0.1.0-py2.py3-none-any.whl

---
+++ packages/django-q2-email-backend/django_q2_email_backend-0.1.0-py2.py3-none-any.whl/django_q2_email_backend/utils.py
@@ -0,0 +1,28 @@
+from django.core.mail import EmailMessage
+from django.core.mail import EmailMultiAlternatives
+
+
+def to_dict(email_message):
+    if not isinstance(email_message, EmailMessage):
+        return email_message
+    email_message_data = {
+        "cc": email_message.cc,
+        "subject": email_message.subject,
+        "body": email_message.body,
+        "from_email": email_message.from_email,
+        "to": email_message.to,
+        "bcc": email_message.bcc,
+        "attachments": email_message.attachments,
+        "headers": email_message.extra_headers,
+        "reply_to": email_message.reply_to,
+    }
+    if isinstance(email_message, EmailMultiAlternatives):
+        email_message_data["alternatives"] = email_message.alternatives
+    return email_message_data
+
+
+def from_dict(email_message_data):
+    kwargs = dict(email_message_data)
+    if alternatives := kwargs.pop("alternatives", None):
+        return EmailMultiAlternatives(alternatives=alternatives, **kwargs)
+    return EmailMessage(**kwargs)
