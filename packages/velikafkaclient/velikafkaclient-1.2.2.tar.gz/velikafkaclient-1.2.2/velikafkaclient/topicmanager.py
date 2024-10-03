e3aec22557651bde8d38b38667868e622dc6ad70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:06:51 +0000

    Add package velikafkaclient/1.2.2/velikafkaclient-1.2.2.tar.gz

---
+++ packages/velikafkaclient/velikafkaclient-1.2.2.tar.gz/velikafkaclient-1.2.2/velikafkaclient/topicmanager.py
@@ -0,0 +1,39 @@
+from confluent_kafka.admin import AdminClient, NewTopic
+
+
+class KafkaTopicManager:
+
+    def __init__(self, bootstrap_servers):
+        self.bootstrap_servers = bootstrap_servers
+        self.client = AdminClient({
+            "bootstrap.servers": bootstrap_servers
+        })
+
+    def wait_for_complete(self, future_dict):
+        for f in future_dict:
+            future_dict[f].result()
+
+    def create_topic(self, topic_name: str, num_partitions=1, replication_factor=3):
+        topics_list = [NewTopic(topic_name, num_partitions=num_partitions, replication_factor=replication_factor)]
+        result = self.client.create_topics(topics_list)
+        self.wait_for_complete(result)
+
+    def create_topics(self, topic_names: [str], num_partitions=1, replication_factor=3):
+        topics_list = [NewTopic(tn, num_partitions=num_partitions, replication_factor=replication_factor) for tn in
+                       topic_names]
+        result = self.client.create_topics(topics_list)
+        self.wait_for_complete(result)
+
+    def list_topics(self) -> [str]:
+        return list(self.client.list_topics().topics.keys())
+
+    def topic_exists(self, topic_name) -> bool:
+        return self.client.list_topics().topics.get(topic_name) is not None
+
+    def delete_topic(self, topic_name):
+        result = self.client.delete_topics(topics=[topic_name])
+        self.wait_for_complete(result)
+
+    def delete_topics(self, topic_names):
+        result = self.client.delete_topics(topics=topic_names)
+        self.wait_for_complete(result)
