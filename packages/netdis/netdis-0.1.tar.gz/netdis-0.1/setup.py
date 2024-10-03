28ba400877146882d41ca76dfb9ec8a3e1107e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 06:04:48 +0000

    Add package netdis/0.1/netdis-0.1.tar.gz

---
+++ packages/netdis/netdis-0.1.tar.gz/netdis-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='netdis',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    package_data={'netdis': ['*.py', '*.ipynb','*.zip']},
+)
