4c108c80d4a59dca4025f9c17b6fa5947c09b54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 16:05:35 +0000

    Add package simple-name-sdk/0.1.0/simple_name_sdk-0.1.0.tar.gz

---
+++ packages/simple-name-sdk/simple_name_sdk-0.1.0.tar.gz/simple_name_sdk-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple-name-sdk',
+    version='0.1.0',
+    author='Adithya k',
+    author_email='adithya.k@setu.co',
+    description='A simple SDK to combine first and last names.',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
