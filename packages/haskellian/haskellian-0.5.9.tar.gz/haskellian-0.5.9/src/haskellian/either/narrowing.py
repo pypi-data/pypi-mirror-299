80e496296a33eac2a4fb0b3b63eed12fef7269aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 05:07:18 +0000

    Add package haskellian/0.5.9/haskellian-0.5.9.tar.gz

---
+++ packages/haskellian/haskellian-0.5.9.tar.gz/haskellian-0.5.9/src/haskellian/either/narrowing.py
@@ -0,0 +1,11 @@
+from typing_extensions import TypeVar, TypeGuard
+from .either import Either, Left, Right
+
+L = TypeVar('L')
+R = TypeVar('R')
+
+def is_left(either: Either[L, R]) -> TypeGuard[Left[L, R]]:
+  return either.tag == 'left'
+
+def is_right(either: Either[L, R]) -> TypeGuard[Right[L, R]]:
+  return either.tag == 'right'
