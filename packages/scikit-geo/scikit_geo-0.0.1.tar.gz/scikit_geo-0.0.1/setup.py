262abe50dad9cb353be8636fe5e2a5f3c7a1b7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 14:05:41 +0000

    Add package scikit-geo/0.0.1/scikit_geo-0.0.1.tar.gz

---
+++ packages/scikit-geo/scikit_geo-0.0.1.tar.gz/scikit_geo-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+setup(name='scikit-geo',  # 包名
+      version='0.0.1',  # 版本号
+      description='A small example package',
+      long_description=open('README.md', 'r').read(),
+      author='zjzj',
+      author_email='773598627@qq.com',
+      url='https://mp.weixin.qq.com/',
+      install_requires=[],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
