1530a2bf0d1907357a57c57afa40142d9ef7b27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-03 15:05:07 +0000

    Add package matgraphdb/0.0.2/matgraphdb-0.0.2.tar.gz

---
+++ packages/matgraphdb/matgraphdb-0.0.2.tar.gz/matgraphdb-0.0.2/matgraphdb/__init__.py
@@ -0,0 +1,6 @@
+# from matgraphdb.data.manager import DBManager
+# from matgraphdb.graph.neo4j_gds_manager import Neo4jGDSManager
+# from matgraphdb.graph.neo4j_manager import Neo4jManager
+# from matgraphdb.graph.graph_generator import GraphGenerator
+
+from matgraphdb.core import MatGraphDB
