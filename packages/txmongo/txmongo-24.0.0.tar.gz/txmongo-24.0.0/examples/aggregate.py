ffe513b9bdb85f4bcb98931b75ab3707708455d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 16:05:11 +0000

    Add package txmongo/24.0.0/txmongo-24.0.0.tar.gz

---
+++ packages/txmongo/txmongo-24.0.0.tar.gz/txmongo-24.0.0/examples/aggregate.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright 2009-2014 The txmongo authors.  All rights reserved.
+# Use of this source code is governed by the Apache License that can be
+# found in the LICENSE file.
+
+import _local_path
+from twisted.internet import defer, reactor
+
+import txmongo
+
+
+@defer.inlineCallbacks
+def example():
+    mongo = yield txmongo.MongoConnection()
+
+    foo = mongo.foo  # `foo` database
+    test = foo.test  # `test` collection
+
+    yield test.insert({"src": "Twitter", "content": "bla bla"}, safe=True)
+    yield test.insert({"src": "Twitter", "content": "more data"}, safe=True)
+    yield test.insert({"src": "Wordpress", "content": "blog article 1"}, safe=True)
+    yield test.insert({"src": "Wordpress", "content": "blog article 2"}, safe=True)
+    yield test.insert({"src": "Wordpress", "content": "some comments"}, safe=True)
+
+    # Read more about the aggregation pipeline in MongoDB's docs
+    pipeline = [{"$group": {"_id": "$src", "content_list": {"$push": "$content"}}}]
+    result = yield test.aggregate(pipeline)
+
+    print(("result:", result))
+
+
+if __name__ == "__main__":
+    example().addCallback(lambda ign: reactor.stop())
+    reactor.run()
