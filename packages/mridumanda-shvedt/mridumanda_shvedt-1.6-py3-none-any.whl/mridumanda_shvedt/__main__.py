516f405517c625e7b82fa5435861c49125dca2d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 15:05:09 +0000

    Add package mridumanda-shvedt/1.6/mridumanda_shvedt-1.6-py3-none-any.whl

---
+++ packages/mridumanda-shvedt/mridumanda_shvedt-1.6-py3-none-any.whl/mridumanda_shvedt/__main__.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+import sys
+from mridumanda_shvedt.weather import get_weather, display_weather, display_weather_one_liner, display_weather_data_ascii
+from mridumanda_shvedt.weather import display_weather_data_ascii_color
+
+
+def main():
+    init()
+    
+    parser = argparse.ArgumentParser(description="Weather display options and city input")
+    
+    parser.add_argument('-o', '--one-liner', action='store_true', help='Display weather data in one-liner format')
+    parser.add_argument('-g', '--graphical', action='store_true', help='Display weather data with ascii representations')
+    parser.add_argument('-gc', '--graphical-color', action='store_true', help='Display weather data with color and ascii representations')
+    parser.add_argument('-c', '--city', type=str, help='Specify city to display weather')
+    
+    args = parser.parse_args()
+    
+    weather_data = get_weather(city = args.city) if args.city else get_weather(None)
+    
+    if weather_data is not None:
+        if args.one_liner:
+            display_weather_one_liner(weather_data)
+        elif args.graphical:
+            display_weather_data_ascii(weather_data)
+        elif args.graphical_color:
+            display_weather_data_ascii_color(weather_data)
+        else:
+            display_weather(weather_data)
+    else:
+        sys.exit(1)
+
+
+def init():
+    home_dir = os.path.expanduser('~')
+    
+    if os.name == 'NT':
+        program_dir = os.path.join(home_dir, 'AppData', 'Local', 'mridumanda')
+    else:
+        program_dir = os.path.join(home_dir, '.config', 'mridumanda')
+    
+    if not os.path.isfile(os.path.join(program_dir, 'api_key.txt')):
+        os.makedirs(program_dir, exist_ok=True)
+        
+        api_key = input('Enter API key: ')
+        
+        with open(os.path.join(program_dir, 'api_key.txt'), 'w') as f:
+            f.write(f"API={api_key}")
+        
+        print('API key saved successfully.')
+        print('Run the program again, to use it.')
+        
+        sys.exit(0)
