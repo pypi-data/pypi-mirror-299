40a9eace6849a49409cedabcc393265c7110b6c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 16:05:14 +0000

    Add package a360-python-libraries/0.8.0/a360_python_libraries-0.8.0-py3-none-any.whl

---
+++ packages/a360-python-libraries/a360_python_libraries-0.8.0-py3-none-any.whl/a360_security/dto/user.py
@@ -0,0 +1,17 @@
+import uuid
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserDTO(BaseModel):
+    username: str
+    email: Optional[EmailStr]
+    sub: uuid.UUID
+    roles: list[str]
+    practice_id: Optional[uuid.UUID]
+    is_admin: bool
+    is_practice_dependant: bool
+
+    class Config:
+        from_attributes = True
