28f6b939f124faa809e5d9f05388decd52f7de41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 05:05:19 +0000

    Add package finvizfinance/1.1.0/finvizfinance-1.1.0-py3-none-any.whl

---
+++ packages/finvizfinance/finvizfinance-1.1.0-py3-none-any.whl/finvizfinance/__init__.py
@@ -0,0 +1,9 @@
+"""
+.. module:: __init__
+    :synopsis: finvizfinance package general information
+
+.. moduleauthor:: Tianning Li <ltianningli@gmail.com>
+"""
+
+__version__ = "1.0.1"
+__author__ = "Tianning Li"
