37ae09736935bb51d8adeebfe8f2503657d31835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 06:05:53 +0000

    Add package hahomematic/2024.10.0/hahomematic-2024.10.0.tar.gz

---
+++ packages/hahomematic/hahomematic-2024.10.0.tar.gz/hahomematic-2024.10.0/hahomematic/__init__.py
@@ -0,0 +1,40 @@
+"""
+hahomematic is a Python 3 module for Home Assistant.
+
+The lib interacts with HomeMatic and HomematicIP devices.
+https://github.com/danielperna84/hahomematic
+"""
+
+from __future__ import annotations
+
+import asyncio
+from contextvars import ContextVar
+import logging
+import signal
+import sys
+import threading
+from typing import Final
+
+from hahomematic import central as hmcu
+
+# context var for storing if call is running within a service
+IN_SERVICE_VAR: ContextVar[bool] = ContextVar("in_service_var", default=False)
+
+if sys.stdout.isatty():
+    logging.basicConfig(level=logging.INFO)
+
+_LOGGER: Final = logging.getLogger(__name__)
+
+
+# pylint: disable=unused-argument
+# noinspection PyUnusedLocal
+def signal_handler(sig, frame):  # type: ignore[no-untyped-def]
+    """Handle signal to shut down central."""
+    _LOGGER.info("Got signal: %s. Shutting down central", str(sig))
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    for central in hmcu.CENTRAL_INSTANCES.values():
+        asyncio.run_coroutine_threadsafe(central.stop(), asyncio.get_running_loop())
+
+
+if threading.current_thread() is threading.main_thread() and sys.stdout.isatty():
+    signal.signal(signal.SIGINT, signal_handler)
