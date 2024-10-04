6ba34e406827e27135f490b57752ab1b7f670a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 05:05:24 +0000

    Add package antbase/0.1a7/antbase-0.1a7-py3-none-any.whl

---
+++ packages/antbase/antbase-0.1a7-py3-none-any.whl/antbase/type/order.py
@@ -0,0 +1,47 @@
+class Qouta_request:
+    pass
+
+class Quota:
+    pass
+
+class Design_order:
+    pass
+
+class Print_order:
+    pass
+
+class Cast_order:
+    pass
+
+class Production_order:
+    pass
+
+class Suplay_order:
+    pass
+
+class Sort_order:
+    pass
+
+class Certificate_order:
+    pass
+
+class Cut_order:
+    pass
+
+class Shipment_order:
+    pass
+
+class Service_order:
+    pass
+
+class Photo_order:
+    pass
+
+class Video_order:
+    pass
+
+class Contract:
+    pass
+
+class Obligation:
+    pass
