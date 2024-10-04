8af27fe6970b831bc919029a5805b1e33eb75a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 05:05:50 +0000

    Add package hspylib-kafman/0.9.148/hspylib_kafman-0.9.148-py3-none-any.whl

---
+++ packages/hspylib-kafman/hspylib_kafman-0.9.148-py3-none-any.whl/kafman/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-kafman v0.9.148
+#
+# Package: main.kafman
+"""Package initialization."""
+
+__all__ = [
+    'core', 
+    'views'
+]
+__version__ = '0.9.148'
