d0dd49c94ac4d974ac7d9f4b123a9940b45b4e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 16:05:10 +0000

    Add package yggdrasil-scaffold/0.1.0.dev5/yggdrasil_scaffold-0.1.0.dev5-py3-none-any.whl

---
+++ packages/yggdrasil-scaffold/yggdrasil_scaffold-0.1.0.dev5-py3-none-any.whl/yggdrasil/endpoints/session.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+"""注册 Yggdrasil API 的会话端点"""
+from typing import Annotated
+
+from Crypto.PublicKey.RSA import RsaKey
+from fastapi import APIRouter, Depends, Response
+
+from adofai import SerializedProfile
+from adofai.models import FulfilledGameProfile
+from yggdrasil.endpoints import handlers
+from yggdrasil.exceptions import InvalidToken
+
+session_endpoints = APIRouter(prefix="/sessionserver/session/minecraft")
+
+
+@session_endpoints.post("/join", status_code=204)
+async def join(result: Annotated[bool, Depends(handlers.session.join)]) -> None:
+    """处理玩家侧正版验证逻辑"""
+    if not result:
+        raise InvalidToken
+
+
+@session_endpoints.get("/hasJoined")
+async def has_joined(game_profile: Annotated[FulfilledGameProfile, Depends(handlers.session.has_joined)],
+                     sign_key: Annotated[RsaKey, Depends(handlers.root.sign_key)],
+                     rsp: Response) -> SerializedProfile | None:
+    """处理服务侧正版验证逻辑"""
+    if game_profile is not None:
+        return game_profile.serialize("full", sign_key)
+    else:
+        rsp.status_code = 204
+        return None
