7a6b999fc80fb3391ad4f9749236153a720061f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:40 +0000

    Add package shopyo-auth/1.2.0/shopyo_auth-1.2.0.tar.gz

---
+++ packages/shopyo-auth/shopyo_auth-1.2.0.tar.gz/shopyo_auth-1.2.0/shopyo_auth/__init__.py
@@ -0,0 +1,31 @@
+from typing import Any
+import os
+import json
+
+from flask import Flask
+from .view import module_blueprint
+from .upload import upload
+
+__version__ = "1.2.0"
+
+info = {}
+with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + "info.json") as f:
+    info = json.load(f)
+
+
+class ShopyoAuth:
+    def __init__(self, app: Any = None) -> None:
+        if app is not None:
+            self.init_app(app)
+        self.upload = upload 
+
+    def init_app(self, app: Flask) -> None:
+        if not hasattr(app, "extensions"):
+            app.extensions = {}
+
+        app.extensions["shopyo_auth"] = self
+        bp = module_blueprint
+        app.register_blueprint(bp)
+
+    def get_info(self):
+        return info
