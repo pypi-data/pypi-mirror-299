d116b76a8f835ddc9bc410431db3ef0daf479f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 03:09:38 +0000

    Add package djangocms-catalog-books/1.1.0/djangocms_catalog_books-1.1.0-py3-none-any.whl

---
+++ packages/djangocms-catalog-books/djangocms_catalog_books-1.1.0-py3-none-any.whl/catalog_books/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, register_converter
+
+from .url_converters import BooksOrderByConverter, ListTypeConverter
+from .views import BookDetailView, BookListView
+
+register_converter(BooksOrderByConverter, 'order')
+register_converter(ListTypeConverter, 'lstype')
+
+urlpatterns = [
+    # Note: The default url for the application must remain empty path ''.
+    path('', BookListView.as_view(), name='book_list'),
+    path('books-<lstype:list_type>/<order:order>/', BookListView.as_view(), name='book_list_order'),
+    path('detail/<slug:pk>/', BookDetailView.as_view(), name='book_detail'),
+]
