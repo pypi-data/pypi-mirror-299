83ffb33933b0c0165efccaf3da0ca484561c0f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 03:09:41 +0000

    Add package pgamit/1.2.1/pgamit-1.2.1-py3-none-any.whl

---
+++ packages/pgamit/pgamit-1.2.1-py3-none-any.whl/pgamit/__init__.py
@@ -0,0 +1,12 @@
+"""
+pgamit: a Python wrapper to parallelize GAMIT executions
+"""
+
+from pgamit.classes import *
+from pgamit.com import *
+
+__version__ = "1.2.1"
+__all__ = [
+        "classes",
+        "com",
+        "stacker"]
