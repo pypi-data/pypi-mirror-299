a0d753e86866d4909cbf425640bbcce32eb9e9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:22 +0000

    Add package julep/1.8.0/julep-1.8.0.tar.gz

---
+++ packages/julep/julep-1.8.0.tar.gz/julep-1.8.0/src/julep/types/agents/tool_list_response.py
@@ -0,0 +1,58 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Union, Optional
+from datetime import datetime
+from typing_extensions import Literal
+
+from ..._models import BaseModel
+
+__all__ = ["ToolListResponse", "Function", "Integration", "System"]
+
+
+class Function(BaseModel):
+    description: Optional[str] = None
+
+    name: Optional[object] = None
+
+    parameters: Optional[object] = None
+
+
+class Integration(BaseModel):
+    provider: Union[Literal["dummy", "hacker_news", "weather", "wikipedia", "spider", "brave", "browserbase"], str]
+
+    arguments: Optional[object] = None
+
+    description: Optional[str] = None
+
+    method: Optional[str] = None
+
+    setup: Optional[object] = None
+
+
+class System(BaseModel):
+    call: str
+
+    arguments: Optional[object] = None
+
+    description: Optional[str] = None
+
+
+class ToolListResponse(BaseModel):
+    id: str
+
+    created_at: datetime
+
+    name: str
+
+    updated_at: datetime
+
+    function: Optional[Function] = None
+    """Function definition"""
+
+    integration: Optional[Integration] = None
+    """Integration definition"""
+
+    system: Optional[System] = None
+    """System definition"""
+
+    type: Optional[Literal["function", "integration", "system", "api_call"]] = None
