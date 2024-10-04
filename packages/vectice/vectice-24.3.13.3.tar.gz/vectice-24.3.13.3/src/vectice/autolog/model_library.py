6f5bb7030b1c2c54e623843c9eee6c130134381a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 15:06:12 +0000

    Add package vectice/24.3.13.3/vectice-24.3.13.3.tar.gz

---
+++ packages/vectice/vectice-24.3.13.3.tar.gz/vectice-24.3.13.3/src/vectice/autolog/model_library.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class ModelLibrary(Enum):
+    """Enumeration that defines what the model library."""
+
+    SKLEARN = "SKLEARN"
+    SKLEARN_PIPELINE = "SKLEARN_PIPELINE"
+    SKLEARN_SEARCH = "SKLEARN_SEARCH"
+    LIGHTGBM = "LIGHTGBM"
+    CATBOOST = "CATBOOST"
+    KERAS = "KERAS"
+    STATSMODEL = "STATSMODEL"
+    PYTORCH = "PYTORCH"
+    NONE = "NONE"
