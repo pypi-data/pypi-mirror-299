07e3abcab0fe12fdb781ba14b5c176b7011449c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 15:05:00 +0000

    Add package MyCommonLib/0.5.0/mycommonlib-0.5.0.tar.gz

---
+++ packages/MyCommonLib/mycommonlib-0.5.0.tar.gz/mycommonlib-0.5.0/src/MyCommonLib/__init__.py
@@ -0,0 +1,6 @@
+from MyCommonLib.configuration import Configure, read_yaml, write_yaml
+from MyCommonLib.constants import *
+from MyCommonLib.version import Vers
+from MyCommonLib.dictman import dict2Table
+
+__version__ = Vers(VERSION)
