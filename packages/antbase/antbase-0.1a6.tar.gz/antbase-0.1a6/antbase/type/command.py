bab9e7e64714d4c44f0a74364b7f3dfeeb58a8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 05:05:21 +0000

    Add package antbase/0.1a6/antbase-0.1a6.tar.gz

---
+++ packages/antbase/antbase-0.1a6.tar.gz/antbase-0.1a6/antbase/type/command.py
@@ -0,0 +1,20 @@
+class Command:
+    command = [
+        "new",
+        "mergr"
+        "split",
+        "append",
+        "get",
+        "put",
+        "finish",
+        "use",
+        "new",
+        "update",
+        "cancel",
+        "finish",
+        "suspend",
+        "activate",
+        "deactivate",
+        "publish",
+        "show"
+    ]
