1ed53016b0a05647f9aa0862620c836ce26d48f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:04 +0000

    Add package adsGenericFunctions/0.2.30/adsgenericfunctions-0.2.30-py3-none-any.whl

---
+++ packages/adsGenericFunctions/adsgenericfunctions-0.2.30-py3-none-any.whl/src/demo/demo_insertion.py
@@ -0,0 +1,52 @@
+import src as ads
+from to_sort.env import *
+import logging
+import psycopg2
+
+# Établit une connexion pour le my_package pour l'écriture en base
+logger_connection = psycopg2.connect(database=pg_dwh_db, user=pg_dwh_user, password=pg_dwh_pwd, port=pg_dwh_port,
+                                     host=pg_dwh_host)
+logger = ads.Logger(logger_connection, logging.INFO, "AdsLogger", "LOGS", "LOGS_details")
+logger.info("Début de la démonstration.")
+
+# Active le timer, les requêtes seront chronométrées
+ads.set_timer(True)
+
+# On définit une source de connexion à laquelle on affecte notre my_package
+source1 = ads.dbPgsql({'database': pg_dwh_db
+                          , 'user': pg_dwh_user
+                          , 'password': pg_dwh_pwd
+                          , 'port': pg_dwh_port
+                          , 'host': pg_dwh_host},
+                      logger)
+# Cette opération sera loguée et chronométrée
+source1.connect()
+
+# Création d'une table
+source1.exec('''
+    CREATE TABLE IF NOT EXISTS demo_insert (
+        id SERIAL PRIMARY KEY,
+        tenantname VARCHAR(255),
+        fichier VARCHAR(255)
+    );
+''')
+logger.info("Table créée avec succès.")
+
+# Insertion d'une ligne
+source1.insert('demo_insert', ['tenantname', 'fichier'], ['tenant_example', 'file_example'])
+
+# Insertion de plusieurs lignes
+rows_to_insert = [
+    ('tenant1', 'file1.txt'),
+    ('tenant2', 'file2.txt'),
+    ('tenant3', 'file3.txt')
+]
+source1.insertBulk('demo_insert', ['tenantname', 'fichier'], rows_to_insert)
+
+data1 = source1.sqlQuery('''SELECT * FROM demo_insert ''')
+print(list(data1))
+
+# Suppression de la table
+source1.exec(''' DROP TABLE demo_insert ''')
+
+logger.info("Fin de la démonstration !")
---
+++ packages/adsGenericFunctions/adsgenericfunctions-0.2.30-py3-none-any.whl/src/to_sort/pipeline.py
@@ -0,0 +1,94 @@
+import logging
+
+class pipeline:
+    def __init__(self, dictionnary : dict):
+        self.__logger = logging.getLogger('src')
+        self.__db_source=dictionnary['db_source']
+        self.__query_source=dictionnary['query_source']
+        self.__db_destination=dictionnary['db_destination']
+        self.__table=dictionnary['table']
+        self.__cols=dictionnary['cols']
+
+    def run(self):
+        try:
+
+            self.__logger.info(f"Connexion établie avec la base de données.")
+            self.__db_source.connect()
+            self.__db_destination.connect()
+
+            data=[]
+            [data.append(element) for element in self.__db_source.sqlQuery(self.__query_source)]
+            self.__db_destination.insert(table=self.__table, cols=self.__cols,rows=data)
+
+        except Exception as e:
+            self.__logger.error(f"Échec de la connexion à la base de données.")
+            raise
+
+class pipelineTableau:
+    def __init__(self, dictionnary : dict):
+        self.__logger = logging.getLogger('src')
+        self.__tableau=dictionnary['tableau']
+        self.__db_destination=dictionnary['db_destination']
+        self.__table=dictionnary['table']
+        self.__cols=dictionnary['cols']
+
+    def run(self):
+        try:
+
+            self.__logger.info(f"Connexion établie avec la base de données.")
+            self.__db_destination.connect()
+
+            data=[]
+            [data.append(element) for element in self.__tableau]
+            self.__db_destination.insert(table=self.__table, cols=self.__cols,rows=data)
+
+        except Exception as e:
+            self.__logger.error(f"Échec de la connexion à la base de données.")
+            raise
+
+
+class pipelineBulk:
+    def __init__(self, dictionnary : dict):
+        self.__logger = logging.getLogger('src')
+        self.__db_source=dictionnary['db_source']
+        self.__query_source=dictionnary['query_source']
+        self.__db_destination=dictionnary['db_destination']
+        self.__table=dictionnary['table']
+        self.__cols=dictionnary['cols']
+
+    def run(self):
+        try:
+
+            self.__logger.info(f"Connexion établie avec la base de données.")
+            self.__db_source.connect()
+            self.__db_destination.connect()
+
+            data=[]
+            [data.append(element) for element in self.__db_source.sqlQuery(self.__query_source)]
+            self.__db_destination.insertBulk(table=self.__table, cols=self.__cols,rows=data)
+
+        except Exception as e:
+            self.__logger.error(f"Échec de la connexion à la base de données.")
+            raise
+
+class pipelineTableauBulk:
+    def __init__(self, dictionnary : dict):
+        self.__logger = logging.getLogger('src')
+        self.__tableau=dictionnary['tableau']
+        self.__db_destination=dictionnary['db_destination']
+        self.__table=dictionnary['table']
+        self.__cols=dictionnary['cols']
+
+    def run(self):
+        try:
+
+            self.__logger.info(f"Connexion établie avec la base de données.")
+            self.__db_destination.connect()
+
+            data=[]
+            [data.append(element) for element in self.__tableau]
+            self.__db_destination.insertBulk(table=self.__table, cols=self.__cols,rows=data)
+
+        except Exception as e:
+            self.__logger.error(f"Échec de la connexion à la base de données.")
+            raise
