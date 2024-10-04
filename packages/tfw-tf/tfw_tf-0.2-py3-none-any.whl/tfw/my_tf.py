0fed5a97bea40ccfdd39d176446732d6c0c0d775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 16:05:19 +0000

    Add package tfw-tf/0.2/tfw_tf-0.2-py3-none-any.whl

---
+++ packages/tfw-tf/tfw_tf-0.2-py3-none-any.whl/tfw/my_tf.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# my_tf.py
+
+class tf:
+    def __init__(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+    
+    def X_val(self):
+        return self.X_train
+    
+    def y_val(self):
+        return self.y_train
+    
+    def X_test(self):
+        return self.X_train
+    
+    def y_test(self):
+        return self.y_train
