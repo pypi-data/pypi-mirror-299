6748d9ddbe5c79fdac5b5878762aec6bef8bc844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 14:05:37 +0000

    Add package django-wafris/0.1.0/django_wafris-0.1.0.tar.gz

---
+++ packages/django-wafris/django_wafris-0.1.0.tar.gz/django_wafris-0.1.0/django_wafris/settings.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+
+wafris_settings = {
+    "REDIS_URL": "redis://localhost:6379/0",
+}
+
+user_settings = getattr(settings, "WAFRIS", {})
+wafris_settings.update(user_settings)
+
+print(wafris_settings)
