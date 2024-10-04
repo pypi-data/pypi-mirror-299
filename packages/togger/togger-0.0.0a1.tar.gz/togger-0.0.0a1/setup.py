e97dd06023d9822f6c98b893e6d8289802d53d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 14:05:39 +0000

    Add package togger/0.0.0a1/togger-0.0.0a1.tar.gz

---
+++ packages/togger/togger-0.0.0a1.tar.gz/togger-0.0.0a1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='togger',
+    description="coming soon",
+    version="0.0.0a1",
+    author='Jason Fried',
+    author_email='me@jasonfried.info',
+)
