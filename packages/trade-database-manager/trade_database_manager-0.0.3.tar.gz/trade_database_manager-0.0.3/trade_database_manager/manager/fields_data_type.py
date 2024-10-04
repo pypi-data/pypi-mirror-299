e6331866c13bc855242695221122d2e80d047b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 15:06:01 +0000

    Add package trade-database-manager/0.0.3/trade_database_manager-0.0.3.tar.gz

---
+++ packages/trade-database-manager/trade_database_manager-0.0.3.tar.gz/trade_database_manager-0.0.3/trade_database_manager/manager/fields_data_type.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/4/19 16:44
+# @Author  : YQ Tsui
+# @File    : fields_data_type.py
+# @Purpose :
+
+FIELD_DATA_TYPE_SQL = {
+    "ticker": "VARCHAR(20)",
+    "name": "VARCHAR(20)",
+    "currency": "VARCHAR(6)",
+    "exchange": "VARCHAR(10)",
+    "timezone": "VARCHAR(30)",
+    "tick_size": "REAL",
+    "lot_size": "REAL",
+    "min_lots": "REAL",
+    "market_tplus": "INTEGER",
+    "listed_date": "DATE",
+    "delisted_date": "DATE",
+    "sector": "VARCHAR(30)",
+    "industry": "VARCHAR(36)",
+    "country": "VARCHAR(36)",
+    "board_type": "VARCHAR(20)",
+}
