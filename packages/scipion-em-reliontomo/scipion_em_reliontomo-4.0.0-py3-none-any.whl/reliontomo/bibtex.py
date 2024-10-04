19746f2d29c97c09fdf5e095f6866e93b9f318e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:47 +0000

    Add package scipion-em-reliontomo/4.0.0/scipion_em_reliontomo-4.0.0-py3-none-any.whl

---
+++ packages/scipion-em-reliontomo/scipion_em_reliontomo-4.0.0-py3-none-any.whl/reliontomo/bibtex.py
@@ -0,0 +1,58 @@
+# coding: latin-1
+# **************************************************************************
+# *
+# * Authors:     J.M. De la Rosa Trevin (delarosatrevin@scilifelab.se) [1]
+# *
+# * [1] SciLifeLab, Stockholm University
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+"""
+
+@article {Zivanov2022,
+article_type = "journal",
+title = "A Bayesian approach to single-particle electron cryo-tomography in RELION-4.0",
+author = "Zivanov, Jasenko and Otón, Joaquín and Ke, Zunlong and von Kügelgen, Andriko and Pyle, Euan and Qu, Kun and Morado, Dustin and Castan?o-Díez, Daniel and Zanetti, Giulia and Bharat, Tanmay AM and Briggs, John AG and Scheres, Sjors HW",
+editor = "Egelman, Edward H and Dötsch, Volker and Bartesaghi, Alberto",
+volume = 11,
+year = 2022,
+month = "dec",
+pub_date = "2022-12-05",
+pages = "e83724",
+citation = "eLife 2022;11:e83724",
+doi = " https://doi.org/10.7554/eLife.83724",
+url = "https://doi.org/10.7554/eLife.83724",
+journal = "eLife",
+issn = "2050-084X",
+publisher = "eLife Sciences Publications, Ltd",
+}
+
+@article{Burt2024,
+author = "Burt, Alister and Toader, Bogdan and Warshamanage, Rangana and von Kugelgen, Andriko and Pyle, Euan and Zivanov, Jasenko and Kimanius, Dari and Bharat, Tanmay A.M. and Scheres, Sjors H.W.",
+title = "An image processing pipeline for electron cryo-tomography in RELION-5",
+year = "2024",
+doi = "https://doi.org/10.1101/2024.04.26.591129",
+publisher = "Cold Spring Harbor Laboratory",
+URL = "https://www.biorxiv.org/content/early/2024/04/27/2024.04.26.591129",
+eprint = "https://www.biorxiv.org/content/early/2024/04/27/2024.04.26.591129.full.pdf",
+journal = "bioRxiv",
+}
+
+"""
