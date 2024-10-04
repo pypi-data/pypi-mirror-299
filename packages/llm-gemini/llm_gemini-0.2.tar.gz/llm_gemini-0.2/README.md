4a8f780e50af2fb2295714cd8930144eee1ac02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 06:05:43 +0000

    Add package llm-gemini/0.2/llm_gemini-0.2.tar.gz

---
+++ packages/llm-gemini/llm_gemini-0.2.tar.gz/llm_gemini-0.2/README.md
@@ -0,0 +1,82 @@
+# llm-gemini
+
+[![PyPI](https://img.shields.io/pypi/v/llm-gemini.svg)](https://pypi.org/project/llm-gemini/)
+[![Changelog](https://img.shields.io/github/v/release/simonw/llm-gemini?include_prereleases&label=changelog)](https://github.com/simonw/llm-gemini/releases)
+[![Tests](https://github.com/simonw/llm-gemini/workflows/Test/badge.svg)](https://github.com/simonw/llm-gemini/actions?query=workflow%3ATest)
+[![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](https://github.com/simonw/llm-gemini/blob/main/LICENSE)
+
+API access to Google's Gemini models
+
+## Installation
+
+Install this plugin in the same environment as [LLM](https://llm.datasette.io/).
+```bash
+llm install llm-gemini
+```
+## Usage
+
+Configure the model by setting a key called "gemini" to your [API key](https://aistudio.google.com/app/apikey):
+
+```bash
+llm keys set gemini
+```
+```
+<paste key here>
+```
+
+Now run the model using `-m gemini-pro`, for example:
+
+```bash
+llm -m gemini-pro "A joke about a pelican and a walrus"
+```
+
+> Why did the pelican get mad at the walrus?
+>
+> Because he called him a hippo-crit.
+
+To chat interactively with the model, run `llm chat`:
+
+```bash
+llm chat -m gemini-pro
+```
+
+If you have access to the Gemini 1.5 Pro preview you can use `-m gemini-1.5-pro-latest` to work with that model.
+
+### Embeddings
+
+The plugin also adds support for the `text-embedding-004` embedding model.
+
+Run that against a single string like this:
+```bash
+llm embed -m text-embedding-004 -c 'hello world'
+```
+This returns a JSON array of 768 numbers.
+
+This command will embed every `README.md` file in child directories of the current directory and store the results in a SQLite database called `embed.db` in a collection called `readmes`:
+
+```bash
+llm embed-multi readmes --files . '*/README.md' -d embed.db -m text-embedding-004
+```
+You can then run similarity searches against that collection like this:
+```bash
+llm similar readmes -c 'upload csvs to stuff' -d embed.db
+```
+
+See the [LLM embeddings documentation](https://llm.datasette.io/en/stable/embeddings/cli.html) for further details.
+
+## Development
+
+To set up this plugin locally, first checkout the code. Then create a new virtual environment:
+```bash
+cd llm-gemini
+python3 -m venv venv
+source venv/bin/activate
+```
+Now install the dependencies and test dependencies:
+```bash
+llm install -e '.[test]'
+```
+To run the tests:
+```bash
+pytest
+```
---
+++ packages/llm-gemini/llm_gemini-0.2.tar.gz/llm_gemini-0.2/llm_gemini.py
@@ -0,0 +1,134 @@
+import httpx
+import ijson
+import llm
+import urllib.parse
+
+# We disable all of these to avoid random unexpected errors
+SAFETY_SETTINGS = [
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+]
+
+
+@llm.hookimpl
+def register_models(register):
+    register(GeminiPro("gemini-pro"))
+    register(GeminiPro("gemini-1.5-pro-latest"))
+    register(GeminiPro("gemini-1.5-flash-latest"))
+    register(GeminiPro("gemini-1.5-pro-001"))
+    register(GeminiPro("gemini-1.5-flash-001"))
+    register(GeminiPro("gemini-1.5-pro-002"))
+    register(GeminiPro("gemini-1.5-flash-002"))
+    register(GeminiPro("gemini-1.5-flash-8b-latest"))
+    register(GeminiPro("gemini-1.5-flash-8b-001"))
+
+
+class GeminiPro(llm.Model):
+    can_stream = True
+
+    def __init__(self, model_id):
+        self.model_id = model_id
+
+    def build_messages(self, prompt, conversation):
+        if not conversation:
+            return [{"role": "user", "parts": [{"text": prompt.prompt}]}]
+        messages = []
+        for response in conversation.responses:
+            messages.append(
+                {"role": "user", "parts": [{"text": response.prompt.prompt}]}
+            )
+            messages.append({"role": "model", "parts": [{"text": response.text()}]})
+        messages.append({"role": "user", "parts": [{"text": prompt.prompt}]})
+        return messages
+
+    def execute(self, prompt, stream, response, conversation):
+        key = llm.get_key("", "gemini", "LLM_GEMINI_KEY")
+        url = "https://generativelanguage.googleapis.com/v1beta/models/{}:streamGenerateContent?".format(
+            self.model_id
+        ) + urllib.parse.urlencode(
+            {"key": key}
+        )
+        gathered = []
+        body = {
+            "contents": self.build_messages(prompt, conversation),
+            "safetySettings": SAFETY_SETTINGS,
+        }
+        if prompt.system:
+            body["systemInstruction"] = {"parts": [{"text": prompt.system}]}
+        with httpx.stream(
+            "POST",
+            url,
+            timeout=None,
+            json=body,
+        ) as http_response:
+            events = ijson.sendable_list()
+            coro = ijson.items_coro(events, "item")
+            for chunk in http_response.iter_bytes():
+                coro.send(chunk)
+                if events:
+                    event = events[0]
+                    if isinstance(event, dict) and "error" in event:
+                        raise llm.ModelError(event["error"]["message"])
+                    try:
+                        yield event["candidates"][0]["content"]["parts"][0]["text"]
+                    except KeyError:
+                        yield ""
+                    gathered.append(event)
+                    events.clear()
+        response.response_json = gathered
+
+
+@llm.hookimpl
+def register_embedding_models(register):
+    register(
+        GeminiEmbeddingModel("text-embedding-004", "text-embedding-004"),
+    )
+
+
+class GeminiEmbeddingModel(llm.EmbeddingModel):
+    needs_key = "gemini"
+    key_env_var = "LLM_GEMINI_KEY"
+    batch_size = 20
+
+    def __init__(self, model_id, gemini_model_id):
+        self.model_id = model_id
+        self.gemini_model_id = gemini_model_id
+
+    def embed_batch(self, items):
+        headers = {
+            "Content-Type": "application/json",
+        }
+        data = {
+            "requests": [
+                {
+                    "model": "models/" + self.gemini_model_id,
+                    "content": {"parts": [{"text": item}]},
+                }
+                for item in items
+            ]
+        }
+
+        with httpx.Client() as client:
+            response = client.post(
+                f"https://generativelanguage.googleapis.com/v1beta/models/{self.gemini_model_id}:batchEmbedContents?key={self.get_key()}",
+                headers=headers,
+                json=data,
+                timeout=None,
+            )
+
+        response.raise_for_status()
+        return [item["values"] for item in response.json()["embeddings"]]
