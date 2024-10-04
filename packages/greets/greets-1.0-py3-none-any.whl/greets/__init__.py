205b3ce74ec054712e222f5b28d02c02911c4d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 03:10:34 +0000

    Add package greets/1.0/greets-1.0-py3-none-any.whl

---
+++ packages/greets/greets-1.0-py3-none-any.whl/greets/__init__.py
@@ -0,0 +1,7 @@
+def languages():
+	print("English,Spanish,French,Italian")
+
+from greets.English import greet as English_greet
+from greets.Spanish import greet as Spanish_greet
+from greets.French import greet as French_greet
+from greets.Italian import greet as Italian_greet
