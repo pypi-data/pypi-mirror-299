ae612815c1b75be2d229f95cbc248bf203400a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:51 +0000

    Add package scbk-mlops/0.2.1/scbk_mlops-0.2.1.tar.gz

---
+++ packages/scbk-mlops/scbk_mlops-0.2.1.tar.gz/scbk_mlops-0.2.1/scbk_mlops/data_engineering.py
@@ -0,0 +1,47 @@
+"""
+Data Engineering Module
+--------------------------------------
+해당 모듈은 데이터 전처리 및 Feature Engineering을 수행하는 모듈입니다.
+"""
+
+def add_change_features():
+    """
+    Adding Change Features
+    """
+    pass
+
+def add_penetration_features():
+    """
+    Calculate penetration features by dividing account balances by AUM balances
+    """
+    pass
+
+def process_null_values():
+    """
+    Preprocess null values
+    """
+    pass
+
+def split_data():
+    """
+    Split data into S1, S2, S3
+    """
+    pass
+
+def feature_selection():
+    """
+    GrootCV feature selection using train (S2) data
+    """
+    pass
+
+def apply_feature_selection():
+    """
+    Apply feature selection result to train, test data
+    """
+    pass
+
+def output_eligible_dataset():
+    """
+    Output eligible dataset for model input
+    """
+    pass
---
+++ packages/scbk-mlops/scbk_mlops-0.2.1.tar.gz/scbk_mlops-0.2.1/scbk_mlops/model_evaluation.py
@@ -0,0 +1,66 @@
+"""
+Model Evaluation Module
+--------------------------------------
+해당 모듈은 모델링 결과를 평가하는 기능을 제공하는 모듈입니다.
+주로 Classification 및 Clustering을 위한 함수를 제공하며, 해당 함수를 활용하여 모델링 결과를 평가하시면 됩니다.
+"""
+
+def create_leaderboard():
+    """
+    Create model leaderboard based on @30 S3
+    """
+    pass
+
+def output_evaluation_artifacts():
+    """
+    공정성 편향 등과 같은 다양한 RAI 및 거버넌스 평가 자료 출력
+    """
+    pass
+
+def model_evaluation_rai():
+    """
+    Responsible AI (RAI) 도구를 고려하여 공정성 검토
+    """
+    pass
+
+def generate_leaderboard_report():
+    """
+    모델 비교 (e.g. S1 vs S2 S1 vs S3) 리더보드 보고서 생성
+    """
+    pass
+
+def feature_impact():
+    """
+    GrootCV 결과와의 차이를 평가
+    """
+    pass
+
+def model_explainability():
+    """
+    모델 해석 가능성 통찰 제공 (Global Explainability/Feature Impact/Local Explainability)
+    """
+    pass
+
+def bias_fairness_assessment():
+    """
+    Wasserstein 거리 플롯 또는 기타 공정성 메트릭을 사용하여 편향 및 공정성 평가
+    """
+    pass
+
+def check_governance_measures():
+    """
+    모든 거버넌스 관련 조치를 고려하도록 확인
+    """
+    pass
+
+def explainable_ai():
+    """
+    SHAP LIME과 같은 해석 가능성 방법을 통합하여 투명성과 해석 가능성 강화
+    """
+    pass
+
+def model_fairness_check():
+    """
+    모델 예측에서 편향을 식별하고 완화하기 위한 공정성 감사 수행
+    """
+    pass
