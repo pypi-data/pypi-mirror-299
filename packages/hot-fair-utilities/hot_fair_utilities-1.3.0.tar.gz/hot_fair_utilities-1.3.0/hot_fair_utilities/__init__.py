b45d0e252f653359bbad4d2bbebdd729c4076340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 16:05:11 +0000

    Add package hot-fair-utilities/1.3.0/hot_fair_utilities-1.3.0.tar.gz

---
+++ packages/hot-fair-utilities/hot_fair_utilities-1.3.0.tar.gz/hot_fair_utilities-1.3.0/hot_fair_utilities/__init__.py
@@ -0,0 +1,6 @@
+from .georeferencing import georeference
+from .inference import predict
+from .postprocessing import polygonize, vectorize
+from .preprocessing import preprocess
+from .training import train
+from .utils import bbox2tiles, tms2img
