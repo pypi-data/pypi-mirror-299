97256699635b18c76911dbd4acb11128979b37af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 15:05:12 +0000

    Add package rank2plan/0.3.0/rank2plan-0.3.0-py3-none-any.whl

---
+++ packages/rank2plan/rank2plan-0.3.0-py3-none-any.whl/rank2plan/types.py
@@ -0,0 +1,30 @@
+from enum import Enum, auto
+from numpy import ndarray
+from scipy.sparse import csr_matrix
+from typing import TypeAlias
+
+Matrix: TypeAlias = ndarray | csr_matrix
+
+
+class Pair:
+    def __init__(self, i: int, j: int, gap: float = 1.0, sample_weight: float = 1.0):
+        self.i = i
+        self.j = j
+        self.gap = gap
+        self.sample_weight = sample_weight
+
+
+class LossType(Enum):
+    Hinge = auto()
+    SquaredHinge = auto()
+
+
+class PenalisationType(Enum):
+    L1 = auto()
+    L2 = auto()
+
+
+class TuningMetric(Enum):
+    KendallTau = auto()
+    # See documents/theory.pdf for what the LpMainObjective is
+    LpMainObjective = auto()
