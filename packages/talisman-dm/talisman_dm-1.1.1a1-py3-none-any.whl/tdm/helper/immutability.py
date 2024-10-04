e72eaf00e5931922adcf84904d91a06d576ce313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:46 +0000

    Add package talisman-dm/1.1.1a1/talisman_dm-1.1.1a1-py3-none-any.whl

---
+++ packages/talisman-dm/talisman_dm-1.1.1a1-py3-none-any.whl/tdm/helper/immutability.py
@@ -0,0 +1,38 @@
+from typing import Mapping, Sequence
+
+from immutabledict import immutabledict
+from pydantic import BaseModel
+
+
+def freeze_dict(obj: Mapping) -> immutabledict:
+    return immutabledict((k, freeze(v)) for k, v in obj.items())
+
+
+def freeze_sequence(obj: Sequence) -> tuple:
+    return tuple(freeze(v) for v in obj)
+
+
+def freeze(obj):
+    if obj is None or isinstance(obj, (str, int, float, bool)):
+        return obj
+    if isinstance(obj, Mapping):
+        return freeze_dict(obj)
+    if isinstance(obj, set):
+        return frozenset(freeze(v) for v in obj)
+    if isinstance(obj, Sequence):
+        return freeze_sequence(obj)
+    raise ValueError
+
+
+def unfreeze(obj):
+    if obj is None or isinstance(obj, (str, int, float, bool)):
+        return obj
+    if isinstance(obj, Mapping):
+        return {k: unfreeze(v) for k, v in obj.items()}
+    if isinstance(obj, (frozenset, set)):
+        return {unfreeze(v) for v in obj}
+    if isinstance(obj, Sequence):
+        t = type(obj)
+        return t(unfreeze(v) for v in obj)
+    if isinstance(obj, BaseModel):
+        return unfreeze(obj.dict())
