6bc97df003d77f9ded708727efe7930ea9baf103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:06:00 +0000

    Add package drf-recursive/0.4.0/drf_recursive-0.4.0.tar.gz

---
+++ packages/drf-recursive/drf_recursive-0.4.0.tar.gz/drf_recursive-0.4.0/src/django_rest_framework_recursive/__init__.py
@@ -0,0 +1,6 @@
+"""
+This package provides a `RecursiveField` that allows you to serialize a tree,
+linked list, or even a directed acyclic graph, where the children may be the
+same type as the parent. It also supports validation, deserialization,
+ModelSerializers, and multi-step recursive structures.
+"""
