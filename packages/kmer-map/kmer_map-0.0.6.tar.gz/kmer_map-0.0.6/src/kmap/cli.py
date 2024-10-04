ea186f0227d4b75f8ea88223fd4b7dfa4ff6d57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 07:05:48 +0000

    Add package kmer-map/0.0.6/kmer_map-0.0.6.tar.gz

---
+++ packages/kmer-map/kmer_map-0.0.6.tar.gz/kmer_map-0.0.6/src/kmap/cli.py
@@ -0,0 +1,30 @@
+import click
+from .kmer_count import preproc
+from .motif_discovery import  scan_motif, ex_hamball, draw_logo
+from .visualization import visualize_kmers
+from .util import align_conseq, extract_motif_locations
+
+@click.group()
+def cli():
+    """
+    KMAP: visualize kmers in 2d.
+    """
+    pass
+
+def display_paper_info():
+    print()
+    print("Citation")
+    print("KMAP: Kmer Manifold Approximation and Projection for visualizing DNA sequences")
+    print("Chengbo Fu, Einari A. Niskanen, Gong-Hong Wei, Zhirong Yang, Marta Sanvicente-García, Marc Güell, Lu Cheng*")
+    print("BioRxiv")
+    print("2024")
+    print("DOI: https://doi.org/10.1101/2024.04.12.589197")
+
+
+cli.add_command(preproc)
+cli.add_command(scan_motif)
+cli.add_command(ex_hamball)
+cli.add_command(draw_logo)
+cli.add_command(visualize_kmers)
+cli.add_command(align_conseq)
+cli.add_command(extract_motif_locations)
