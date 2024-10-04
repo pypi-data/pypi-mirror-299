d2cb714fb4aa5e26dd1304a0f44cc70d6310da85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 15:05:52 +0000

    Add package omnijp/2.17.0/omnijp-2.17.0-py3-none-any.whl

---
+++ packages/omnijp/omnijp-2.17.0-py3-none-any.whl/src/dbrequest/db_request_builder.py
@@ -0,0 +1,32 @@
+from src.dbdisk.types import DbType
+from src.dbrequest.db_request import DbRequest
+from src.dbrequest.db_request_executor import DbRequestExecutor
+from src.dbrequest.db_results import DbResult
+
+
+class DbRequestBuilder:
+    def __init__(self):
+        self.db_request = DbRequest()
+
+    @classmethod
+    def create(cls, setup):
+        builder = cls()
+        setup(builder)
+        return builder
+    
+    def set_db_type(self, db_type: DbType):
+        self.db_request.db_type = db_type
+        return self
+    def set_connection_string(self, connection_string):
+        self.db_request.connection_string = connection_string
+        return self
+    def set_table_list(self, table_list):
+        self.db_request.table_list = table_list
+        return self
+    
+    def execute(self, query)->DbResult:
+        with DbRequestExecutor(self.db_request) as executor:
+            return executor.execute(query)
+
+    
+
