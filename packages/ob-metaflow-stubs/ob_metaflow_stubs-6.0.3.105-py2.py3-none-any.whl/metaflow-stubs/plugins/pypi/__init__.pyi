f5d9904d217cc7d22b6994b201ef50d431486d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 16:05:19 +0000

    Add package ob-metaflow-stubs/6.0.3.105/ob_metaflow_stubs-6.0.3.105-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.105-py2.py3-none-any.whl/metaflow-stubs/plugins/pypi/__init__.pyi
@@ -0,0 +1,18 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.24.1+obcheckpoint(0.0.11);ob(v1)                              #
+# Generated on 2024-10-04T10:13:10.667586                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+class MetaflowException(Exception, metaclass=type):
+    def __init__(self, msg = "", lineno = None):
+        ...
+    def __str__(self):
+        ...
+    ...
+
+MAGIC_FILE: str
+
