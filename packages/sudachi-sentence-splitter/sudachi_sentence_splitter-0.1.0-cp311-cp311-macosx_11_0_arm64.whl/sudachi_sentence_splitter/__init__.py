ea3a6e2556c3345b5f7dfa7412b593e6cbe87ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:06:05 +0000

    Add package sudachi-sentence-splitter/0.1.0/sudachi_sentence_splitter-0.1.0-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/sudachi-sentence-splitter/sudachi_sentence_splitter-0.1.0-cp311-cp311-macosx_11_0_arm64.whl/sudachi_sentence_splitter/__init__.py
@@ -0,0 +1,5 @@
+from .sudachi_sentence_splitter import *
+
+__doc__ = sudachi_sentence_splitter.__doc__
+if hasattr(sudachi_sentence_splitter, "__all__"):
+    __all__ = sudachi_sentence_splitter.__all__
