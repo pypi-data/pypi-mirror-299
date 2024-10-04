27915cd8138fe5327dd87367b2c0a45618a327d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 05:05:20 +0000

    Add package yt-monk/0.1.1a2/yt_monk-0.1.1a2-py3-none-any.whl

---
+++ packages/yt-monk/yt_monk-0.1.1a2-py3-none-any.whl/yt_monk/__main__.py
@@ -0,0 +1,8 @@
+from yt_monk import Downloader  # Import the class or function that contains your CLI logic
+
+def main():
+    downloader = Downloader()
+    downloader.main()
+'''
+if __name__ == '__main__':
+    main()'''
