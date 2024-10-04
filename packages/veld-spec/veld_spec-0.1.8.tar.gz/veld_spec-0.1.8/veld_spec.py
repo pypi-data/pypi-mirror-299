91505609ed49ac68c7d85f91558827de0ec8c5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 05:05:09 +0000

    Add package veld-spec/0.1.8/veld_spec-0.1.8.tar.gz

---
+++ packages/veld-spec/veld_spec-0.1.8.tar.gz/veld_spec-0.1.8/veld_spec.py
@@ -0,0 +1,17 @@
+import json
+
+import jsonschema
+
+
+def validate():
+    print(1)
+    # with open("./veld_schema.json", "r") as schema_file:
+    #     schema_dict = json.load(schema_file)
+    #     try:
+    #         jsonschema.validate(instance=veld_metadata, schema=schema_dict)
+    #         print("valid.")
+    #     except jsonschema.exceptions.ValidationError as err:
+    #         print("invalid")
+    #         raise err
+    
+
