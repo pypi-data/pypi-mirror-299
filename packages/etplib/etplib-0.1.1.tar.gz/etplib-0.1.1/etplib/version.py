0b6484b691d257cfe61d90f7e21d45d9c023f41b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 15:06:04 +0000

    Add package etplib/0.1.1/etplib-0.1.1.tar.gz

---
+++ packages/etplib/etplib-0.1.1.tar.gz/etplib-0.1.1/etplib/version.py
@@ -0,0 +1,12 @@
+"""
+Copyright (C) 2024, Jabez Winston C
+
+Embedded Tester Protocol Library - ADC
+
+Author  : Jabez Winston C <jabezwinston@gmail.com>
+License : MIT
+Date    : 04-Oct-2024
+
+"""
+
+VERSION="0.1.1"
