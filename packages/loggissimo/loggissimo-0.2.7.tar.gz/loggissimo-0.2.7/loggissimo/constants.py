32b266ee19d43a55161f53d5ac6a36b6cd72724d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 15:04:57 +0000

    Add package loggissimo/0.2.7/loggissimo-0.2.7.tar.gz

---
+++ packages/loggissimo/loggissimo-0.2.7.tar.gz/loggissimo-0.2.7/loggissimo/constants.py
@@ -0,0 +1,21 @@
+from typing import Final
+from enum import IntEnum
+
+DEFAULT_LOGGER_NAME: Final[str] = "default"
+
+START_LOGGER_TRACE = "[Start Loggissimo Trace]"
+END_LOGGER_TRACE = "[End Loggissimo Trace]"
+
+
+class Level(IntEnum):
+    TRACE = 5
+    DELETE = 9
+    DEBUG = 10
+    INFO = 20
+    SUCCESS = 25
+    WARNING = 30
+    ERROR = 40
+    CRITICAL = 50
+
+    def __str__(self) -> str:
+        return self.name
