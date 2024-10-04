d7f68788dadbacb4c9120f5d01477aad9796079f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 03:10:05 +0000

    Add package hello-earth/1.3.0/hello_earth-1.3.0.tar.gz

---
+++ packages/hello-earth/hello_earth-1.3.0.tar.gz/hello_earth-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+with open('README.md', "r") as file:
+    description = file.read()
+setup(
+    name='hello_earth',
+    version="1.3.0",
+    packages=find_packages(),
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
