cdf3c8f6048793d9dd23a12d23b8f936553b7bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:38 +0000

    Add package piccolo-admin/1.8.0/piccolo_admin-1.8.0-py3-none-any.whl

---
+++ packages/piccolo-admin/piccolo_admin-1.8.0-py3-none-any.whl/piccolo_admin/sandbox.py
@@ -0,0 +1,48 @@
+"""
+Runs the admin in read only mode - useful for letting people evaluate the admin
+online without risk of abuse.
+"""
+
+import targ
+import uvicorn
+
+from piccolo_admin.endpoints import create_admin
+from piccolo_admin.example.tables import (
+    Director,
+    Movie,
+    Sessions,
+    User,
+    create_schema,
+    populate_data,
+    set_engine,
+)
+
+APP = create_admin(
+    [Director, Movie],
+    auth_table=User,
+    session_table=Sessions,
+    read_only=True,
+)
+
+
+def sandbox(host: str = "localhost", port: int = 8080):
+    """
+    Run the Piccolo Admin in read only mode with a SQLite database.
+
+    :param host:
+        Which host to serve the app on.
+    :param host:
+        Which port to serve the app on.
+
+    """
+    set_engine()
+    create_schema(persist=False)
+    populate_data()
+
+    uvicorn.run(APP, host=host, port=port)
+
+
+if __name__ == "__main__":
+    cli = targ.CLI()
+    cli.register(sandbox)
+    cli.run(solo=True)
