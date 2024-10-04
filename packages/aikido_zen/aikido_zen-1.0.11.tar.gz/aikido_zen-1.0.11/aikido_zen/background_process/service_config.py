27011a556add522c949db9d082c35d03854bb8c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 03:10:16 +0000

    Add package aikido_zen/1.0.11/aikido_zen-1.0.11.tar.gz

---
+++ packages/aikido_zen/aikido_zen-1.0.11.tar.gz/aikido_zen-1.0.11/aikido_zen/background_process/service_config.py
@@ -0,0 +1,35 @@
+"""
+Exports ServiceConfig class
+"""
+
+from aikido_zen.helpers.match_endpoints import match_endpoints
+
+
+class ServiceConfig:
+    """Class holding the config of the connection_manager"""
+
+    def __init__(
+        self, endpoints, last_updated_at, blocked_uids, bypassed_ips, received_any_stats
+    ):
+        self.endpoints = [
+            endpoint for endpoint in endpoints if not endpoint.get("graphql")
+        ]
+        self.last_updated_at = last_updated_at
+        self.bypassed_ips = set(bypassed_ips)
+        self.blocked_uids = set(blocked_uids)
+        self.received_any_stats = bool(received_any_stats)
+
+    def get_endpoints(self, route_metadata):
+        """
+        Gets the endpoint that matches the current context
+        route_metadata object includes route, url and method
+        """
+        return match_endpoints(route_metadata, self.endpoints)
+
+    def is_bypassed_ip(self, ip):
+        """Checks if the IP is on the bypass list"""
+        return ip in self.bypassed_ips
+
+    def is_user_blocked(self, user_id):
+        """Checks if the user id is blocked"""
+        return user_id in self.blocked_uids
