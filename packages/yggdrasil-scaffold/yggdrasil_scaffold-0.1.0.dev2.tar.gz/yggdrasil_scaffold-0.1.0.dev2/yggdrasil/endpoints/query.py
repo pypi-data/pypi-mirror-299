3a0b60c58bc53f68877e19f4021b7dabd2563243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 14:05:31 +0000

    Add package yggdrasil-scaffold/0.1.0.dev2/yggdrasil_scaffold-0.1.0.dev2.tar.gz

---
+++ packages/yggdrasil-scaffold/yggdrasil_scaffold-0.1.0.dev2.tar.gz/yggdrasil_scaffold-0.1.0.dev2/yggdrasil/endpoints/query.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+"""注册 Yggdrasil API 的查询端点"""
+from typing import Annotated, Optional
+
+from Crypto.PublicKey.RSA import RsaKey
+from fastapi import APIRouter, Depends, Response
+
+from yggdrasil.endpoints import handlers
+from adofai import SerializedProfile
+from adofai.models import GameProfile
+
+query_endpoints = APIRouter()  # 实际上是两类 Vanilla API 的整合，所以前缀不固定
+
+
+@query_endpoints.get("/sessionserver/session/minecraft/profile/{uuid}")
+async def from_uuid(rsp: Response,
+                    result: Annotated[Optional[GameProfile], Depends(handlers.query.from_uuid)],
+                    sign_key: Annotated[RsaKey, Depends(handlers.root.sign_key)],
+                    unsigned: bool = True) -> SerializedProfile | None:
+    """从UUID查询单个玩家"""
+    if result is not None:
+        if unsigned:
+            return result.serialize("unsigned")
+        else:
+            return result.serialize("full", sign_key)
+    else:
+        rsp.status_code = 204
+        return None
+
+
+@query_endpoints.post("/api/profiles/minecraft")
+async def from_name_batch(result: Annotated[
+    list[GameProfile], Depends(handlers.query.from_name_batch)
+]) -> list[SerializedProfile]:
+    """从用户名批量查询用户的UUID"""
+    return [i.serialize("minimum") for i in result]
