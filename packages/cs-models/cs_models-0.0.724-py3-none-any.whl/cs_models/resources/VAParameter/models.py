d2b2e87171bcf4ee37d0e5df98679478b521d2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 06:05:58 +0000

    Add package cs-models/0.0.724/cs_models-0.0.724-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.724-py3-none-any.whl/cs_models/resources/VAParameter/models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+)
+from datetime import datetime
+
+from ...database import Base
+
+
+class VAParameterModel(Base):
+    __tablename__ = 'va_parameters'
+
+    pid = Column(Integer, primary_key=True)
+    cid = Column(Integer, nullable=False, index=True)
+    pname = Column(String(191), nullable=False)
+    ciso = Column(String(20), nullable=True)
+    u = Column(Integer, nullable=True)
+    sign = Column(String(10), nullable=True)
+    ppid = Column(
+        Integer,
+        ForeignKey('va_parameters.id'),
+        nullable=True
+    )
+    ftid = Column(Integer, nullable=True)
+    dpname = Column(String(191), nullable=True)
+    scid = Column(Integer, nullable=True)
+    so = Column(Integer, nullable=True)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
