f1db54400951c5266337d1faa6601ef064ea61c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 06:06:10 +0000

    Add package m3u8-analyzer/1.0.4.3/m3u8_analyzer-1.0.4.3.tar.gz

---
+++ packages/m3u8-analyzer/m3u8_analyzer-1.0.4.3.tar.gz/m3u8_analyzer-1.0.4.3/CHANGELOG.md
@@ -0,0 +1,16 @@
+![Versão](https://img.shields.io/badge/version-1.0.4.2-orange)
+
+-
+---
+![Correção](https://img.shields.io/badge/status-correção-brightgreen)
+
+- [x] 
+---
+### ![Bugs](https://img.shields.io/badge/status-bugs-red)
+- Para reportar bugs, [clique aqui](https://github.com/PauloCesar-dev404/M3u8_Analyzer/issues).
+---
+![Novas Funcionalidades](https://img.shields.io/badge/status-novas_funcionalidades-blue)
+
+- [x] `Wrapper`: para parsear de maneria limpa playlists m3u8
+- [x] `EncryptSuport`: Separado a lógica para suporte a criptografia AES-128
+- [x] `M3u8Downloader`: Soporte a downloads de playlists (requer **ffmpeg**)
---
+++ packages/m3u8-analyzer/m3u8_analyzer-1.0.4.3.tar.gz/m3u8_analyzer-1.0.4.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+# Lê o conteúdo do README.md
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+# Lê o conteúdo do CHANGELOG.md
+with open('CHANGELOG.md', 'r', encoding='utf-8') as file:
+    changelog = file.read()
+
+# Concatena o CHANGELOG.md ao README.md
+long_description += "\n\n# CHANGELOG\n\n" + changelog
+
+setup(
+    name="m3u8-analyzer",
+    version="1.0.4.3",
+    description="Análise de dados de HLS m3u8",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="PauloCesar0073-dev404",
+    author_email="paulocesar0073dev404@gmail.com",
+    url='https://paulocesar-dev404.github.io/M3u8_Analyzer/',
+    license="MIT",
+    keywords=["hls", "m3u8", "m3u8_analyzer", "M3u8Analyzer"],
+    packages=find_packages(),
+    install_requires=['colorama', 'requests', 'cryptography'],
+    include_package_data=True,
+    platforms=["any"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+    ],
+)
