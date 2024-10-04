732cf449e984960ef1ccc583f250e61684228e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 14:06:09 +0000

    Add package dcore/3.5.0/dcore-3.5.0-py3-none-any.whl

---
+++ packages/dcore/dcore-3.5.0-py3-none-any.whl/dcore/lattice_models/__init__.py
@@ -0,0 +1,17 @@
+from .wannier90_model import Wannier90Model
+from .predefined_models import ChainModel, SquareModel, CubicModel, BetheModel
+from .external_model import ExternalModel
+import sys
+
+def create_lattice_model(params):
+    model_name =  params["model"]["lattice"]
+
+    for c in all_lattice_models:
+        if model_name == c.name():
+            return c(params)
+
+    # raise RuntimeError('Unknown lattice model name: ' + model_name)
+    sys.exit(f"ERROR: Unknown lattice model name: [model] lattice={model_name!r}")
+
+
+all_lattice_models = [ChainModel, SquareModel, CubicModel, BetheModel, Wannier90Model, ExternalModel]
