1e7d457546264d0cbfb9fab0bf415bd5e392687f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:21 +0000

    Add package greenland-runtime/0.0.0/greenland_runtime-0.0.0.tar.gz

---
+++ packages/greenland-runtime/greenland_runtime-0.0.0.tar.gz/greenland_runtime-0.0.0/src/greenland/runtime/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present Markus E Leypold <pythonista-4sda212@m-e-leypold.de>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+__version__ = "0.0.1"
---
+++ packages/greenland-runtime/greenland_runtime-0.0.0.tar.gz/greenland_runtime-0.0.0/src/greenland/runtime/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Markus E Leypold <pythonista-4sda212@m-e-leypold.de>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
