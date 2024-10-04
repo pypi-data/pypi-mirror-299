cd2dba0815e464a49b406c2dcd25cd76000ac141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:22 +0000

    Add package julep/1.8.0/julep-1.8.0-py3-none-any.whl

---
+++ packages/julep/julep-1.8.0-py3-none-any.whl/julep/types/tool.py
@@ -0,0 +1,53 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Union, Optional
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["Tool", "Function", "Integration", "System"]
+
+
+class Function(BaseModel):
+    description: Optional[str] = None
+
+    name: Optional[object] = None
+
+    parameters: Optional[object] = None
+
+
+class Integration(BaseModel):
+    provider: Union[Literal["dummy", "hacker_news", "weather", "wikipedia", "spider", "brave", "browserbase"], str]
+
+    arguments: Optional[object] = None
+
+    description: Optional[str] = None
+
+    method: Optional[str] = None
+
+    setup: Optional[object] = None
+
+
+class System(BaseModel):
+    call: str
+
+    arguments: Optional[object] = None
+
+    description: Optional[str] = None
+
+
+class Tool(BaseModel):
+    name: str
+
+    function: Optional[Function] = None
+    """Function definition"""
+
+    inherited: Optional[bool] = None
+
+    integration: Optional[Integration] = None
+    """Integration definition"""
+
+    system: Optional[System] = None
+    """System definition"""
+
+    type: Optional[Literal["function", "integration", "system", "api_call"]] = None
