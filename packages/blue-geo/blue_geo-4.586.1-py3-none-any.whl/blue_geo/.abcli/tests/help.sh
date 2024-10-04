b08cd8665cd9f59e37f8a5bc2f034a2e7530a49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 15:05:54 +0000

    Add package blue-geo/4.586.1/blue_geo-4.586.1-py3-none-any.whl

---
+++ packages/blue-geo/blue_geo-4.586.1-py3-none-any.whl/blue_geo/.abcli/tests/help.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/env bash
+
+function test_blue_geo_help() {
+    local options=$1
+
+    local module
+    for module in \
+        "blue_geo" \
+        \
+        "blue_geo catalog" \
+        "blue_geo catalog browse" \
+        "blue_geo catalog get" \
+        "blue_geo catalog list" \
+        "blue_geo catalog query" \
+        \
+        "blue_geo datacube" \
+        "blue_geo datacube crop" \
+        "blue_geo datacube generate" \
+        "blue_geo datacube get" \
+        "blue_geo datacube ingest" \
+        "blue_geo datacube list" \
+        \
+        "blue_geo gdal" \
+        \
+        "blue_geo QGIS" \
+        "blue_geo QGIS_expressions" \
+        "blue_geo QGIS_server" \
+        \
+        "blue_geo watch" \
+        "blue_geo watch map" \
+        "blue_geo watch reduce" \
+        "blue_geo watch targets"; do
+        abcli_eval ,$options \
+            $module help
+        [[ $? -ne 0 ]] && return 1
+    done
+
+    return 0
+}
