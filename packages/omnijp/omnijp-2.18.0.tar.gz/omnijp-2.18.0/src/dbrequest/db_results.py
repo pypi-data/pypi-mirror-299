7d74213785a3756e48ee1e36a4ba7079cf7dfec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 15:05:47 +0000

    Add package omnijp/2.18.0/omnijp-2.18.0.tar.gz

---
+++ packages/omnijp/omnijp-2.18.0.tar.gz/omnijp-2.18.0/src/dbrequest/db_results.py
@@ -0,0 +1,45 @@
+from dataclasses import asdict, dataclass
+import json
+import socket
+from datetime import datetime
+
+@dataclass
+class TableResult:
+    name: str
+    row_count: int
+    header: list
+    data:   list
+    time_taken: str = ""
+    
+class DbResult:
+     DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
+     def __init__(self):
+            self.start_time = None
+            self.end_time = None
+            self.host_name:str = socket.gethostname()
+            self.total_tables: int = 0
+            self.total_rows: int = 0
+            self.tables:list[TableResult] = []
+
+     def set_start_time(self):
+        self.start_time =  datetime.now().strftime(self.DATETIME_FORMAT)[:-3]
+
+     def set_end_time(self):
+        self.end_time =  datetime.now().strftime(self.DATETIME_FORMAT)[:-3]
+
+     def add_table(self, table_result: TableResult):
+        self.tables.append(table_result)
+        self.total_tables += 1
+        self.total_rows += table_result.row_count
+
+     def to_dict(self):
+        return {
+            "start_time": self.start_time,
+            "end_time": self.end_time,
+            "host_name": self.host_name,
+            "total_tables": self.total_tables,
+            "total_rows": self.total_rows
+        }
+
+     def to_json(self, indent=2):
+        return json.dumps(self.to_dict(), indent=indent)
