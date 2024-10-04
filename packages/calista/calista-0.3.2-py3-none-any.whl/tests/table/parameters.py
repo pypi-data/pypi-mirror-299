d5d9aa1b214ebb14b21b32e466b700531409749a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 03:10:18 +0000

    Add package calista/0.3.2/calista-0.3.2-py3-none-any.whl

---
+++ packages/calista/calista-0.3.2-py3-none-any.whl/tests/table/parameters.py
@@ -0,0 +1,12 @@
+SNOWFLAKE_CONN_PARAMS = {
+        "credentials": {
+        "account": "eo53442.europe-west2.gcp",
+        "user": "mtsenkung",
+        "password": "Aubay2024@92"
+    }
+}
+
+BIGQUERY_CONN_PARAMS = {
+        "connection_string": "bigquery://test-calista/RH",
+        "credentials_path": "bigquery_private_key.json",
+}
