750eef4d0bd80806d6f90cd674b74c4365f9981a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 07:05:50 +0000

    Add package py-inkwell/0.0.10/py_inkwell-0.0.10-py3-none-any.whl

---
+++ packages/py-inkwell/py_inkwell-0.0.10-py3-none-any.whl/inkwell/models/__init__.py
@@ -0,0 +1,15 @@
+from inkwell.models.model_registry import ModelRegistry
+from inkwell.models.models import InferenceBackend
+from inkwell.models.phi3_vision_model_loader import Phi3VisionModelWrapper
+from inkwell.models.phi3_vision_model_loader_vllm import (
+    Phi3VisionModelWrapperVLLM,
+)
+from inkwell.models.qwen2_2b_vision_model_loader import Qwen2VL2VModelWrapper
+
+__all__ = [
+    "Phi3VisionModelWrapper",
+    "Qwen2VL2VModelWrapper",
+    "ModelRegistry",
+    "Phi3VisionModelWrapperVLLM",
+    "InferenceBackend",
+]
