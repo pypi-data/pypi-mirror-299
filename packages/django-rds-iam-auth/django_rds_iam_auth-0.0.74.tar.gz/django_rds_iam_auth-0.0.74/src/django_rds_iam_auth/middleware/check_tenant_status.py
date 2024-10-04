24690ad722ad23ffe78fc8a2c84bd7f6b4bb882e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 16:05:10 +0000

    Add package django-rds-iam-auth/0.0.74/django_rds_iam_auth-0.0.74.tar.gz

---
+++ packages/django-rds-iam-auth/django_rds_iam_auth-0.0.74.tar.gz/django_rds_iam_auth-0.0.74/src/django_rds_iam_auth/middleware/check_tenant_status.py
@@ -0,0 +1,29 @@
+import json
+
+from rest_framework import status
+from django.http import HttpResponse
+from django.contrib.auth.models import AnonymousUser
+
+from django_rds_iam_auth.models import Tenant
+
+
+class CheckTenantStatusMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if (
+                not isinstance(request.user, AnonymousUser)
+                and not request.user.is_superuser
+                and not Tenant.objects.get(id=request.user.origin_tenant_id).is_active
+        ):
+            response = HttpResponse(
+                content=json.dumps({'message': 'Your organization account is no longer available, please contact your administrator'}),
+                content_type='application/json',
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        else:
+            response = self.get_response(request)
+
+        return response
