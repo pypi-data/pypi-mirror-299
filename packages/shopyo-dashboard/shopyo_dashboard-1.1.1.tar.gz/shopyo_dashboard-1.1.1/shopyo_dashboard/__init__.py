b5eac77e47c50b84c858017e0480c56e673bb7c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:20 +0000

    Add package shopyo-dashboard/1.1.1/shopyo_dashboard-1.1.1.tar.gz

---
+++ packages/shopyo-dashboard/shopyo_dashboard-1.1.1.tar.gz/shopyo_dashboard-1.1.1/shopyo_dashboard/__init__.py
@@ -0,0 +1,28 @@
+from typing import Any
+import os
+import json
+from flask import Flask
+from shopyo_dashboard.view import module_blueprint
+
+__version__ = "1.1.1"
+
+info = {}
+with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + "info.json") as f:
+    info = json.load(f)
+
+
+class ShopyoDashboard:
+    def __init__(self, app: Any = None) -> None:
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app: Flask) -> None:
+        if not hasattr(app, "extensions"):
+            app.extensions = {}
+
+        app.extensions["shopyo_dashboard"] = self
+        bp = module_blueprint
+        app.register_blueprint(bp)
+
+    def get_info(self):
+        return info
