3b249b30fbe21337361efb71158df3d56c6b54af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:17 +0000

    Add package adsGenericFunctions/0.2.37/adsgenericfunctions-0.2.37-py3-none-any.whl

---
+++ packages/adsGenericFunctions/adsgenericfunctions-0.2.37-py3-none-any.whl/adsGenericFunctions/__init__.py
@@ -0,0 +1,5 @@
+from .logger import Logger
+from .timer import timer, get_timer
+from .global_config import set_timer
+
+__all__ = ["Logger", "timer", "set_timer", "get_timer"]
---
+++ packages/adsGenericFunctions/adsgenericfunctions-0.2.37-py3-none-any.whl/adsGenericFunctions/demo/demo_timer_logger.py
@@ -0,0 +1,42 @@
+import adsGenericFunctions as ads
+
+from to_sort.env import *
+import logging
+import psycopg2
+
+# Établit une connexion pour le my_package pour l'écriture en base
+logger_connection = psycopg2.connect(database=pg_dwh_db, user=pg_dwh_user, password=pg_dwh_pwd, port=pg_dwh_port,
+                                     host=pg_dwh_host)
+logger = ads.Logger(logger_connection, logging.INFO, "AdsLogger", "LOGS", "LOGS_details")
+logger.info("Début de la démonstration.")
+
+
+# Active le timer, les requêtes seront chronométrées
+ads.set_timer(True)
+
+# On définit une source de connexion à laquelle on affecte notre my_package
+source1 = ads.dbPgsql({'database': pg_dwh_db
+                          , 'user': pg_dwh_user
+                          , 'password': pg_dwh_pwd
+                          , 'port': pg_dwh_port
+                          , 'host': pg_dwh_host},
+                      logger)
+
+# Cette opération sera loguée et chronométrée
+source1.connect()
+
+# Celle-ci le sera dès que le flux (générateur) renvoyé par sqlQuery sera consommé
+data1 = source1.sqlQuery('''SELECT tenantname, fichier FROM onyx_qs."diskcheck" LIMIT 10''')
+print(f"First read: {list(data1)}")
+# Après ce print viennent les notifications d'insertions des logs en base et le temps d'exécution
+
+# On peut désactiver les logs
+logger.disable_logging()
+logger.info("Ceci est un message d'information qui n'est pas censé s'afficher.")
+
+data2 = source1.sqlQuery('''SELECT tenantname, fichier FROM onyx_qs."diskcheck" LIMIT 10''')
+print(f"Second read: {list(data2)}")
+# Ici pas d'insertions en base de logs non plus, et pas de temps d'exécutions affiché
+
+logger.enable_logging()
+logger.info("Fin de la démonstration !")
