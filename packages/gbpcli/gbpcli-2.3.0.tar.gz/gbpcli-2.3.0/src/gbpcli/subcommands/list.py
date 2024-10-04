cd5b9ffc3d567c54cdcafee98959c65aca323c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 14:05:34 +0000

    Add package gbpcli/2.3.0/gbpcli-2.3.0.tar.gz

---
+++ packages/gbpcli/gbpcli-2.3.0.tar.gz/gbpcli-2.3.0/src/gbpcli/subcommands/list.py
@@ -0,0 +1,61 @@
+"""List builds for the given machines"""
+
+import argparse
+
+from rich import box
+from rich.table import Table
+
+from gbpcli import GBP, render
+from gbpcli.subcommands import completers as comp
+from gbpcli.types import Console
+
+HELP = """List builds for the given machines
+
+Key for the "Flags" column:
+
+    *: Packages were build for the build
+    K: The build has been marked for keeping
+    P: This build is published
+    N: This build has a note attached
+
+"""
+
+
+def handler(args: argparse.Namespace, gbp: GBP, console: Console) -> int:
+    """List a machine's builds"""
+    builds = gbp.builds(args.machine, with_packages=True)
+    table = Table(
+        title=f"\N{PERSONAL COMPUTER} {render.format_machine(args.machine, args)}",
+        box=box.ROUNDED,
+        title_style="header",
+        style="box",
+    )
+    table.add_column("Flags", header_style="header")
+    table.add_column("ID", justify="right", header_style="header")
+    table.add_column("Built", header_style="header")
+    table.add_column("Tags", header_style="header")
+
+    for build in builds:
+        assert build.info is not None
+
+        # In the old days, we didn't have a "built" field. Fall back to submitted
+        timestamp = build.info.built or build.info.submitted
+
+        table.add_row(
+            render.format_flags(build),
+            render.format_build_number(build.number),
+            render.format_timestamp(timestamp.astimezone(render.LOCAL_TIMEZONE)),
+            render.format_tags(build.info.tags),
+        )
+
+    console.out.print(table)
+
+    return 0
+
+
+def parse_args(parser: argparse.ArgumentParser) -> None:
+    """Set subcommand arguments"""
+    comp.set(
+        parser.add_argument("machine", metavar="MACHINE", help="name of the machine"),
+        comp.machines,
+    )
---
+++ packages/gbpcli/gbpcli-2.3.0.tar.gz/gbpcli-2.3.0/src/gbpcli/subcommands/machines.py
@@ -0,0 +1,57 @@
+"""List machines with builds"""
+
+import argparse
+from typing import Any
+
+from rich import box
+from rich.table import Table
+
+from gbpcli import GBP, render, utils
+from gbpcli.types import Console
+
+HELP = """List machines with builds"""
+
+
+def handler(args: argparse.Namespace, gbp: GBP, console: Console) -> int:
+    """List machines with builds"""
+    names = utils.get_my_machines_from_args(args) if args.mine else None
+    machines = gbp.machines(names=names)
+    table = Table(
+        title=f"{len(machines)} Machines",
+        box=box.ROUNDED,
+        title_style="header",
+        style="box",
+    )
+    table.add_column("Machine", header_style="header")
+    table.add_column("Builds", justify="right", header_style="header")
+    table.add_column("Latest", justify="right", header_style="header")
+
+    for machine, builds, latest in machines:
+        table.add_row(
+            render.format_machine(machine, args),
+            str(builds),
+            latest_build_to_str(latest),
+        )
+
+    console.out.print(table)
+    return 0
+
+
+def parse_args(parser: argparse.ArgumentParser) -> None:
+    """Set subcommand arguments"""
+    parser.add_argument(
+        "--mine",
+        action="store_true",
+        default=False,
+        help="Only display machine info for --my-machines",
+    )
+
+
+def latest_build_to_str(build: dict[str, Any]) -> str:
+    """Return the "Latest" column for the given build"""
+    build_id = build["id"].rpartition(".")[2]
+
+    if build["published"]:
+        build_id = f"[published]{build_id}[/published]"
+
+    return f"[build_id]{build_id}[/build_id]"
