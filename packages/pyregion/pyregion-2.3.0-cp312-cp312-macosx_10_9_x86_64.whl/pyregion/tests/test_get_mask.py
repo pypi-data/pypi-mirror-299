378b843417ce44334f51155202f31ac948625ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 16:05:17 +0000

    Add package pyregion/2.3.0/pyregion-2.3.0-cp312-cp312-macosx_10_9_x86_64.whl

---
+++ packages/pyregion/pyregion-2.3.0-cp312-cp312-macosx_10_9_x86_64.whl/pyregion/tests/test_get_mask.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+from os.path import join
+from astropy.io.fits import Header
+
+from pyregion import open as pyregion_open
+
+rootdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+
+
+def demo_header():
+    return Header.fromtextfile(join(rootdir, "sample_fits01.header"))
+
+
+def test_region():
+    ref_name = "test01_img.reg"
+    header = demo_header()
+
+    ref_region = pyregion_open(join(rootdir, ref_name)).as_imagecoord(header)
+    mask = ref_region.get_mask(shape=(100, 100))
+
+    assert isinstance(mask, np.ndarray) and mask.shape == (100, 100)
+
+    # TODO: assert the content of the mask, too
