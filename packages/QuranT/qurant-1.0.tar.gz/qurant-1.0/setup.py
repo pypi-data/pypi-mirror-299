f73212057d9f44d4d9180ae04201729c33f77a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 06:05:35 +0000

    Add package QuranT/1.0/qurant-1.0.tar.gz

---
+++ packages/QuranT/qurant-1.0.tar.gz/qurant-1.0/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="QuranT",
+    version="1.0",
+    author="Khattab",
+    description="A complete Quran library with Tafser and Search functionality.",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
