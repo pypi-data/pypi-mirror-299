6e7810365781d308551e12b918e85bef2994c3af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 15:06:00 +0000

    Add package plover-q-and-a/0.4.4/plover_q_and_a-0.4.4-py3-none-any.whl

---
+++ packages/plover-q-and-a/plover_q_and_a-0.4.4-py3-none-any.whl/plover_q_and_a/config/extractor.py
@@ -0,0 +1,26 @@
+"""
+Module to handle extracting config from JSON files
+"""
+
+import json
+from pathlib import Path
+from typing import Any
+
+
+def load(filepath: Path) -> dict[str, Any]:
+    """
+    Reads in data from a JSON file
+
+    Raises an error if the specified config file is not JSON format.
+    """
+    data: dict[str, Any]
+    try:
+        with filepath.open(encoding="utf-8") as file:
+            data = json.load(file)
+            file.close()
+    except FileNotFoundError:
+        data = {}
+    except json.JSONDecodeError as exc:
+        raise ValueError("Unable to decode file contents as JSON") from exc
+
+    return data
---
+++ packages/plover-q-and-a/plover_q_and_a-0.4.4-py3-none-any.whl/plover_q_and_a/config/transformer/question.py
@@ -0,0 +1,26 @@
+"""
+Module to handle parsing and formatting of a question from config.
+"""
+
+from typing import Any
+
+
+# Default formatting values
+_QUESTION_BEGIN_MARKER_PRE_FORMATTING: str = "\t"
+_QUESTION_BEGIN_MARKER: str = "Q"
+_QUESTION_BEGIN_MARKER_POST_FORMATTING: str = "\t"
+
+def marker(data: dict[str, Any]) -> str:
+    """
+    Format a question from config.
+    """
+    question_marker: dict[str, str] = (
+        # REF: https://stackoverflow.com/a/77230846/567863
+        (data.get("question") or {}).get("marker", {})
+    )
+
+    return (
+        question_marker.get("pre", _QUESTION_BEGIN_MARKER_PRE_FORMATTING)
+        + question_marker.get("text", _QUESTION_BEGIN_MARKER)
+        + question_marker.get("post", _QUESTION_BEGIN_MARKER_POST_FORMATTING)
+    )
