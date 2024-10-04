82b9597f7cdf6111128df84eecee0c7a6d8cf763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:17 +0000

    Add package eric-ide-server/24.10/eric_ide_server-24.10-py3-none-any.whl

---
+++ packages/eric-ide-server/eric_ide_server-24.10-py3-none-any.whl/eric7/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2024 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module containing some eric-ide version information.
+"""
+
+Version = "24.10 (rev. e6a500446d9d)"
+VersionOnly = "24.10"
