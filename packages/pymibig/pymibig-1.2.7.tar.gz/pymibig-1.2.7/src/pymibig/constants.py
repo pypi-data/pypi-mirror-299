5d58566f1a308dbfe1280eea22de7d6c2d673b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:05:04 +0000

    Add package pymibig/1.2.7/pymibig-1.2.7.tar.gz

---
+++ packages/pymibig/pymibig-1.2.7.tar.gz/pymibig-1.2.7/src/pymibig/constants.py
@@ -0,0 +1,11 @@
+"""
+Constant values
+"""
+
+JSON_LINK: str = "https://dl.secondarymetabolites.org/mibig/mibig_json_3.1.tar.gz"
+GBK_LINK: str = "https://dl.secondarymetabolites.org/mibig/mibig_gbk_3.1.tar.gz"
+FASTA_LINK: str = "https://dl.secondarymetabolites.org/mibig/mibig_prot_seqs_3.1.fasta"
+METADATA: str = "mibig_json_3.1.tar.gz"
+NUCLEOTIDE: str = "mibig_gbk_3.1.tar.gz"
+AA: str = "mibig_prot_seqs_3.1.fasta.gz"
+CHUNK_SIZE: int = 10 * 1024
