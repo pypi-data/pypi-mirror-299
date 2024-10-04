9b106c4a94ddf95d0864f24b179eb24dbff82b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 04:06:04 +0000

    Add package eric-ide-server/24.10.1/eric_ide_server-24.10.1.tar.gz

---
+++ packages/eric-ide-server/eric_ide_server-24.10.1.tar.gz/eric_ide_server-24.10.1/src/eric7/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2024 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module containing some eric-ide version information.
+"""
+
+Version = "24.10.1 (rev. c8df46dd566d)"
+VersionOnly = "24.10.1"
