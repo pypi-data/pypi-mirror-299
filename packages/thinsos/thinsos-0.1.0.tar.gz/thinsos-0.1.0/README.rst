578dd377546ca4fc4d96fd2f6fe967b0dbd84a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-04 14:06:11 +0000

    Add package thinsos/0.1.0/thinsos-0.1.0.tar.gz

---
+++ packages/thinsos/thinsos-0.1.0.tar.gz/thinsos-0.1.0/README.rst
@@ -0,0 +1,17 @@
+ThinSOS - Python API to the 52North SOS implementation
+===================================================================
+Still a work in progress...but GetCapabilities, GetDataAvailability, GetFeatureOfInterest, and GetObservation have been implemented.
+
+This package was inspired by `py4sos <https://github.com/manuGil/py4sos>`_.
+
+ThinSOS GitHub repo is here: `ThinSOS <https://github.com/mullenkamp/ThinSOS>`_
+
+Installation
+------------
+ThinSOS can be installed via pip or conda::
+
+  pip install ThinSOS
+
+or::
+
+  conda install -c mullenkamp ThinSOS
---
+++ packages/thinsos/thinsos-0.1.0.tar.gz/thinsos-0.1.0/thinsos/niwa/monthly_met_data.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 22 10:26:24 2019
+
+@author: michaelek
+"""
+
+########################################
+###
+
+observableProperty = {'MTHLY_STATS: MEAN DAILY RADIATION (Global) (MTHLY: MEAN RAD (G))': 'R_s',
+                      'MTHLY_STATS: MEAN MAXIMUM TEMPERATURE from daily Maxs (MTHLY: MEAN MAX TEMP)': 'T_max',
+                      'MTHLY_STATS: MEAN MINIMUM TEMPERATURE from daily Mins (MTHLY: MEAN MIN TEMP)': 'T_min',
+                      'MTHLY_STATS: MEAN VAPOUR PRESSURE (MTHLY: MEAN VP)': 'P',
+                      'MTHLY_STATS: MEAN WIND SPEED (NO direction) (MTHLY: MEAN WIND SPEED)': 'U_z',
+                      'MTHLY_STATS: TOTAL RAINFALL (MTHLY: TOTAL RAIN)': 'rain',
+                      'MTHLY_STATS: TOTAL SUNSHINE (MTHLY: TOTAL SUN)': 'n_sun',
+                      'MTHLY_STATS: MEAN 9AM RELATIVE HUMIDITY (MTHLY: MEAN 9AM RH)': 'RH_mean',
+                      'MTHLY_STATS: TOTAL PENMAN POTENTIAL EVAPO-TRANSPIRATION (MTHLY: TOTAL PENMAN PE)': 'PET'}
