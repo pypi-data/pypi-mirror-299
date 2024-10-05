b59e1796466575c969cf42252345751f8233632e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:08:31 +0000

    Add package otpcr/52/otpcr-52.tar.gz

---
+++ packages/otpcr/otpcr-52.tar.gz/otpcr-52/setup.py
@@ -0,0 +1,16 @@
+# This file is placed in the Public Domain.
+
+
+"stub"
+
+
+import setuptools
+
+
+if __name__ == "__main__":
+    setuptools.setup(scripts=[
+        'bin/otpcr',
+        'bin/otpcrc',
+        'bin/otpcrd',
+        'bin/otpcrs'
+    ])
