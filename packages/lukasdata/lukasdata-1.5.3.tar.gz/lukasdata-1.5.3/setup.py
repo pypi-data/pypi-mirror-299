522ad1021606989629587b9d6e0c18094041323a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:08:56 +0000

    Add package lukasdata/1.5.3/lukasdata-1.5.3.tar.gz

---
+++ packages/lukasdata/lukasdata-1.5.3.tar.gz/lukasdata-1.5.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lukasdata',
+    packages=find_packages(),
+    version='1.5.3',
+    install_requires=["numpy","pandas","matplotlib"
+    ]
+)
