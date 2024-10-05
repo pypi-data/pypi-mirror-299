c2ae964f71573d1890320f8ef49783cbadd14285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:08:37 +0000

    Add package tf-validator/0.1/tf-validator-0.1.tar.gz

---
+++ packages/tf-validator/tf-validator-0.1.tar.gz/tf-validator-0.1/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+setup(
+    name='tf-validator',
+    version='0.1',
+    packages=find_packages(),
+    description='Use for train and test model',
+    author='Abinesh',
+    author_email='abinesh530@gmail.com',
+    python_requires='>=3.5',  # Specify the required Python version
+)
---
+++ packages/tf-validator/tf-validator-0.1.tar.gz/tf-validator-0.1/tf_validator/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# tfw/tfw/__init__.py
+class tf_val:
+    def __init__(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+    
+    def X_val(self):
+        return self.X_train
+    
+    def y_val(self):
+        return self.y_train
+    
+    def X_test(self):
+        return self.X_train
+    
+    def y_test(self):
+        return self.y_train
