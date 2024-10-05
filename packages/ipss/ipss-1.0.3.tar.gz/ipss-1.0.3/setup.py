ce3544156054ff50b57e2af29f32bd802073f803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:09:00 +0000

    Add package ipss/1.0.3/ipss-1.0.3.tar.gz

---
+++ packages/ipss/ipss-1.0.3.tar.gz/ipss-1.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ipss',
+    version='1.0.3',
+    author='Omar Melikechi',
+    author_email='omar.melikechi@gmail.com',
+    packages=find_packages(),
+    description='Python implementation of integrated path stability selection (IPSS)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'joblib',
+        'matplotlib',
+        'numpy',
+        'scikit-learn',
+        'xgboost'
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    classifiers=[
+        # Classifiers help users find your project
+        # Full list: https://pypi.org/classifiers/
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
