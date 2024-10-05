6ee14e08689eab8aa975165b00ba523696dc8be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:08:25 +0000

    Add package simple-ddl-parser/1.7.1/simple_ddl_parser-1.7.1.tar.gz

---
+++ packages/simple-ddl-parser/simple_ddl_parser-1.7.1.tar.gz/simple_ddl_parser-1.7.1/simple_ddl_parser/dialects/mysql.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class MySQL:
+    def p_engine(self, p: List) -> None:
+        """expr : expr ENGINE EQ id"""
+        p_list = list(p)
+        p[0] = p[1]
+        p[0]["engine"] = p_list[-1]
+
+    def p_db_properties(self, p: List) -> None:
+        """expr : expr id EQ id_or_string"""
+        p_list = list(p)
+        p[0] = p[1]
+        p[0][p[2]] = p_list[-1]
+
+    def p_on_update(self, p: List) -> None:
+        """on_update : ON UPDATE id
+        | ON UPDATE STRING
+        | ON UPDATE f_call
+        """
+        p_list = list(p)
+        if not ")" == p_list[-1]:
+            p[0] = {"on_update": p_list[-1]}
+        else:
+            p[0] = {"on_update": p_list[-2]}
