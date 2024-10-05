8108c78ae663dabaf7640d84201f63a89c06ec2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:09:13 +0000

    Add package MoreUtilspy/0.1/MoreUtilspy-0.1-py3-none-any.whl

---
+++ packages/MoreUtilspy/MoreUtilspy-0.1-py3-none-any.whl/my_utils/__init__.py
@@ -0,0 +1,41 @@
+
+
+from .converter_utils import (
+    text_to_number,
+    number_to_text,
+    int_to_roman,
+    roman_to_int,
+    snake_to_camel,
+    camel_to_snake,
+    camel_to_pascal,
+    seconds_to_text,
+    text_to_seconds,
+    date_to_text,
+    text_to_date,
+    bytes_to_text,
+    text_to_bytes,
+    int_to_ordinal,
+    ordinal_to_int
+)
+
+from .text_utils import (
+    is_palindrome,
+    are_anagrams,
+    count_vowels,
+    count_consonants,
+    reverse_words,
+    reverse_string,
+    remove_duplicates,
+    is_pangram,
+    is_isogram,
+    is_palindrome_permutation,
+    is_anagram_of_palindrome,
+    is_valid_palindrome,
+    is_valid_pangram,
+    levenshtein_distance,
+    longest_common_subsequence,
+    longest_common_substring,
+    generate_password,
+    json_to_csv,
+    csv_to_json
+)
