73aada9fa790e8daec022f0763f96b148efd2562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-05 03:08:47 +0000

    Add package my-little-ocr/0.1.0/my_little_ocr-0.1.0.tar.gz

---
+++ packages/my-little-ocr/my_little_ocr-0.1.0.tar.gz/my_little_ocr-0.1.0/my_little_ocr/ocr_engines/__init__.py
@@ -0,0 +1,41 @@
+from my_little_ocr.base_engine.engine_config import get_engine as get_engine_after_register
+from my_little_ocr.base_engine.base_ocr_engine import BaseOCREngine
+import importlib
+from typing import Type
+from pathlib import Path
+
+engine_instances :dict[str, BaseOCREngine] = {}
+
+def deal_with_engine_name(engine_name: str) -> str:
+    engine_name_with_engine = engine_name if engine_name.endswith("_engine") else engine_name + "_engine"
+    engine_name = engine_name_with_engine.replace("_engine", "")
+    return engine_name, engine_name_with_engine
+
+
+def get_engine_class(engine_name: str) -> Type[BaseOCREngine]:
+    engine_name, engine_name_with_engine = deal_with_engine_name(engine_name)
+    importlib.import_module(f"my_little_ocr.ocr_engines.{engine_name_with_engine}")
+    return get_engine_after_register(engine_name).engine_class
+
+def get_engine_instance(engine_name: str) -> BaseOCREngine:
+    engine_name, engine_name_with_engine = deal_with_engine_name(engine_name)
+    engine_class = get_engine_class(engine_name)
+    if engine_name_with_engine in engine_instances:
+        return engine_instances[engine_name_with_engine]
+    engine_instances[engine_name_with_engine] = engine_class()
+    return engine_instances[engine_name_with_engine]
+
+def get_all_engines():
+    result: dict[str, Type[BaseOCREngine]] = {}
+    for file_or_folder in Path(__file__).parent.iterdir():
+        try:
+            stem = file_or_folder.stem
+            if stem.endswith("_engine"):
+                engine_name = stem[:-7]
+                engine_class = get_engine_class(engine_name)
+                result[engine_name] = engine_class
+        except Exception as e:
+            print(f"Error: {e}")
+    return result
+
+__all__ = ["get_engine_instance", "get_all_engines",  "get_engine_class"]
---
+++ packages/my-little-ocr/my_little_ocr-0.1.0.tar.gz/my_little_ocr-0.1.0/my_little_ocr/ocr_engines/wechat_ocr_engine/wechat_ocr_engine.py
@@ -0,0 +1,93 @@
+from .wechat_ocr_modified_lib import OcrManager
+from pydantic import BaseModel, Field, field_validator
+from typing import Callable
+from my_little_ocr.base_engine.base_ocr_engine import (
+    BaseOCREngine,
+    OCRItem,
+    convert_imagelike_to_type,
+    ImageLike,
+    OCRResult,
+)
+from threading import Lock
+from concurrent.futures import Future
+from pathlib import Path
+
+from .install import install
+install()
+
+class WechatOCRSettings(BaseModel):
+    dir: str = Field(..., description="The directory of the WeChat OCR binary")
+    exe_path: str = Field(..., description="The path to the WeChat OCR executable")
+    
+    @field_validator("dir", "exe_path", mode="before")
+    def convert_to_path(cls, v):
+        return str(Path(v).resolve())
+
+
+class WechatOCREngine(BaseOCREngine):
+    ocr_engine_name = "WeChat OCR"
+
+    def __init__(
+        self,
+        dir: str = Path(__file__).parent / "wxocr-binary",
+        exe_path: str = Path(__file__).parent / "wxocr-binary" / "WeChatOCR.exe",
+        *args,
+        **kwargs
+    ):
+        self.ocr_settings = WechatOCRSettings(dir=dir, exe_path=exe_path, **kwargs)
+
+        self.ocr_manager: OcrManager = None
+        self._future_results: dict[str, Future] = {}
+        self._lock = Lock()
+
+        self.init_wechat_ocr()
+
+    def _wrapper_callback(self, img_path: str, wechat_ocr_results: dict):
+        ocr_result = wechat_ocr_results["ocrResult"]
+        ocr_item_list = []
+        for item_dict in ocr_result:
+            location = item_dict["location"]  # dict
+            left, top, right, bottom = (
+                location["left"],
+                location["top"],
+                location["right"],
+                location["bottom"],
+            )
+            position = [[left, top], [right, top], [right, bottom], [left, bottom]]
+            score = item_dict.get("score")
+            ocr_item = OCRItem(
+                text=item_dict["text"], box=position, confidence=score
+            )
+            ocr_item_list.append(ocr_item)
+
+        future = self._future_results.get(str(img_path))
+        if future:
+            future.set_result(ocr_item_list)
+
+    def init_wechat_ocr(self):
+        self.ocr_manager: OcrManager = OcrManager(self.ocr_settings.dir)
+        self.ocr_manager.SetExePath(self.ocr_settings.exe_path)
+        self.ocr_manager.SetUsrLibDir(self.ocr_settings.dir)
+        self.ocr_manager.SetOcrResultCallback(self._wrapper_callback)
+        self.ocr_manager.StartWeChatOCR()
+
+    def ocr_image_using_callback(self, img_path: str):
+        self.ocr_manager.DoOCRTask(img_path)
+
+    def ocr(self, img: ImageLike) -> OCRResult:
+        img_path = convert_imagelike_to_type(img, "filepath")
+        img_path = str(Path(img_path).resolve())
+        future = Future()
+        with self._lock:
+            self._future_results[img_path] = future
+
+        print(f"OCR method img_path: {img_path}")  # Debug statement
+
+        self.ocr_image_using_callback(img_path)
+
+        result = future.result(timeout=10)
+
+        with self._lock:
+            del self._future_results[img_path]
+
+        return OCRResult(ocr_items=result)
